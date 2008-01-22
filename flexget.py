fc8c8309ce790a04edbf8866da09ac4bca9a82eb
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-01-22 13:48:01 +0000

    Added ability to define multiple same modules for feed in form of keyword[1] .. keyword[16]
    git-svn-id: http://svn.flexget.com/trunk@30 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget.py
+++ flexget.py
@@ -522,7 +522,7 @@ class Feed:
         """
         if type(self.config[keyword])==types.DictType:
             if not self.config[keyword].has_key('url'):
-                raise Exception("Input %s has invalid configuration, url is missing." % keyword)
+                raise Exception('Input %s has invalid configuration, url is missing.' % keyword)
             return self.config[keyword]['url']
         else:
             return self.config[keyword]
@@ -533,7 +533,7 @@ class Feed:
         keyword = module['keyword']
         if self.config.has_key(keyword):
             if type(self.config[keyword])==types.DictType:
-                order = self.config[keyword].get("order", order)
+                order = self.config[keyword].get('order', order)
         return order
 
     def __sort_modules(self, a, b):
@@ -547,21 +547,25 @@ class Feed:
         # Order can be also configured in which case given value overwrites module default.
         modules.sort(self.__sort_modules)
         for module in modules:
-            if self.config.has_key(module['keyword']) or (module['builtin'] and not self.config.get('disable_builtins', False)):
-                try:
-                    # set cache namespaces
-                    self.cache.set_namespace(module['keyword'])
-                    self.shared_cache.set_namespace(module['keyword'])
-                    # call module
-                    logging.debug('executing %s %s' % (module_type, module['keyword']))
-                    module['callback'](self)
-                    # check for priority operations
-                    self.__filter_immediately()
-                    if self.__abort: return
-                except Warning, w:
-                    logging.warning(w)
-                except Exception, e:
-                    logging.exception("Module %s: %s" % (module['keyword'], e))
+            # keyword, keyword[0..16]
+            keywords = [module['keyword']] + ['%s[%i]' % (module['keyword'], i) for i in range(16)]
+            for keyword in keywords:
+                # run if keyword present, or builtin (but not if [0..16] or disabled)
+                if self.config.has_key(keyword) or (module['builtin'] and not self.config.get('disable_builtins', False) and keywords.index(keyword) == 0):
+                    try:
+                        # set cache namespaces
+                        self.cache.set_namespace(keyword)
+                        self.shared_cache.set_namespace(keyword)
+                        # call module
+                        logging.debug('executing %s %s' % (module_type, keyword))
+                        module['callback'](self)
+                        # check for priority operations
+                        self.__filter_immediately()
+                        if self.__abort: return
+                    except Warning, w:
+                        logging.warning(w)
+                    except Exception, e:
+                        logging.exception('Module %s: %s' % (keyword, e))
 
     def execute(self):
         """Execute this feed, runs all associated modules in order by type"""
