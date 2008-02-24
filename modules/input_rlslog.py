d63b6f6b9807e1248bb54e70c8b338079478fb35
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-02-24 00:46:10 +0000

    Cleaned InputRlsLog description
    git-svn-id: http://svn.flexget.com/trunk@56 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/input_rlslog.py
+++ modules/input_rlslog.py
@@ -22,13 +22,6 @@ class RlsLog:
     """
         Adds support for rlslog.net as a feed.
 
-        If rlslog entry has NewTorrents download link then torrent url is parsed from there.
-        If rlslog entry has NewTorrents search link, we try to look from there if any of the results match entry title.
-        On multiple NewTorrents-links per entry have unknown effects ...
-
-        Module caches all successfull NewTorrents 'download torrent'-parses, hence module makes only one request per
-        rlslog-entry to NewTorrents thus eliminating any potential DDOS effect and or bandwith wasting.
-
         In case of movies the module supplies pre-parses IMDB-details (helps when chaining with filter_imdb).
     """
 
