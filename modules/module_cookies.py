8bf6dbe7bab8902af80b787e2088f5dc77ab60e6
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-03-03 20:57:35 +0000

    Critical fixes to ModuleCookies
    git-svn-id: http://svn.flexget.com/trunk@82 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/module_cookies.py
+++ modules/module_cookies.py
@@ -23,34 +23,37 @@ class ModuleCookies:
         manager.register(instance=self, event='exit', keyword='cookies', callback=self.exit)
 
     def start(self, feed):
+        log.debug('cookies start')
+        if not feed.config.has_key('cookies'):
+            return
+        # cookies enabled
         config = feed.config['cookies']
-        if config.has_key('cookies'):
-            # check that require configuration is present
-            if not config['cookies'].has_key('type'):
-                raise Warning('Cookies configuration is missing required field: type')
-            if not config['cookie'].has_key('file'):
-                raise Warning('Cookies configuration is missing required field: file')
-            # create cookiejar
-            import cookielib
-            t = config['cookies']['type']
-            if t == 'mozilla':
-                cj = cookielib.MozillaCookieJar()
-            elif t == 'lpw':
-                cj = cookielib.LWPCookieJar()
-            elif t == 'msie':
-                cj = cookielib.MSIECookieJar()
-            else:
-                raise Warning('Unknown cookie type %s' % ctype)
-            try:
-                cj.load(filename=config['cookies']['file'], ignore_expires=True)
-                log.debug('Cookies loaded')
-            except (cookielib.LoadError, IOError), e:
-                log.exception(e)
-                raise Warning('Aborted feed because cookies could not be loaded.')
-            # create new opener for urllib2
-            log.debug('Removing urllib2 opener')
-            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-            urllib2.install_opener(opener)
+        # check that require configuration is present
+        if not config.has_key('type'):
+            raise Warning('Cookies configuration is missing required field: type')
+        if not config.has_key('file'):
+            raise Warning('Cookies configuration is missing required field: file')
+        # create cookiejar
+        import cookielib
+        t = config['type']
+        if t == 'mozilla':
+            cj = cookielib.MozillaCookieJar()
+        elif t == 'lpw':
+            cj = cookielib.LWPCookieJar()
+        elif t == 'msie':
+            cj = cookielib.MSIECookieJar()
+        else:
+            raise Warning('Unknown cookie type %s' % ctype)
+        try:
+            cj.load(filename=config['file'], ignore_expires=True)
+            log.debug('Cookies loaded')
+        except (cookielib.LoadError, IOError), e:
+            log.exception(e)
+            raise Warning('Aborted feed because cookies could not be loaded.')
+        # create new opener for urllib2
+        log.debug('Installing urllib2 opener')
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        urllib2.install_opener(opener)
 
     def exit(self, feed):
         log.debug('Removing urllib2 opener')
