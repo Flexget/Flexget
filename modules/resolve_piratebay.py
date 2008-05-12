45142d40be48f9a2e7b0bc30ac958de81eb89eb7
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-05-12 19:32:23 +0000

    Fixes to PirateBay resolver.
    git-svn-id: http://svn.flexget.com/trunk@144 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/resolve_piratebay.py
+++ modules/resolve_piratebay.py
@@ -4,22 +4,24 @@ import urlparse
 import logging
 from feed import ResolverException
 
-log = logging.getLogger("piratebay")
+log = logging.getLogger('piratebay')
 
 # this way we don't force users to install bs incase they do not want to use module
 soup_present = True
-soup_err = "Module newtorrents requires BeautifulSoup. Please install it from http://www.crummy.com/software/BeautifulSoup/ or from your distribution repository."
 
 try:
     from BeautifulSoup import BeautifulSoup
 except:
-    log.warning(soup_err)
     soup_present = False
 
 class ResolvePirateBay:
     """PirateBay resolver."""
 
     def register(self, manager, parser):
+        # disable module if soup is not present
+        if not soup_present:
+            log.info('Resolver disabled. BeautifulSoup is not installed.')
+            return
         manager.register_resolver(instance=self, name='piratebay')
 
     def resolvable(self, feed, entry):
@@ -30,13 +32,12 @@ class ResolvePirateBay:
             return False
         
     def resolve(self, feed, entry):
-        if not soup_present: raise Exception(soup_err)
         try:
             page = urllib2.urlopen(entry['url'])
             soup = BeautifulSoup(page)
-            tag_div = soup.find("div", attrs={"class":"download"})
-            tag_a = tag_div.find("a")
+            tag_div = soup.find('div', attrs={'class':'download'})
+            tag_a = tag_div.find('a')
             torrent_url = tag_a.get('href')
             entry['url'] = torrent_url
         except Exception, e:
-            raise ResolverException(e.message)
+            raise ResolverException(e)
