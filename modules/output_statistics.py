a9831336c40a2ecd3ac6f3f703785fa307a41e06
lepinkainen <shrike@addiktit.net>
2008-05-14 20:08:58 +0000

    Per-feed statistics Only generate one html page, fetch feeds from google
    git-svn-id: http://svn.flexget.com/trunk@162 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/output_statistics.py
+++ modules/output_statistics.py
@@ -58,14 +58,32 @@ class Statistics:
     def generate_statistics(self, feed):
         if not has_pygooglechart:
             raise Exception('module statistics requires pygooglechart library.')
+
+        prefix = feed.manager.configname
         
-        dbname = os.path.join(sys.path[0], feed.manager.configname+".db")
+        dbname = os.path.join(sys.path[0], prefix+".db")
         con = sqlite.connect(dbname)
 
-        self.weekly_stats(feed, con)
-        self.hourly_stats(feed, con)
+        charts = []
+        charts.append(self.weekly_stats(con))
+        charts.append(self.hourly_stats(con))
+        charts.append(self.weekly_stats_by_feed(con))
+        charts.append(self.hourly_stats_by_feed(con))
 
-    def hourly_stats(self, feed, con):
+        self.save_index(prefix, charts)
+
+    def save_index(self, prefix, charts):
+        imagelinks = ""
+        for chart in charts:
+            imagelinks += """<img src="%s" alt="" />""" % chart
+
+        index = index_html % imagelinks
+
+        f = file(prefix+"_statistics.html", 'w')
+        f.write(index)
+        f.close()
+
+    def hourly_stats(self, con):
         sql = """
         select strftime("%H", timestamp, 'localtime') as hour, sum(success) from statistics group by hour
         """
@@ -91,16 +109,52 @@ class Statistics:
             if data[i] > 0:
                 chart.add_marker(0, i, 't%s'%data[i], '000000', 13)
 
-        chartname = os.path.join(sys.path[0], feed.manager.configname + "_hourly.png")
-        charthtml = os.path.join(sys.path[0], feed.manager.configname + "_hourly.html")
+        return chart.get_url()
 
-        chart.download(chartname)
-        url = chart.get_url()
-        f = file(charthtml, 'w')
-        f.write(url+'\n')
-        f.close()
 
-    def weekly_stats(self, feed, con):
+    def hourly_stats_by_feed(self, con):
+        sql = """
+        select feed, strftime("%H", timestamp, 'localtime') as hour, sum(success) from statistics group by feed, hour;
+        """
+
+        cur = con.cursor()
+        cur.execute(sql)
+
+        chart = StackedVerticalBarChart(800, 200, title="Releases by source")
+        axislabels = [str(i) for i in range(24)]
+            
+        axis = chart.set_axis_labels(Axis.BOTTOM, axislabels)
+        chart.set_axis_style(axis, '000000', alignment=-1)
+
+        feedname = ""
+        maxdata = 0
+        legend = []
+        for feed, hour, success in cur:
+            # clear data array for this feed
+            if feed != feedname:
+                feedname = feed
+                legend.append(feedname)
+                data = 24*[0]
+                # add data set
+                chart.add_data(data)
+
+            success = int(success)
+            if success > maxdata:
+                maxdata = success
+            data[int(hour)] = success
+
+        # random colors
+        #import random as rn
+        #colors = ["".join(["%02x" % rn.randrange(256) for x in range(3)]) for i in range(len(legend))]
+        colors = ('00FFFF', '0000FF', 'FF00FF', '008000', '808080', '00FF00', '800000', '000080', '808000', '800080', 'FF0000', 'C0C0C0', '008080', 'FFFF00')
+        chart.set_colours(colors)
+
+        chart.set_axis_range(Axis.LEFT, 0, maxdata)
+        chart.set_legend(legend)
+
+        return chart.get_url()
+
+    def weekly_stats(self, con):
         sql = """
         select strftime("%w", timestamp, 'localtime') as dow, sum(success) from statistics group by dow
         """
@@ -128,11 +182,78 @@ class Statistics:
             if data[i] > 0:
                 chart.add_marker(0, i, 't%s'%data[i], '000000', 13)
 
-        chartname = os.path.join(sys.path[0], feed.manager.configname + "_weekly.png")
-        charthtml = os.path.join(sys.path[0], feed.manager.configname + "_weekly.html")
+        return chart.get_url()
 
-        chart.download(chartname)
-        url = chart.get_url()
-        f = file(charthtml, 'w')
-        f.write(url+'\n')
-        f.close()
+    def weekly_stats_by_feed(self, con):
+        sql = """
+        select feed, strftime("%w", timestamp, 'localtime') as hour, sum(success) from statistics group by feed, hour;
+        """
+
+        cur = con.cursor()
+        cur.execute(sql)
+
+        chart = StackedVerticalBarChart(350, 200, title="Releases by source")
+        axislabels = [str(i) for i in range(24)]
+            
+        axis = chart.set_axis_labels(Axis.BOTTOM, axislabels)
+        chart.set_axis_style(axis, '000000', alignment=-1)
+
+        feedname = ""
+        maxdata = 0
+        legend = []
+        for feed, hour, success in cur:
+            # clear data array for this feed
+            if feed != feedname:
+                feedname = feed
+                legend.append(feedname)
+                data = 7*[0]
+                # add data set
+                chart.add_data(data)
+
+            success = int(success)
+            if success > maxdata:
+                maxdata = success
+            data[int(hour)] = success
+
+        # random colors
+        #import random as rn
+        #colors = ["".join(["%02x" % rn.randrange(256) for x in range(3)]) for i in range(len(legend))]
+        colors = ('00FFFF', '0000FF', 'FF00FF', '008000', '808080', '00FF00', '800000', '000080', '808000', '800080', 'FF0000', 'C0C0C0', '008080', 'FFFF00')
+        chart.set_colours(colors)
+
+        chart.set_axis_range(Axis.LEFT, 0, maxdata)
+        chart.set_legend(legend)
+
+        return chart.get_url()
+
+
+index_html = """
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>Flexget statistics</title>
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.1/build/reset-fonts-grids/reset-fonts-grids.css" />
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.5.1/build/base/base-min.css" />
+</head>
+<body class="yui-skin-sam">
+<h1>Stats</h1>
+
+<div id="charts">
+%s
+</div>
+
+</body>
+</html>
+"""
+
+if __name__ == "__main__":
+    dbname = os.path.join("../test.db")
+    con = sqlite.connect(dbname)
+
+    s = Statistics()
+
+    s.weekly_stats_by_feed('config', con)
+    #s.weekly_stats('config', con)
+    #s.hourly_stats('config', con)
