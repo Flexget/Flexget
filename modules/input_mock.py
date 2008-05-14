897ab103b3dbd98883e7af2d593d30f2624f8950
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-05-14 19:38:09 +0000

    Added test module
    git-svn-id: http://svn.flexget.com/trunk@161 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ modules/input_mock.py
@@ -0,0 +1,25 @@
+import logging
+from feed import Entry
+
+log = logging.getLogger('input_mock')
+
+class InputMock:
+
+    """
+        Allows adding mock input entries. Example:
+        
+        input_mock:
+          - {title: foobar, url: http://some.com }
+          - {title: mock, url: http://another.com }
+    """
+
+    def register(self, manager, parser):
+        manager.register(instance=self, event='input', keyword='input_mock', callback=self.run, debug_module=True)
+
+    def run(self, feed):
+        config = feed.config.get('input_mock', [])
+        for line in config:
+            entry = Entry()
+            for k,v in line.iteritems():
+                entry[k] = v
+            feed.entries.append(entry)
