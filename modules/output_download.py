ec46c42e817d2f66a5f6aa8ebe0fea2c1efde16c
lepinkainen <shrike@addiktit.net>
2008-05-18 20:06:43 +0000

    Fixed #19
    git-svn-id: http://svn.flexget.com/trunk@176 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/output_download.py
+++ modules/output_download.py
@@ -135,6 +135,9 @@ class ModuleDownload:
         if os.path.exists(destfile):
             raise Warning("File '%s' already exists" % destfile)
         # write file
+        if not os.path.exists(destfile):
+            log.error("Cannot write output file %s, does the path exist?" % destfile)
+            return
         try:
             f = file(destfile, 'w')
             f.write(entry['data'])
