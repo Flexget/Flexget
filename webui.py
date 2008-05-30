c87b6e44410c50393b35b65a993e284e795707bf
lepinkainen <shrike@addiktit.net>
2008-05-30 09:39:25 +0000

    Fancier module display (ok, not really - a work in progress)
    git-svn-id: http://svn.flexget.com/trunk@214 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- webui.py
+++ webui.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+import string
 
 import web
 from manager import Manager
@@ -15,15 +16,42 @@ urls = (
 render = web.template.render('templates/', cache=False)
 
 m = Manager()
-m.initialize()
+manager = m
+manager.initialize()
 
 class modules:
     def GET(self):
-        m.print_module_list()
+        modules = []
+        roles = {}
+        for event in manager.EVENTS:
+            ml = manager.get_modules_by_event(event)
+            for m in ml:
+                dupe = False
+                for module in modules:
+                    if module['keyword'] == m['keyword']: dupe = True
+                if not dupe:
+                    modules.append(m)
+            # build roles list
+            for m in ml:
+                if roles.has_key(m['keyword']):
+                    roles[m['keyword']].append(event)
+                else:
+                    roles[m['keyword']] = [event]
+        for module in modules:
+            # do not include test classes, unless in debug mode
+            if module.get('debug_module', False):
+                continue
+            event = module['event']
+            if modules.index(module) > 0: event = ""
+            doc = "Yes"
+            if not module['instance'].__doc__:
+                doc = "No"
+            #print "%-20s%-30s%s" % (module['keyword'], string.join(roles[module['keyword']], ', '), doc)
+
+        print render.modules(roles, modules);
 
 class run:
     def GET(self):
-        print "running"
         start = time.time()
         m.execute()
         end = time.time()
@@ -44,5 +72,7 @@ class index:
 
 web.webapi.internalerror = web.debugerror
 
-if __name__ == "__main__": web.run(urls, globals(), web.reloader)
+if __name__ == "__main__":
+    print "FlexGet Web UI starting..."
+    web.run(urls, globals(), web.reloader)
 
