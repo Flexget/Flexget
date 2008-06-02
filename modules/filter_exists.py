3ff23186aad0cc4eb797f4587097220ed425ba34
Marko Koivusalo <marko.koivusalo@gmail.com>
2008-06-02 10:41:20 +0000

    Removed debug messages
    git-svn-id: http://svn.flexget.com/trunk@229 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- modules/filter_exists.py
+++ modules/filter_exists.py
@@ -26,7 +26,6 @@ class FilterExists:
             raise Warning('Path %s does not exists' % path)
         # scan trough
         for root, dirs, files in os.walk(path):
-            log.debug('Checking %s' % root)
             # convert filelists into utf-8 to avoid unicode problems
             dirs = [x.decode('utf-8', 'ignore') for x in dirs]
             files = [x.decode('utf-8', 'ignore') for x in files]
@@ -35,18 +34,3 @@ class FilterExists:
                 if name in dirs or name in files:
                     log.debug('Found %s in %s' % (name, root))
                     feed.filter(entry)
-                
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    from test_tools import MockFeed
-    feed = MockFeed()
-    feed.config['exists'] = sys.argv[1]
-    e = {}
-    e['url'] = 'http://127.0.0.1/test'
-    e['title'] = sys.argv[2]
-    feed.entries.append(e)
-    
-    fe = FilterExists()
-    fe.run(feed)
-    
