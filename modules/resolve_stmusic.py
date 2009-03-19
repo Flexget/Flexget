5799e8b1c60a3e36dba67441715fb4fb62bcf887
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-03-19 10:29:02 +0000

    Added STMusic resolver.
    git-svn-id: http://svn.flexget.com/branches/0.9@457 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ modules/resolve_stmusic.py
@@ -0,0 +1,19 @@
+import logging
+
+__pychecker__ = 'unusednames=parser,feed'
+
+log = logging.getLogger("stmusic")
+
+class ResolveSTMusic:
+    """STMusic resolver."""
+
+    def register(self, manager, parser):
+        manager.register('stmusic', group='resolver')
+
+    def resolvable(self, feed, entry):
+        return entry['url'].startswith('http://www.stmusic.org/details.php?id=')
+
+    def resolve(self, feed, entry):
+        import urllib
+        entry['url'] = entry['url'].replace('details.php?id=', 'download.php/')
+        entry['url'] = entry['url'] + '/%s.torrent' % (urllib.quote(entry['title'], safe=''))
