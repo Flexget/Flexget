004574f1e2e5bd12630584e6a26b3b922cdcff30
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-05-22 22:06:02 +0000

    Added debug module
    git-svn-id: http://svn.flexget.com/trunk@638 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ plugins/output_dump_config.py
@@ -0,0 +1,24 @@
+import logging
+
+log = logging.getLogger('dump_config')
+
+class OutputDumpConfig:
+
+    """
+        Dumps feed config in STDOUT in yaml at exit or abort event.
+    """
+
+    def register(self, manager, parser):
+        manager.register('dump_config', debug_module=True)
+        
+    def validator(self, config):
+        import validator
+        return validator.factory('boolean')
+
+    def feed_exit(self, feed):
+        import yaml
+        print '--- %s' % feed.name
+        print yaml.safe_dump(feed.config)
+        print '---'
+        
+    feed_abort = feed_exit
