edea05abdbbdde265b4c38917d6042ac5b3c4f51
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-06-07 20:53:31 +0000

    Added Firefox 3.x cookie support. Closes #281
    git-svn-id: http://svn.flexget.com/trunk@678 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/module_cookies.py
+++ flexget/plugins/module_cookies.py
@@ -1,8 +1,7 @@
 import logging
 import urllib2
+import cookielib
 from flexget.manager import PluginWarning
-
-
 
 log = logging.getLogger('cookies')
 
@@ -11,14 +10,12 @@ class PluginCookies:
     """
         Adds cookie to all requests (rss, resolvers, download). Anything
         that uses urllib2 to be exact.
+        
+        Currently supports Firefox 3 cookies only.
 
-        Configuration:
+        Example:
 
-        cookies:
-          type: mozilla
-          file: /path/to/cookie
-
-        Possible cookie types are: mozilla, msie, lpw
+        cookies: /path/firefox/profile/something/cookies.sqlite
     """
 
     def register(self, manager, parser):
@@ -26,39 +23,48 @@ class PluginCookies:
 
     def validator(self,):
         from flexget import validator
-        # perhaps this plugin goes way of the dodo in 1.0 ....
-        log.warning('TODO: cookies validator')
-        return validator.factory('any')
-    
-        """
-        from validator import DictValidator
-        cookies = DictValidator()
-        cookies.accept('type', ['mozilla', 'msie', 'lpw'], required=True)
-        cookies.accept('file', str, required=True)
-        cookies.validate(config) 
-        return cookies.errors.messages
-        """
+        return validator.factory('text') # TODO: file (existing)
+
+    def sqlite2cookie(self, filename):
+        from cStringIO import StringIO
+        try:
+            from pysqlite2 import dbapi2 as sqlite
+        except ImportError:
+            raise PluginWarning('Unable to use pysqlite2', log)
+ 
+        con = sqlite.connect(filename)
+ 
+        cur = con.cursor()
+        cur.execute('select host, path, isSecure, expiry, name, value from moz_cookies')
+ 
+        ftstr = ['FALSE', 'TRUE']
+ 
+        s = StringIO()
+        s.write("""\
+# Netscape HTTP Cookie File
+# http://www.netscape.com/newsref/std/cookie_spec.html
+# This is a generated file!  Do not edit.
+""")
+        count = 0
+        for item in cur.fetchall():
+            s.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (item[0], ftstr[item[0].startswith('.')], item[1],
+                                                      ftstr[item[2]], item[3], item[4], item[5]))
+            log.log(5, 'Adding cookie for %s. key: %s value: %s' % (item[0], item[4], item[5]))
+            count += 1
+            
+        log.debug('Added %s cookies to jar' % count)
+ 
+        s.seek(0)
+        con.close()
+ 
+        cookie_jar = cookielib.MozillaCookieJar()
+        cookie_jar._really_load(s, '', True, True)
+        return cookie_jar
 
     def feed_start(self, feed):
         """Feed starting, install cookiejar"""
-        config = feed.config['cookies']
-        # create cookiejar
-        import cookielib
-        t = config['type']
-        if t == 'mozilla':
-            cj = cookielib.MozillaCookieJar()
-        elif t == 'lpw':
-            cj = cookielib.LWPCookieJar()
-        elif t == 'msie':
-            cj = cookielib.MSIECookieJar()
-        else:
-            raise PluginWarning('Unknown cookie type %s' % t, log)
-        try:
-            cj.load(filename=config['file'], ignore_expires=True)
-            log.debug('Cookies loaded')
-        except (cookielib.LoadError, IOError):
-            import sys
-            raise PluginWarning('Cookies could not be loaded: %s' % sys.exc_info()[1], log)
+        cj = self.sqlite2cookie(feed.config['cookies'])
+        
         # create new opener for urllib2
         log.debug('Installing urllib2 opener')
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
