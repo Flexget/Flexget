5989e3432a62da9600b483f24c85da38137285ed
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-06-24 08:03:51 +0000

    Added feedparser dependency
    git-svn-id: http://svn.flexget.com/trunk@710 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- pavement.py
+++ pavement.py
@@ -15,7 +15,7 @@ setup(
     author='Marko Koivusalo',
     author_email='',
     url='http://flexget.com',
-    install_requires=['SQLAlchemy>0.5', 'PyYAML', 'BeautifulSoup', 'html5lib>=0.11'],
+    install_requires=['FeedParser', 'SQLAlchemy>0.5', 'PyYAML', 'BeautifulSoup', 'html5lib>=0.11'],
     packages=['flexget', 'flexget.plugins', 'flexget.utils'],
     package_data=find_package_data("flexget", package="flexget",
                                    only_in_packages=False),
