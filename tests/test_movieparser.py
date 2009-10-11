ea6bb42fb3bfb762f056fe9a446b601fdbd52b7e
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-10-11 20:30:35 +0000

    Added tests ..
    git-svn-id: http://svn.flexget.com/trunk@810 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ tests/test_movieparser.py
@@ -0,0 +1,33 @@
+from flexget.utils.titles import MovieParser, ParseWarning
+
+class TestMovieParser:
+
+    def parse(self, data):
+        movieparser = MovieParser()
+        movieparser.data = data
+        movieparser.parse()
+        return movieparser
+
+    def test_parsing(self):
+        movie = self.parse('The.Matrix.1999.1080p.HDDVD.x264-FlexGet')
+        assert movie.name == 'The Matrix', 'failed to parse %s' % movie.data
+        assert movie.year == 1999, 'failed to parse year from %s' % movie.data
+
+        movie = self.parse('WALL-E 720p BluRay x264-FlexGet')
+        assert movie.name == 'WALL-E', 'failed to parse %s' % movie.data
+        assert movie.quality == '720p', 'failed to parse quality from %s' % movie.data
+
+        movie = self.parse('The.Pianist.2002.HDDVD.1080p.DTS.x264-FlexGet')
+        assert movie.name == 'The Pianist', 'failed to parse %s' % movie.data
+        assert movie.year == 2002, 'failed to parse year from %s' % movie.data
+        assert movie.quality == '1080p', 'failed to parse quality from %s' % movie.data
+
+        movie = self.parse("Howl's_Moving_Castle_(2004)_[720p,HDTV,x264,DTS]_-_THORA")
+        assert movie.name == "Howl's Moving Castle", 'failed to parse %s' % movie.data
+        assert movie.year == 2004, 'failed to parse year from %s' % movie.data
+        assert movie.quality == '720p', 'failed to parse quality from %s' % movie.data
+
+        movie = self.parse('Coraline.3D.1080p.BluRay.x264-FlexGet')
+        print movie.name
+        assert movie.name == 'Coraline', 'failed to parse %s' % movie.data
+        assert movie.quality == '1080p', 'failed to parse quality from %s' % movie.data
