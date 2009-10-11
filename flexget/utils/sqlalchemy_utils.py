1021d6e7f954d5438ebe694cf6eb7b41b9dba090
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-10-11 11:34:18 +0000

    Argh, I suck
    git-svn-id: http://svn.flexget.com/trunk@802 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/utils/sqlalchemy_utils.py
+++ flexget/utils/sqlalchemy_utils.py
@@ -7,7 +7,5 @@ def table_exists(table_name, feed):
         meta = MetaData()
         reflect = Table(table_name, meta, autoload=True, autoload_with=feed.session.connection())
     except NoSuchTableError:
-        running_old = False
-    if running_old:
-        log.critical('Running old database! Please see bleeding edge news!')
-        feed.abort()
+        return True
+    return False
