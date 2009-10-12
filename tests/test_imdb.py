d05a7b278afc050aee0774942800f93fe45a8cc8
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-10-12 21:21:58 +0000

    Improved unit tests
    git-svn-id: http://svn.flexget.com/trunk@814 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- tests/test_imdb.py
+++ tests/test_imdb.py
@@ -1,6 +1,5 @@
 from tests import FlexGetBase
 from nose.plugins.attrib import attr
-import os
 
 class TestImdbOnline(FlexGetBase):
     __yaml__ = """
@@ -24,14 +23,14 @@ class TestImdbOnline(FlexGetBase):
               min_year: 2003
     """
     @attr(online=True)
-    def testLookup(self):
+    def test_lookup(self):
         self.execute_feed('test')
         assert self.feed.find_entry(imdb_name='Sen to Chihiro no kamikakushi'), 'Failed IMDB lookup (search)'
         assert self.feed.find_entry(imdb_name='Mononoke-hime'), 'Failed imdb lookup (direct)'
         assert self.feed.find_entry(imdb_name='Taken', imdb_url='http://www.imdb.com/title/tt0936501/'), 'Failed to pick correct Taken from search results'
 
     @attr(online=True)
-    def testYear(self):
+    def test_year(self):
         self.execute_feed('year')
         assert self.feed.find_entry('accepted', imdb_name='Taken'), 'Taken should\'ve been accepted'
         # mononoke should not be accepted or rejected
@@ -47,7 +46,7 @@ class TestScanImdb(FlexGetBase):
               - {title: 'Scan Test 2', description: '<a href="http://imdb.com/title/tt0472198/">IMDb</a>'}
               - {title: 'Scan Test 3', description: 'nothing here'}
     """
-    def testScanImdb(self):
+    def test_scan_imdb(self):
         self.execute_feed('test')
         assert self.feed.find_entry(imdb_url='http://www.imdb.com/title/tt0330793'), 'Failed pick url from test 1'
         assert self.feed.find_entry(imdb_url='http://imdb.com/title/tt0472198'), 'Failed pick url from test 2'
@@ -57,14 +56,14 @@ class TestImdbRequired(FlexGetBase):
         feeds:
           test:
             input_mock:
-              - {title: 'Require Test 1', imdb_url: 'http://imdb.com/something' }
-              - {title: 'Require Test 2'}
+              - {title: 'Taken[2008]DvDrip[Eng]-FOO', imdb_url: 'http://www.imdb.com/title/tt0936501/'}
+              - {title: 'ASDFASDFASDF'}
             imdb_required: yes
     """
     
-    def testImdbRequired(self):
-        return # hmm, tricky ...
-        # self.execute_feed('test')
-        #assert not self.feed.find_entry(title='Require Test 2'), 'Test 2 should have been rejected'
-        #assert self.feed.find_entry(title='Require Test 1'), 'Test 1 should NOT have been rejected'
+    @attr(online=True)
+    def test_imdb_required(self):
+        self.execute_feed('test')
+        assert not self.feed.find_entry('rejected', title='Taken[2008]DvDrip[Eng]-FOO'), 'Taken should NOT have been rejected'
+        assert self.feed.find_entry('rejected', title='ASDFASDFASDF'), 'ASDFASDFASDF should have been rejected'
     
