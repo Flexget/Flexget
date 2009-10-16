0fa1e7994b9c68bf388bbaf917c0796a4a38e8a2
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-10-16 19:26:43 +0000

    Fixes #330. Previous fix seemed to have disappeared completely in the abyss.
    git-svn-id: http://svn.flexget.com/trunk@828 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/resolve_isohunt.py
+++ flexget/plugins/resolve_isohunt.py
@@ -8,6 +8,12 @@ class ResolveIsoHunt:
 
     def resolvable(self, feed, entry):
         url = entry['url']
+        # search is not supported
+        if url.startswith('http://isohunt.com/torrents/?ihq='):
+            return False
+        # not replaceable
+        if not 'torrent_details' in url:
+            return False
         return url.startswith('http://isohunt.com') and url.find('download') == -1
         
     def resolve(self, feed, entry):
