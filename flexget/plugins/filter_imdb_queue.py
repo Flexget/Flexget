b43d02714aa028894395d3f02134b2de233a42b8
lepinkainen <shrike@addiktit.net>
2009-10-24 00:27:49 +0000

    Refactor code and a complete overhaul of command line argument handling
    git-svn-id: http://svn.flexget.com/trunk@867 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter_imdb_queue.py
+++ flexget/plugins/filter_imdb_queue.py
@@ -28,6 +28,29 @@ class ImdbQueue(Base):
     def __str__(self):
         return '<ImdbQueue(%s qual %s)>' % (self.imdb_id, self.quality)
 
+_imdb_queue = {}
+def optik_imdb_queue(option, opt, value, parser):
+    """
+    Callback for Optik
+    --imdb-queue (add|del|list) IMDB-URL [quality]
+    """
+    if len(parser.rargs) == 0:
+        print "Usage: --imdb-queue (add|del|list) [IMDB_URL] [QUALITY]"
+        return
+
+    _imdb_queue['action'] = parser.rargs[0].lower()
+    
+    if len(parser.rargs) == 1:
+        return
+    # more than 2 args, we've got quality
+    if len(parser.rargs) == 2:
+        _imdb_queue['imdb_url'] = parser.rargs[1]
+
+    if len(parser.rargs) >= 3:
+        _imdb_queue['quality'] = parser.rargs[2]
+    else:
+        _imdb_queue['quality'] = 'dvd' # TODO: Get defaul from config somehow?
+
 class FilterImdbQueue:
     """
     Allows a queue of upcoming movies that will be forcibly allowed if the given quality matches
@@ -42,7 +65,6 @@ class FilterImdbQueue:
         return validator.factory('boolean')
 
     def on_feed_filter(self, feed):
-        log.debug("imdb queue run")
         for entry in feed.entries:
             # make sure the entry has IMDB fields filled
             try:
@@ -62,12 +84,15 @@ class FilterImdbQueue:
 
                 if not imdb_id: 
                     log.warning("No imdb id could be determined for %s" % entry['title'])
-                    return
+                    continue
 
-                # TODO: Check for quality
-                item = feed.session.query(ImdbQueue).filter(ImdbQueue.imdb_id == imdb_id).first()
+                if not 'quality' in entry:
+                    log.warning("No quality found for %s, skipping..." % entry['title'])
+                    continue
+
+                item = feed.session.query(ImdbQueue).filter(ImdbQueue.imdb_id == imdb_id).filter(ImdbQueue.quality == entry['quality']).first()
                 if item:
-                    log.info("Accepting %s from queue" % entry['title'])
+                    log.info("Accepting %s from queue with quality %s" % (entry['title'], entry['quality']))
                     # entry is in the database, accept over all other filters
                     entry['immortal'] = item.immortal
                     feed.accept(entry)
@@ -78,65 +103,110 @@ class FilterImdbQueue:
 
             feed.session.commit()
             
-class ImdbQueueAdd:
+class ImdbQueueManager:
     """
-    Add item to IMDb queue with an optional quality specifier
+    Handle IMDb queue management; add, delete and list
     """
+
+    valid_actions = ['add', 'del', 'list']
 
     def on_process_start(self, feed):
-        if feed.manager.options.imdb_queue_url:
-            feed.manager.disable_feeds()
-
-            imdb_id = extract_id(feed.manager.options.imdb_queue_url)
-            quality = feed.manager.options.imdb_queue_quality
-
-            from flexget.utils.titles.parser import TitleParser
-            if quality not in TitleParser.qualities:
-                print 'Unknown quality: %s' % quality
-                return
-
-            from flexget.manager import Session
-            session = Session()
-
-            # check if the item is already queued
-            item = session.query(ImdbQueue).filter(ImdbQueue.imdb_id == imdb_id).first()
-            if not item:
-                item = ImdbQueue(imdb_id, quality, True)
-                session.add(item)
-                log.debug("Added %s to queue with quality %s" % (imdb_id, quality))
-            else:
-                log.info("%s is already in the queue" % imdb_id)
-
-            session.commit()
+        """
+        Handle IMDb queue management
+        """
         
+        if not _imdb_queue: return
 
-class ImdbQueueList:
-    """
-    List IMDb queue url + quality
+        feed.manager.disable_feeds()
 
-    TODO: Maybe get movie name from DB or something? Parse the page with utils.imdb when adding
-    """
+        action = _imdb_queue['action']
 
-    def on_process_start(self, feed):
-        if feed.manager.options.imdb_queue_list:
-            feed.manager.disable_feeds()
+        if action not in self.valid_actions:
+            self.error("Invalid action, valid actions are: " + ", ".join(self.valid_actions))
+            return
+
+        # all actions except list require imdb_url to work
+        if action != 'list' and not 'imdb_url' in _imdb_queue:
+            self.error("No URL given")
+            return
             
-            from flexget.manager import Session
-            session = Session()            
+        if action == 'add':            
+            self.queue_add(_imdb_queue)
+            return
+        elif action == 'del':
+            self.queue_del(_imdb_queue)
+            return
+        elif action == 'list':
+            self.queue_list()
+            return
 
-            # check if the item is already queued
-            items = session.query(ImdbQueue)
-            for item in items:
-                print "http://www.imdb.com/title/"+item.imdb_id, item.quality
+    def error(self, msg):
+        print "IMDb Queue error: %s" % msg
+
+    def queue_add(self, queue_item):
+        """Add an item to the queue with the specified quality"""
+
+        imdb_id = extract_id(queue_item['imdb_url'])
+        quality = queue_item['quality']
+
+        # Check that the quality is valid
+        from flexget.utils.titles.parser import TitleParser
+        if quality not in TitleParser.qualities:
+            print 'Unknown quality: %s' % quality
+            return
+
+        from flexget.manager import Session
+        session = Session()
+
+        # check if the item is already queued
+        item = session.query(ImdbQueue).filter(ImdbQueue.imdb_id == imdb_id).first()
+        if not item:
+            item = ImdbQueue(imdb_id, quality, True)
+            session.add(item)
+            session.commit()
+            print "Added %s to queue with quality %s" % (imdb_id, quality)
+        else:
+            log.info("%s is already in the queue" % imdb_id)
 
 
+
+    def queue_del(self, queue_item):
+        """Delete the given item from the queue"""
+
+        imdb_id = extract_id(queue_item['imdb_url'])
+
+        from flexget.manager import Session
+        session = Session()
+
+        # check if the item is already queued
+        item = session.query(ImdbQueue).filter(ImdbQueue.imdb_id == imdb_id).first()
+        if item:
+            session.delete(item)
+            session.commit()
+            print "Deleted %s from queue" % (imdb_id)
+        else:
+            log.info("%s is not in the queue" % imdb_id)
+
+    def queue_list(self):
+        """List IMDb queue"""
+
+        from flexget.manager import Session
+        session = Session()            
+
+        items = session.query(ImdbQueue)
+
+        found = False
+
+        for item in items:
+            found = True
+            # TODO: Pretty printing
+            print "http://www.imdb.com/title/"+item.imdb_id, item.quality
+
+        if not found:
+            print "IMDb queue is empty"
+                
 register_plugin(FilterImdbQueue, 'imdb_queue', priorities={'filter': 129})
-register_plugin(ImdbQueueAdd, 'imdb_queue_add', builtin=True)
-register_plugin(ImdbQueueList, 'imdb_queue_list', builtin=True)
+register_plugin(ImdbQueueManager, 'imdb_queue_manager', builtin=True)
 
-register_parser_option('--imdb-queue', action='store', dest='imdb_queue_url', metavar="IMDB_URL",
-                       default=False, help="Add movie to queue")
-register_parser_option('--imdb-queue-list', action='store_true', dest='imdb_queue_list',
-                       default=False, help="List movie queue")
-register_parser_option('--imdb-queue-quality', action='store', dest='imdb_queue_quality', metavar="QUALITY", 
-                       default="dvdrip", help="Specify movie quality for the queue")
+register_parser_option('--imdb-queue', action='callback', callback=optik_imdb_queue,
+                       help='(add|del|list) [IMDB_URL] [QUALITY]')
