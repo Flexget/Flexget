4d640964faa38f8cba9e6c9a77babb3cc1a030ae
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-11-11 14:59:04 +0000

    Less strict manipulating with set plugin.
    git-svn-id: http://svn.flexget.com/trunk@947 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/modify_set.py
+++ flexget/plugins/modify_set.py
@@ -1,9 +1,8 @@
 import logging
 from flexget.plugin import *
 
-#from validator_pluginset import PluginSetValidator
-
 log = logging.getLogger('set')
+
 
 class ModifySet:
 
@@ -42,22 +41,24 @@ class ModifySet:
 
     def on_feed_modify(self, feed):
         for entry in feed.accepted:
-            self.modify(entry, feed.config['set'])
+            self.modify(entry, feed.config['set'], False)
             
-    def modify(self, entry, config):
+    def modify(self, entry, config, validate=True):
         """
         this can be called from a plugin to add set values to an entry
         """
 
-        from flexget import validator
-        v = validator.factory('dict')
-        for key in self.keys:
-            v.accept(self.keys[key], key=key)
-            
-        if not v.validate(config):
-            log.info("set parameters are invalid, error follows")
-            log.info(v.errors.messages)
-            return
+        if validate:
+            from flexget import validator
+            v = validator.factory('dict')
+            for key in self.keys:
+                v.accept(self.keys[key], key=key)
+
+            if not v.validate(config):
+                log.info('set parameters are invalid, error follows')
+                log.info(v.errors.messages)
+                return
+
         log.debug('adding set: info to entry:"%s" %s' % (entry['title'], config))
         for key, value in config.iteritems():
             entry[key] = value
