fbfecdd3b9935ca307a47fde4c4af9f54999c712
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-11-21 14:52:54 +0000

    Renamed `--try` to `--try-regexp`
    git-svn-id: http://svn.flexget.com/trunk@966 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/module_try_regexp.py
+++ flexget/plugins/module_try_regexp.py
@@ -1,14 +1,15 @@
-import logging, re
+import logging
+import re
 from flexget.plugin import *
 
-__pychecker__ = 'unusednames=parser'
-
 log = logging.getLogger('try_regexp')
+
 
 class PluginTryRegexp:
     """
         This plugin allows user to test regexps for a feed.
     """
+
     def __init__(self):
         self.abort = False
 
@@ -22,7 +23,7 @@ class PluginTryRegexp:
         for field, value in entry.iteritems():
             if not isinstance(value, basestring):
                 continue
-            if re.search(regexp, value, re.IGNORECASE|re.UNICODE):
+            if re.search(regexp, value, re.IGNORECASE | re.UNICODE):
                 return (True, field)
         return (False, None)
         
@@ -32,16 +33,16 @@ class PluginTryRegexp:
         if self.abort: 
             return
         
-        print '-'*79
+        print '-' * 79
         print 'Hi there, welcome to try regexps in realtime!'
         print 'Press ^D or type \'exit\' to continue. Type \'continue\' to continue non-interactive execution.'
         print 'Feed \'%s\' has %s entries, enter regexp to see what matches it.' % (feed.name, len(feed.entries))
         while (True):
             try:
                 s = raw_input('--> ')
-                if s=='exit': 
+                if s == 'exit':
                     break
-                if s=='abort' or s=='continue':
+                if s == 'abort' or s == 'continue':
                     self.abort = True
                     break
             except EOFError:
@@ -60,6 +61,6 @@ class PluginTryRegexp:
             print '%s of %s entries matched' % (count, len(feed.entries))
         print 'Bye!'
 
-register_plugin(PluginTryRegexp, 'try_regexp', builtin=True)
-register_parser_option('--try', action='store_true', dest='try_regexp', default=False,
+register_plugin(PluginTryRegexp, '--try-regexp', builtin=True)
+register_parser_option('--try-regexp', action='store_true', dest='try_regexp', default=False,
                        help='Try regular expressions interactively.')
