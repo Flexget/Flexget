24e1e6ea08a4d8f73d4485ca8b86b6371f0c4efb
lepinkainen <shrike@addiktit.net>
2009-11-26 11:57:40 +0000

    Added BDRip to qualities (divx release encoded from a BD source)
    git-svn-id: http://svn.flexget.com/trunk@970 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/utils/titles/parser.py
+++ flexget/utils/titles/parser.py
@@ -1,13 +1,16 @@
 import re
 
+
 class ParseWarning(Warning):
+
     def __init__(self, value, **kwargs):
         self.value = value
         self.kwargs = kwargs
 
+
 class TitleParser(object):
     
-    qualities = ['1080p', '1080', '720p', '720', 'hr', 'dvd', 'dvdrip', 'hdtv', 
+    qualities = ['1080p', '1080', '720p', '720', 'hr', 'dvd', 'bdrip', 'dvdrip', 'hdtv', 
                  'pdtv', 'dsr', 'dsrip', 'unknown']
     
     propers = ['proper', 'repack', 'rerip']
@@ -40,4 +43,3 @@ class TitleParser(object):
         """Case insensitive string replace"""
         pattern = re.compile(re.escape(old), re.I)
         return re.sub(pattern, new, str, count)
-
