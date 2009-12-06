202316f82076b70b1ca201d2877e5d5009ff7377
Marko Koivusalo <marko.koivusalo@gmail.com>
2009-12-06 17:23:43 +0000

    Added `flexget_vanilla.py` to allow execution without virtualenv
    git-svn-id: http://svn.flexget.com/trunk@997 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget_vanilla.py
@@ -0,0 +1,10 @@
+"""
+    Run FlexGet without bootstrap and virtualenv.
+    
+    You need to have all dependencies installed in site-packages when using this.
+"""
+
+import flexget
+
+if __name__ == '__main__':
+    flexget.main()
