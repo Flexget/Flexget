2855ae5fd8724cb7604f590bf1b06c44b7b007af
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-01-02 15:57:18 +0000

    Added stub for plugin `make_html`
    git-svn-id: http://svn.flexget.com/trunk@1049 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/plugins/output_html.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from flexget.plugin import *
+
+log = logging.getLogger('make_html')
+
+
+class OutputHtml:
+
+    # TODO: implement
+    def __validator(self):
+        from flexget import validator
+        return validator.factory('text')
+
+    def on_feed_output(self, feed):
+        try:
+            from Cheetah.Template import Template
+        except:
+            raise PluginError('make_html requires Cheetah template engine')
+
+        config = feed.config['make_html']
+
+        filename = os.path.expanduser(config['template'])
+        output = os.path.expanduser(config['file'])
+
+        import warnings
+        warnings.simplefilter('ignore', UserWarning)
+
+        # create the template
+        template = Template(file=filename)
+
+        # populate it
+        template.accepted = feed.accepted
+        template.rejected = feed.rejected
+        template.entries = feed.entries
+
+        f = open(output, 'w')
+        f.write(unicode(template))
+        f.close()
+
+register_plugin(OutputHtml, 'make_html')
