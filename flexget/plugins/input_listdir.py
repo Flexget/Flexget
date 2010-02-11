1f5511017f3f69cf5330ef014aa1fdef453a589c
Chase Sterling <chase.sterling@gmail.com>
2010-02-11 21:04:56 +0000

    Implements #472
    git-svn-id: http://svn.flexget.com/trunk@1110 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/input_listdir.py
+++ flexget/plugins/input_listdir.py
@@ -15,16 +15,29 @@ class InputListdir:
     
     def validator(self):
         from flexget import validator
-        return validator.factory('path')
+        root = validator.factory()
+        root.accept('path')
+        bundle = root.accept('list')
+        bundle.accept('path')
+        return root
+        
+    def get_config(self, feed):
+        config = feed.config.get('listdir', None)
+        #if only a single path is passed turn it into a 1 element list
+        if isinstance(config, basestring):
+            config = [config]
+        return config
 
     def on_feed_input(self, feed):
         from flexget.feed import Entry
         import os
-        for name in os.listdir(feed.config['listdir']):
-            e = Entry()
-            e['title'] = name
-            e['url'] = 'file://%s' % (os.path.join(feed.config['listdir'], name))
-            e['location'] = os.path.join(feed.config['listdir'], name)
-            feed.entries.append(e)
+        config = self.get_config(feed)
+        for path in config: 
+            for name in os.listdir(path):
+                e = Entry()
+                e['title'] = name
+                e['url'] = 'file://%s' % (os.path.join(path, name))
+                e['location'] = os.path.join(path, name)
+                feed.entries.append(e)
 
 register_plugin(InputListdir, 'listdir')
