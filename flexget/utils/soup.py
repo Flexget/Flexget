00ae10769d95445b80be0e8d129fbc76b63aca5a
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-02-17 20:52:19 +0000

    Hide DataLossWarnings that appeared with html5lib 0.90 or so.
    git-svn-id: http://svn.flexget.com/trunk@1124 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/utils/soup.py
+++ flexget/utils/soup.py
@@ -2,8 +2,15 @@ import html5lib
 from html5lib import treebuilders
 from cStringIO import StringIO
 
+# Hack, hide DataLossWarnings
+# Based on html5lib code namespaceHTMLElements=False should do it, but nope ...
+import warnings
+from html5lib.constants import DataLossWarning
+warnings.simplefilter('ignore', DataLossWarning)
+
+
 def get_soup(obj):
     if isinstance(obj, basestring):
         obj = StringIO(obj)
-    parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('beautifulsoup'))
+    parser = html5lib.HTMLParser(namespaceHTMLElements=False, tree=treebuilders.getTreeBuilder('beautifulsoup'))
     return parser.parse(obj)
