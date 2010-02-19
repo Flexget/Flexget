d8f233bf7baaaa2733347f842e62f5e85f78b708
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-02-19 21:32:17 +0000

    Removed namespaceHTMLElements=False, not available in older html5lib
    git-svn-id: http://svn.flexget.com/trunk@1129 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/utils/soup.py
+++ flexget/utils/soup.py
@@ -4,6 +4,7 @@ from cStringIO import StringIO
 
 # Hack, hide DataLossWarnings
 # Based on html5lib code namespaceHTMLElements=False should do it, but nope ...
+# Also it doesn't seem to be available in older version from html5lib, removing it
 import warnings
 from html5lib.constants import DataLossWarning
 warnings.simplefilter('ignore', DataLossWarning)
@@ -12,5 +13,5 @@ warnings.simplefilter('ignore', DataLossWarning)
 def get_soup(obj):
     if isinstance(obj, basestring):
         obj = StringIO(obj)
-    parser = html5lib.HTMLParser(namespaceHTMLElements=False, tree=treebuilders.getTreeBuilder('beautifulsoup'))
+    parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('beautifulsoup'))
     return parser.parse(obj)
