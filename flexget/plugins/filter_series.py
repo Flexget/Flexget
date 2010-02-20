Flexget/Flexget

commit 645420c3acdc2ad285d55d75cd1928055d2bbc1a

Refs #388. Improved rounding and the message was possibly shown occasionally at wrong situations.
git-svn-id: http://svn.flexget.com/trunk@1120 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/plugins/filter_series.py
@@ -885,12 +885,6 @@ class FilterSeries(SeriesPlugin):
                 return True
 
         # expire timeframe, accept anything
-        diff = datetime.now() - self.get_first_seen(feed.session, best)
-        if (diff.seconds < 60) and not feed.manager.unit_test:
-            entry = self.parser2entry[best]
-            log.info('Timeframe waiting %s for %s hours, currently best is %s' % \
-                (series_name, timeframe.seconds / 60 ** 2, entry['title']))
-
         first_seen = self.get_first_seen(feed.session, best)
         log.debug('timeframe: %s' % timeframe)
         log.debug('first_seen: %s' % first_seen)
@@ -911,6 +905,16 @@ class FilterSeries(SeriesPlugin):
                 feed.reject(entry, 'wrong quality')
             return True
         else:
+            # just verboses waiting
+            diff = datetime.now() - self.get_first_seen(feed.session, best)
+            if (diff.seconds < 60) and not feed.manager.unit_test:
+                import math
+                entry = self.parser2entry[best]
+                remain = math.ceil(timeframe.seconds / float(60) ** 2)
+                log.info('Timeframe waiting %1d for %s hours, currently best is %s' % \
+                    (series_name, remain, entry['title']))
+
+            # reject all episodes that are in timeframe
             log.debug('timeframe waiting %s episode %s, rejecting all occurrences' % (series_name, best.identifier))
             for ep in eps:
                 feed.reject(self.parser2entry[ep], 'timeframe is waiting')

commit b0279b5f8ddceccc76ea1a0a610df0147b8809da

Fixes series timeframe crash introduced in r1120. Refs #388
git-svn-id: http://svn.flexget.com/trunk@1130 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/plugins/filter_series.py
@@ -907,11 +907,11 @@ class FilterSeries(SeriesPlugin):
         else:
             # just verboses waiting
             diff = datetime.now() - self.get_first_seen(feed.session, best)
-            if (diff.seconds < 60) and not feed.manager.unit_test:
+            if diff.seconds < 60:
                 import math
                 entry = self.parser2entry[best]
                 remain = math.ceil(timeframe.seconds / float(60) ** 2)
-                log.info('Timeframe waiting %1d for %s hours, currently best is %s' % \
+                log.info('Timeframe waiting %s for %1d hours, currently best is %s' % \
                     (series_name, remain, entry['title']))
 
             # reject all episodes that are in timeframe