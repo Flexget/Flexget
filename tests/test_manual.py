81ae54ac9c683cb2bc3bdc2f919e2d0d87471ec1
NAME <ressu@svn.flexget.com>
2010-03-09 22:38:48 +0000

    Add plugin for manually downloading certain feeds. This allows a feed to be excluded from automatic downloads but still be available for manual fetches. Useful for backup feeds that are too slow to process all the time.
    The feed will only be downloaded if defined on the commandline with --feed=feedname
    
    
    git-svn-id: http://svn.flexget.com/trunk@1189 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ tests/test_manual.py
@@ -0,0 +1,44 @@
+import os
+from tests import FlexGetBase
+from nose.plugins.attrib import attr
+from nose.tools import raises
+from flexget.feed import EntryUnicodeError, Entry
+
+
+class TestManualAutomatic(FlexGetBase):
+    """
+        Test manual download feeds
+    """
+
+    __yaml__ = """
+        feeds:
+            test:
+                manual: true
+                mock:
+                    - {title: 'nodownload', url: 'http://localhost/nodownload'}
+    """
+
+    def test_manual_without_onlyfeed(self):
+        self.execute_feed('test')
+        assert not self.feed.find_entry(title='nodownload'), \
+                'Manual feeds downloaded on automatic run'
+
+
+class TestManualOnlyfeed(FlexGetBase):
+    """
+        Test manual download feeds
+    """
+
+    __yaml__ = """
+        feeds:
+            test2:
+                manual: true
+                mock:
+                    - {title: 'download', url: 'http://localhost/download'}
+    """
+
+    def test_manual_with_onlyfeed(self):
+        self.manager.options.onlyfeed = 'test2'
+        self.execute_feed('test2')
+        assert self.feed.find_entry(title='download'), \
+                'Manual feeds failed to download on manual run'
