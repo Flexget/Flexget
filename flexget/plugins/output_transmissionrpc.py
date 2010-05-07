a3851678a5529dfe91c2910a79f99da1fca84342
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-05-07 17:18:49 +0000

    Implements #549. Adds ability to remove finished torrents from transmission.
    git-svn-id: http://svn.flexget.com/trunk@1277 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/output_transmissionrpc.py
+++ flexget/plugins/output_transmissionrpc.py
@@ -20,6 +20,7 @@ class PluginTransmissionrpc:
         password: mypassword
         path: the download location
         debug: True
+        removewhendone: True
 
     Default values for the config elements:
 
@@ -28,6 +29,7 @@ class PluginTransmissionrpc:
         port: 9091
         enabled: True
         debug: False
+        removewhendone: False
     """
 
     def validator(self):
@@ -48,6 +50,8 @@ class PluginTransmissionrpc:
         advanced.accept('decimal', key='ratio', required=False)
         advanced.accept('boolean', key='enabled')
         advanced.accept('boolean', key='debug')
+        advanced.accept('text', key='loglevel')
+        advanced.accept('boolean', key='removewhendone')
         return root
 
     def get_config(self, feed):
@@ -58,18 +62,23 @@ class PluginTransmissionrpc:
         config.setdefault('debug', False)
         config.setdefault('host', 'localhost')
         config.setdefault('port', 9091)
+        config.setdefault('removewhendone', False)
         return config
 
     def on_process_start(self, feed):
         '''event handler'''
+        self.client = None
         set_plugin = get_plugin_by_name('set')
-
         set_plugin.instance.register_keys({'path': 'text', \
                                            'addpaused': 'boolean', \
                                            'maxconnections': 'number', \
                                            'maxupspeed': 'number',  \
                                            'maxdownspeed': 'number', \
                                            'ratio': 'decimal'})
+        config = self.get_config(feed)
+        if config['enabled'] and config['removewhendone']:
+            self.client = self.create_rpc_client(feed)
+            self.remove_finished(self.client)
 
     def on_feed_download(self, feed):
         """
@@ -91,9 +100,14 @@ class PluginTransmissionrpc:
         # don't add when learning
         if feed.manager.options.learn:
             return
-        if not feed.accepted or not config['enabled']:
+        if not config['enabled']:
             return
-        self.add_to_transmission(feed)
+        # Do not run if there is nothing to do
+        if len(feed.accepted) == 0:
+            return
+        if self.client == None:
+            self.client = self.create_rpc_client(feed)
+        self.add_to_transmission(self.client, feed)
 
     def _make_torrent_options_dict(self, feed, entry):
 
@@ -139,30 +153,32 @@ class PluginTransmissionrpc:
 
         return options
 
-    def add_to_transmission(self, feed):
-        """ adds accepted entries to transmission """
+    def create_rpc_client(self, feed):
         conf = self.get_config(feed)
         try:
             import transmissionrpc
             from transmissionrpc.transmission import TransmissionError
         except:
             raise PluginError('Transmissionrpc module required.', log)
+        # Set log level with loglevel
+        if 'loglevel' in conf:
+            levels = {'debug': logging.DEBUG, 'info': logging.INFO, 'warning': logging.WARNING, 'error': logging.ERROR, }
+            if conf['loglevel'] in levels.keys():
+                log.setLevel(levels[conf['loglevel']])
+        # Override log level with debug
         if conf['debug']:
             log.setLevel(logging.DEBUG)
 
         user, password = None, None
 
         if 'netrc' in conf:
-
             try:
                 user, account, password = netrc(conf['netrc']).authenticators(conf['host'])
             except IOError, e:
                 log.error('netrc: unable to open: %s' % e.filename)
             except NetrcParseError, e:
                 log.error('netrc: %s, file: %s, line: %n' % (e.msg, e.filename, e.line))
-
         else:
-
             if 'username' in conf:
                 user = conf['username']
             if 'password' in conf:
@@ -177,7 +193,11 @@ class PluginTransmissionrpc:
             urllib2.install_opener(prev_opener)
         else:
             cli = transmissionrpc.Client(conf['host'], conf['port'], user, password)
+        
+        return cli
 
+    def add_to_transmission(self, cli, feed):
+        """ adds accepted entries to transmission """
         for entry in feed.accepted:
             if feed.manager.options.test:
                 log.info('Would add %s to transmission' % entry['url'])
@@ -212,5 +232,19 @@ class PluginTransmissionrpc:
             if not 'download' in feed.config:
                 os.remove(entry['file'])
                 del(entry['file'])
+        
+    def remove_finished(self, cli):
+        # Get a list of active transfers
+        transfers = cli.info(arguments=['id', 'hashString', 'name', 'status', 'uploadRatio', 'seedRatioLimit'])
+        remove_ids = []
+        # Go through the list of active transfers and add finished transfers to remove_ids.
+        for tid, transfer in transfers.iteritems():
+            log.debug('Transfer "%s": status: "%s" upload ratio: %.2f seed ratio: %.2f' % (transfer.name, transfer.status, transfer.uploadRatio, transfer.seedRatioLimit))
+            if transfer.status == 'stopped' and transfer.uploadRatio >= transfer.seedRatioLimit:
+                log.info('Remove torrent "%s" from transmission' % (transfer.name))
+                remove_ids.append(transfer.id)
+        # Remove finished transfers
+        if len(remove_ids) > 0:
+            cli.remove(remove_ids)
 
 register_plugin(PluginTransmissionrpc, 'transmissionrpc')
