Flexget/Flexget

commit efaace550f59a6d9535976ab0b04761d3815fb7f

Fixes downloading from URLS with non latin1 characters. Fixes torrent filename generation with non latin1 characters.
git-svn-id: http://svn.flexget.com/trunk@1357 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/plugins/modify_torrent.py
@@ -34,7 +34,7 @@ class Torrent(object):
         # single file torrent
         if 'length' in self.content['info']:
             t = {}
-            t['name'] = self.content['info']['name']
+            t['name'] = self.content['info']['name'].decode('utf-8')
             t['size'] = self.content['info']['length']
             t['path'] = ''
             files.append(t)
@@ -43,7 +43,7 @@ class Torrent(object):
             for item in self.content['info']['files']:
                 t = {}
                 t['path'] = '/'.join(item['path'][:-1])
-                t['name'] = item['path'][-1:]
+                t['name'] = item['path'][-1:].decode('utf-8')
                 t['size'] = item['length']
                 files.append(t)
         return files
@@ -233,6 +233,7 @@ class TorrentFilename(object):
 
     def make_filename(self, torrent, entry):
         """Build a filename for this torrent"""
+        
         title = entry['title']
         files = torrent.get_filelist()
         if len(files) == 1:
@@ -244,7 +245,9 @@ class TorrentFilename(object):
         # neatify title
         title = title.replace('/', '_')
         title = title.replace(' ', '_')
-        title = title.encode('iso8859-1', 'ignore') # Damn \u200b -character, how I loathe thee
+        title = title.replace('\u200b', '')
+        
+        #title = title.encode('iso8859-1', 'ignore') # Damn \u200b -character, how I loathe thee
         # TODO: replace only zero width spaces, leave unicode alone?
 
         fn = '%s.torrent' % title

flexget/plugins/output_download.py
@@ -108,8 +108,26 @@ class PluginDownload:
                     log.error('The server couldn\'t fulfill the request. Error code: %s' % e.code)
 
     def download(self, feed, entry):
-        url = urllib.quote(entry['url'].encode('latin1'), safe=':/~?=&%')
+        """Downloads :entry:. May raise exception(s) PluginWarning"""
+    
+        # see http://bugs.python.org/issue1712522
+        # note, url is already unicode ...
+        url = entry['url']
+        
+        try:
+            url = url.encode('latin1')
+        except UnicodeEncodeError:
+            log.warning('URL for `%s` is garbled' % entry['title'])
+
+        try:
+            url = urllib.quote(url, safe=':/~?=&%')
+        except:
+            try:
+                url = urllib.quote(url.encode('utf-8'), safe=':/~?=&%')
+            except:
+                log.warning('Unable to URL-encode URL for `%s`' % entry['title'])
         log.debug('Downloading url \'%s\'' % url)
+
         # get content
         if 'basic_auth_password' in entry and 'basic_auth_username' in entry:
             # TODO: should just add handler if default opener is present, now this will lose all other

commit 69eb7bfd20a5c453a5165c8290aaaef5afb95b22

Fixes torrent parsing broken in r1357. Refs #646
git-svn-id: http://svn.flexget.com/trunk@1358 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/plugins/modify_torrent.py
@@ -43,7 +43,7 @@ class Torrent(object):
             for item in self.content['info']['files']:
                 t = {}
                 t['path'] = '/'.join(item['path'][:-1])
-                t['name'] = item['path'][-1:].decode('utf-8')
+                t['name'] = item['path'][-1].decode('utf-8')
                 t['size'] = item['length']
                 files.append(t)
         return files