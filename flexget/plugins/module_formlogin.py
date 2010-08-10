04b7169412d3218f54042ae08537e814c2e31fc3
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-08-10 14:23:45 +0000

    Added configuration validation, error handling and fixed unregistering urllib2 opener on feed abort & finish.
    git-svn-id: http://svn.flexget.com/trunk@1360 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/module_formlogin.py
+++ flexget/plugins/module_formlogin.py
@@ -6,23 +6,36 @@ import urllib2
 log = logging.getLogger('formlogin')
 
 
-class InputFormLogin(object):
+class FormLogin(object):
     """
     Login on form
     """
+
+    def validator(self):
+        from flexget import validator
+        root = validator.factory('dict')
+        root.accept('url', key='url', required=True)
+        root.accept('text', key='username', required=True)
+        root.accept('text', key='password', required=True)
+        return root
 
     def on_feed_start(self, feed):
         try:
             from mechanize import Browser
         except ImportError:
-            raise PluginError('mechanize module required.', log)
+            raise PluginError('mechanize required (python module), please install it.', log)
+
         url = feed.config['form']['url']
         username = feed.config['form']['username']
         password = feed.config['form']['password']
 
         br = Browser()
         br.set_handle_robots(False)
-        br.open(url)
+        try:
+            br.open(url)
+        except Exception, e:
+            # TODO: improve error handling
+            raise PluginError('Unable to post login form', log)
 
         #br.set_debug_redirects(True)
         #br.set_debug_responses(True)
@@ -43,4 +56,12 @@ class InputFormLogin(object):
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
         urllib2.install_opener(opener)                                
 
-register_plugin(InputFormLogin, 'form')
+    def on_feed_exit(self, feed):
+        """Feed exiting, remove cookiejar"""
+        log.debug('Removing urllib2 opener')
+        urllib2.install_opener(None)
+
+    # Feed aborted, unhook the cookiejar
+    on_feed_abort = on_feed_exit                                    
+
+register_plugin(FormLogin, 'form')
