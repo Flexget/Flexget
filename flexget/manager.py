Flexget/Flexget

commit 99271f5fa40db755cfb798d2f327dca498523785

Remove erroneous warning when windows paths were listed
git-svn-id: http://svn.flexget.com/trunk@1344 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/manager.py
@@ -189,11 +189,13 @@ class Manager(object):
                 if indentation < level:
                     duplicates[level] = {}
             if ':' in line:
-                name = line.split(':', 1)[0].strip()
-                ns = duplicates.setdefault(indentation, {})
-                if name in ns:
-                    log.warning('Trying to set value for `%s` in the line %s, but it is already defined in the line %s!' % (name, line_num, ns[name]))
-                ns[name] = line_num
+                # Check that : is not embedded in a string, like 'd:\files'
+                if not line.split(':', 1)[1] or line.split(':', 1)[1].startswith(' '):
+                    name = line.split(':', 1)[0].strip()
+                    ns = duplicates.setdefault(indentation, {})
+                    if name in ns:
+                        log.warning('Trying to set value for `%s` in line %s, but it is already defined in line %s!' % (stripped, line_num, ns[name]))
+                    ns[name] = line_num
 
             prev_indentation = indentation
             # this line is a mapping (ends with :)

commit 1d4e104d9c3ef61ca0ae7e20b32969a86a77954a

Fix bug in pre-checker introduced in r1344
git-svn-id: http://svn.flexget.com/trunk@1359 3942dd89-8c5d-46d7-aeed-044bccf3e60c

flexget/manager.py
@@ -201,7 +201,7 @@ class Manager(object):
                     name = line.split(':', 1)[0].strip()
                     ns = duplicates.setdefault(indentation, {})
                     if name in ns:
-                        log.warning('Trying to set value for `%s` in line %s, but it is already defined in line %s!' % (stripped, line_num, ns[name]))
+                        log.warning('Trying to set value for `%s` in line %s, but it is already defined in line %s!' % (name, line_num, ns[name]))
                     ns[name] = line_num
 
             prev_indentation = indentation