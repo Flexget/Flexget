69423eca1daba17be14017812b92a0e203d8619a
Chase Sterling <chase.sterling@gmail.com>
2010-09-08 21:53:54 +0000

    Manipulate plugin now accepts a list with single key, value pairs instead of a dict of them. Fixes bug where whitespace was added or removed when using manipulate extract. Updates unit tests for manipulate. -This line, and those below, will be ignored--
    M    tests/test_manipulate.py
    M    flexget/plugins/module_manipulate.py
    
    
    git-svn-id: http://svn.flexget.com/trunk@1395 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/module_manipulate.py
+++ flexget/plugins/module_manipulate.py
@@ -26,8 +26,12 @@ class Manipulate(object):
     
     def validator(self):
         from flexget import validator
-        root = validator.factory('dict')
-        edit = root.accept_any_key('dict')
+        root = validator.factory()
+        bundle = root.accept('list').accept('dict')
+        # prevent invalid indentation level
+        bundle.reject_keys(['from', 'extract', 'replace'],
+            'Option \'$key\' has invalid indentation level. It needs 2 more spaces.')
+        edit = bundle.accept_any_key('dict')
         edit.accept('text', key='from')
         edit.accept('regexp', key='extract')
         replace = edit.accept('dict', key='replace')
@@ -43,33 +47,32 @@ class Manipulate(object):
     def process(self, feed, entry):
         config = feed.config['manipulate']
 
-        for field, config in config.iteritems():
-            from_field = field
-            if 'from' in config:
-                from_field = config['from']
-            field_value = entry.get(from_field)
-            log.debug('field: %s from_field: %s field_value: %s' % (field, from_field, field_value))
+        for item in config:
+            for field, config in item.iteritems():
+                from_field = field
+                if 'from' in config:
+                    from_field = config['from']
+                field_value = entry.get(from_field)
+                log.debug('field: %s from_field: %s field_value: %s' % (field, from_field, field_value))
 
-            if 'extract' in config:
-                if not field_value:
-                    log.warning('Cannot extract, field %s is not present' % from_field)
-                    continue
-                match = re.match(config['extract'], field_value)
-                if match:
-                    groups = [x for x in match.groups() if x is not None]
-                    log.debug('groups: %s' % groups)
-                    entry[field] = ' '.join(groups)
-                    # remove duplicate spaces
-                    entry[field] = ' '.join(entry[field].split())
+                if 'extract' in config:
+                    if not field_value:
+                        log.warning('Cannot extract, field %s is not present' % from_field)
+                        continue
+                    match = re.match(config['extract'], field_value)
+                    if match:
+                        groups = [x for x in match.groups() if x is not None]
+                        log.debug('groups: %s' % groups)
+                        entry[field] = ''.join(groups)
+                        feed.verbose_details('Field %s is now %s' % (field, entry[field]))
+                        log.debug('field %s is now %s' % (field, entry[field]))
+
+                if 'replace' in config:
+                    if not field_value:
+                        log.warning('Cannot replace, field %s is not present' % from_field)
+                        continue
+                    replace_config = config['replace']
+                    entry[field] = re.sub(replace_config['regexp'], replace_config['format'], field_value)
                     feed.verbose_details('Field %s is now %s' % (field, entry[field]))
-                    log.debug('field %s is now %s' % (field, entry[field]))
-
-            if 'replace' in config:
-                if not field_value:
-                    log.warning('Cannot replace, field %s is not present' % from_field)
-                    continue
-                replace_config = config['replace']
-                entry[field] = re.sub(replace_config['regexp'], replace_config['format'], field_value)
-                feed.verbose_details('Field %s is now %s' % (field, entry[field]))
 
 register_plugin(Manipulate, 'manipulate')
--- tests/test_manipulate.py
+++ tests/test_manipulate.py
@@ -9,16 +9,26 @@ class TestManipulate(FlexGetBase):
             mock:
               - {title: 'abc FOO'}
             manipulate:
-              title:
-                replace:
-                  regexp: FOO
-                  format: BAR
+              - title:
+                  replace:
+                    regexp: FOO
+                    format: BAR
           test_2:
             mock:
               - {title: '1234 abc'}
             manipulate:
-              title:
-                extract: \d+(.*)
+              - title:
+                  extract: \d+\s*(.*)
+          test_multiple_edits:
+            mock:
+              - {title: 'abc def'}
+            manipulate:
+              - title:
+                  replace:
+                    regexp: abc
+                    format: "123"
+              - title:
+                  extract: \d+\s+(.*)
     """
 
     def test_replace(self):
@@ -28,3 +38,7 @@ class TestManipulate(FlexGetBase):
     def test_extract(self):
         self.execute_feed('test_2')
         assert self.feed.find_entry('entries', title='abc'), 'extract failed'
+
+    def test_multiple_edits(self):
+        self.execute_feed('test_multiple_edits')
+        assert self.feed.find_entry('entries', title='def'), 'multiple edits on 1 field failed'
