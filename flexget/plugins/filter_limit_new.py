1d6805c03e4cea57a3bed4cb13795967a172621c
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-09-12 21:04:38 +0000

    Fixed `limit_new` which was broken few revisions ago. Added unit tests for the plugin.
    git-svn-id: http://svn.flexget.com/trunk@1419 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter_limit_new.py
+++ flexget/plugins/filter_limit_new.py
@@ -30,7 +30,7 @@ class FilterLimitNew(object):
         i = 1
         rejected = 0
         passed = []
-        for entry in feed.accepted + [i for i in feed.entries if i not in feed.accepted]:
+        for entry in feed.accepted + [e for e in feed.entries if e not in feed.accepted]:
             # if entry is marked as passed, don't remove it
             # this is because we used accepted + entries and it may be listed in both ..
             if entry in passed:
