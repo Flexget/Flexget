23050c72c9be8325394a292c376fc66d29e3e838
Chase Sterling <chase.sterling@gmail.com>
2010-10-02 16:56:11 +0000

    Add unit tests for add_ and remove_trackers. Set more svn:eol-style to native.
    git-svn-id: http://svn.flexget.com/trunk@1476 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- tests/test_torrent.py
+++ tests/test_torrent.py
@@ -1,5 +1,6 @@
 import shutil
 from tests import FlexGetBase
+from flexget.plugins.modify_torrent import Torrent
 
 
 class TestInfoHash(FlexGetBase):
@@ -17,3 +18,46 @@ class TestInfoHash(FlexGetBase):
         hash = self.feed.entries[0].get('torrent_info_hash')
         assert hash == '20AE692114DC343C86DF5B07C276E5077E581766', \
             'InfoHash does not match (got %s)' % hash
+
+
+class TestModifyTrackers(FlexGetBase):
+
+    __yaml__ = """
+        feeds:
+          test_add_trackers:
+            mock:
+              - {title: 'test', file: 'tests/test_add_trackers.torrent'}
+            add_trackers:
+              - udp://thetracker.com/announce
+
+          test_remove_trackers:
+            mock:
+              - {title: 'test', file: 'tests/test_remove_trackers.torrent'}
+            remove_trackers:
+              - ubuntu
+    """
+
+    testfiles = ['tests/test_add_trackers.torrent', 'tests/test_remove_trackers.torrent']
+
+    def setup(self):
+        FlexGetBase.setup(self)
+        for filename in self.testfiles:
+            shutil.copy('tests/test.torrent', filename)
+
+    def load_torrent(self, filename):
+        f = open(filename, 'rb')
+        data = f.read()
+        f.close()
+        return Torrent(data)
+
+    def test_add_trackers(self):
+        self.execute_feed('test_add_trackers')
+        torrent = self.load_torrent('tests/test_add_trackers.torrent')
+        assert 'udp://thetracker.com/announce' in torrent.get_multitrackers(), \
+            'udp://thetracker.com/announce should have been added to trackers'
+
+    def test_remove_trackers(self):
+        self.execute_feed('test_remove_trackers')
+        torrent = self.load_torrent('tests/test_remove_trackers.torrent')
+        assert 'http://torrent.ubuntu.com:6969/announce' not in torrent.get_multitrackers(), \
+            'ubuntu tracker should have been removed'
