a8984f0f869279089fcaac1f0f01c8021e2d2f60
Chase Sterling <chase.sterling@gmail.com>
2010-11-01 02:47:50 +0000

    Move filter_quality priority before series and imdb.
    git-svn-id: http://svn.flexget.com/trunk@1574 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter_quality.py
+++ flexget/plugins/filter_quality.py
@@ -1,5 +1,5 @@
 import logging
-from flexget.plugin import register_plugin
+from flexget.plugin import register_plugin, priority
 import flexget.utils.qualities as quals
 
 log = logging.getLogger('quality')
@@ -38,6 +38,8 @@ class FilterQuality(object):
             config['quality'] = [config['quality']]
         return config
 
+    # Run before series and imdb plugins, so correct qualities are chosen
+    @priority(130)
     def on_feed_filter(self, feed):
         config = self.get_config(feed)
         for entry in feed.entries:
