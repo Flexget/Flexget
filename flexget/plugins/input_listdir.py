a893223d4964f946d9413a17e62871e2660843a8
Chase Sterling <chase.sterling@gmail.com>
2010-11-04 04:00:45 +0000

    Fix url of entries made by listdir on Windows.
    git-svn-id: http://svn.flexget.com/trunk@1586 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/input_listdir.py
+++ flexget/plugins/input_listdir.py
@@ -1,5 +1,5 @@
 import logging
-from flexget.plugin import *
+from flexget.plugin import register_plugin
 
 log = logging.getLogger('listdir')
 
@@ -7,12 +7,12 @@ log = logging.getLogger('listdir')
 class InputListdir:
     """
         Uses local path content as an input.
-        
+
         Example:
-        
+
         listdir: /storage/movies/
     """
-    
+
     def validator(self):
         from flexget import validator
         root = validator.factory()
@@ -20,10 +20,10 @@ class InputListdir:
         bundle = root.accept('list')
         bundle.accept('path')
         return root
-        
+
     def get_config(self, feed):
         config = feed.config.get('listdir', None)
-        #if only a single path is passed turn it into a 1 element list
+        # If only a single path is passed turn it into a 1 element list
         if isinstance(config, basestring):
             config = [config]
         return config
@@ -32,11 +32,15 @@ class InputListdir:
         from flexget.feed import Entry
         import os
         config = self.get_config(feed)
-        for path in config: 
+        for path in config:
             for name in os.listdir(unicode(path)):
                 e = Entry()
                 e['title'] = name
-                e['url'] = 'file://%s' % (os.path.join(path, name))
+                filepath = os.path.join(path, name)
+                # Windows paths need an extra / prepended to them
+                if not filepath.startswith('/'):
+                    filepath = '/' + filepath
+                e['url'] = 'file://%s' % (filepath)
                 e['location'] = os.path.join(path, name)
                 feed.entries.append(e)
 
