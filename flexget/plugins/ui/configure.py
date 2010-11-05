999d19ff00b2ca92a04c312adf46e61e67a0ab6d
Marko Koivusalo <marko.koivusalo@gmail.com>
2010-11-05 20:31:14 +0000

    Implementing basics of webui
    git-svn-id: http://svn.flexget.com/branches/webui@1599 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/ui/configure.py
+++ flexget/plugins/ui/configure.py
@@ -1,17 +1,28 @@
 from flexget.webui import app, manager, register_menu
-from flask import render_template
+from flask import render_template, request, flash
+import yaml
 
 
 @app.route('/configure')
-def plugins():
+def configure():
     return render_template('configure.html')
 
 
-@app.route('/configure/<root>/<name>')
-def configure(root, name):
-    # TODO: plugins = manager.feeds ...
-    plugins = ['hard coded', 'list']
-    return render_template('configure.html')
+@app.route('/configure/<root>/<name>', methods=['POST', 'GET'])
+def edit(root, name):
+    config = manager.feeds[name].config
+
+    if request.method == 'POST':
+        # TODO: validate configuration
+        flash('Configuration saved')
+        config = yaml.load(request.form['config'])
+
+    context = {
+        'name': name,
+        'root': root,
+        'config': yaml.dump(config, default_flow_style=False)}
+
+    return render_template('configure_edit.html', **context)
 
 
 register_menu('/configure', 'Configure', order=10)
