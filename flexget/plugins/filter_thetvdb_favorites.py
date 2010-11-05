96a91e9f043648f07be0d541459217afcc5e2197
Chase Sterling <chase.sterling@gmail.com>
2010-11-05 04:46:28 +0000

    thetvdb_favorites now stores series names in database, updated once per hour. fixes #785 If thetvdb_favorites fails to update from thetvdb it falls back on last known series list.
    git-svn-id: http://svn.flexget.com/trunk@1591 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter_thetvdb_favorites.py
+++ flexget/plugins/filter_thetvdb_favorites.py
@@ -1,9 +1,31 @@
 import logging
-from flexget.plugin import register_plugin, internet, get_plugin_by_name, PluginError
+from flexget.plugin import register_plugin, register_parser_option, internet, get_plugin_by_name, PluginError
+from flexget.manager import Base, Session
+from flexget.utils.tools import urlopener
 from BeautifulSoup import BeautifulStoneSoup
-import urllib
+from sqlalchemy import Column, Integer, Unicode, DateTime, String
+from datetime import datetime, timedelta
+import urllib2
 
 log = logging.getLogger('thetvdb_favorites')
+
+
+class ThetvdbFavorites(Base):
+
+    __tablename__ = 'thetvdb_favorites'
+
+    id = Column(Integer, primary_key=True)
+    account_id = Column(String, index=True)
+    series_name = Column(Unicode)
+    added = Column(DateTime)
+
+    def __init__(self, account_id, series_name):
+        self.series_name = series_name
+        self.account_id = account_id
+        self.added = datetime.now()
+
+    def __repr__(self):
+        return '<series_favorites(account_id=%s, series_name=%s)>' % (self.account_id, self.series_name)
 
 
 class FilterThetvdbFavorites:
@@ -19,29 +41,50 @@ class FilterThetvdbFavorites:
     def validator(self):
         from flexget import validator
         root = validator.factory('dict')
-        root.accept('text', key='account_id')
+        root.accept('text', key='account_id', required=True)
         root.accept('text', key='series_group')
         return root
 
     @internet(log)
     def on_process_start(self, feed):
         config = feed.config.get('thetvdb_favorites')
-        url = "http://thetvdb.com/api/User_Favorites.php?accountid=%s" % str(config['account_id'])
-        log.debug('requesting %s' % url)
-        data = BeautifulStoneSoup(urllib.urlopen(url))
-        favorite_ids = []
-        for i in data.favorites.findAll("series", recursive=False):
-            favorite_ids.append(int(i.string))
-        if not favorite_ids:
+        account_id = str(config['account_id'])
+        session = Session()
+        cache = session.query(ThetvdbFavorites).filter(ThetvdbFavorites.account_id == account_id)
+        if cache.count() and cache.first().added > datetime.now() - timedelta(minutes=1):
+            log.debug('Using cached thetvdb favorites')
+        else:
+            log.debug('Updating favorites from thetvdb.com')
+            try:
+                url = 'http://thetvdb.com/api/User_Favorites.php?accountid=%s' % account_id
+                log.debug('requesting %s' % url)
+                data = BeautifulStoneSoup(urlopener(url, log))
+                favorite_ids = []
+                for i in data.favorites.findAll("series", recursive=False):
+                    favorite_ids.append(int(i.string))
+                # We found new data, remove old cached data
+                items = []
+                for fid in favorite_ids:
+                    data = BeautifulStoneSoup(urlopener("http://thetvdb.com/data/series/%s/" % str(fid), log), \
+                        convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+                    items.append(ThetvdbFavorites(account_id, data.series.seriesname.string))
+            except (urllib2.URLError, IOError):
+                log.error('Error retrieving favorites from thetvdb, using cache.')
+            else:
+                # Successfully updated from tvdb, update the database
+                log.debug('Successfully updated favorites from thetvdb.com')
+                cache.delete()
+                session.add_all(items)
+                session.commit()
+                cache = session.query(ThetvdbFavorites).filter(ThetvdbFavorites.account_id == account_id)
+        if not cache.count():
             log.info("Didn't find any thetvdb.com favorites.")
             return
         tvdb_series_config = {}
         series_group = config.get('series_group', 'thetvdb_favs')
         tvdb_series_config[series_group] = []
-        for fid in favorite_ids:
-            data = BeautifulStoneSoup(urllib.urlopen("http://thetvdb.com/data/series/%s/" % str(fid)), \
-                convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
-            tvdb_series_config[series_group].append(data.series.seriesname.string)
+        for series in cache:
+            tvdb_series_config[series_group].append(series.series_name)
         # If the series plugin is not configured on this feed, make a blank config
         if not 'series' in feed.config:
             feed.config['series'] = {}
