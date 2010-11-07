fe070fcf3a6b1596eab2543497deb753b8843e0e
Chase Sterling <chase.sterling@gmail.com>
2010-11-07 10:15:30 +0000

    First stab at a log viewer.
    git-svn-id: http://svn.flexget.com/branches/webui@1607 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/ui/log_viewer.py
+++ flexget/plugins/ui/log_viewer.py
@@ -1,7 +1,40 @@
-from flexget.webui import register_plugin
-from flask import render_template, Module
+import logging
+from flexget.webui import register_plugin, manager
+from flexget.manager import Base, Session
+from flask import render_template, Module, jsonify, request
+from sqlalchemy import Column, DateTime, Integer, Unicode, String
+import time
+from datetime import datetime
 
 log_viewer = Module(__name__)
+
+
+class LogEntry(Base):
+    __tablename__ = 'log'
+
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime)
+    logger = Column(String)
+    levelno = Column(Integer)
+    message = Column(String)
+
+    def __init__(self, record):
+        self.created = datetime.fromtimestamp(record.created) #datetime.now()
+        self.logger = record.name
+        self.levelno = record.levelno
+        self.message = record.getMessage()
+
+
+class DBLogHandler(logging.Handler):
+
+    def __init__(self, session):
+        logging.Handler.__init__(self)
+        self.session = session
+
+    def emit(self, record):
+        logentry = LogEntry(record)
+        self.session.add(logentry)
+        self.session.commit()
 
 
 @log_viewer.route('/')
@@ -9,4 +42,27 @@ def index():
     return render_template('log.html')
 
 
+@log_viewer.route('/_get_updates')
+def get_updates():
+    last_time = datetime.fromtimestamp(float(request.args.get('last_time', 0)))
+    results = Session().query(LogEntry).filter(LogEntry.created > last_time)
+    items = []
+    for entry in results:
+        args = {'asctime': entry.created.strftime('%Y-%m-%d %H:%M'),
+                'levelname': logging.getLevelName(entry.levelno),
+                'logger': entry.logger,
+                'message': entry.message}
+        items.append('%(asctime)-15s %(levelname)-6s %(logger)-10s %(message)s' % args)
+    return jsonify(items=items, time=time.mktime(datetime.now().timetuple()))
+
+
+def on_load():
+    Base.metadata.create_all(bind=manager.engine)
+    # Register db handler with base logger
+    logger = logging.getLogger()
+    handler = DBLogHandler(Session())
+    logger.addHandler(handler)
+
+
+on_load()
 register_plugin(log_viewer, url_prefix='/log', menu='Log', order=256)
