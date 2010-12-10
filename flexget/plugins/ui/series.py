a3918d35fe48969548c8751a723bfc0c5d1b42d9
joshuajonah <joshuajonah@svn.flexget.com>
2010-12-10 07:09:29 +0000

    Added friendly first_seen dates to episode items.
    git-svn-id: http://svn.flexget.com/branches/webui@1714 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/ui/series.py
+++ flexget/plugins/ui/series.py
@@ -6,6 +6,11 @@ from sqlalchemy.sql.expression import desc, asc
 from flexget.plugin import PluginDependencyError
 from flexget.webui import register_plugin, db_session
 
+import datetime
+import time
+
+from utils import pretty_date
+
 
 try:
     from flexget.plugins.filter_series import Series, Episode, Release
@@ -18,8 +23,12 @@ series_module = Module(__name__, url_prefix='/series')
 @series_module.route('/')
 def index():
     
+    releases = db_session.query(Release).order_by(desc(Release.id)).slice(0, 10)
+    for rel in releases:
+        rel.episode.first_seen = pretty_date(time.mktime(rel.episode.first_seen.timetuple()))
+        
     context = {'report': db_session.query(Series).order_by(asc(Series.name)).all(), 
-                'releases': db_session.query(Release).order_by(desc(Release.id)).all()}
+                'releases': releases}
     
     return render_template('series.html', **context)
 
