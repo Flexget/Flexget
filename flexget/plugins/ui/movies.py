2a717544dd4489d68a2430068f306db2a9b5e9dd
joshuajonah <joshuajonah@svn.flexget.com>
2010-12-11 19:29:59 +0000

    Added stub for Movies plugin to webui.
    git-svn-id: http://svn.flexget.com/branches/webui@1742 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/plugins/ui/movies.py
@@ -0,0 +1,57 @@
+from itertools import groupby
+
+from flask import render_template, Module
+from sqlalchemy.sql.expression import desc, asc
+
+from flexget.plugin import PluginDependencyError
+from flexget.webui import register_plugin, db_session, app
+
+
+import datetime
+import time
+import logging
+
+from utils import pretty_date
+
+try:
+    from flexget.plugins.filter_imdb_queue import ImdbQueue
+    
+except ImportError:
+    raise PluginDependencyError('Requires imdb plugin', 'movies')
+
+
+movies_module = Module(__name__, url_prefix='/movies')
+log = logging.getLogger('ui.movies')
+
+
+# TODO: refactor this filter to some globally usable place (webui.py?)
+#       also flexget/plugins/ui/utils.py needs to be removed
+#       ... mainly because we have flexget/utils for that :)
+
+
+@app.template_filter('pretty_age')
+def pretty_age_filter(value):
+    return pretty_date(time.mktime(value.timetuple()))
+
+
+@movies_module.route('/')
+def index():
+    movies = db_session.query(ImdbQueue).all()# order_by(desc(Release.id)).slice(0, 10)
+    context = {'movies': movies}
+    return render_template('movies.html', **context)
+
+# 
+# @movies_module.context_processor
+# def series_list():
+#     """Add series list to all pages under series"""
+#     return {'report': db_session.query(Series).order_by(asc(Series.name)).all()}
+# 
+# 
+# @movies_module.route('/<name>')
+# def episodes(name):
+#     series = db_session.query(Series).filter(Series.name == name).first()
+#     context = {'episodes': series.episodes, 'name': name}
+#     return render_template('series.html', **context)
+
+
+register_plugin(movies_module, menu='Movies')
