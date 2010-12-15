9b6272370682e2c965007e8f5d628d3175ae1a72
joshuajonah <joshuajonah@svn.flexget.com>
2010-12-15 17:26:36 +0000

    Added first version of self built themoviedb.py. Currently pulls in yaml from themoviedb. Need somebody more familiar with FlexGet to show me how to read the yaml with our current tools into a python data object.
    git-svn-id: http://svn.flexget.com/trunk@1808 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/ui/plugins/movies/themoviedb.py
@@ -0,0 +1,69 @@
+import urllib
+import logging
+
+log = logging.getLogger('ui.themoviedb')
+
+
+class TMDB(object):
+
+    def __init__(self, api_key='50f722dc90c43df28eced4a212a01f74', markup='yaml', lang='en'):
+        '''
+        TMDB Client
+        '''
+        self.lang = lang
+        self.markup = markup
+        self.key = api_key
+        self.server = 'http://api.themoviedb.org'
+
+    def connection(self, url):
+        '''
+        Return URL Content
+        '''
+        data = None
+        try:
+            client = urllib.urlopen(url)
+            data = client.read()
+            client.close()
+            log.debug('Recieved data: %s' % data)
+        except Exception, e:
+            log.debug('Connection failure: %s' % e)
+        return data
+
+    def build_url(self, look, term):
+        '''
+        Methods => search, imdbLookup, getInfo, getImages
+        '''
+        return '%s/2.1/Movie.%s/%s/%s/%s/%s' % (
+            self.server, look, self.lang, self.markup, self.key, term)
+
+    def search_results(self, term):
+        '''
+        Search Wrapper
+        '''
+        return self.connection(self.build_url('search', term))
+
+    def get_info(self, tmdb_id):
+        '''
+        GetInfo Wrapper
+        '''
+        return self.connection(self.build_url('getInfo', tmdb_id))
+
+    def imdb_results(self, imdb_id):
+        '''
+        IMDB Search Wrapper
+        '''
+        log.debug('Looking up imdb id: %s' % imdb_id)
+        return self.connection(self.build_url('imdbLookup', imdb_id))
+
+    def imdb_images(self, imdb_id):
+        '''
+        IMDB Search Wrapper
+        '''
+        imdb_id = 'tt0%s' % imdb_id
+        return self.connection(self.build_url('getImages', imdb_id))
+
+    def tmdb_images(self, tmdb_id):
+        '''
+        GetInfo Wrapper
+        '''
+        return self.connection(self.build_url('getImages', tmdb_id))
