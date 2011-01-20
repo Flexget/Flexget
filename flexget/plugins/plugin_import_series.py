572820d4888733a123b3df4f58bc022f8ba80b44
Chase Sterling <chase.sterling@gmail.com>
2011-01-20 17:40:55 +0000

    Improve validator messages for import_series. refs #770
    git-svn-id: http://svn.flexget.com/trunk@1876 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/plugin_import_series.py
+++ flexget/plugins/plugin_import_series.py
@@ -37,7 +37,12 @@ class ImportSeries(FilterSeriesBase):
         # Build a dict validator that accepts the available input plugins and their settings
         for plugin in valid_inputs:
             if hasattr(plugin.instance, 'validator'):
-                from_section.valid[plugin.name] = [plugin.instance.validator()]
+                validator = plugin.instance.validator()
+                if validator.name == 'root':
+                    # If a root validator is returned, grab the list of child validators
+                    from_section.valid[plugin.name] = validator.valid
+                else:
+                    from_section.valid[plugin.name] = [plugin.instance.validator()]
             else:
                 from_section.valid[plugin.name] = [validator.factory('any')]
         return root
