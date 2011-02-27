a34148b2803045d3989265417b497b41dbe37eda
Chase Sterling <chase.sterling@gmail.com>
2011-02-27 05:11:22 +0000

    Adds sleep plugin which creates a delay before feed execution. fixes #981
    git-svn-id: http://svn.flexget.com/trunk@1970 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/plugins/plugin_sleep.py
@@ -0,0 +1,22 @@
+import logging
+import time
+from flexget.plugin import register_plugin, priority
+
+log = logging.getLogger('sleep')
+
+
+class PluginSleep(object):
+    """Causes a pause to occur before execution of a feed"""
+
+    def validator(self):
+        from flexget import validator
+        return validator.factory('number')
+
+    @priority(255)
+    def on_feed_start(self, feed, config):
+        if config:
+            logger = log.debug if feed.manager.options.quiet else log.info
+            logger('Sleeping for %d seconds.' % config)
+            time.sleep(config)
+
+register_plugin(PluginSleep, 'sleep', api_ver=2)
