661f99ae226d26883c83dd986acfe91815fe416c
Marko Koivusalo <marko.koivusalo@gmail.com>
2011-04-29 22:51:56 +0000

    Fixes demonoid urlrewriting. Closes #883
    git-svn-id: http://svn.flexget.com/trunk@2178 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/urlrewrite_demonoid.py
+++ flexget/plugins/urlrewrite_demonoid.py
@@ -1,14 +1,14 @@
 import logging
-from flexget.plugin import *
+from flexget.plugin import register_plugin
 
-log = logging.getLogger("demonoid")
+log = logging.getLogger('demonoid')
 
 
 class UrlRewriteDemonoid:
     """Demonoid urlrewriter."""
 
     def url_rewritable(self, feed, entry):
-        return entry['url'].startswith('http://www.demonoid.com/files/details/')
+        return entry['url'].startswith('http://www.demonoid.me/files/details/')
 
     def url_rewrite(self, feed, entry):
         entry['url'] = entry['url'].replace('details', 'download/HTTP')
