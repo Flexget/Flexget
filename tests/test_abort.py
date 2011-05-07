19e37533c51d5884a901dfddfe248f4f529e03a0
Chase Sterling <chase.sterling@gmail.com>
2011-05-07 02:26:03 +0000

    Fix abort test
    git-svn-id: http://svn.flexget.com/trunk@2197 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- tests/test_abort.py
+++ tests/test_abort.py
@@ -2,21 +2,21 @@ from tests import FlexGetBase
 
 
 class TestAbort(FlexGetBase):
-    
+
     __yaml__ = """
         feeds:
           test:
             # causes on_feed_abort to be called
-            disable_builtins: yes 
+            disable_builtins: yes
 
             # causes abort
-            interval: 10 days
-            
+            nzb_size: 10
+
             # another event hookup with this plugin
             headers:
-              test: value 
+              test: value
     """
-    
+
     def test_abort(self):
         self.execute_feed('test')
         assert self.feed._abort, 'Feed not aborted'
