6cb7d467c913e522f8e11a4ccd8317184e40855c
Chase Sterling <chase.sterling@gmail.com>
2011-05-10 22:08:27 +0000

    Refactors limit_new a bit, no longer saves undecided entries to backlog.
    git-svn-id: http://svn.flexget.com/trunk@2216 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter/limit_new.py
+++ flexget/plugins/filter/limit_new.py
@@ -26,36 +26,29 @@ class FilterLimitNew(object):
         from flexget import validator
         return validator.factory('integer')
 
-    def on_process_start(self, feed):
+    def on_process_start(self, feed, config):
         try:
             self.backlog = get_plugin_by_name('backlog').instance
         except DependencyError:
             log.warning('Unable utilize backlog plugin, entries may slip trough limit_new in some rare cases')
 
     @priority(-255)
-    def on_feed_filter(self, feed):
+    def on_feed_filter(self, feed, config):
         if feed.manager.options.learn:
             log.info('Plugin limit_new is disabled with --learn / --reset')
             return
 
-        amount = feed.config.get('limit_new', len(feed.entries))
-        i = 1
-        rejected = 0
-        passed = []
-        for entry in feed.accepted + [e for e in feed.entries if e not in feed.accepted]:
-            # if entry is marked as passed, don't remove it
-            # this is because we used accepted + entries and it may be listed in both ..
-            if entry in passed:
-                continue
-            if i > amount:
-                rejected += 1
-                feed.reject(entry, 'limit exceeded')
-                if self.backlog:
-                    self.backlog.add_backlog(feed, entry, '48 hours')
-            else:
-                passed.append(entry)
+        amount = config
+        for index, entry in enumerate(feed.accepted):
+            if index < amount:
                 log.verbose('Allowed %s (%s)' % (entry['title'], entry['url']))
-            i += 1
-        log.debug('Rejected: %s Allowed: %s' % (rejected, len(passed)))
+            else:
+                feed.reject(entry, 'limit exceeded')
+                # Also save this in backlog so that it can be accepted next time.
+                if self.backlog:
+                    self.backlog.add_backlog(feed, entry)
 
-register_plugin(FilterLimitNew, 'limit_new')
+        log.debug('Rejected: %s Allowed: %s' % (len(feed.accepted[amount:]), len(feed.accepted[:amount])))
+
+
+register_plugin(FilterLimitNew, 'limit_new', api_ver=2)
