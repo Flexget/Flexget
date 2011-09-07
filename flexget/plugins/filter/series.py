463026def438665b21f29eff200ee01d04c409e2
Chase Sterling <chase.sterling@gmail.com>
2011-09-07 19:03:15 +0000

    Revert r2341 refs #1232
    git-svn-id: http://svn.flexget.com/trunk@2342 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter/series.py
+++ flexget/plugins/filter/series.py
@@ -285,8 +285,7 @@ class SeriesPlugin(object):
             if parser.season and parser.episode:
                 episode.season = parser.season
                 episode.number = parser.episode
-            episode.series_id = series.id
-            session.add(episode)
+            series.episodes.append(episode) # pylint:disable=E1103
             log.debug('-> added %s' % episode)
 
         # if release does not exists in episodes, add new
@@ -306,8 +305,7 @@ class SeriesPlugin(object):
             release.quality = parser.quality
             release.proper_count = parser.proper_count
             release.title = parser.data
-            release.episode_id = episode.id
-            session.add(release)
+            episode.releases.append(release) # pylint:disable=E1103
             log.debug('-> added %s' % release)
         return release
 
