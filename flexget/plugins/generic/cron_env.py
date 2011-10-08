4fd75bce6cc9ce6c3c5af78e2df13d52c6d20619
Marko Koivusalo <marko.koivusalo@gmail.com>
2011-10-08 23:57:54 +0000

    Refs #801. Added crontab fs encoding verification plugin.
    git-svn-id: http://svn.flexget.com/trunk@2418 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/plugins/generic/cron_env.py
@@ -0,0 +1,45 @@
+from flexget.utils.log import log_once
+
+__author__ = 'paranoidi'
+
+import sys
+from flexget.utils.simple_persistence import SimplePersistence
+from flexget.plugin import register_plugin
+import logging
+
+log = logging.getLogger('cron_env')
+
+
+class CronEnvPlugin(object):
+
+    def __init__(self, ):
+        self.executed = False
+        self.persistence = SimplePersistence(plugin='cron_env')
+
+    def on_process_start(self, feed, entries):
+        if self.executed:
+            return
+
+        encoding = sys.getfilesystemencoding()
+        if feed.manager.options.quiet:
+            if 'terminal_encoding' in self.persistence:
+                terminal_encoding = self.persistence['terminal_encoding']
+                if terminal_encoding != encoding:
+                    log.warning('Your cron environment has different filesystem encoding '
+                                '(%s) compared to your terminal environment (%s).' %
+                                (encoding, terminal_encoding))
+                    if encoding == 'ANSI_X3.4-1968':
+                        log.warning('Your current cron environment results filesystem encoding ANSI_X3.4-1968 '
+                                    'which supports only ASCII letters in filenames.')
+                else:
+                    log_once('Good! Your crontab environment seems to be same as terminal.')
+            else:
+                log.info('Please run FlexGet manually once for environment verification purposes.')
+        else:
+            log.debug('Encoding %s stored' % encoding)
+            self.persistence['terminal_encoding'] = encoding
+        self.executed = True
+
+
+if not sys.platform.startswith('win'):
+    register_plugin(CronEnvPlugin, 'cron_env', api_ver=2, builtin=True)
