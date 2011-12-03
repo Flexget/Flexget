794c0778450e9e43ff27b095a738a1095ec44455
pyroscope <pyroscope.project@gmail.com>
2011-12-03 12:53:36 +0000

    Added glob pattern support to --feed
    git-svn-id: http://svn.flexget.com/trunk@2485 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/operate/feed_execution.py
+++ flexget/plugins/operate/feed_execution.py
@@ -1,4 +1,5 @@
 import logging
+import fnmatch
 from flexget.plugin import register_plugin, register_parser_option, PluginError
 
 log = logging.getLogger('feed_control')
@@ -13,25 +14,35 @@ class OnlyFeed(object):
 
         Multiple feeds:
         flexget --feed feeda,feedb
+
+        Patterns:
+        flexget --feed 'tv*'
     """
 
     def on_process_start(self, feed):
         # If --feed hasn't been specified don't do anything
         if not feed.manager.options.onlyfeed:
             return
-        # Make a list of the specified feeds to run
+
+        # Make a list of the specified feeds to run, and those available
         onlyfeeds = feed.manager.options.onlyfeed.split(',')
 
         # Make sure the specified feeds exist
         enabled_feeds = [f.name.lower() for f in feed.manager.feeds.itervalues() if f.enabled]
         for onlyfeed in onlyfeeds:
-            if onlyfeed.lower() not in enabled_feeds:
+            if any(i in onlyfeed for i in '*?['):
+                # Try globbing
+                if not any(fnmatch.fnmatchcase(f.lower(), onlyfeed.lower()) for f in enabled_feeds):
+                    feed.manager.disable_feeds()
+                    raise PluginError('No match for feed pattern \'%s\'' % onlyfeed, log)
+            elif onlyfeed.lower() not in enabled_feeds:
                 # If any of the feeds do not exist, exit with an error
                 feed.manager.disable_feeds()
                 raise PluginError('Could not find feed \'%s\'' % onlyfeed, log)
 
         # If current feed is not among the specified feeds, disable it
-        if feed.name.lower() not in [f.lower() for f in onlyfeeds]:
+        if not any(feed.name.lower() == f.lower() or fnmatch.fnmatchcase(feed.name.lower(), f.lower())
+                for f in onlyfeeds):
             feed.enabled = False
 
 
@@ -53,5 +64,6 @@ class ManualFeed(object):
 
 register_plugin(OnlyFeed, '--feed', builtin=True)
 register_plugin(ManualFeed, 'manual')
-register_parser_option('--feed', action='store', dest='onlyfeed', default=None,
-                       metavar='FEED', help='Run only specified feed.')
+register_parser_option('--feed', action='store', dest='onlyfeed', default=None, metavar='FEED[,...]', 
+                       help='Run only specified feed(s), optionally using glob patterns ("tv-*").'
+                            ' Matching is case-insensitive.')
