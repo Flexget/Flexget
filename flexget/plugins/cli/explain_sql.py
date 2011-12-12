f141025af744c71ff11280d07038785356d69fa7
Chase Sterling <chase.sterling@gmail.com>
2011-12-12 19:35:11 +0000

    Add query timer to --explain-sql
    git-svn-id: http://svn.flexget.com/trunk@2517 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/cli/explain_sql.py
+++ flexget/plugins/cli/explain_sql.py
@@ -1,11 +1,12 @@
 import logging
 from optparse import SUPPRESS_HELP
+from time import time
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import Executable, ClauseElement, _literal_as_text
 from flexget import manager
-from flexget.plugin import register_plugin, register_parser_option
+from flexget.plugin import register_parser_option
 from flexget.event import event
 
 log = logging.getLogger('explain_sql')
@@ -18,7 +19,7 @@ class Explain(Executable, ClauseElement):
 
         
 @compiles(Explain)
-def pg_explain(element, compiler, **kw):
+def explain(element, compiler, **kw):
     text = 'EXPLAIN QUERY PLAN ' + compiler.process(element.statement)
     return text
 
@@ -29,8 +30,10 @@ class ExplainQuery(Query):
         log.info('Query:\n\t%s' % unicode(self).replace('\n', '\n\t'))
         explain = self.session.execute(Explain(self)).fetchall()
         text = '\n\t'.join('|'.join(str(x) for x in line) for line in explain)
-        log.info('Explain Query Plan:\n\t%s' % text)
-        return Query.__iter__(self)
+        before = time()
+        result = Query.__iter__(self)
+        log.info('Query Time: %0.3f Explain Query Plan:\n\t%s' % (time() - before, text))
+        return result
 
 
 @event('manager.execute.started')
