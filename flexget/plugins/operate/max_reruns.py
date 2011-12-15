07382185daf673092301564c9c0220f8e43d316f
Marko Koivusalo <marko.koivusalo@gmail.com>
2011-12-15 22:01:42 +0000

    Improve max_reruns plugin
    git-svn-id: http://svn.flexget.com/trunk@2532 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/operate/max_reruns.py
+++ flexget/plugins/operate/max_reruns.py
@@ -1,5 +1,6 @@
 import logging
 from flexget import validator
+from flexget.feed import Feed
 from flexget.plugin import register_plugin
 
 log = logging.getLogger('max_reruns')
@@ -8,12 +9,23 @@ log = logging.getLogger('max_reruns')
 class MaxReRuns(object):
     """Overrides the maximum amount of re-runs allowed by a feed."""
 
+    def __init__(self):
+        self.default = Feed.max_reruns
+
     def validator(self):
         root = validator.factory('integer')
         return root
 
-    def on_process_start(self, feed, config):
+    def on_feed_start(self, feed, config):
+        self.default = feed.max_reruns
         feed.max_reruns = config
         log.debug('changing max feed rerun variable to: %s' % config)
+
+    def on_feed_exit(self, feed, config):
+        log.debug('restoring max feed rerun variable to: %s' % self.default)
+        feed.max_reruns = self.default
+
+    on_feed_abort = on_feed_exit
+
 
 register_plugin(MaxReRuns, 'max_reruns', api_ver=2)
