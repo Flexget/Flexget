93983f632c4646c7208acf7742263782e08ea11b
moohpah <kyron.vermeulen@gmail.com>
2011-12-16 00:32:42 +0000

    Submitted by:	Moohpah
    git-svn-id: http://svn.flexget.com/trunk@2534 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/filter/torrent_alive.py
+++ flexget/plugins/filter/torrent_alive.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from urllib import quote
 from urllib2 import URLError
 from flexget.utils.tools import urlopener
@@ -6,6 +7,52 @@ from flexget.utils.bittorrent import bdecode
 from flexget.plugin import register_plugin, priority
 
 log = logging.getLogger('torrent_alive')
+
+
+class TorrentAliveThread(threading.Thread):
+
+    def __init__(self, tracker, info_hash):
+        threading.Thread.__init__(self)
+        self.tracker = tracker
+        self.hashfile = info_hash
+
+    def run(self):
+        try:
+            self.tracker_seeds = self.get_tracker_seeds(self.tracker, self.hashfile)
+        except URLError, e:
+            log.debug('Error scraping %s: %s' % (self.tracker, e))
+            self.tracker_seeds = 0
+        else:
+            log.debug('%s seeds found from %s' % (self.tracker_seeds, self.get_scrape_url(self.tracker, self.hashfile)))
+
+    def get_scrape_url(self, tracker_url, info_hash):
+        if 'announce' in tracker_url:
+            result = tracker_url.replace('announce', 'scrape')
+        else:
+            log.debug('`announce` not contained in tracker url, guessing scrape address.')
+            result = tracker_url + '/scrape'
+        if result.startswith('udp:'):
+            result = result.replace('udp:', 'http:')
+        result += '&' if '?' in result else '?'
+        result += 'info_hash=%s' % quote(info_hash.decode('hex'))
+        return result
+
+    def get_tracker_seeds(self, url, info_hash):
+        url = self.get_scrape_url(url, info_hash)
+        if not url:
+            log.debug('if not url is true returning 0')
+            return 0
+        log.debug('Checking for seeds from %s' % url)
+        try:
+            data = bdecode(urlopener(url, log, retries=1, timeout=10).read()).get('files')
+        except SyntaxError, e:
+            log.warning('Error decoding tracker response: %s' % e)
+            return 0
+        if not data:
+            log.debug('the moose is loose')
+            return 0
+        log.debug('get_tracker_seeds is returning: %s' % data.values()[0]['complete'])
+        return data.values()[0]['complete']
 
 
 class TorrentAlive(object):
@@ -30,8 +77,9 @@ class TorrentAlive(object):
     def on_feed_output(self, feed, config):
         if not config:
             return
-        # Convert True to 1
+            # Convert True to 1
         min_seeds = int(config)
+
         for entry in feed.accepted:
             if entry.get('torrent_seeds'):
                 log.debug('Not checking trackers for seeds, as torrent_seeds is already filled.')
@@ -44,63 +92,31 @@ class TorrentAlive(object):
                 announce_list = torrent.content.get('announce-list')
                 if announce_list:
                     # Multitracker torrent
+                    threadlist = []
                     for tier in announce_list:
                         for tracker in tier:
-                            try:
-                                tracker_seeds = self.get_tracker_seeds(tracker, info_hash)
-                            except URLError, e:
-                                log.debug('Error scraping %s: %s' % (tracker, e))
-                                # Error connecting to tracker, try the next in this tier
-                                continue
-                            else:
-                                log.debug('%s seeds found from %s' % (tracker_seeds, tracker))
-                                # The same peer may be connected to multiple trackers, so just use the max seeds
-                                # found by any one tracker.
-                                seeds = max(seeds, tracker_seeds)
-                                # If we successfully connect to a tracker in this tier, no need to try the others
-                                break
-                        if seeds >= min_seeds:
-                            # If we already found enough seeds, no need to continue
-                            break
+                            background = TorrentAliveThread(tracker, info_hash)
+                            threadlist.append(background)
+                            background.start()
+                            log.debug('Started thread to scrape %s with info hash %s' % (tracker, info_hash))
+
+                    for background in threadlist:
+                        log.debug('Coming up next: %s' % background.tracker)
+                        background.join()
+                        seeds = max(seeds, background.tracker_seeds)
+                        log.debug('Current hightest number of seeds found: %s' % seeds)
+                    log.debug('Highest number of seeds found: %s' % seeds)
                 else:
                     # Single tracker
-                    tracker = torrent.content['announce']
-                    try:
-                        seeds = self.get_tracker_seeds(tracker, info_hash)
-                    except URLError, e:
-                        log.debug('Error scraping %s: %s' % (tracker, e))
+                    background = TorrentAliveThread(tracker, info_hash)
+                    background()
+                    seeds = background.tracker_seeds
+                    #Final reject
                 if seeds < min_seeds:
                     feed.reject(entry, reason='Tracker(s) had < %s required seeds. (%s)' % (min_seeds, seeds),
-                                remember_time='1 hour')
+                        remember_time='1 hour')
                     feed.rerun()
                 else:
                     log.debug('Found %i seeds from trackers' % seeds)
-
-    def get_scrape_url(self, tracker_url, info_hash):
-        if 'announce' in tracker_url:
-            result = tracker_url.replace('announce', 'scrape')
-        else:
-            log.debug('`announce` not contained in tracker url, guessing scrape address.')
-            result = tracker_url + '/scrape'
-        if result.startswith('udp:'):
-            result = result.replace('udp:', 'http:')
-        result += '&' if '?' in result else '?'
-        result += 'info_hash=%s' % quote(info_hash.decode('hex'))
-        return result
-
-    def get_tracker_seeds(self, url, info_hash):
-        url = self.get_scrape_url(url, info_hash)
-        if not url:
-            return 0
-        log.debug('Checking for seeds from %s' % url)
-        try:
-            data = bdecode(urlopener(url, log, retries=1, timeout=10).read()).get('files')
-        except SyntaxError, e:
-            log.warning('Error bdecoding tracker response: %s' % e)
-            return 0
-        if not data:
-            return 0
-        return data.values()[0]['complete']
-
 
 register_plugin(TorrentAlive, 'torrent_alive', api_ver=2)
