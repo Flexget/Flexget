b118d11decf7842569345ace0d6051f7dac629c2
Chase Sterling <chase.sterling@gmail.com>
2012-08-15 19:27:40 +0000

    Fix unit test for content_filter. Make --dump-config not run the feeds, add it to --help. fix #1345
    git-svn-id: http://svn.flexget.com/trunk@3084 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- flexget/plugins/output/dump_config.py
+++ flexget/plugins/output/dump_config.py
@@ -1,6 +1,6 @@
 import logging
 from argparse import SUPPRESS
-from flexget.plugin import register_plugin, register_parser_option
+from flexget.plugin import register_plugin, register_parser_option, priority
 
 log = logging.getLogger('dump_config')
 
@@ -10,19 +10,20 @@ class OutputDumpConfig(object):
         Dumps task config in STDOUT in yaml at exit or abort event.
     """
 
-    def on_task_exit(self, task):
+    @priority(-255)
+    def on_task_start(self, task):
         if task.manager.options.dump_config:
             import yaml
             print '--- config from task: %s' % task.name
             print yaml.safe_dump(task.config)
             print '---'
+            task.abort(silent=True)
         if task.manager.options.dump_config_python:
             print task.config
-
-    on_task_abort = on_task_exit
+            task.abort(silent=True)
 
 register_plugin(OutputDumpConfig, 'dump_config', debug=True, builtin=True)
-register_parser_option('--dump-config', action='store_true', dest='dump_config', default=False, \
-                       help=SUPPRESS)
-register_parser_option('--dump-config-python', action='store_true', dest='dump_config_python', default=False, \
+register_parser_option('--dump-config', action='store_true', dest='dump_config', default=False,
+                       help='Display the config of each feed after preset merging/config generation occurs.')
+register_parser_option('--dump-config-python', action='store_true', dest='dump_config_python', default=False,
                        help=SUPPRESS)
--- tests/test_content_filter.py
+++ tests/test_content_filter.py
@@ -54,44 +54,44 @@ class TestContentFilter(FlexGetBase):
 
     @with_filecopy('test.torrent', 'test_reject1.torrent')
     def test_reject1(self):
-        """Content Size: torrent with min size"""
+        """Content Filter: torrent with min size"""
         self.execute_task('test_reject1')
         assert self.task.find_entry('rejected', title='test'), \
             'should have rejected, contains *.iso'
 
     @with_filecopy('test.torrent', 'test_reject2.torrent')
     def test_reject2(self):
-        """Content Size: torrent with max size"""
+        """Content Filter: torrent with max size"""
         self.execute_task('test_reject2')
         assert self.task.find_entry('accepted', title='test'), \
             'should have accepted, doesn\t contain *.avi'
 
     @with_filecopy('test.torrent', 'test_require1.torrent')
     def test_require1(self):
-        """Content Size: torrent with min size"""
+        """Content Filter: torrent with min size"""
         self.execute_task('test_require1')
         assert self.task.find_entry('accepted', title='test'), \
             'should have accepted, contains *.iso'
 
     @with_filecopy('test.torrent', 'test_require2.torrent')
     def test_require2(self):
-        """Content Size: torrent with max size"""
+        """Content Filter: torrent with max size"""
         self.execute_task('test_require2')
         assert self.task.find_entry('rejected', title='test'), \
             'should have rejected, doesn\t contain *.avi'
 
     @with_filecopy('test.torrent', 'test_strict.torrent')
     def test_strict(self):
-        """Content Size: strict enabled"""
+        """Content Filter: strict enabled"""
         self.execute_task('test_strict')
         assert self.task.find_entry('rejected', title='test'), \
             'should have rejected non torrent'
 
     def test_cache(self):
-        """Content Size: caching"""
+        """Content Filter: caching"""
         self.execute_task('test_cache')
 
-        assert self.task.find_entry('rejected', title='test', rejected_by='content_filter'), \
+        assert self.task.find_entry('rejected', title='test'), \
             'should have rejected, contains *.iso'
 
         # Test that remember_rejected rejects the entry before us next time
