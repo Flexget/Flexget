1784e363da005e27d8405288bb007b7ec1dc92f1
Chase Sterling <chase.sterling@gmail.com>
2012-09-07 15:43:18 +0000

    Add a unit test for cookies plugin.
    git-svn-id: http://svn.flexget.com/trunk@3138 3942dd89-8c5d-46d7-aeed-044bccf3e60c

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,20 +1,19 @@
 from tests import FlexGetBase
 from nose.plugins.attrib import attr
 
-class TestHeaders(FlexGetBase):
+class TestCookies(FlexGetBase):
     __yaml__ = """
         tasks:
-          test_headers:
+          test_cookies:
             text:
               url: http://httpbin.org/cookies
               entry:
                 title: '\"title\": \"(.*)\"'
                 url: '\"url\": \"(.*)\"'
-            headers:
-              Cookie: "title=blah; url=other"
+            cookies: cookies.txt
     """
 
     @attr(online=True)
-    def test_headers(self):
-        self.execute_task('test_headers')
-        assert self.task.find_entry(title='blah', url='other'), 'Entry should have been created.'
+    def test_cookies(self):
+        self.execute_task('test_cookies')
+        assert self.task.find_entry(title='blah', url='aoeu'), 'Entry should have been created.'
