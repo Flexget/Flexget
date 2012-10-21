cbeb7db264b2cd62d25e9762e037a6837022f204
Chase Sterling <chase.sterling@gmail.com>
2012-10-21 22:17:33 +0000

    Add 'magnets' plugin which rejects entries with only magnet links.
    git-svn-id: http://svn.flexget.com/trunk@3177 3942dd89-8c5d-46d7-aeed-044bccf3e60c

---
+++ flexget/plugins/filter/magnets.py
@@ -0,0 +1,23 @@
+from flexget import plugin
+from flexget import validator
+
+class Magnets(object):
+    """Removes magnet urls form the urls list. Rejects entries that have nothing but magnet urls."""
+    def validator(self):
+        return validator.factory('boolean')
+
+    @plugin.priority(0)
+    def on_task_urlrewrite(self, task, config):
+        if config is not False:
+            return
+        for entry in task.accepted:
+            if 'urls' in entry:
+                entry['urls'] = filter(lambda url: not url.startswith('magnet:'), entry['urls'])
+
+            if entry['url'].startswith('magnet:'):
+                if entry.get('urls'):
+                    entry['url'] = entry['urls'][0]
+                else:
+                    entry.reject('Magnet urls not allowed.', remember=True)
+
+plugin.register_plugin(Magnets, 'magnets', api_ver=2)
