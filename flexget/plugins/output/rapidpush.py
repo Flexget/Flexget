fa11740d870c5a8d5ed15efde8fcde5dd44d29d2
Chase Sterling <chase.sterling@gmail.com>
2013-02-04 10:54:33 -0500

    Move priority of all notification outputs to last.

--- flexget/plugins/output/rapidpush.py
+++ flexget/plugins/output/rapidpush.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 import logging
-import json
-from flexget.plugin import register_plugin
+
+from flexget.utils import json
+from flexget.plugin import register_plugin, priority
 from flexget.utils.template import RenderError
 
 log = logging.getLogger('rapidpush')
@@ -20,7 +21,7 @@ class OutputRapidPush(object):
         [category: category, default FlexGet]
         [title: title, default New release]
         [group: device group, default no group]
-        [message: the message, to include the title from flexget insert {{title}} at the wanted position, default {{title}}]
+        [message: the message, supports Jinja templating, default {{title}}]
         [priority: 0 - 6 (6 = highest), default 2 (normal)]
 
     Configuration parameters are also supported from entries (eg. through set).
@@ -39,7 +40,7 @@ class OutputRapidPush(object):
 
     def prepare_config(self, config):
         if isinstance(config, bool):
-            config = {'enabled': config}	
+            config = {'enabled': config}
         config.setdefault('title', 'New release')
         config.setdefault('category', 'FlexGet')
         config.setdefault('priority', 2)
@@ -47,6 +48,8 @@ class OutputRapidPush(object):
         config.setdefault('message', '{{title}}')
         return config
 
+    # Run last to make sure other outputs are successful before sending notification
+    @priority(0)
     def on_task_output(self, task, config):
         # get the parameters
         config = self.prepare_config(config)
