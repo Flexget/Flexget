118d444357193a5eb798f52c0b34f874d30e1942
paranoidi <marko.koivusalo@gmail.com>
2013-02-12 00:55:16 +0200

    Remove imdb_id support from rottentomatoes_lookup.
    - Fixes unit tests
    - Using imdb_id might have worked with api_key

--- tests/test_rottentomatoes.py
+++ tests/test_rottentomatoes.py
@@ -15,8 +15,6 @@ class TestRottenTomatoesLookup(FlexGetBase):
               - {title: 'Star Wars: Episode I - The Phantom Menace (in 3D)'}
               # tests direct id
               - {title: '[Group] Taken 720p', rt_id: 770680780}
-              # tests imdb id
-              - {title: 'Gone in 60 Seconds', imdb_id: 'tt0071571'}
               # tests title + year
               - {title: 'Rush.Hour[1998]1080p[Eng]-FOO'}
               # test short title, with repack and without year
@@ -28,18 +26,18 @@ class TestRottenTomatoesLookup(FlexGetBase):
     def test_rottentomatoes_lookup(self):
         self.execute_task('test')
         # check that these were created
-        assert self.task.find_entry(rt_name='Toy Story', rt_year=1995, rt_id=9559, imdb_id='tt0114709'),\
-                'Didn\'t populate RT info for Toy Story'
+        assert self.task.find_entry(rt_name='Toy Story', rt_year=1995, rt_id=9559, imdb_id='tt0114709'), \
+            'Didn\'t populate RT info for Toy Story'
+        assert self.task.find_entry(imdb_id='tt0114709'), \
+            'Didn\'t populate imdb_id info for Toy Story'
         assert self.task.find_entry(rt_name='The Matrix', rt_year=1999, rt_id=12897, imdb_id='tt0133093'), \
-                'Didn\'t populate RT info for The Matrix'
+            'Didn\'t populate RT info for The Matrix'
         assert self.task.find_entry(rt_name='Star Wars: Episode I - The Phantom Menace',
-                rt_year=1999, rt_id=10008),\
-                'Didn\'t populate RT info for Star Wars: Episode I - The Phantom Menace (in 3D)'
-        assert self.task.find_entry(rt_name='Taken', rt_year=2008, rt_id=770680780),\
-                'Didn\'t populate RT info for Taken'
-        assert self.task.find_entry(rt_name='Gone in 60 Seconds', rt_year=1974, rt_id=10730),\
-                'Didn\'t populate RT info for Gone in 60 Seconds'
-        assert self.task.find_entry(rt_name='Rush Hour', rt_year=1998, rt_id=10201),\
-                'Didn\'t populate RT info for Rush Hour'
-        assert self.task.find_entry(rt_name='Up', rt_year=2009, rt_id=770671912),\
-                'Didn\'t populate RT info for Up'
+                                    rt_year=1999, rt_id=10008), \
+            'Didn\'t populate RT info for Star Wars: Episode I - The Phantom Menace (in 3D)'
+        assert self.task.find_entry(rt_name='Taken', rt_year=2008, rt_id=770680780), \
+            'Didn\'t populate RT info for Taken'
+        assert self.task.find_entry(rt_name='Rush Hour', rt_year=1998, rt_id=10201), \
+            'Didn\'t populate RT info for Rush Hour'
+        assert self.task.find_entry(rt_name='Up', rt_year=2009, rt_id=770671912), \
+            'Didn\'t populate RT info for Up'
