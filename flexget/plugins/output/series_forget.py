abc05c8bb99f5241b509c3e403eb399413a60dac
Elias Badenes <ebadenes@gmail.com>
2013-04-29 19:32:32 +0200

    change if test

--- flexget/plugins/output/series_forget.py
+++ flexget/plugins/output/series_forget.py
@@ -2,10 +2,9 @@ import logging
 from flexget import validator
 from flexget.plugin import register_plugin
 from flexget.plugins.filter import seen
-from flexget.plugins.filter.series import forget_series_episode
 
 try:
-    from flexget.plugins.filter.series import (forget_series_episode)
+    from flexget.plugins.filter.series import forget_series_episode
 except ImportError:
     raise DependencyError(issued_by='series_forget', missing='series', 
           message='series_forget plugin need series plugin to work')
@@ -27,7 +26,7 @@ class OutputSeriesForget(object):
                 seen.forget(entry['title'])
                 log.info('Removed %s from seen database' % entry['title'])
 
-            if 'series' in config and entry['series_name'] != '' and entry['series_id'] != '':
+            if 'series' in config and 'series_name' in entry and 'series_id' in entry:
                 try:
                     forget_series_episode(entry['series_name'], entry['series_id'])
                     log.info('Removed episode `%s` from series `%s`.' % (entry['series_id'], entry['series_name']))
