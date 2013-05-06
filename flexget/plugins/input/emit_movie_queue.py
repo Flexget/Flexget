24edb7e1927aa7f84f25a011d19a948945526808
Chase Sterling <chase.sterling@gmail.com>
2013-05-05 22:35:02 -0400

    Add try_every option to emit_movie_queue

--- flexget/plugins/input/emit_movie_queue.py
+++ flexget/plugins/input/emit_movie_queue.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals, division, absolute_import
 import logging
+from datetime import datetime
+
 from flexget.entry import Entry
 from flexget.plugin import register_plugin, get_plugin_by_name, DependencyError
+from flexget.utils.imdb import make_url as make_imdb_url
+from flexget.utils.tools import parse_timedelta
 
 try:
     from flexget.plugins.filter.movie_queue import queue_get
@@ -11,8 +15,8 @@ except ImportError:
 log = logging.getLogger('emit_movie_queue')
 
 
-class EmitIMDBQueue(object):
-    """Use your imdb queue as an input by emitting the content of it"""
+class EmitMovieQueue(object):
+    """Use your movie queue as an input by emitting the content of it"""
 
     def validator(self):
         from flexget import validator
@@ -21,26 +25,35 @@ class EmitIMDBQueue(object):
         advanced = root.accept('dict')
         advanced.accept('boolean', key='year')
         advanced.accept('boolean', key='quality')
+        advanced.accept('interval', key='try_every')
         return root
 
     def prepare_config(self, config):
         if isinstance(config, bool):
-            config = {'year': True, 'quality': True}
+            config = {}
+        config.setdefault('year', True)
+        config.setdefault('quality', False)
+        config.setdefault('try_every', '0 seconds')
         return config
 
     def on_task_input(self, task, config):
         if not config:
             return
         config = self.prepare_config(config)
+        max_last_emit = datetime.utcnow() - parse_timedelta(config['try_every'])
 
         entries = []
 
-        for queue_item in queue_get():
+        for queue_item in queue_get(session=task.session):
+            if queue_item.last_emit and queue_item.last_emit > max_last_emit:
+                log.debug('Waiting to emit %s', queue_item.title)
+                continue
+            queue_item.last_emit = datetime.utcnow()
             entry = Entry()
             # make sure the entry has IMDB fields filled
             entry['url'] = ''
             if queue_item.imdb_id:
-                entry['imdb_url'] = 'http://www.imdb.com/title/' + queue_item.imdb_id
+                entry['imdb_url'] = make_imdb_url(queue_item.imdb_id)
                 entry['imdb_id'] = queue_item.imdb_id
             if queue_item.tmdb_id:
                 entry['tmdb_id'] = queue_item.tmdb_id
@@ -63,12 +76,13 @@ class EmitIMDBQueue(object):
             if config.get('year') and entry.get('movie_year'):
                 entry['title'] += ' %s' % entry['movie_year']
             # TODO: qualities can now be ranges.. how should we handle this?
-            #if config.get('quality') and queue_item.quality != 'ANY':
-            #    entry['title'] += ' %s' % queue_item.quality
+            if config.get('quality') and queue_item.quality != 'ANY':
+                log.info('quality option of emit_movie_queue is disabled while we figure out how to handle ranges')
+                #entry['title'] += ' %s' % queue_item.quality
             entries.append(entry)
             log.debug('Added title and IMDB id to new entry: %s - %s' %
                      (entry['title'], entry['imdb_id']))
 
         return entries
 
-register_plugin(EmitIMDBQueue, 'emit_movie_queue', api_ver=2)
+register_plugin(EmitMovieQueue, 'emit_movie_queue', api_ver=2)
