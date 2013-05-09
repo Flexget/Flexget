cf0e2aa384e2a9478deed619f6a2ad5a3bc59899
deksan <deksan@gmail.com>
2013-05-09 11:09:56 +0200

    Added an interval parameter : for each entry provided by entry an internal interval is created so the same entry won't be search before next interval.

--- flexget/plugins/input/discover.py
+++ flexget/plugins/input/discover.py
@@ -4,6 +4,9 @@ from flexget.utils.cached_input import cached
 from flexget.utils.search import StringComparator, MovieComparator, AnyComparator, clean_title
 from flexget.plugin import register_plugin, get_plugin_by_name, PluginError, \
     get_plugins_by_group, get_plugins_by_phase, PluginWarning
+import datetime
+from flexget.utils.tools import parse_timedelta
+
 
 log = logging.getLogger('discover')
 
@@ -39,6 +42,7 @@ class Discover(object):
                 no_config.accept(plugin.name)
 
         discover.accept('integer', key='limit')
+        discover.accept('interval', key='interval')
         discover.accept('choice', key='type').accept_choices(['any', 'normal', 'exact', 'movies'])
         return discover
 
@@ -119,13 +123,38 @@ class Discover(object):
         released = get_plugin_by_name("est_released").instance
         return released.filter_helper(task, arg_entries, config)
 
+    def execute_check_lastexecution(sekf, config, task, arg_entries):
+        if ('interval' not in config):
+            config['interval'] = "1 hour"
+        interval = parse_timedelta(config['interval'])
+        entries = []
+        for entry in arg_entries:
+            last_time = task.simple_persistence.get(entry['title'])
+            if not last_time:
+                log.info('%s -> No previous run recorded, running now' % entry['title'])
+            elif task.manager.options.interval_ignore:
+                log.info('Ignoring interval because of --now')
+            else:
+                log.debug('last_time: %r' % last_time)
+                log.debug('interval: %s' % config)
+                next_time = last_time + interval
+                log.debug('next_time: %r' % next_time)
+                if datetime.datetime.now() < next_time:
+                    log.debug('interval not met')
+                    log.verbose('Interval %s not met for %s. Use --now to override.' % (config['interval'], entry['title']))
+                    continue
+            log.debug('interval passed')
+            task.simple_persistence[entry['title']] = datetime.datetime.now()
+            entries.append(entry)
+        return entries
+
     @cached('discover')
     def on_task_input(self, task, config):
         entries = self.execute_inputs(config, task)
         log.verbose('Discovering %i titles ...' % len(entries))
         if len(entries) > 500:
             log.critical('Looks like your inputs in discover configuration produced over 500 entries, please reduce the amount!')
-
+        entries = self.execute_check_lastexecution(config, task, entries)
         entries = self.execute_check_released(config, task, entries)
         return self.execute_searches(config, entries)
 
