dbb695cf2c100ec1bafc5a48641c7dec17dc1038
deksan <deksan@gmail.com>
2013-05-09 10:41:04 +0200

    estimate_released plugin for movies

---
+++ flexget/plugins/metainfo/est_released_movies.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals, division, absolute_import
+import logging
+from flexget.plugin import register_plugin
+from datetime import datetime
+
+log = logging.getLogger('est_movies')
+
+
+class EstimatesRelasedMovies(object):
+
+    def is_released(self, task, entry):
+        if ('tmdb_released' in entry):
+            log.info("Checking %s " % entry['title'])
+            now = datetime.now()
+            try:
+                if now.date() < entry['tmdb_released'].date():
+                    log.debug("title hasn't aired yet : air %s" % (entry['tmdb_released'].date()))
+                    return False
+            except:
+                return None
+            return True
+        return None
+
+register_plugin(EstimatesRelasedMovies, 'est_relased_movies', groups=['estimate_released'])
