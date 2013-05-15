71c8f437b7ae40346078c0f3443ead55bd475ea9
ube <ube@alienautopsy.net>
2013-05-15 18:21:28 +0200

    Fixed indentation.

--- flexget/plugins/input/plex.py
+++ flexget/plugins/input/plex.py
@@ -8,6 +8,7 @@ from flexget.entry import Entry
 
 log = logging.getLogger('plex')
 
+
 class InputPlex(object):
     """
     Uses a plex media server (www.plexapp.com) tv section as an input.
@@ -19,7 +20,8 @@ class InputPlex(object):
         - recentlyAdded
         - recentlyViewed
         - recentlyViewedShows
-      'all' and 'recentlyViewedShows' will only produce a list of show names while the other three will produce filename and download url 
+    'all' and 'recentlyViewedShows' will only produce a list of show names 
+    while the other three will produce filename and download url 
     'username'          Myplex (http://my.plexapp.com) username, used to connect to shared PMS' 
     'password'          Myplex (http://my.plexapp.com) password, used to connect to shared PMS' 
     'server'            IP of PMS to connect to. 
@@ -57,7 +59,7 @@ class InputPlex(object):
     def prepare_config(self, config):
         config.setdefault('server', 'localhost')
         config.setdefault('port', 32400)
-        config.setdefault('selection', 'all');
+        config.setdefault('selection', 'all')
         config.setdefault('username', '')
         config.setdefault('password', '')
         config.setdefault('lowercase_title', False)
@@ -71,9 +73,11 @@ class InputPlex(object):
             header = {'X-Plex-Client-Identifier': 'flexget'} 
             log.debug("Trying to to connect to myplex.")
             try:
-                r = requests.post('https://my.plexapp.com/users/sign_in.xml', auth=(config['username'], config['password']), headers=header)
+                r = requests.post('https://my.plexapp.com/users/sign_in.xml', 
+                    auth=(config['username'], config['password']), headers=header)
             except requests.RequestException as e:
-                raise PluginError('Could not login to my.plexapp.com: %s. Username: %s Password: %s' % (e, config['username'], config['password']))
+                raise PluginError('Could not login to my.plexapp.com: %s. Username: %s Password: %s' % 
+                    (e, config['username'], config['password']))
             log.debug("Managed to connect to myplex.")
             if 'Invalid email' in r.text:
                 raise PluginError('Could not login to my.plexapp.com: invalid username and/or password!')
@@ -84,9 +88,10 @@ class InputPlex(object):
             try:
                 r = requests.get("https://my.plexapp.com/pms/servers?X-Plex-Token=%s" % plextoken)
             except requests.RequestException as e:
-                raise PluginError('Could not get servers from my.plexapp.com using authentication-token: %s.' % plextoken)
+                raise PluginError('Could not get servers from my.plexapp.com using authentication-token: %s.' % 
+                    plextoken)
             dom = parseString(r.text)
-            for node in  dom.getElementsByTagName('Server'):
+            for node in dom.getElementsByTagName('Server'):
                 if node.getAttribute('address') == config['server']:
                     accesstoken = node.getAttribute('accessToken') 
                     log.debug("Got accesstoken: %s" % plextoken)
@@ -94,7 +99,8 @@ class InputPlex(object):
             if accesstoken == "":
                 raise PluginError('Could not retrieve accesstoken for %s.' % config['server'])
         try:
-            r = requests.get("http://%s:%d/library/sections/%d/%s%s" % (config['server'], config['port'], config['section'], config['selection'], accesstoken))
+            r = requests.get("http://%s:%d/library/sections/%d/%s%s" % 
+                (config['server'], config['port'], config['section'], config['selection'], accesstoken))
         except requests.RequestException as e:
             raise PluginError('Error retrieving source: %s' % e)
         dom = parseString(r.text.encode("utf-8"))
@@ -103,10 +109,10 @@ class InputPlex(object):
             for node in dom.getElementsByTagName('Directory'):
                 title=node.getAttribute('title')
                 if config['strip_year']:
-                    title=re.sub(r'^(.*)\(\d+\)$', r'\1', title)
-                title=re.sub(r'[\(\)]', r'', title)
-                title=re.sub(r'\&', r'And', title)
-                title=re.sub(r'[^A-Za-z0-9- ]', r'', title)
+                    title = re.sub(r'^(.*)\(\d+\)$', r'\1', title)
+                title = re.sub(r'[\(\)]', r'', title)
+                title = re.sub(r'\&', r'And', title)
+                title = re.sub(r'[^A-Za-z0-9- ]', r'', title)
                 if config['lowercase_title']:
                     title = title.lower()
                 e = Entry()
@@ -117,10 +123,10 @@ class InputPlex(object):
             for node in dom.getElementsByTagName('Video'):
                 title = node.getAttribute('grandparentTitle')
                 if config['strip_year']:
-                    title=re.sub(r'^(.*)\(\d+\)$', r'\1', title)
-                title=re.sub(r'[\(\)]', r'', title)
-                title=re.sub(r'\&', r'And', title).strip()
-                title=re.sub(r'[^A-Za-z0-9- ]', r'', title).replace(" ", ".")
+                    title = re.sub(r'^(.*)\(\d+\)$', r'\1', title)
+                title = re.sub(r'[\(\)]', r'', title)
+                title = re.sub(r'\&', r'And', title).strip()
+                title = re.sub(r'[^A-Za-z0-9- ]', r'', title).replace(" ", ".")
                 if config['lowercase_title']:
                     title = title.lower()
                 season = int(node.getAttribute('parentIndex'))
