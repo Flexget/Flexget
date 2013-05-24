a6e15a663fdacd8cffa247d5ee0b8a57d7d060df
Chase Sterling <chase.sterling@gmail.com>
2013-05-24 13:50:47 -0400

    Start emit_series branch

--- flexget/plugins/input/emit_series.py
+++ flexget/plugins/input/emit_series.py
@@ -11,6 +11,7 @@ except ImportError as e:
     log.error(e.message)
     raise DependencyError(issued_by='emit_series', missing='series')
 
+# TODO: Make this plugin awesome
 
 class EmitSeries(SeriesDatabase):
     """
