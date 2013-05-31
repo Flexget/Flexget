b8b68a53a488b07e4fb5798e278f3006f573a15a
Chase Sterling <chase.sterling@gmail.com>
2013-05-31 00:20:48 -0400

    Movie estimator falls back to movie_year

--- flexget/plugins/est_released_movies.py
+++ flexget/plugins/est_released_movies.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals, division, absolute_import
+from datetime import datetime
 import logging
+
 from flexget.plugin import register_plugin
 
 log = logging.getLogger('est_movies')
@@ -10,10 +12,9 @@ class EstimatesReleasedMovies(object):
     def estimate(self, entry):
         if 'tmdb_released' in entry:
             log.verbose('Querying release estimation for %s' % entry['title'])
-            try:
-                return entry['tmdb_released']
-            except Exception as e:
-                log.exception(e)
+            return entry['tmdb_released']
+        elif 'movie_year' in entry:
+            return datetime(year=entry['movie_year'], month=1, day=1)
         else:
             log.debug('Unable to check release for %s, tmdb_release field is not defined' %
                       entry['title'])
