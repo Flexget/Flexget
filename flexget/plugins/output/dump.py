05906c2ed7b623f73f04f57d79be6fc7dce58f1c
Chase Sterling <chase.sterling@gmail.com>
2013-06-04 19:54:23 -0400

    --dump now prints title and url above other fields

--- flexget/plugins/output/dump.py
+++ flexget/plugins/output/dump.py
@@ -15,22 +15,22 @@ def dump(entries, debug=False, eval_lazy=False, trace=False):
     :param bool eval_lazy: Evaluate lazy fields.
     :param bool trace: Display trace information.
     """
+    def sort_key(field):
+        # Sort certain fields above the rest
+        if field == 'title':
+            return 0
+        if field == 'url':
+            return 1
+        if field == 'original_url':
+            return 2
+        return field
 
-    invalid = object()
     for entry in entries:
-        #c = entry.copy()
-        #sanitize(c)
-        #print yaml.safe_dump(entry)
-        for field in sorted(entry):
-            value = invalid
-            if entry.is_lazy(field):
-                if eval_lazy:
-                    value = entry[field]
-                else:
-                    value = '<LazyField - value will be determined when it\'s accessed>'
+        for field in sorted(entry, key=sort_key):
+            if entry.is_lazy(field) and not eval_lazy:
+                value = '<LazyField - value will be determined when it\'s accessed>'
             else:
                 value = entry[field]
-            assert value is not invalid
             if isinstance(value, basestring):
                 try:
                     console('%-17s: %s' % (field, value.replace('\r', '').replace('\n', '')))
