0afc219ca8851445eaa27956b5e00d2d6fe70598
Chase Sterling <chase.sterling@gmail.com>
2013-07-12 14:52:06 -0400

    Make sure movie title parser cuts at "director's"

--- flexget/utils/titles/parser.py
+++ flexget/utils/titles/parser.py
@@ -22,7 +22,7 @@ class TitleParser(object):
 
     # lowercase required
     cutoffs = ['limited', 'xvid', 'h264', 'x264', 'h.264', 'x.264', 'screener', 'unrated', '3d', 'extended',
-               'directors', 'multisubs', 'dubbed', 'subbed', 'multi'] + propers + specials + editions
+               'directors', 'director\'s', 'multisubs', 'dubbed', 'subbed', 'multi'] + propers + specials + editions
 
     remove = ['imax']
 
