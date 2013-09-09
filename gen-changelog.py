ba8b984d28c742b834c03491c15a0f6cdd021076
Chase Sterling <chase.sterling@gmail.com>
2013-09-09 15:05:48 -0400

    Add links to upgrade actions in the change log

--- gen-changelog.py
+++ gen-changelog.py
@@ -9,6 +9,9 @@ import subprocess
 import sys
 
 import dateutil.parser
+import requests
+
+from flexget.utils.soup import get_soup
 
 out_path = 'ChangeLog'
 if len(sys.argv) > 1:
@@ -17,6 +20,9 @@ if len(sys.argv) > 1:
         print 'Output dir doesn\'t exist: %s' % sys.argv[1]
         sys.exit(1)
     out_path = sys.argv[1]
+
+ua_response = requests.get('http://flexget.com/wiki/UpgradeActions')
+ua_soup = get_soup(ua_response.text)
 
 # 1.0.3280 was last revision on svn
 git_log_output = subprocess.check_output(['git', 'log', '--pretty=%n---%n.%d%n%ci%n%h%n%s%n%-b%n---%n',
@@ -31,7 +37,12 @@ with codecs.open(out_path, 'w', encoding='utf-8') as out_file:
         commit_hash = next(git_log_iter)
         body = list(iter(git_log_iter.next, '---'))
         if tag:
-            out_file.write('\n=== %s (%s) ===\n\n' % (tag.group(1), date.strftime('%Y.%m.%d')))
+            ver = tag.group(1)
+            ua_link = ''
+            result = ua_soup.find('h3', text=re.compile(re.escape(ver)))
+            if result:
+                ua_link = '^[wiki:UpgradeActions#%s upgrade actions]^ ' % result['id']
+            out_file.write('\n=== %s (%s) %s===\n\n' % (ver, date.strftime('%Y.%m.%d'), ua_link))
         out_file.write(' * (%s) %s\n' % (commit_hash, '[[BR]]\n   '.join(body)))
 
 
