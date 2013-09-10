4fc26cb77c0f76caa815d83e28d67525de18cc18
Chase Sterling <chase.sterling@gmail.com>
2013-09-10 14:21:19 -0400

    Open the range of pack detection a bit. fix #1432

--- flexget/utils/titles/series.py
+++ flexget/utils/titles/series.py
@@ -503,7 +503,7 @@ class SeriesParser(TitleParser):
                 end_episode = None
                 if len(matches) == 3 and matches[2]:
                     end_episode = int(matches[2])
-                    if end_episode <= episode or end_episode > episode + 8:
+                    if end_episode <= episode or end_episode > episode + 12:
                         # end episode cannot be before start episode
                         # Assume large ranges are not episode packs, ticket #1271 TODO: is this the best way?
                         end_episode = None
