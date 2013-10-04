8cd1ca6d80c53f794667b41d7e6a8d400e865998
Chase Sterling <chase.sterling@gmail.com>
2013-10-04 12:06:45 -0400

    Start fleshing out json api for config

--- flexget/ui/api.py
+++ flexget/ui/api.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify, Blueprint, Response
 
 import flexget
+from flexget.manager import manager
 from flexget.options import get_parser
 from flexget.ui.options import RaiseErrorArgumentParser
 from flexget.ui.executor import BufferQueue
@@ -42,3 +43,36 @@ def execute():
 
     return Response(streamer(), mimetype='text/plain'), 200
 
+
+# TODO: return proper schemas in headers, also none of these should allow setting invalid config
+@api.route('/config', methods=['GET', 'PUT'])
+def config_root():
+    return jsonify(manager.config)
+
+
+@api.route('/config/<section>', methods=['GET', 'PUT', 'DELETE'])
+def config_section(section):
+    if request.method == 'GET':
+        return jsonify(manager.config[section])
+    elif request.method == 'PUT':
+        manager.config[section] = request.json
+    elif request.method == 'DELETE':
+        del manager.config[section]
+
+
+@api.route('/config/tasks/<taskname>', methods=['GET', 'PUT', 'DELETE'])
+def config_tasks(taskname):
+    if request.method != 'PUT':
+        if taskname not in manager.config['tasks']:
+            return jsonify(error='Requested task does not exist'), 404
+    status_code = 200
+    if request.method == 'PUT':
+        if 'rename' in request.args:
+            pass  # TODO: Rename the task, return 204 with new location header
+        if taskname not in manager.config['tasks']:
+            status_code = 201
+        manager.config['tasks'][taskname] = request.json
+    elif request.method == 'DELETE':
+        del manager.config['tasks'][taskname]
+        return Response(status=204)
+    return jsonify(manager.config['tasks'][taskname]), status_code
