b8de36746262a7d8055be70b36090aa5be924b24
Chase Sterling <chase.sterling@gmail.com>
2013-10-11 02:12:37 -0400

    small comment fix

--- flexget/manager.py
+++ flexget/manager.py
@@ -136,11 +136,11 @@ class Manager(object):
                     tasks.update(matches)
                 # Set the option as a list of matching task names so plugins can use it easily
                 options.tasks = list(tasks)
+            # TODO: 1.2 This is a hack to make task priorities work still, not sure if it's the best one
             tasks = sorted(tasks, key=lambda t: manager.config['tasks'][t].get('priority', 65535))
 
             port = self.check_ipc_port()
             if port:
-                # TODO: 1.2 This is a hack to make task priorities work still, not sure if it's the best one
                 for task in tasks:
                     remote_execute(port, task, options)
                 self.shutdown()
