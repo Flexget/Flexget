c56fc0429044fb5240c664d25ace26e2068bf6f7
Chase Sterling <chase.sterling@gmail.com>
2013-10-19 17:20:39 -0400

    Upgrade ipc protocol to support multiple commands

--- flexget/ipc.py
+++ flexget/ipc.py
@@ -5,6 +5,7 @@ import socket
 from SocketServer import BaseRequestHandler, ThreadingTCPServer
 import threading
 
+from flexget.config_schema import process_config
 from flexget.scheduler import BufferQueue
 from flexget.utils import json
 from flexget.utils.tools import console
@@ -21,7 +22,7 @@ def remote_execute(port, task, options):
         if isinstance(options, argparse.Namespace):
             options = options.__dict__
             options.pop('__parent__', None)
-        s.sendall(json.dumps({'task': task, 'options': options}) + '\n')
+        s.sendall(json.dumps({'action': 'execute', 'args': {'task': task, 'options': options}}) + '\n')
         for line in s.makefile():
             console(line.decode('utf-8').rstrip())
     except socket.error as e:
@@ -32,23 +33,58 @@ def remote_execute(port, task, options):
         s.close()
 
 
+# Used to validate our incoming requests
+ipc_message_schema = {
+    'type': 'object',
+    'properties': {
+        'action': {'enum': ['execute', 'reload', 'shutdown']},
+        'args': {'type': 'object', 'default': {}}
+    },
+    'required': ['action'],
+    'additionalProperties': False
+}
+
+
 class RequestHandler(BaseRequestHandler):
     def handle(self):
         data = self.request.makefile().readline().decode('utf-8')
         try:
-            args = json.loads(data)
+            ipc_message = json.loads(data)
         except ValueError:
-            log.error('Error decoding ipc message.')
+            log.error('Error decoding json from ipc message.')
             return
+        errors = process_config(ipc_message, ipc_message_schema)
+        if errors:
+            log.error('Invalid IPC message received.')
+            return
+        if ipc_message['action'] == 'execute':
+            if 'task' not in ipc_message['args']:
+                log.error('Task must be specified in execute ipc message')
+                return
+            self.handle_execute(ipc_message['args']['task'], ipc_message['kwargs'].get('options'))
+        elif ipc_message['action'] == 'reload':
+            self.handle_reload()
+        elif ipc_message == 'shutdown':
+            self.handle_shutdown(ipc_message['args'].get('finish_queue', True))
+
+    def handle_execute(self, task, options):
         if self.server.manager.scheduler.run_queue.qsize() > 0:
             self.request.sendall('There is already a task executing. This task will execute next.\n')
-        log.info('Executing task `%s` for client at %s.' % (args['task'], self.client_address))
+        log.info('Executing task `%s` for client at %s.' % (task, self.client_address))
         bufferqueue = BufferQueue()
-        self.server.manager.scheduler.execute(args['task'], options=args['options'], output=bufferqueue)
+        self.server.manager.scheduler.execute(task, options=options, output=bufferqueue)
         # If this is a --cron execution, don't stream back the log
-        if not args['options'].get('cron'):
+        if not options.get('cron'):
             for line in bufferqueue:
                 self.request.sendall(line.encode('utf-8'))
+
+    def handle_reload(self):
+        # TODO: Reload config
+        pass
+
+    def handle_shutdown(self, finish_queue):
+        self.server.manager.scheduler.shutdown(finish_queue=finish_queue)
+        self.request.sendall('Daemon shutdown requested.')
 
 
 class IPCServer(threading.Thread):
