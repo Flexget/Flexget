55a82bde74b9f861e8101ca4ce38159e93b71a44
Toilal <toilal.dev@gmail.com>
2013-11-04 10:00:30 +0100

    Add guessit dependency

--- pavement.py
+++ pavement.py
@@ -23,7 +23,7 @@ install_requires = ['FeedParser>=5.1.3', 'SQLAlchemy >=0.7, <0.7.99', 'PyYAML', 
                     # There is a bug in beautifulsoup 4.2.0 that breaks imdb parsing, see http://flexget.com/ticket/2091
                     'beautifulsoup4>=4.1, !=4.2.0, <4.4', 'html5lib>=0.11', 'PyRSS2Gen', 'pynzb', 'progressbar', 'rpyc',
                     'jinja2', 'requests>=1.0, <2.99', 'python-dateutil!=2.0, !=2.2', 'jsonschema>=2.0', 'python-tvrage',
-                    'tmdb3']
+                    'tmdb3', 'guessit>=0.6.1']
 if sys.version_info < (2, 7):
     # argparse is part of the standard library in python 2.7+
     install_requires.append('argparse')
