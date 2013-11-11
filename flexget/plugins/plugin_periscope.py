e63d182f69886206c8e1335e4a692527c82d42cc
Giorgio Gelardi <ggelardi@gmail.com>
2013-11-11 23:20:25 +0100

    converted in output plugin

--- flexget/plugins/plugin_periscope.py
+++ flexget/plugins/plugin_periscope.py
@@ -2,6 +2,7 @@ import logging
 import os
 import periscope
 import tempfile
+import urllib
 
 from flexget.plugin import register_plugin
 
@@ -9,7 +10,6 @@ log = logging.getLogger('subtitles')
 psc = periscope.Periscope(tempfile.gettempdir())
 
 # should this stuff be in config?
-EXTVID = ['.avi', '.mp4', '.mkv']
 EXTSUB = ['.srt']
 
 # to avoid A LOT of info/warnings/errors coming from periscope:
@@ -18,53 +18,76 @@ logging.getLogger("periscope").setLevel(logging.CRITICAL)
 
 class PluginPeriscope(object):
     """
-    Search and download subtitles for all the video files in a given path.
-    (it uses Periscope by Patrick Dessalle, http://code.google.com/p/periscope/)
-
+    Search and download subtitles using Periscope by Patrick Dessalle 
+    (http://code.google.com/p/periscope/).
+    It process only accepted entries referred to local files, and it can be 
+    instructed to ignore (not overwrite) video already associated with subtitles.
+    
     Example::
     
       periscope:
-        path: d:\media\series
         languages:
           - it
           - en
         overwrite: no
     
-    With overwrite=no the plugin will ignore video with associated subtitles.
+    Example (complete task)::
+
+      subs:
+        find:
+          path: 
+            - d:\media\incoming
+          regexp: '.*\.(avi|mkv|mp4)$'
+          recursive: yes
+        accept_all: yes
+        periscope:
+          languages:
+            - it
+            - en
     """
     
     schema = {
         'type': 'object',
         'properties': {
-            'path': {'type': 'string', 'format': 'path'},
             'languages': {"type": "array", "items": {"type": "string"}},
             'overwrite': {'type': 'boolean', 'default': False}
         },
         'additionalProperties': False
     }
     
-    def check_file(self, filename):
-        if not (os.path.splitext(filename)[1] in EXTVID):
-            return
-        if not self.overwrite:
-            for s in EXTSUB:
-                if os.path.exists(os.path.splitext(filename)[0] + s):
-                    return
-        if psc.downloadSubtitle(filename.encode("utf8"), self.langs):
-            log.info('Subtitles found for %s' % filename)
+    def subbed(self, filename):
+        for ext in EXTSUB:
+            if os.path.exists(os.path.splitext(filename)[0] + ext):
+                return True
+        return False
     
-    def on_task_start(self, task, config):
-        """Download subtitles using Periscope"""
-        self.overwrite = config['overwrite']
-        # just to avoid some UnicodeWarning in periscope:
-        self.langs = []
+    def on_task_output(self, task, config):
+        if not task.accepted:
+            log.debug('nothing accepted, aborting')
+            return
+        langs = []
         for u in config['languages']:
-            self.langs.append(u.encode('utf8'))
-        # scanning path
-        for root, dirs, files in os.walk(config['path']):
-            if '$RECYCLE.BIN' in root:  # Windows weird stuff
+            langs.append(u.encode('utf8'))  # unicode warnings in periscope
+        if not langs:
+            log.debug('missing language preferences, aborting')
+            return
+        for entry in task.accepted:
+            if not entry['url'].startswith('file:'):
+                entry.reject('is not a local file')
                 continue
-            for video in files:
-                self.check_file(os.path.join(root, video))
+            # find and listdir write the full path in the "location" attr,
+            # but AFAIK only "url" and "title" are mandatory in entries, so:
+            fn = urllib.url2pathname(entry['url'][5:])
+            if '$RECYCLE.BIN' in fn:  # happens in connected network shares
+                entry.reject("is in Windows recycle-bin")
+            elif not os.path.exists(fn):
+                entry.reject('file not found')  # periscope works on hashes
+            elif not config['overwrite'] and self.subbed(fn):
+                entry.reject('cannot overwrite existing subs')
+            elif psc.downloadSubtitle(fn.encode("utf8"), langs):
+                log.info('Subtitles found for %s' % fn)
+            else:
+                entry.reject('cannot find subtitles for now.')
+
 
 register_plugin(PluginPeriscope, 'periscope', api_ver=2)
