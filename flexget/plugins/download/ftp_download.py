7fb1201c92b0fb52a9a72d566c080ad68392d986
François-Xavier Payet <fx.payet@tfdn.org>
2013-12-06 15:34:46 +0100

    Correcting recursion Allowing to resume

--- flexget/plugins/download/ftp_download.py
+++ flexget/plugins/download/ftp_download.py
@@ -33,114 +33,126 @@ class OutputFtp(object):
         root = validator.factory('dict')
         root.accept('boolean', key='use-ssl')
         root.accept('path', key='ftp_tmp_path')
+        root.accept('boolean', key='delete_origin')
         return root
 
     def prepare_config(self, config, task):
         config.setdefault('use-ssl', False)
+        config.setdefault('delete_origin', False)
         config.setdefault('ftp_tmp_path', os.path.join(task.manager.config_base, 'temp'))
 
         return config
+    
+    def ftp_connect(self, config, ftp_url, current_path):
+        if config['use-ssl']:
+            ftp = ftplib.FTP_TLS()
+        else:
+            ftp = ftplib.FTP()
+        log.debug("Connecting to " + ftp_url.hostname)
+        ftp.connect(ftp_url.hostname, ftp_url.port)
+        ftp.login(ftp_url.username, ftp_url.password)
+        ftp.sendcmd('TYPE I')
+        ftp.set_pasv(True)
+        ftp.cwd(current_path)
+        
+        return ftp
 
     def on_task_download(self, task, config):
         config = self.prepare_config(config, task)
         for entry in task.accepted:
             ftp_url = urlparse(entry.get('url'))
             title = entry.get('title')
-            
-            if config['use-ssl']:
-                ftp = ftplib.FTP_TLS()
-            else:
-                ftp = ftplib.FTP()
-
+            current_path = os.path.dirname(ftp_url.path)
             try:
-                # ftp.set_debuglevel(2)
-                ftp.connect(ftp_url.hostname, ftp_url.port)
-                ftp.login(ftp_url.username, ftp_url.password)
-                ftp.sendcmd('TYPE I')
-                ftp.set_pasv(True)
+                ftp = self.ftp_connect(config, ftp_url, current_path)
             except ftplib.error_perm:
                 log.error('Connection error')
+                return
 
             if not os.path.isdir(config['ftp_tmp_path']):
                 log.debug('creating base path: %s' % config['ftp_tmp_path'])
                 os.mkdir(config['ftp_tmp_path'])
     
-            tmp_path = os.path.join(config['ftp_tmp_path'], title)
-            log.info('temp path %s' % tmp_path)
-            if not os.path.isdir(tmp_path):
-                log.debug('creating tmp_path %s' % tmp_path)
-                os.mkdir(tmp_path)
-
+            file_name = os.path.basename(ftp_url.path)
+            
             try:
                 # Directory
-                ftp.cwd(ftp_url.path)
-                self.ftp_walk(ftp, tmp_path)
+                ftp.cwd(file_name)
+                self.ftp_walk(ftp, os.path.join(config['ftp_tmp_path'],file_name),config,ftp_url,ftp_url.path)
             except ftplib.error_perm:
                 # File
-                self.ftp_down(ftp, ftp_url.path, tmp_path)
+                self.ftp_down(ftp, file_name, config['ftp_tmp_path'],config,ftp_url,current_path)
+                
+            ftp.close()
             
-    def ftp_walk(self, ftp, tmp_path):
-        log.debug("DIR->" + ftp.pwd())
+    def ftp_walk(self, ftp, tmp_path, config,ftp_url,current_path):
+        log.info("DIR->" + ftp.pwd())
+        log.info("FTP tmp_path : " + tmp_path)
         try:
             dirs = ftp.nlst(ftp.pwd())
         except ftplib.error_perm as ex:
             log.info("Error %s" % ex)
-            return
+            return ftp
 
         if not dirs: 
-            return
+            return ftp
 
-        for item in (path for path in dirs if path not in ('.', '..')):
-            log.debug('Item: ' + item)
-            base_item_name = os.path.basename(item)
+        for file_name in (path for path in dirs if path not in ('.', '..')):
+            log.info('Filename: ' + file_name)
             try:
-                ftp.cwd(item)
-                log.debug('DIRECTORY: %s' % ftp.pwd())
-                new_tmp_dir = os.path.join(tmp_path, base_item_name)
-
-                if not os.path.isdir(new_tmp_dir):
-                    os.mkdir(new_tmp_dir)
-                    log.info('Creating tmp_path %s' % new_tmp_dir)
-        
-                self.ftp_walk(ftp, new_tmp_dir)
+                ftp.cwd(file_name)
+                if not os.path.isdir(tmp_path):
+                    os.mkdir(tmp_path)
+                    log.info("Directory %s created" % tmp_path)
+                tmp_path = os.path.join(tmp_path, os.path.basename(file_name))
+                current_path = os.path.join(current_path,os.path.basename(file_name))
+                ftp = self.ftp_walk(ftp, tmp_path, config, ftp_url, current_path)
                 ftp.cwd('..')
             except ftplib.error_perm:
-                log.info("downloading " + base_item_name + " in " + tmp_path)
-                self.ftp_down(ftp, item, tmp_path)
+                ftp = self.ftp_down(ftp, os.path.basename(file_name), tmp_path, config, ftp_url, current_path)
+        return ftp
 
-    def ftp_down(self, ftp, file_name, tmp_path):
-        file_name = os.path.basename(file_name)
+    def ftp_down(self, ftp, file_name, tmp_path, config, ftp_url, current_path):
+        log.info("Downloading %s into %s" % (file_name, tmp_path))
 
         if not os.path.exists(tmp_path):
             os.makedirs(tmp_path)
-
-        src_filesize = 0
-        try:
-            ftp.voidcmd('TYPE I') # para que el ftp.size funcione correctamente
-            src_filesize = ftp.size(file_name)
-        except ftplib.error_perm, ex:
-            #IS directory! walk
-            self.ftp_walk(ftp, tmp_path)
-
-        try:
-            dst_file = os.path.join(tmp_path, file_name)
-            action = 'Download'
-            if os.path.exists(dst_file):
-                dst_filesize = os.stat(dst_file).st_size
-                if src_filesize == dst_filesize:
-                    log.info('File ' + file_name + ' completed')
-                    action = 'NoDownload'
-
-            if action == 'Download':
-                with open(os.path.join(tmp_path, file_name), 'wb') as f:
-                    def callback(data):
-                        f.write(data)
-                    ftp.retrbinary('RETR %s' % file_name, callback)
-                    f.close()
-                    log.info('RETR: ' + os.path.join(tmp_path, file_name))
-            else:
-                log.info('NoDownload: ' + file_name + ' -> ' + os.path.join(tmp_path, file_name))
-        except Exception, e:
-            log.exception(e) 
+        
+        local_file = open(os.path.join(tmp_path, file_name), 'a+b')    
+        file_size = ftp.size(file_name)
+        
+        max_attempts = 5
+        
+        log.debug("Starting download of %s into %s" % (file_name,tmp_path))
+        
+        while file_size != local_file.tell():
+            try:
+                if local_file.tell() != 0:
+                    ftp.retrbinary('RETR %s' % file_name, local_file.write, local_file.tell())
+                else:
+                    ftp.retrbinary('RETR %s' % file_name, local_file.write)
+            except Exception as error:
+                if max_attempts != 0:
+                    ftp = self.ftp_connect(config, ftp_url, current_path)
+                    log.info("Retrying download after error %s" % error);
+                else:
+                    log.error("Too many errors downloading %s. Aborting." % file_name)
+                    break
+        
+        if local_file.tell() != file_size:
+            local_file.close()
+            # There was an error, we delete the temp file
+            try:
+                os.remove(os.path.join(tmp_path, file_name))
+            except OSError as e:
+                local_file.close()
+                pass
+        else :
+            local_file.close()
+            if config['delete_origin']:
+                log.info("Remotely deleting %s" % file_name)
+                ftp.delete(file_name)
+                
+        return ftp
 
 register_plugin(OutputFtp, 'ftp_download', api_ver=2)
