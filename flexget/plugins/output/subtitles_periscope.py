5025f18965f451dfe3afda38002df23945a2826b
Chase Sterling <chase.sterling@gmail.com>
2013-12-31 12:30:47 -0500

    Upgrade periscope plugin

--- flexget/plugins/output/subtitles_periscope.py
+++ flexget/plugins/output/subtitles_periscope.py
@@ -2,21 +2,22 @@ import logging
 import os
 import tempfile
 
-from flexget.plugin import register_plugin, DependencyError
+from flexget import plugin
+from flexget.event import event
 
 log = logging.getLogger('subtitles')
 
 
 class PluginPeriscope(object):
     """
-    Search and download subtitles using Periscope by Patrick Dessalle 
+    Search and download subtitles using Periscope by Patrick Dessalle
     (http://code.google.com/p/periscope/).
-    
+
     Example (complete task)::
 
       subs:
         find:
-          path: 
+          path:
             - d:\media\incoming
           regexp: '.*\.(avi|mkv|mp4)$'
           recursive: yes
@@ -26,7 +27,7 @@ class PluginPeriscope(object):
             - it
             - en
     """
-    
+
     schema = {
         'type': 'object',
         'properties': {
@@ -43,15 +44,15 @@ class PluginPeriscope(object):
             import periscope
         except ImportError as e:
             log.debug('Error importing Periscope: %s' % e)
-            raise DependencyError('periscope', 'periscope', 
-                                  'Periscope module required. ImportError: %s' % e)
-    
-    def subbed(self, filename):
-        for ext in self.exts:
+            raise plugin.DependencyError('periscope', 'periscope',
+                                         'Periscope module required. ImportError: %s' % e)
+
+    def subbed(self, filename, exts):
+        for ext in exts:
             if os.path.exists(os.path.splitext(filename)[0] + ext):
                 return True
         return False
-    
+
     def on_task_output(self, task, config):
         """
         Configuration::
@@ -65,7 +66,7 @@ class PluginPeriscope(object):
         psc = periscope.Periscope(tempfile.gettempdir())
         # to avoid A LOT of info/warnings/errors coming from periscope:
         logging.getLogger("periscope").setLevel(logging.CRITICAL)
-        
+
         if not task.accepted:
             log.debug('nothing accepted, aborting')
             return
@@ -74,7 +75,7 @@ class PluginPeriscope(object):
         if 'alternatives' in config:
             alts = [s.encode('utf8') for s in config['alternatives']]
         if not config['overwrite']:
-            self.exts = ['.'+s for s in config['subexts']]
+            exts = ['.'+s for s in config['subexts']]
         for entry in task.accepted:
             if not 'location' in entry:
                 entry.reject('is not a local file')
@@ -83,7 +84,7 @@ class PluginPeriscope(object):
                 entry.reject("is in Windows recycle-bin")
             elif not os.path.exists(entry['location']):
                 entry.reject('file not found')  # periscope works on hashes
-            elif not config['overwrite'] and self.subbed(entry['location']):
+            elif not config['overwrite'] and self.subbed(entry['location'], exts):
                 entry.reject('cannot overwrite existing subs')
             elif psc.downloadSubtitle(entry['location'].encode("utf8"), langs):
                 log.info('Subtitles found for %s' % entry['location'])
@@ -93,4 +94,6 @@ class PluginPeriscope(object):
                 entry.reject('cannot find any subtitles for now.')
 
 
-register_plugin(PluginPeriscope, 'periscope', api_ver=2)
+@event('plugin.register')
+def register_plugin():
+    plugin.register(PluginPeriscope, 'periscope', api_ver=2)
