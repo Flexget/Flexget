294977d6b5711b12e1b9450fd8de0e80a95e7597
Giorgio Gelardi <ggelardi@gmail.com>
2014-01-30 10:45:44 +0100

    Local subtitles check
    A metainfo plugin to check local files for subtitles.

---
+++ flexget/plugins/metainfo/subtitles_check.py
@@ -0,0 +1,66 @@
+from __future__ import unicode_literals, division, absolute_import
+import logging
+import os
+import tempfile
+
+from flexget import plugin
+from flexget.event import event
+
+log = logging.getLogger('check_subtitles')
+
+
+class MetainfoSubs(object):
+    """
+    Set 'subtitles' field for entries, if they are local video files with subs.
+    The field is a list of language codes (3-letter ISO-639-3) for each subtitles 
+    file found on disk and/or subs track found inside video (for MKVs).
+    Special "und" code is for unidentified language (i.e. files without language 
+    code before extension).
+    """
+
+    schema = {'type': 'boolean'}
+    
+    def lazy_subs_check(self, entry, field):
+        if not ('location' in entry) or ('$RECYCLE.BIN' in entry['location']) or \
+            not os.path.exists(entry['location']) or entry.get('subtitles', eval_lazy=False):
+            return
+        import subliminal
+        try:
+            video = subliminal.scan_video(entry['location'])
+            lst = [l.alpha3 for l in video.subtitle_languages]
+            if lst:
+                entry['subtitles'] = lst
+                log.trace('Found subtitles %s for %s' % ('/'.join(lst), entry['title']))
+        except Exception as e:
+            log.debug('Error checking local subtitles for %s: %s' % (entry['title'], e.message))
+            entry.unregister_lazy_fields(['subtitles'], self.lazy_subs_check)
+        return entry[field]
+    
+    def on_task_start(self, task, config):
+        try:
+            import subliminal
+        except ImportError as e:
+            log.debug('Error importing Subliminal: %s' % e)
+            raise plugin.DependencyError('subliminal', 'subliminal', 
+                'Subliminal module required. ImportError: %s' % e)
+        from dogpile.cache.exception import RegionAlreadyConfigured
+        try:
+            subliminal.cache_region.configure('dogpile.cache.dbm', 
+                arguments={'filename': os.path.join(tempfile.gettempdir(), 'cachefile.dbm'), 
+                           'lock_factory': subliminal.MutexLock})
+        except RegionAlreadyConfigured:
+            pass
+        logging.getLogger("subliminal").setLevel(logging.CRITICAL)
+        logging.getLogger("enzyme").setLevel(logging.WARNING)
+
+    def on_task_metainfo(self, task, config):
+        # check if explicitly disabled (value set to false)
+        if config is False:
+            return
+        for entry in task.entries:
+            entry.register_lazy_fields(['subtitles'], self.lazy_subs_check)
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(MetainfoSubs, 'check_subtitles', api_ver=2, builtin=True)
