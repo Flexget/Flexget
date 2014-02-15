004fc6cdc09434231ba51f9b2d111575da1798ed
Giorgio Gelardi <ggelardi@gmail.com>
2014-02-15 15:07:33 +0100

    Metainfo version
    (chopped out everything about the db) set the trakt_watched field on
    entries in metainfo phase.

--- flexget/plugins/services/trakt_watched.py
+++ flexget/plugins/services/trakt_watched.py
@@ -2,103 +2,40 @@ from __future__ import unicode_literals, division, absolute_import
 import hashlib
 import logging
 
-from datetime import datetime
 from requests import RequestException
-from sqlalchemy import Column, Integer, DateTime
 
-from flexget import db_schema, plugin
+from flexget import plugin
 from flexget.event import event
 from flexget.utils import json
-from flexget.plugins.filter.series import Series, set_series_begin
 
-Base = db_schema.versioned_base('trakt_watched', 0)
-
-
-class TraktWatched(Base):
-    __tablename__ = 'trakt_watched'
-
-    tvdb_id = Column(Integer, primary_key=True, autoincrement=False)
-    season = Column(Integer)
-    episode = Column(Integer)
-    last_update = Column(DateTime)
+log = logging.getLogger('trakt_watched')
 
 
-class TraktSeenLookup(object):
+class TraktWatched(object):
     """
-    Evaluate data collected by trakt_seen_update to set a trakt.tv watched flag 
-    (tsl_ep_watched) for episodes found in entries. Uses tvdb_id, series_season 
-    and series_episode.
+    Query trakt.tv for watched episodes to set the trakt_watched flag on entries.
+    Uses tvdb_id, series_season and series_episode fields (metainfo_series plus 
+    thetvdb_lookup or trakt_lookup plugins can provide them).
     
-    Example:
+    Example task:
     
-      trakt_seen_lookup: yes
-    
-    
-    Set the following fields in entries having a tvdb_id:
-    
-    - tsl_last_season:   last watched episode season number (int).
-    - tsl_last_episode:  last watched episode episode number (int).
-    - tsl_last_ep_id:    last watched episode id (string, S00E00 format).
-    - tsl_last_update:   last update time (datetime).
-    
-    Set this field too in entries having series_season and series_episode:
-    
-    - tsl_ep_watched:    watched status (boolean).
-    
-    
-    Note: entries about series not found in trakt_seen_update plugin collected 
-    data will not have any of the above fields set.
-    
-    """
-
-    schema = {'type': 'boolean'}
-    
-    log = logging.getLogger('trakt_seen_lookup')
-    
-    # Run after thetvdb_lookup
-    @plugin.priority(100)
-    def on_task_metainfo(self, task, config):
-        # check if explicitly disabled (value set to false)
-        if config is False:
-            return
-        for entry in task.entries:
-            if entry.get('tvdb_id'):
-                series = task.session.query(TraktWatched).filter(TraktWatched.tvdb_id == entry['tvdb_id']).first()
-                if series:
-                    entry['tsl_last_season'] = series.season
-                    entry['tsl_last_episode'] = series.episode
-                    entry['tsl_last_ep_id'] = 'S%02dE%02d' % (series.season, series.episode)
-                    entry['tsl_last_update'] = series.last_update
-                    if entry.get('series_season') and entry.get('series_episode'):
-                        entry['tsl_ep_watched'] = entry['series_season'] < series.season or \
-                            (entry['series_season'] == series.season and entry['series_episode'] <= series.episode)
-
-
-class TraktSeenUpdate(object):
-    """
-    Collect trakt.tv last watched episodes for series found in entries (needs a 
-    valid tvdb_id per series).
-    
-    Example task::
-    
-      my_watched_stuff:
-        disable_builtins:
-          - seen
-          - backlog
-          - retry_failed
-        trakt_list:
-          username: myusername
-          password: mypassword
-          api_key: myapikey
-          strip_dates: yes
-          series: watchlist
-        accept_all: yes
-        trakt_seen_update:
-          username: myusername
-          password: mypassword
-          api_key: myapikey
-          update_series_begin: yes
-    
+      Purge watched episodes:
+        find:
+          path:
+            - D:\Media\Incoming\series
+          regexp: '.*\.(avi|mkv|mp4)$'
+          recursive: yes
+        metainfo_series: yes
+        thetvdb_lookup: yes
+        trakt_watched:
+          username: xxx
+          password: xxx
+          api_key: xxx
+        if:
+          - trakt_watched: accept
+        move:
+          to: "D:\\Media\\Purge\\{{ tvdb_series_name|default(series_name) }}"
+          clean_source: 10
     """
 
     schema = {
@@ -106,112 +43,50 @@ class TraktSeenUpdate(object):
         'properties': {
             'username': {'type': 'string'},
             'password': {'type': 'string'},
-            'api_key': {'type': 'string'},
-            'update_series_begin': {'type': 'boolean', 'default': False}
+            'api_key': {'type': 'string'}
         },
         'required': ['username', 'api_key'],
         'additionalProperties': False
     }
     
-    log = logging.getLogger('trakt_seen_update')
-    
-    def on_task_output(self, task, config):
-        """
-        Configuration::
-            trakt_seen_update:
-                username: Your trakt.tv username.
-                api_key: Your trakt.tv API key.
-        """
-        if not config or not task.accepted:
+    # Run after metainfo_series and thetvdb_lookup
+    @plugin.priority(100)
+    def on_task_metainfo(self, task, config):
+        if not task.entries:
             return
-        shows = []
-        for entry in task.accepted:
-            if not 'tvdb_id' in entry:
-                self.log.debug('"%s" has no tvdb_id, ignored' % entry['title'])
-                continue
-            if not entry['tvdb_id'] in shows:
-                shows.append(str(entry['tvdb_id']))
-        if not shows:
-            self.log.verbose("No series info found in entries, we're done.")
-            return
+        url = 'http://api.trakt.tv/user/library/shows/watched.json/%s/%s' % \
+            (config['api_key'], config['username'])
         auth = None
         if 'password' in config:
             auth = {'username': config['username'],
                     'password': hashlib.sha1(config['password']).hexdigest()}
-        url = 'http://api.trakt.tv/user/progress/watched.json/%s/%s/%s' % \
-            (config['api_key'], config['username'], ','.join(shows))
-        self.log.debug('Opening url %s ...' % url)
         try:
             data = task.requests.get(url, data=json.dumps(auth)).json()
         except RequestException as e:
             raise plugin.PluginError('Unable to get data from trakt.tv: %s' % e)
-        if not data:
-            self.log.info('No data returned: the user does not have seen records on trakt.tv for these shows.')
-            return
-        if not isinstance(data, list):
-            self.log.error('Unexpected response from trakt.tv, perhaps API is changed?')
-            return
         def check_auth():
             if task.requests.post('http://api.trakt.tv/account/test/' + config['api_key'],
                 data=json.dumps(auth), raise_status=False).status_code != 200:
                 raise plugin.PluginError('Authentication to trakt failed.')
-        if 'error' in data:
-            check_auth()
-            raise plugin.PluginError('Error getting trakt list: %s' % data['error'])
         if not data:
             check_auth()
             self.log.warning('No data returned from trakt.')
             return
-        # response is a list with series info as elements
-        for rshow in data:
-            title = rshow['show']['title']
-            tvdb_id = rshow['show']['tvdb_id']
-            self.log.debug('Checking watched status for "%s" (TVDB: %s)...' % (title, tvdb_id))
-            try:
-                lseas = rshow['seasons'][-1]['season']
-                lepis = rshow['seasons'][-1]['aired']
-                if rshow['progress']['percentage'] < 100:
-                    for seas in reversed(rshow['seasons']):
-                        lepis = seas['aired']
-                        for epis in reversed(range(seas['aired'])):
-                            if seas['episodes'][str(epis+1)]:
-                                break
-                            lepis -= 1
-                        else:
-                            lseas -= 1
-                            continue
-                        break
-            except Exception as e:
-                self.log.error('Unexpected item structure, perhaps API is changed?')
-                return
-            wshow = task.session.query(TraktWatched).filter(TraktWatched.tvdb_id == tvdb_id).first()
-            if not wshow:
-                self.log.debug('No data stored for "%s", creating record...' % title)
-                wshow = TraktWatched()
-                wshow.tvdb_id = tvdb_id
-                task.session.add(wshow)
-            elif (wshow.season == lseas and wshow.episode == lepis):
-                self.log.verbose('Last watched episode for "%s" has not changed.' % title)
-                continue
-            wshow.season = lseas
-            wshow.episode = lepis
-            wshow.last_update = datetime.now()
-            lepid = 'S%02dE%02d' % (lseas, lepis)
-            self.log.info('Last watched episode set to %s for "%s"' % (lepid, title))
-            if config['update_series_begin']:
-                fshow = task.session.query(Series).filter(Series.name == title).first()
-                if not fshow:
-                    fshow = Series()
-                    fshow.name = title
-                    task.session.add(fshow)
-                try:
-                    set_series_begin(fshow, lepid)
-                except ValueError as e:
-                    self.log.error('An error occurred trying to set the first series episode: ' % e)
-                self.log.info('First series episode set too by update_series_begin request.')
+        if 'error' in data:
+            check_auth()
+            raise plugin.PluginError('Error getting trakt list: %s' % data['error'])
+        series = {}
+        for item in data:
+            series[item['tvdb_id']] = item
+        for entry in task.entries:
+            if entry.get('tvdb_id') and entry.get('series_season') and entry.get('series_episode'):
+                entry['trakt_watched'] = False
+                if series.get(entry['tvdb_id']):
+                    for s in series[entry['tvdb_id']]['seasons']:
+                        if s['season'] == entry['series_season']:
+                            entry['trakt_watched'] = entry['series_episode'] in s['episodes']
 
 
 @event('plugin.register')
 def register_plugin():
-    plugin.register(TraktSeenLookup, 'trakt_seen_lookup', api_ver=2)
-    plugin.register(TraktSeenUpdate, 'trakt_seen_update', api_ver=2)
+    plugin.register(TraktWatched, 'trakt_watched', api_ver=2)
