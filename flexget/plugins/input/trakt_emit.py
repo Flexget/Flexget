b1149e08e9bc7cc35a70a3ad87e6f69ef26ba029
Chase Sterling <chase.sterling@gmail.com>
2014-02-24 09:07:24 -0500

    Fix trakt_emit schema defaults failing Copy trakt list slug making ability from trakt_list

--- flexget/plugins/input/trakt_emit.py
+++ flexget/plugins/input/trakt_emit.py
@@ -12,9 +12,21 @@ from flexget.utils import json
 log = logging.getLogger('trakt_emit')
 
 
+def make_list_slug(name):
+    """Return the slug for use in url for given list name."""
+    slug = name.lower()
+    # These characters are just stripped in the url
+    for char in '!@#$%^*()[]{}/=?+\\|-_':
+        slug = slug.replace(char, '')
+    # These characters get replaced
+    slug = slug.replace('&', 'and')
+    slug = slug.replace(' ', '-')
+    return slug
+
+
 class TraktEmit(object):
     """
-    Creates an entry for the latest or the next item in your watched or collected 
+    Creates an entry for the latest or the next item in your watched or collected
     episodes in your trakt account.
 
     Syntax:
@@ -27,23 +39,23 @@ class TraktEmit(object):
       list: <value>
 
     Options username, password and api_key are required.
-    
+
     """
-    
+
     schema = {
         'type': 'object',
         'properties': {
             'username': {'type': 'string'},
             'password': {'type': 'string'},
             'api_key': {'type': 'string'},
-            'position': {'enum': ['last', 'next'], 'default': ['next']},
-            'context': {'enum': ['watched', 'collected'], 'default': ['watched']},
+            'position': {'type': 'string', 'enum': ['last', 'next'], 'default': 'next'},
+            'context': {'type': 'string', 'enum': ['watched', 'collected'], 'default': 'watched'},
             'list': {'type': 'string'}
         },
         'required': ['username', 'password', 'api_key'],
         'additionalProperties': False
     }
-    
+
     def get_trakt_data(self, task, config, url, null_data=None):
         log.debug('Opening %s' % url)
         auth = {'username': config['username'],
@@ -52,10 +64,12 @@ class TraktEmit(object):
             data = task.requests.get(url, data=json.dumps(auth)).json()
         except RequestException as e:
             raise plugin.PluginError('Unable to get data from trakt.tv: %s' % e)
+
         def check_auth():
-            if task.requests.post('http://api.trakt.tv/account/test/' + config['api_key'],
-                data=json.dumps(auth), raise_status=False).status_code != 200:
+            auth_url = 'http://api.trakt.tv/account/test/' + config['api_key']
+            if task.requests.post(auth_url, data=json.dumps(auth), raise_status=False).status_code != 200:
                 raise plugin.PluginError('Authentication to trakt failed.')
+
         if not data:
             check_auth()
             log.warning('No data returned from trakt.')
@@ -64,12 +78,12 @@ class TraktEmit(object):
             check_auth()
             raise plugin.PluginError('Error getting trakt list: %s' % data['error'])
         return data
-    
+
     def on_task_input(self, task, config):
         series = {}
         if config.get('list'):
             url = 'http://api.trakt.tv/user/list.json/%s/%s/%s' % \
-                (config['api_key'], config['username'], config['list'])
+                (config['api_key'], config['username'], make_list_slug(config['list']))
             data = self.get_trakt_data(task, config, url, null_data={})
             if not data.get('items') or len(data['items']) <= 0:
                 log.warning('The list "%s" is empty.' % config['list'])
@@ -122,7 +136,7 @@ class TraktEmit(object):
                 eps = item['next_episode']['season']
                 epn = item['next_episode']['number']
             if eps and epn:
-                entry = add_entry(item['show']['tvdb_id'], item['show']['title'], 
+                entry = add_entry(item['show']['tvdb_id'], item['show']['title'],
                                   eps, epn, item['show']['imdb_id'])
                 if entry['tvdb_id'] in series:
                     del series[entry['tvdb_id']]
