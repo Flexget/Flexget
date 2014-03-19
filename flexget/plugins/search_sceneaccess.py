538c605bb68cac393c8320eb2b777ddf2a4e25fa
Gargauth <tolanri@gmail.com>
2014-03-20 00:48:01 +0100

    Fix login issue for sceneaccess
    Caused when user-agent header is absent

--- flexget/plugins/search_sceneaccess.py
+++ flexget/plugins/search_sceneaccess.py
@@ -218,10 +218,11 @@ class SceneAccessSearch(object):
 
         # Login...
         params = {'username': config['username'],
-                       'password': config['password'],
-                       'submit': 'come on in'}
+                  'password': config['password'],
+                  'submit': 'come on in'}
 
         session = Session()
+        session.headers = {'User agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0'}
         log.debug('Logging in to %s...' % URL)
         session.post(URL + 'login', data=params)
 
