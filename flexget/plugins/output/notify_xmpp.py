9e0f6993f62ce76718f3a092f3b7a1536b9ea32c
Giorgio Gelardi <ggelardi@users.noreply.github.com>
2014-05-02 13:19:27 +0200

    Added dnspython as plugin requirement
    Without a dns module most systems will loop for minutes (or forever), trying to connect.

--- flexget/plugins/output/notify_xmpp.py
+++ flexget/plugins/output/notify_xmpp.py
@@ -52,8 +52,12 @@ class OutputNotifyXmpp(object):
             import sleekxmpp
         except ImportError as e:
             log.debug('Error importing SleekXMPP: %s' % e)
-            raise plugin.DependencyError('notify_xmpp', 'sleekxmpp', 
-                'SleekXMPP module required. ImportError: %s' % e)
+            raise plugin.DependencyError('notify_xmpp', 'sleekxmpp', 'SleekXMPP module required. ImportError: %s' % e)
+        try:
+            import dnspython
+        except ImportError as e:
+            log.debug('Error importing dnspython: %s' % e)
+            raise plugin.DependencyError('notify_xmpp', 'dnspython', 'dnspython module required. ImportError: %s' % e)
     
     def on_task_output(self, task, config):
         """
