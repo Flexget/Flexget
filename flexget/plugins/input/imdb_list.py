821176f0cbd0a89fde97cb96f2f6ef45d4a2fe60
Đĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2014-08-11 17:16:29 +0530

    auto fallback to html parsing if rss failed for imdb_list import
    when the imdb watchlist page returns html (as at 11/8/2014) instead of a
    valid rss feed, imdb_list will now automatically parse that html for
    entries. will go back to rss when the feed corrects itself without any
    need for flexget config changes.

--- flexget/plugins/input/imdb_list.py
+++ flexget/plugins/input/imdb_list.py
@@ -36,12 +36,10 @@ class ImdbList(object):
     @cached('imdb_list', persist='2 hours')
     def on_task_input(self, task, config):
         log.verbose('Retrieving list %s ...' % config['list'])
-
-        # Get the imdb list in HTML format
         if config['list'] in ['watchlist', 'ratings', 'checkins']:
-            url = 'http://imdb.com/user/%s/%s' % (config['user_id'], config['list'])
+            url = 'http://rss.imdb.com/user/%s/%s' % (config['user_id'], config['list'])
         else:
-            url = 'http://imdb.com/list/%s' % config['list']
+            url = 'http://rss.imdb.com/list/%s' % config['list']
 
         log.debug('Requesting %s' % url)
         page = task.requests.get(url)
@@ -50,21 +48,42 @@ class ImdbList(object):
         if page.status_code != 200:
             raise plugin.PluginError('Unable to get imdb list. Either list is private or does not exist.')
 
-        soup = get_soup(page.text)
-        divs = soup.find_all('div', attrs={'class':'title'})
-        soup = get_soup(str(divs))
-        links = soup.find_all('a')
+        try:
+            rss = feedparser.parse(page.text)
+        except LookupError as e:
+            raise plugin.PluginError('Failed to parse RSS feed for list `%s` correctly: %s' % (config['list'], e))
 
-        # Create an Entry for each movie in the list
         entries = []
-        for a in links:
-                link = 'http://imdb.com' + a.get('href').replace('/?ref_=wl_li_tt','')
-                entry = Entry()
-                entry['title'] = a.string
-                entry['url'] = link
-                entry['imdb_id'] = extract_id(link)
-                entry['imdb_name'] = a.string
-                entries.append(entry)
+        if not rss.entries:
+            log.debug('No RSS items found. Using HTML parsing.')
+            soup = get_soup(page.text)
+            divs = soup.find_all('div', attrs={'class':'title'})
+            soup = get_soup(str(divs))
+            links = soup.find_all('a')
+            for a in links:
+                    link = 'http://imdb.com' + a.get('href').replace('/?ref_=wl_li_tt','')
+                    entry = Entry()
+                    entry['title'] = a.string
+                    entry['url'] = link
+                    entry['imdb_id'] = extract_id(link)
+                    entry['imdb_name'] = a.string
+                    entries.append(entry)
+        else:
+            log.debug('Creating entries from RSS')
+            title_re = re.compile(r'(.*) \((\d{4})?.*?\)$')
+            for entry in rss.entries:
+                try:
+                    # IMDb puts some extra stuff in the titles, e.g. "Battlestar Galactica (2004 TV Series)"
+                    # Strip out everything but the date
+                    match = title_re.match(entry.title)
+                    title = match.group(1)
+                    if match.group(2):
+                        title += ' (%s)' % match.group(2)
+                    entries.append(
+                        Entry(title=title, url=entry.link, imdb_id=extract_id(entry.link), imdb_name=match.group(1)))
+                except IndexError:
+                    log.critical('IndexError! Unable to handle RSS entry: %s' % entry)
+
         return entries
 
 @event('plugin.register')
