8d5c0380937715d1e75865c117c683519f377971
Đĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2014-08-12 10:44:21 +0530

    imdb_list finalized

--- flexget/plugins/input/imdb_list.py
+++ flexget/plugins/input/imdb_list.py
@@ -13,6 +13,7 @@ from flexget.utils.soup import get_soup
 log = logging.getLogger('imdb_list')
 USER_ID_RE = r'^ur\d{7,8}$'
 
+
 class ImdbList(object):
     """"Creates an entry for each movie in your imdb list."""
 
@@ -33,20 +34,21 @@ class ImdbList(object):
     @cached('imdb_list', persist='2 hours')
     def on_task_input(self, task, config):
         # Create movie entries by parsing imdb list page(s) html using beautifulsoup
-        log.verbose('Retrieving list %s ...' % config['list'])
+        log.verbose('Retrieving list: %s...' % config['list'])
 
+        params = {'view': 'compact'}
         if config['list'] in ['watchlist', 'ratings', 'checkins']:
-            url = 'http://www.imdb.com/user/%s/%s' % (config['user_id'], config['list']) + '?view=compact'
+            url = 'http://www.imdb.com/user/%s/%s' % (config['user_id'], config['list'])
         else:
-            url = 'http://www.imdb.com/list/%s' % config['list'] + '?view=compact'
+            url = 'http://www.imdb.com/list/%s' % config['list']
 
-        log.debug('Requesting %s' % url)
-        page = task.requests.get(url)
+        log.debug('Requesting: %s' % url)
+        page = task.requests.get(url, params=params)
         if page.status_code != 200:
             raise plugin.PluginError('Unable to get imdb list. Either list is private or does not exist.')
 
-        soup = get_soup(page.text,'html.parser')
-        div = soup.find('div',class_='desc')
+        soup = get_soup(page.text, 'html.parser')
+        div = soup.find('div', class_='desc')
         if div:
             total_movie_count = int(div.get('data-size'))
         else:
@@ -56,20 +58,16 @@ class ImdbList(object):
             log.verbose('No movies were found in imdb list.')
             return
 
-        number_of_pages = int(math.ceil(total_movie_count/250))
         entries = []
-
-        for i in xrange(number_of_pages):
-            current_page = i + 1
-            if current_page > 1:
-                log.debug('Requesting page: %s of imdb list' % current_page)
-                start = current_page * 250 - 250 + 1
-                page = task.requests.get(url + '&start=' + str(start))
+        for start_movie in xrange(1, total_movie_count + 1, 250):
+            if start_movie > 250:
+                params['start'] = start_movie
+                page = task.requests.get(url, params=params)
                 if page.status_code != 200:
-                    raise plugin.PluginError('Unable to get page: %s of imdb list' % current_page)
-                soup = get_soup(page.text,'html.parser')
+                    raise plugin.PluginError('Unable to get imdb list.')
+                soup = get_soup(page.text, 'html.parser')
 
-            tds = soup.find_all('td',class_='title')
+            tds = soup.find_all('td', class_='title')
             for td in tds:
                 a = td.find('a')
                 link = ('http://www.imdb.com' + a.get('href')).rstrip('/')
