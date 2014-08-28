8b9cdefc6a2546054c7c8a3c2a7e455e854928da
Chase Sterling <chase.sterling@gmail.com>
2014-08-28 19:57:41 -0400

    Fix unicode handling in exists_series. Fix #1462

--- flexget/plugins/filter/exists_series.py
+++ flexget/plugins/filter/exists_series.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 import copy
-import os
 import logging
+
+from path import path
 
 from flexget import plugin
 from flexget.event import event
@@ -58,66 +59,61 @@ class FilterExistsSeries(object):
             if 'series_parser' in entry:
                 if entry['series_parser'].valid:
                     accepted_series.setdefault(entry['series_parser'].name, []).append(entry)
-                    for path in config['path']:
+                    for folder in config['path']:
                         try:
-                            paths.add(entry.render(path))
+                            paths.add(entry.render(folder))
                         except RenderError as e:
-                            log.error('Error rendering path `%s`: %s', path, e)
+                            log.error('Error rendering path `%s`: %s', folder, e)
                 else:
                     log.debug('entry %s series_parser invalid', entry['title'])
         if not accepted_series:
             log.warning('No accepted entries have series information. exists_series cannot filter them')
             return
 
-        for path in paths:
-            log.verbose('Scanning %s', path)
-            # crashes on some paths with unicode
-            path = str(os.path.expanduser(path))
-            if not os.path.exists(path):
-                raise plugin.PluginWarning('Path %s does not exist' % path, log)
-            # scan through
-            for root, dirs, files in os.walk(path, followlinks=True):
-                # convert filelists into utf-8 to avoid unicode problems
-                dirs = [x.decode('utf-8', 'ignore') for x in dirs]
-                files = [x.decode('utf-8', 'ignore') for x in files]
-                # For speed, only test accepted entries since our priority should be after everything is accepted.
-                for series in accepted_series:
-                    # make new parser from parser in entry
-                    disk_parser = copy.copy(accepted_series[series][0]['series_parser'])
-                    for name in files + dirs:
-                        # run parser on filename data
-                        disk_parser.data = name
-                        try:
-                            disk_parser.parse(data=name)
-                        except ParseWarning as pw:
-                            log_once(pw.value, logger=log)
-                        if disk_parser.valid:
-                            log.debug('name %s is same series as %s', name, series)
-                            log.debug('disk_parser.identifier = %s', disk_parser.identifier)
-                            log.debug('disk_parser.quality = %s', disk_parser.quality)
-                            log.debug('disk_parser.proper_count = %s', disk_parser.proper_count)
+        # scan through
+        # For speed, only test accepted entries since our priority should be after everything is accepted.
+        for series in accepted_series:
+            # make new parser from parser in entry
+            disk_parser = copy.copy(accepted_series[series][0]['series_parser'])
+            for folder in paths:
+                folder = path(folder).expanduser()
+                if not folder.isdir():
+                    log.warn('Directory %s does not exist' % folder, log)
+                    continue
 
-                            for entry in accepted_series[series]:
-                                log.debug('series_parser.identifier = %s', entry['series_parser'].identifier)
-                                if disk_parser.identifier != entry['series_parser'].identifier:
-                                    log.trace('wrong identifier')
+                for filename in folder.walk(errors='warn'):
+                    # run parser on filename data
+                    try:
+                        disk_parser.parse(data=filename.name)
+                    except ParseWarning as pw:
+                        log_once(pw.value, logger=log)
+                    if disk_parser.valid:
+                        log.debug('name %s is same series as %s', filename.name, series)
+                        log.debug('disk_parser.identifier = %s', disk_parser.identifier)
+                        log.debug('disk_parser.quality = %s', disk_parser.quality)
+                        log.debug('disk_parser.proper_count = %s', disk_parser.proper_count)
+
+                        for entry in accepted_series[series]:
+                            log.debug('series_parser.identifier = %s', entry['series_parser'].identifier)
+                            if disk_parser.identifier != entry['series_parser'].identifier:
+                                log.trace('wrong identifier')
+                                continue
+                            log.debug('series_parser.quality = %s', entry['series_parser'].quality)
+                            if config.get('allow_different_qualities') == 'better':
+                                if entry['series_parser'].quality > disk_parser.quality:
+                                    log.trace('better quality')
                                     continue
-                                log.debug('series_parser.quality = %s', entry['series_parser'].quality)
-                                if config.get('allow_different_qualities') == 'better':
-                                    if entry['series_parser'].quality > disk_parser.quality:
-                                        log.trace('better quality')
-                                        continue
-                                elif config.get('allow_different_qualities'):
-                                    if disk_parser.quality != entry['series_parser'].quality:
-                                        log.trace('wrong quality')
-                                        continue
-                                log.debug('entry parser.proper_count = %s', entry['series_parser'].proper_count)
-                                if disk_parser.proper_count >= entry['series_parser'].proper_count:
-                                    entry.reject('proper already exists')
+                            elif config.get('allow_different_qualities'):
+                                if disk_parser.quality != entry['series_parser'].quality:
+                                    log.trace('wrong quality')
                                     continue
-                                else:
-                                    log.trace('new one is better proper, allowing')
-                                    continue
+                            log.debug('entry parser.proper_count = %s', entry['series_parser'].proper_count)
+                            if disk_parser.proper_count >= entry['series_parser'].proper_count:
+                                entry.reject('proper already exists')
+                                continue
+                            else:
+                                log.trace('new one is better proper, allowing')
+                                continue
 
 @event('plugin.register')
 def register_plugin():
