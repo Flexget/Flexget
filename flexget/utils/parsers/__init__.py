bbf2df74f37971fcb718c518cc1f47e42b4c9737
Toilal <toilal.dev@gmail.com>
2014-09-09 17:13:00 +0200

    Move whole guessit code to plugin

--- flexget/utils/parsers/__init__.py
+++ flexget/utils/parsers/__init__.py
@@ -1,3 +1,2 @@
 from parser_common import PARSER_ANY, PARSER_VIDEO, PARSER_MOVIE, PARSER_EPISODE, SERIES_ID_TYPES, ParseWarning
-from parser_guessit import GuessitParser
 from parser_internal import InternalParser
