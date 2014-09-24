7f5f48b2b6749439bd92fdbdbc8e89bfad4924ee
xetix <patrik.westman@phw.se>
2014-09-24 19:37:29 +0200

    Pep8 and documentation

--- flexget/plugins/search_ftp.py
+++ flexget/plugins/search_ftp.py
@@ -14,8 +14,46 @@ log = logging.getLogger('search_ftp')
 
 
 class SearchFTP(object):
-    """
-        Search a file or a directory on a ftp
+
+    """ Search a file or a directory on a ftp
+
+    == Options
+      = ftp-url
+        List of ftp-urls to use in the search-process.
+        The url should be in the following syntax: ftp://user:pass@ftp.address.com:[+]22
+        To use a ssl-connection against the ftp add the plus-sign to the port.
+
+        This is the only option that is required.
+      = search-append
+        If specified, appends a string to the search-string supplied to by the input-plugin.
+        For example, if 'x264' is specified as 'search-append' and 'Riddick' is the output of
+        the input-plugin the final search-string would be 'Riddick x264'.
+      = replace-str
+        Defines a map with strings to replace in the search-string.
+        For example if replace-str is set to {'&': 'and', ' ': '.'}, all '&'-signs would be
+        replaced with the word 'and' and all spaces would be replaced with dots.
+      = strip-chars
+        A regular expression whose matches will be completly removed from the search-string.
+        The intention of this setting were to remove unwanted characters but any regex can be set.
+        It defaults to '[\',:]'.
+      = search-cmd
+        Defines the raw command that should be used to search.
+        Defaults to 'site search'
+      = match-regex
+        Defines the regular expression used to match hits. The expression is matched against the
+        output given when issuing the raw search command.
+
+    == Basic usage
+
+    my-search-task:
+        discover:
+               - ... (e.g. emit_movie_queue)
+        from:
+            - search_ftp:
+                ftp-url:
+                  - "ftp://flexuser:flexpass@127.0.0.1:+13546"
+                search-append: 'x264'
+                replace-str: {'&': 'and'}
     """
 
     schema = {
@@ -67,6 +105,7 @@ class SearchFTP(object):
             config['ftp-url'] = [config['ftp-url']]
 
         for search_string in entry.get('search_strings', [entry['title']]):
+            # setup search string
             search_string = normalize_unicode(clean_title(search_string))
             search_string = self.pepend_searchstring(config, search_string)
             search_cmd = config['search-cmd'] + ' ' + search_string
@@ -74,7 +113,7 @@ class SearchFTP(object):
             for ftp_url in config['ftp-url']:
                 ftp = self.ftp_connect(config, ftp_url)
                 for path in self.ftp_search(ftp, config['match-regex'], search_cmd):
-                    url = re.sub('//+', '/', ftp_url+'/'+path)
+                    url = re.sub('//+', '/', ftp_url + '/' + path)
                     entries.add(Entry(os.path.basename(path), url))
                 ftp.quit()
         return entries
