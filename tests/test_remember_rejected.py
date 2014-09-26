8ed805baa5eedc8224aad0e170bfcc767c97720c
Chase Sterling <chase.sterling@gmail.com>
2014-09-26 16:42:23 -0400

    Fix remember_rejected plugin for per plugin sessions

--- tests/test_remember_rejected.py
+++ tests/test_remember_rejected.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals, division, absolute_import
+
+from flexget import plugin
+from flexget.event import event
+from flexget.utils.tools import parse_timedelta
 from tests import FlexGetBase
+
+
+class RejectRememberPlugin(object):
+    def on_task_filter(self, task, config):
+        for entry in task.all_entries:
+            if isinstance(config, basestring):
+                entry.reject(remember_time=parse_timedelta(config))
+            else:
+                entry.reject(remember=True)
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(RejectRememberPlugin, 'test_remember_reject', api_ver=2, debug=True)
 
 
 class TestRememberRejected(FlexGetBase):
@@ -9,12 +27,12 @@ class TestRememberRejected(FlexGetBase):
           test:
             mock:
               - {title: 'title 1', url: 'http://localhost/title1'}
+            test_remember_reject: yes
     """
 
     def test_remember_rejected(self):
         self.execute_task('test')
-        entry = self.task.find_entry(title='title 1')
-        entry.reject(remember=True)
+        assert self.task.find_entry('rejected', title='title 1', rejected_by='test_remember_reject')
         self.execute_task('test')
         assert self.task.find_entry('rejected', title='title 1', rejected_by='remember_rejected'),\
             'remember_rejected should have rejected'
