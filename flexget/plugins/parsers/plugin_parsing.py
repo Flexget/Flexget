121616e351fb391e51cee86df4c8d19efba89360
Chase Sterling <chase.sterling@gmail.com>
2014-10-31 19:26:42 -0400

    Don't let parsing plugin touch other plugin instances too early refs #2754

--- flexget/plugins/parsers/plugin_parsing.py
+++ flexget/plugins/parsers/plugin_parsing.py
@@ -7,33 +7,37 @@ from flexget.event import event
 log = logging.getLogger('parsing')
 PARSER_TYPES = ['movie', 'series']
 
+# Mapping of parser type to (mapping of parser name to plugin instance)
+parsers = {}
+# Mapping from parser type to the name of the default/selected parser for that type
+default_parsers = {}
+selected_parsers = {}
+
+
+# We need to wait until manager startup to access other plugin instances, to make sure they have all been loaded
+@event('manager.startup')
+def init_parsers(manager):
+    """Prepare our list of parsing plugins and default parsers."""
+    for parser_type in PARSER_TYPES:
+        parsers[parser_type] = {}
+        for p in plugin.get_plugins(group=parser_type + '_parser'):
+            parsers[parser_type][p.name.replace('parser_', '')] = p.instance
+        # Select default parsers based on priority
+        func_name = 'parse_' + parser_type
+        default_parsers[parser_type] = max(parsers[parser_type].iteritems(),
+                                          key=lambda p: getattr(getattr(p[1], func_name), 'priority', 0))[0]
+
 
 class PluginParsing(object):
-    """
-    Provides parsing framework
-    """
-
-    def __init__(self):
-        self.parsers = {}
-        self.parses_names = {}
-        self.default_parser = {}
-        for parser_type in PARSER_TYPES:
-            self.parsers[parser_type] = {}
-            self.parses_names[parser_type] = {}
-            for p in plugin.get_plugins(group=parser_type + '_parser'):
-                self.parsers[parser_type][p.name.replace('parser_', '')] = p.instance
-                self.parses_names[parser_type][p.name.replace('parser_', '')] = p.name
-            # Select default parsers based on priority
-            func_name = 'parse_' + parser_type
-            self.default_parser[parser_type] = max(self.parsers[parser_type].values(),
-                                                   key=lambda p: getattr(getattr(p, func_name), 'priority', 0))
-        self.parser = self.default_parser
+    """Provides parsing framework"""
 
     @property
     def schema(self):
         # Create a schema allowing only our registered parsers to be used under the key of each parser type
-        properties = dict((parser_type, {'type': 'string', 'enum': self.parses_names[parser_type].keys()})
-                          for parser_type in self.parses_names)
+        properties = {}
+        for parser_type in PARSER_TYPES:
+            parser_names = [p.name.replace('parser_', '') for p in plugin.get_plugins(group=parser_type + '_parser')]
+            properties[parser_type] = {'type': 'string', 'enum': parser_names}
         s = {
             'type': 'object',
             'properties': properties,
@@ -42,16 +46,13 @@ class PluginParsing(object):
         return s
 
     def on_task_start(self, task, config):
-        if not config:
-            return
         # Set up user selected parsers from config for this task run
-        self.parser = self.default_parser.copy()
-        for parser_type, parser_name in config.iteritems():
-            self.parser[parser_type] = plugin.get_plugin_by_name('parser_' + parser_name).instance
+        if config:
+            selected_parsers.update(config)
 
     def on_task_end(self, task, config):
         # Restore default parsers for next task run
-        self.parser = self.default_parser
+        selected_parsers.clear()
 
     on_task_abort = on_task_end
 
@@ -65,7 +66,8 @@ class PluginParsing(object):
 
         :returns: An object containing the parsed information. The `valid` attribute will be set depending on success.
         """
-        return self.parser['series'].parse_series(data, name=name, **kwargs)
+        parser = parsers['series'][selected_parsers.get('series', default_parsers.get('series'))]
+        return parser.parse_series(data, name=name, **kwargs)
 
     def parse_movie(self, data, **kwargs):
         """
@@ -75,7 +77,8 @@ class PluginParsing(object):
 
         :returns: An object containing the parsed information. The `valid` attribute will be set depending on success.
         """
-        return self.parser['movie'].parse_movie(data, **kwargs)
+        parser = parsers['movie'][selected_parsers.get('movie') or default_parsers['movie']]
+        return parser.parse_movie(data, **kwargs)
 
 
 @event('plugin.register')
