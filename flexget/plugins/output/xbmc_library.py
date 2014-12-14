3e8d0542b758e8666fa26abc0af2006a2b94ffdf
Claus <clausvium@gmail.com>
2014-12-14 16:33:38 +0100

    now supports sockets which lets xbmc send notifications

--- flexget/plugins/output/xbmc_library.py
+++ flexget/plugins/output/xbmc_library.py
@@ -2,13 +2,15 @@ from __future__ import unicode_literals, division, absolute_import
 
 import os
 import logging
+import socket
 import urlparse
+import posixpath
+import json
 
 import requests
 
 from flexget.event import event
 from flexget import plugin
-from flexget.utils import json
 
 log = logging.getLogger('xbmc_library')
 
@@ -22,6 +24,7 @@ class XBMCLibrary(object):
                     'action': {'type': 'string', 'enum': ['clean', 'scan', 'full_scan']},
                     'category': {'type': 'string', 'enum': ['audio', 'video']},
                     'url': {'type': 'string'},
+                    'port': {'type': 'integer'},
                     'username': {'type': 'string'},
                     'password': {'type': 'string'},
                     'dir': {'type': 'string'},
@@ -44,46 +47,92 @@ class XBMCLibrary(object):
         ]
     }
 
-    def on_task_input(self, task, config):
-        self.on_task_output(task, config)
-
+    # Initialise some variables so we don't have to pass them around everywhere
     def on_task_start(self, task, config):
         self.username = config.get('username', '')
         self.password = config.get('password', '')
-        self.url = urlparse.urljoin(config['url'], 'jsonrpc?request=')
-        if not self.url.startswith('http://'):
-            self.url = 'http://' + self.url
+        self.url = config['url']
+        self.port = config.get('port', 9090)
         self.authentication_required = True if self.username else False
         self.id = 0
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(5)
+        self.use_socket = True
 
+    # Ping the XBMC server to test the connection.
     def ping(self):
         # Sends a ping to XBMC. It should respond with 'pong'
         log.info('Pinging XBMC server.')
         try:
-            response = self.send('JSONRPC.Ping')
-            return response.json().get('result', '') == 'pong'
+            response = self.sendAndRecv('JSONRPC.Ping')
+            return response.get('result', '') == 'pong'
         except requests.HTTPError as ex:
             log.error('Connection to XBMC failed: %s.' % ex.args[0])
             return False
 
-    def send(self, method, params=None, use_params=True):
-        self.id += 1
-        msg = {'id': self.id, 'jsonrpc': '2.0', 'method': method}
-        if use_params:
-            msg['params'] = [] if not params else params
+    # Takes a string of json objects and returns the json objects in a list
+    def decode(self, data):
+        _whitespace = json.decoder.WHITESPACE.match
+        decoder = json._default_decoder
+        idx = _whitespace(data, 0).end()
+        end = len(data)
+        res = []
+        while idx < end:
+            (d, idx) = decoder.raw_decode(data, idx=idx)
+            idx = _whitespace(data, idx).end()
+            res.append(d)
+        return res
 
-        url = self.url + json.dumps(msg)
-        if self.authentication_required:
-            response = requests.get(url, auth=(self.username, self.password))
+    # Send a request to XBMC and wait for the result. Currently stopping after receiving "pong" or "SomethingStarted".
+    # Could be extended to stop after XBMC notifies about finished jobs, although it can take a while.
+    def sendAndRecv(self, method, params=None, use_params=True):
+        BUFFER_SIZE = 32768
+        self.id += 1
+        request = {'id': self.id, 'jsonrpc': '2.0', 'method': method}
+        if use_params:
+            request['params'] = {} if not params else params
+        json_result = {}
+        if self.use_socket:
+            self.socket.send(json.dumps(request))
+            done = False
+            while not done:
+                try:
+                    r = self.socket.recv(BUFFER_SIZE)
+                    for json_obj in self.decode(r):
+                        m = json_obj.get('method') or json_obj.get('result')
+                        if not m:
+                            if 'error' in json_obj:
+                                log.debug('XBMC Error: %s' % unicode(json_obj['error']['message']))
+                                return json_obj
+                            continue
+                        if m.endswith('Started') or m.endswith('pong'):
+                            done = True
+                            json_result = json_obj
+                except socket.timeout:
+                    log.debug('No more data to receive from XBMC.')
+                    break
+                except ValueError as ex:
+                    log.error('Received invalid data: %s.' % ex.args[0])
+                    break
         else:
-            response = requests.get(url)
-        response.raise_for_status()
-        return response
+            url = self.url + json.dumps(request)
+            if self.authentication_required:
+                response = requests.get(url, auth=(self.username, self.password))
+            else:
+                response = requests.get(url)
+            response.raise_for_status()
+            json_result = response.json()
+        return json_result
 
     def experimental(self, action, params):
         log.info('Not implemented yet.')
         #log.warning('Attempting experimental feature.')
         pass
+
+    def make_dir_param(self, path):
+        if not path.endswith(os.sep):
+            path = posixpath.normpath(path) + posixpath.sep
+        return {'directory': path}
 
     def on_task_output(self, task, config):
         if not config:
@@ -92,8 +141,19 @@ class XBMCLibrary(object):
         category = config['category']
         path = config.get('dir', '')
 
+        try:
+            self.socket.connect((self.url, self.port))
+        except socket.error as ex:
+            log.debug('Unable to connect to socket: %s.' % ex.args[1])
+            log.debug('Using XBMC Webserver instead of Websocket.')
+            self.use_socket = False
+            self.url = urlparse.urljoin(self.url, 'jsonrpc?request=')
+            if not self.url.startswith('http://'):
+                self.url = 'http://' + self.url
+
         if not self.ping():
             return
+        log.info('Pong.')
 
         if 'method' in config:
             self.experimental(config['method'], config.get('params', []))
@@ -104,7 +164,12 @@ class XBMCLibrary(object):
             self.full_scan(category, path)
         else:
             self.clean(category)
+        # Close the socket. Could also just let it be garbage collected.
+        if self.use_socket:
+            self.socket.shutdown()
+            self.socket.close()
 
+    # Library scan. Sends a scan request for every entry in the task. May cause timeouts. Needs more testing.
     def scan(self, category, path, task):
         method = category.title() + 'Library.Scan'
         for entry in task.accepted:
@@ -120,39 +185,30 @@ class XBMCLibrary(object):
                 dir_path = os.path.dirname(entry['location'])
             log.debug('Scanning %s.' % dir_path)
             try:
-                response = self.send(method, params=[dir_path])
-                response_json = response.json()
-                if response_json.get('result', '') != 'OK' or response_json.get('error', ''):
-                    log.error('XBMC Error: %s' % unicode(response_json['error']['message']))
-                else:
-                    log.info('Successfully scanned %s for %s content.' % (path, category))
+                response = self.sendAndRecv(method, params=self.make_dir_param(dir_path))
+                if not response.get('error', ''):
+                    log.info('XBMC is now scanning %s for %s content.' % (dir_path, category))
             except requests.HTTPError as ex:
                 log.error('Failed to scan library: %s.' % ex.args[0])
 
+    # Just scan the given path for content.
     def full_scan(self, category, path):
-        # if not os.path.exists(path):
-        #     log.error('Directory does not exist: %s.' % path)
-        #     return
         use_params = False if not path else True
-        log.debug('Scanning %s.' % path)
+        log.debug('Scanning %s for %s.' % (path, category))
         try:
-            response = self.send(category.title() + 'Library.Scan', params=[path], use_params=use_params)
-            response_json = response.json()
-            if response_json.get('result', '') != 'OK' or response_json.get('error', ''):
-                log.debug('XBMC Error: %s' % unicode(response_json['error']['message']))
-            else:
-                log.info('Successfully scanned %s for %s content.' % (path, category))
+            response = self.sendAndRecv(category.title() + 'Library.Scan', params=self.make_dir_param(path),
+                                 use_params=use_params)
+            if not response.get('error', ''):
+                log.info('XBMC is now scanning %s for %s content.' % (path, category))
         except requests.HTTPError as ex:
             log.error('Failed to scan library: %s.' % ex.args[0])
 
+    # Cleans the library ie. removes content that is no longer present.
     def clean(self, category):
         try:
-            response = self.send(category.title() + 'Library.Clean')
-            response_json = response.json()
-            if response_json.get('result', '') != 'OK' or response_json.get('error', ''):
-                log.debug('XBMC Error: %s' % unicode(response_json['error']['message']))
-            else:
-                log.info('Successfully cleaned the XBMC %s library.' % category)
+            response = self.sendAndRecv(category.title() + 'Library.Clean')
+            if not response.get('error'):
+                log.info('XBMC is now cleaning the %s library.' % category)
         except requests.HTTPError as ex:
             log.error('Failed to clean library: %s.' % ex.args[0])
 
