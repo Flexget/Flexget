f0e5c6befbcdfe5c48c7535fd8af1373f3775e1e
Chase Sterling <chase.sterling@gmail.com>
2015-01-08 20:46:26 -0500

    Fix bug in last emit_series commit.

--- flexget/plugins/input/emit_series.py
+++ flexget/plugins/input/emit_series.py
@@ -117,7 +117,7 @@ class EmitSeries(object):
                     latest = get_latest_release(series, season=season, downloaded=check_downloaded)
                     if series.begin and (not latest or latest < series.begin):
                         entries.append(self.search_entry(series, series.begin.season, series.begin.number, task))
-                    elif not config.get('backfill'):
+                    elif latest and not config.get('backfill'):
                         entries.append(self.search_entry(series, latest.season, latest.number + 1, task))
                     elif latest:
                         start_at_ep = 1
