5443ceecfafea3c78a7153eaa2d38febdb54eb3e
Chase Sterling <chase.sterling@gmail.com>
2015-01-11 22:31:50 -0500

    Add basic test for digest plugin

---
+++ tests/test_digest.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals, division, absolute_import
+
+from tests import FlexGetBase
+
+
+class TestDigest(FlexGetBase):
+    __yaml__ = """
+        tasks:
+          digest 1:
+            mock:
+              - title: entry 1
+            accept_all: yes
+            digest: aoeu
+          digest 2:
+            mock:
+              - title: entry 2
+            accept_all: yes
+            digest: aoeu
+          emit digest:
+            emit_digest:
+              list: aoeu
+        """
+
+    def test_multiple_task_merging(self):
+        self.execute_task('digest 1')
+        self.execute_task('digest 2')
+        self.execute_task('emit digest')
+        assert len(self.task.entries) == 2
