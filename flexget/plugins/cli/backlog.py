2c998d98453e83fad0ed997cccddfc85ed893c88
Chase Sterling <chase.sterling@gmail.com>
2015-01-18 15:09:13 -0500

    Tweak backlog cli command a bit

--- flexget/plugins/cli/backlog.py
+++ flexget/plugins/cli/backlog.py
@@ -12,19 +12,19 @@ def do_cli(manager, options):
         num = clear_entries(options.task)
         console('%s entries cleared from backlog.' % num)
     else:
+        cols = '{:<65.64}{:<15.15}'
+        console(cols.format('Title', 'Task'))
+        console('-' * 80)
         with Session() as session:
-            cols = '{:<65.64}{:<15.15}'
-            console(cols.format('Title', 'Task'))
-            console('-' * 80)
-            for entry in get_entries(options.task):
+            entries = get_entries(options.task, session=session)
+            for entry in entries:
                 console(cols.format(entry.title, entry.task))
+            if not entries:
+                console('No items')
 
 
 @event('options.register')
 def register_options():
     parser = options.register_command('backlog', do_cli, help='view or clear entries from backlog plugin')
-    subparsers = parser.add_subparsers(title='actions', metavar='<action>', dest='action')
-    list_parser = subparsers.add_parser('list', help='list entries currently in backlog')
-    list_parser.add_argument('task', nargs='?', help='if specified, only show entries for given task')
-    clear_parser = subparsers.add_parser('clear', help='clear entries currently in backlog')
-    clear_parser.add_argument('task', nargs='?', help='if specified, only clear entries for given task')
+    parser.add_argument('action', choices=['list', 'clear'], help='choose to show items in backlog, or clear them')
+    parser.add_argument('task', nargs='?', help='limit to specific task (if supplied)')
