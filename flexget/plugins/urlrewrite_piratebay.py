30cd4c7f5efa05796c2be338113af4b4c9d5a9e3
Cedric Girard <girard.cedric@gmail.com>
2015-02-08 20:17:11 +0100

    update TPB TLD

--- flexget/plugins/urlrewrite_piratebay.py
+++ flexget/plugins/urlrewrite_piratebay.py
@@ -13,7 +13,7 @@ from flexget.utils.search import torrent_availability, normalize_unicode
 
 log = logging.getLogger('piratebay')
 
-CUR_TLD = 'to'
+CUR_TLD = 'se'
 TLDS = 'com|org|sx|ac|pe|gy|se|%s' % CUR_TLD
 
 URL_MATCH = re.compile('^http://(?:torrents\.)?thepiratebay\.(?:%s)/.*$' % TLDS)
