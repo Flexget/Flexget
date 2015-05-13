b7a3b03b693ac98b78855a8a4ef5e8923853ac67
malkavi <cain1986@gmail.com>
2015-05-14 00:06:24 +0200

    Elitetorrent rewriter + search

---
+++ flexget/plugins/urlrewrite_elitetorrent.py
@@ -0,0 +1,115 @@
+from __future__ import unicode_literals, division, absolute_import
+import logging
+import re
+import urllib2
+import urllib
+import unicodedata
+
+from flexget import plugin
+from flexget.event import event
+from flexget.plugins.plugin_urlrewriting import UrlRewritingError
+from flexget.utils import requests
+from flexget.utils.soup import get_soup
+
+from flexget.entry import Entry
+from flexget.utils.search import torrent_availability, normalize_unicode
+
+from flexget.manager import Session
+
+log = logging.getLogger('elitetorrent')
+
+session = requests.Session()
+
+class UrlRewriteElitetorrent(object):
+    """
+    elitetorrent urlrewriter and search Plugin.
+    """
+   
+    USER_AGENT = 'Mozilla/5.0'
+    
+    schema = {
+        'type': 'object',
+        'properties': {
+            'category': {'type': 'string'},
+        },
+        'additionalProperties': False
+    }
+
+    # urlrewriter API
+    def url_rewritable(self, task, entry):
+        url = entry['url']
+        return (url.startswith('http://www.elitetorrent.net/torrent/'))
+
+    # urlrewriter API
+    def url_rewrite(self, task, entry):
+        entry['url'] = self.parse_download_page(entry['url'])
+
+    @plugin.internet(log)
+    def parse_download_page(self, url):
+        try:
+            page = requests.get(url).content
+            soup = get_soup(page, 'html.parser')
+            download_link = soup.findAll(href=re.compile('/get-torrent/\d+'))
+            download_href = 'http://www.elitetorrent.net' + download_link[0]['href']
+            #if "url" in download_href:
+            #    return download_href[download_href.index('url=') + 4:]
+            #else:
+            return download_href
+        except Exception:
+            raise UrlRewritingError(
+                'Unable to locate torrent from url %s' % url
+            )
+	 
+    def elimina_tildes(self, data):
+	return unicodedata.normalize('NFKD', data).encode('ASCII', 'ignore')
+
+    def search(self, task, entry, config=None):
+        log.debug('Search elitetorrent')
+        url_search = 'http://www.elitetorrent.net/buscar.php'
+        results = set()
+        regex = re.compile("(.+) \(\d\d\d\d\)")
+        #movie_name
+        for search_string in entry.get('search_strings', [entry['title']]):
+            query = normalize_unicode(search_string)
+            query = self.elimina_tildes(regex.findall(query)[0])
+            log.debug('Searching elitetorrent %s' % query)
+            query = query.encode('utf8', 'ignore')
+            #log.debug('Searching elitetorrent %s' % query)
+            data = urllib.urlencode({'buscar': query})
+            req = urllib2.Request(url_search, data)
+            response = urllib2.urlopen(req)
+            content = response.read()
+            #content = normalize_unicode(content).encode('utf8', 'ignore')
+            #log.debug(page.content.decode('utf8'))
+            soup = get_soup(content)
+#            log.debug(soup)
+            #<a class="nombre" href="/torrent/25319/como-entrenar-a-tu-dragon-2-hdrip" title="Como entrenar a tu dragon 2 (HDRip)">Como entrenar a tu dragon 2 (HDRip)</a>
+            children = soup.findAll('a', attrs={'class': 'nombre'})
+            #log.debug(soup2)
+            #soup3 = soup2.find('li')                               
+            #log.debug(len(soup2))
+            #children = soup2.findAll('a', href=True)
+            #log.debug(len(children))
+            #log.debug(children['href'])
+            for child in children:
+                entry = Entry()
+                entry['url'] = 'http://www.elitetorrent.net' + child['href']
+                entry['title'] = child['title']
+                #log.debug(child['href'])
+                results.add(entry)
+            #links = soup2.findAll('a', href=True)
+            #for a in links:
+            #    log.debug(a['href'])
+            #log.debug(links)
+            #for row in [l.find_parent('tr') for l in links]:
+        log.debug('Finish search elitetorrent with %d entries' % len(results))
+        return results
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(
+        UrlRewriteElitetorrent,
+        'elitetorrent',
+        groups=['urlrewriter', 'search'],
+        api_ver=2
+    )
