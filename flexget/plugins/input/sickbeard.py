99809c3b32593aaa72dc1cc9ba38af684f4524ae
Or Carmi <or.carmi82@gmail.com>
2015-05-28 15:20:55 +0300

    Added remarks

--- flexget/plugins/input/sickbeard.py
+++ flexget/plugins/input/sickbeard.py
@@ -23,6 +23,42 @@ class Sickbeard(object):
     }
 
     def on_task_input(self, task, config):
+        '''
+        This plugin returns ALL of the shows monitored by Sickbeard.
+        This includes both ongoing and ended.
+        Syntax:
+
+        sickbeard:
+          base_url=<value>
+          port=<value>
+          api_key=<value>
+
+        Use with input plugin like discover and/or cofnigure_series.
+        Example:
+
+        download-tv-task:
+          configure_series:
+            settings:
+              quality:
+                - 720p
+            from:
+              sickbeard:
+                base_url: http://localhost
+                port: 8531
+                api_key: MYAPIKEY1123
+          discover:
+            what:
+              - emit_series: yes
+            from:
+              torrentz: any
+          download:
+            /download/tv
+
+        Note that when using the configure_series plugin with Sickbeard
+        you are basically synced to it, so removing a show in Sickbeard will
+        remove it in flexget as well,which good be positive or negative,
+        depending on your usage.
+        '''
         url = '%s:%s/api/%s/?cmd=shows' % (config['base_url'], config['port'], config['api_key'])
         json = task.requests.get(url).json()
         entries = []
