a267d9232521fdce288d32438a9bc648f39abbad
Daniel Liabeuf <lildadou@gmail.com>
2015-06-02 07:53:17 +0200

    Code quality improvements. Adding URL field to parsed chartes entries.

--- flexget/utils/charts_snep.py
+++ flexget/utils/charts_snep.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 from __future__ import unicode_literals, division, absolute_import
-import difflib
+from abc import ABCMeta, abstractproperty
 import logging
 import re
 
@@ -18,25 +18,91 @@ requests = Session()
 requests.headers.update({'Accept-Language': 'fr-FR,en;q=0.8'})
 requests.set_domain_delay('snepmusique.com', '1 second')
 
-class SnepChartsEntry(object):
-    """ Plain Old Object for store a charts entry from SNEP website. """
+
+class ParsedChartEntry(ABCMeta(str('ParsedChartEntryABCMeta'), (object,), {})):
+    """
+    This class represents entries parsed form charts provider.
+    """
+    @abstractproperty
+    def artist(self):
+        """@:rtype str"""
+        raise NotImplementedError
+
+    @abstractproperty
+    def song_title(self):
+        """@:rtype str"""
+        raise NotImplementedError
+
+    @abstractproperty
+    def rank(self):
+        """@:rtype int"""
+        raise NotImplementedError
+
+    @abstractproperty
+    def best_rank(self):
+        """@:rtype int"""
+        raise NotImplementedError
+
+    @abstractproperty
+    def charted_weeks(self):
+        """@:rtype int"""
+        raise NotImplementedError
+
+    @abstractproperty
+    def url(self):
+        """@:rtype str"""
+        raise NotImplementedError
+
+
+class SnepParsedChartEntry(ParsedChartEntry):
     def __init__(self):
-        self.title = None
-        self.artist = None
-        self.company = None
+        self._url = None
+        self._song_title = None
+        self._artist = None
+        self._company = None
+        self._rank = None
+        self._best_rank = None
+        self._charted_weeks = 0
 
-        self.stat_position = None
-        self.best_position = None
-        self.weeks_on_charts = 0
+    @property
+    def artist(self):
+        return self._artist
 
-    def __str__(self):
-        def s(value):
-            return value if (value != None) else '-'
+    @property
+    def song_title(self):
+        return self._song_title
 
-        def i(value):
-            return value if (value != None) else -1
+    @property
+    def company(self):
+        return self._company
 
-        return "artist:%s, company:%s, title:%s, stat_position:%i, best_position:%i" % (s(self.artist), s(self.company), s(self.title), i(self.stat_position), i(self.best_position))
+    @property
+    def rank(self):
+        return self._rank
+
+    @property
+    def best_rank(self):
+        return self._best_rank
+
+    @property
+    def charted_weeks(self):
+        return self._charted_weeks
+
+    @property
+    def url(self):
+        return self._url
+
+    @staticmethod
+    def get_entry_map():
+        return {
+            'charts_snep_rank': 'rank',
+            'charts_snep_best_rank': 'best_rank',
+            'charts_snep_weeks': 'charted_weeks',
+            'artist': 'artist',
+            'title': 'song_title',
+            'music_company': 'company',
+            'url': 'url'
+        }
 
 
 class SnepChartsParser(object):
@@ -56,42 +122,42 @@ class SnepChartsParser(object):
     def parse_entry(tag_row):
         """
         :param tag_row: Tag
-        :rtype SnepChartsEntry
+        :rtype SnepParsedChartEntry
         """
 
-        result = SnepChartsEntry()
+        result = SnepParsedChartEntry()
         tag_pos = tag_row.select('td.pos.lwn')
         if len(tag_pos) == 1:
-            result.stat_position = str_to_int(tag_pos[0].text) or -1
+            result._rank = str_to_int(tag_pos[0].text) or -1
 
         tag_best_position = tag_row.find('td', {'class': 'bp'})
         if tag_best_position:
-            result.best_position = str_to_int(tag_best_position.text) or -1
+            result._best_rank = str_to_int(tag_best_position.text) or -1
 
         tag_weeks_on_charts = tag_row.find('td', {'class': 'sw'})
         if tag_weeks_on_charts:
-            result.weeks_on_charts = str_to_int(tag_weeks_on_charts.text) or 0
+            result._charted_weeks = str_to_int(tag_weeks_on_charts.text) or 0
 
         tag_infos = tag_row.find('div', {'class': 'infos'})
         if tag_infos:
             tag_artist = tag_infos.find('strong', {'class': 'artist'})
             if tag_artist:
-                result.artist = tag_artist.text
+                result._artist = tag_artist.text
 
             tag_title = tag_infos.find('p', {'class': 'title'})
             if tag_title:
-                result.title = tag_title.text
+                result._song_title = tag_title.text
 
             tag_company = tag_infos.find('p', {'class': 'company'})
             if tag_company:
-                result.company = tag_company.text
+                result._company = tag_company.text
         return result
 
-    def parse(self, soup):
+    def parse(self, soup, baseUrl=None):
         """
         :param self: SnepChartsParser
         :param soup: bs4.BeautifulSoup
-        :rtype list of [SnepChartsEntry]
+        :rtype list of [SnepParsedChartEntry]
         """
 
         # year_tags = soup.select("#year_top option[selected]")
@@ -100,26 +166,41 @@ class SnepChartsParser(object):
 
         html_entries = soup.select("table.table-top > tbody > tr")
         for unparsed_entry in html_entries:
-            self.charts_entries.append(SnepChartsParser.parse_entry(unparsed_entry))
+            parsed_entry = SnepChartsParser.parse_entry(unparsed_entry)
+            parsed_entry._url = '%s#top-%i' % (baseUrl, parsed_entry.rank)
+            self.charts_entries.append(parsed_entry)
 
+        log.verbose('Produces %i entries' % len(self.charts_entries))
         return self.charts_entries
 
     def retrieve_charts(self, charts_type='radio', date_interval='week', year=None, week=None):
         url = make_url(charts_type,date_interval,year,week)
         page = requests.get(url).text
         soup = get_soup(page)
-        return self.parse(soup)
+        return self.parse(soup, url)
+
 
 def is_snep_url(url):
-    """Tests if url go to snepmusique.com."""
+    """
+    Tests if url go to snepmusique.com
+    :param url: str
+    :rtype bool
+    """
     if not isinstance(url, basestring):
         return
     return re.match(r'https?://[^/]*snepmusique\.com/', url)
 
+
 def make_url(charts_type='radio', date_interval='week', year=None, week=None):
-    """Construit l'url pour le type de charts demandé (type:[all_album, disk_album,
-    digit_album, digit_single, stream_single, compil, back_catalog, radio],
-    interval=[week, year]"""
+    """
+    Build the charts request URL based on charts_type and time interval definition
+    :param charts_type: str [all_album, disk_album,digit_album, digit_single,
+    stream_single, compil, back_catalog, radio]
+    :param interval: str [week, year]
+    :param year: int
+    :param week: int
+    :rtype str
+    """
     date_interval_to_url_path = {
         'year' : 'tops-annuel',
         'week' : 'tops-semaine'
--- tests/test_snep_parser.py
+++ tests/test_snep_parser.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals, division, absolute_import
 
 from flexget.utils.charts_snep import SnepChartsParser
-from flexget.utils.charts_snep import SnepChartsEntry
+from flexget.utils.charts_snep import SnepParsedChartEntry
 from tests import use_vcr
 
 
@@ -15,64 +15,64 @@ class TestSnepParser(object):
         assert (len(parser.charts_entries) == 200), "Expected 200 entries but parser produce %i entries." % len(parser.charts_entries)
         responses = TestSnepParser.build_test_responses()
         for a_response in responses:
-            TestSnepParser.check_one_entry(parser.charts_entries[a_response.stat_position-1], a_response)
+            TestSnepParser.check_one_entry(parser.charts_entries[a_response.rank-1], a_response)
 
     @staticmethod
     def check_one_entry(unchecked, reference):
-        """Kind of SnepChartsEntry.equals
-        :param unchecked: SnepChartsEntry
-        :param reference: SnepChartsEntry
+        """Kind of SnepParsedChartEntry.equals
+        :param unchecked: flexget.utils.charts_snep.SnepParsedChartEntry
+        :param reference: flexget.utils.charts_snep.SnepParsedChartEntry
         """
 
         assert unchecked is not None, "Expected a charts entry but got None"
         assert unchecked.artist == reference.artist, "Expected %s but got %s" % (reference.artist, unchecked.artist)
         assert unchecked.company == reference.company, "Expected %s but got %s" % (reference.company, unchecked.company)
-        assert unchecked.title == reference.title, "Expected %s but got %s" % (reference.title, unchecked.title)
-        assert unchecked.stat_position == reference.stat_position, "Expected %i but got %i" % (reference.stat_position, unchecked.stat_position)
-        assert unchecked.best_position <= reference.best_position, "Expected better position than #%i but got #%i" % (reference.best_position, unchecked.best_position)
-        assert unchecked.weeks_on_charts >= reference.weeks_on_charts, "Expected longer in charts than %i week(s) but got %i" % (reference.weeks_on_charts, unchecked.weeks_on_charts)
+        assert unchecked.song_title == reference.song_title, "Expected %s but got %s" % (reference.song_title, unchecked.song_title)
+        assert unchecked.rank == reference.rank, "Expected %i but got %i" % (reference.rank, unchecked.rank)
+        assert unchecked.best_rank <= reference.best_rank, "Expected better position than #%i but got #%i" % (reference.best_rank, unchecked.best_rank)
+        assert unchecked.charted_weeks >= reference.charted_weeks, "Expected longer in charts than %i week(s) but got %i" % (reference.charted_weeks, unchecked.charted_weeks)
 
     @staticmethod
     def build_test_responses():
         """
         From http://www.snepmusique.com/tops-semaine/top-albums-fusionnes/?ye=2015&we=10
         at 2015-03-30
-        :return: [SnepChartsEntry]
+        :return: [SnepParsedChartEntry]
         """
         result = []
-        an_entry = SnepChartsEntry()
-        an_entry.artist = "Louane"
-        an_entry.company = "MERCURY MUSIC GROUP / FONTANA"
-        an_entry.title = "CHAMBRE 12"
-        an_entry.stat_position = 1
-        an_entry.best_position = 1
-        an_entry.weeks_on_charts = 0
+        an_entry = SnepParsedChartEntry()
+        an_entry._artist = "Louane"
+        an_entry._company = "MERCURY MUSIC GROUP / FONTANA"
+        an_entry._song_title = "CHAMBRE 12"
+        an_entry._rank = 1
+        an_entry._best_rank = 1
+        an_entry._charted_weeks = 0
         result.append(an_entry)
 
-        an_entry = SnepChartsEntry()
-        an_entry.artist = "Christine and the Queens"
-        an_entry.company = "BECAUSE MUSIC / BECAUSE MUSIC"
-        an_entry.title = "CHALEUR HUMAINE"
-        an_entry.stat_position = 2
-        an_entry.best_position = 2
-        an_entry.weeks_on_charts = 38
+        an_entry = SnepParsedChartEntry()
+        an_entry._artist = "Christine and the Queens"
+        an_entry._company = "BECAUSE MUSIC / BECAUSE MUSIC"
+        an_entry._song_title = "CHALEUR HUMAINE"
+        an_entry._rank = 2
+        an_entry._best_rank = 2
+        an_entry._charted_weeks = 38
         result.append(an_entry)
 
-        an_entry = SnepChartsEntry()
-        an_entry.artist = "Multi Interprètes"
-        an_entry.company = "DEF JAM RECORDINGS FRANCE / DEF JAM"
-        an_entry.title = "FIFTY SHADES OF GREY"
-        an_entry.stat_position = 5
-        an_entry.best_position = 3
-        an_entry.weeks_on_charts = 2
+        an_entry = SnepParsedChartEntry()
+        an_entry._artist = "Multi Interprètes"
+        an_entry._company = "DEF JAM RECORDINGS FRANCE / DEF JAM"
+        an_entry._song_title = "FIFTY SHADES OF GREY"
+        an_entry._rank = 5
+        an_entry._best_rank = 3
+        an_entry._charted_weeks = 2
         result.append(an_entry)
 
-        an_entry = SnepChartsEntry()
-        an_entry.artist = "ZAZ"
-        an_entry.company = "PLAY ON / PLAY ON"
-        an_entry.title = "PARIS"
-        an_entry.stat_position = 43
-        an_entry.best_position = 2
-        an_entry.weeks_on_charts = 15
+        an_entry = SnepParsedChartEntry()
+        an_entry._artist = "ZAZ"
+        an_entry._company = "PLAY ON / PLAY ON"
+        an_entry._song_title = "PARIS"
+        an_entry._rank = 43
+        an_entry._best_rank = 2
+        an_entry._charted_weeks = 15
         result.append(an_entry)
         return result
