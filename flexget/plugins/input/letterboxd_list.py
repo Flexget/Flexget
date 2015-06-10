38d0d38d2d534422807ddd3a14d9e04797336527
Chemondelay <chemondelay@gmail.com>
2015-06-10 17:18:20 +0930

    Update letterboxd_list.py

--- flexget/plugins/input/letterboxd_list.py
+++ flexget/plugins/input/letterboxd_list.py
@@ -41,33 +41,33 @@ class LetterboxdList(object):
     @cached('letterboxd_list', persist='2 hours')
     def on_task_input(self, task, config):        
         base_url = 'http://letterboxd.com'
-        list = config['list'].lower().replace(' ', '-')
+        m_list = config['list'].lower().replace(' ', '-')
         max_pages = config.get('max_pages', 1)
 
-        if list == 'watchlist':
+        if m_list == 'watchlist':
             p_slug = '/%s/watchlist/' % config['username']
             m_slug = 'data-film-slug'
             log.verbose('Retrieving %s\'s watchlist from Letterboxd.' % config['username'])
-        elif list == 'diary':
+        elif m_list == 'diary':
             p_slug = '/%s/films/diary/' % config['username']
             m_slug = 'data-film-link'
             log.verbose('Retrieving %s\'s film diary from Letterboxd.' % config['username'])
-        elif list == 'likes':
+        elif m_list == 'likes':
             p_slug = '/%s/likes/films/' % config['username']
             m_slug = 'data-film-link'
             log.verbose('Retrieving list of films %s has liked on Letterboxd.' % config['username'])
-        elif list == 'watched':
+        elif m_list == 'watched':
             p_slug = '/%s/films/' % config['username']
             m_slug = 'data-film-slug'
             log.verbose('Retrieving list of films watched by %s from Letterboxd.' % config['username'])
-        elif list == 'rated':
+        elif m_list == 'rated':
             p_slug = '/%s/films/ratings/' % config['username']
             m_slug = 'data-film-slug'
             log.verbose('Retrieving list of films rated by %s on Letterboxd.' % config['username'])
         else:
-            p_slug =  '/%s/list/%s/' % (config['username'], list)
+            p_slug =  '/%s/list/%s/' % (config['username'], m_list)
             m_slug = 'data-film-slug'
-            log.verbose('Retrieving %s\'s Letterboxd list: %s.' % (config['username'], list))
+            log.verbose('Retrieving %s\'s Letterboxd list: %s.' % (config['username'], m_list))
         
         next_page = ''
         sort_by = ''
@@ -83,13 +83,13 @@ class LetterboxdList(object):
                 raise plugin.PluginError('Can\'t retrieve Letterboxd list from %s. Make sure it\'s not set to private; ' + \
                                          'if not, the URL may be incorrect. Check your config.') % url
             soup = get_soup(page.text)
-            if list == 'diary':
+            if m_list == 'diary':
                 movies = soup.find_all('tr', attrs={'class': 'diary-entry-row'})
             else:
                 movies = soup.find_all('li', attrs={'class': 'poster-container'})
 
             for movie in movies:
-                if list == 'diary':
+                if m_list == 'diary':
                     m_url = base_url + movie.find('td', attrs={'class': 'td-actions'}).get(m_slug)
                 else:
                     m_url = base_url + movie.find('div').get(m_slug)
@@ -105,11 +105,11 @@ class LetterboxdList(object):
                 entry['imdb_id'] = extract_id(imdb_url)
                 tmdb_url = m_soup.find('p', attrs={'class': 'text-link'}).find(href=re.compile('themoviedb'))
                 entry['tmdb_id'] = re.search(r'\/(\d+)\/$', tmdb_url.get('href')).group(1)
-                entry['letterboxd_list'] = '%s (%s)' % (list, config['username'])
+                entry['letterboxd_list'] = '%s (%s)' % (m_list, config['username'])
                 entry['letterboxd_score'] = 0
                 entry['letterboxd_score'] = m_soup.find('span', attrs={'class': 'average-rating'})\
                     .find('meta', attrs={'itemprop': 'average'}).get('content')
-                if list in ['diary', 'rated']:
+                if m_list in ['diary', 'rated']:
                     try:
                         user_rating = movie.find('meta', attrs={'itemprop': 'rating'}).get('content')
                         entry['letterboxd_score'] = user_rating
