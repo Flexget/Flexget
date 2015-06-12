d62de144e4eddb520d664c7c0874e06cdc81734c
Daniel Liabeuf <lildadou@gmail.com>
2015-06-12 08:06:24 +0200

    Use a kludge for pass schema validation

--- tests/test_charts_snep_input.py
+++ tests/test_charts_snep_input.py
@@ -8,9 +8,15 @@ class TestChartsSnepInput(FlexGetBase):
         tasks:
           test:
             charts_snep_input: radio
+          bad source:
+            charts_snep_input: club
     """
 
     @use_vcr
     def test_input(self):
         self.execute_task('test')
         assert len(self.task.entries) == 60, 'Produces %i entries, expected 60' % len(self.task.entries)
+
+    def test_source(self):
+        self.execute_task('bad source')
+        assert len(self.task.entries) == 0, 'Produces %i entries, expected 0' % len(self.task.entries)
