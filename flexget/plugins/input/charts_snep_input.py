d7b7e63f2530ce5f85f6839c3c56f043f9e24254
Daniel Liabeuf <lildadou@gmail.com>
2015-06-13 23:49:45 +0200

    PEP8 and docs

--- flexget/plugins/input/charts_snep_input.py
+++ flexget/plugins/input/charts_snep_input.py
@@ -8,14 +8,25 @@ from flexget.utils.cached_input import cached
 
 log = logging.getLogger('charts_snep_input')
 
-
 source_enum = ['all_album', 'disk_album', 'digit_album',
                'digit_single', 'stream_single',
                'compil', 'back_catalog', 'radio']
 
-class ChartsSnepInput(object):
-    """Create an entrie for each charted single in your charts request"""
 
+def valid_config(config):
+    return config not in source_enum
+
+
+# TODO: Use db for improved perfs?
+class ChartsSnepInput(object):
+    """
+    Create an entrie for each charted single in your charts request.
+
+    Syntax:
+    charts_snep_input: <one of : ['all_album', 'disk_album', 'digit_album',
+               'digit_single', 'stream_single',
+               'compil', 'back_catalog', 'radio']>
+    """
     schema = {
         'type': 'string'
     }
@@ -23,17 +34,19 @@ class ChartsSnepInput(object):
 
     @cached('charts_snep_input', persist='2 hours')
     def on_task_input(self, task, config):
-        if config not in source_enum:
-            log.warning("'%s' is an unknow charts type for charts_snep. Please to use %s."
+        # Check config
+        if not valid_config(config):
+            log.warning("'%s' is an unknow charts type for charts_snep. Please to use one of them : %s."
                         % (config, ', '.join(source_enum)))
             return []
 
-
+        # Charts retrieving
         log.verbose('Retrieving weekly SNEP charts of %s' % config)
         parser = SnepChartsParser()
         chart_entries = parser.retrieve_charts(config)
         """:type chart_entries: list of [flexget.utils.charts_snep.SnepParsedChartEntry]"""
 
+        # Build/map the result
         input_entries = []
         for charts_entry in chart_entries:
             flexget_entry = Entry()
@@ -42,6 +55,7 @@ class ChartsSnepInput(object):
             input_entries.append(flexget_entry)
 
         return input_entries
+
 
 @event('plugin.register')
 def register_plugin():
