9796b8cf9df115abbc73b49bfdb31cbff19fb0b4
Chase Sterling <chase.sterling@gmail.com>
2015-06-25 19:20:16 -0400

    More tweaks to history ui

--- flexget/ui/plugins/history/static/js/controller.js
+++ flexget/ui/plugins/history/static/js/controller.js
@@ -4,6 +4,7 @@ var historyModule = angular.module("historyModule", []);
 registerFlexModule(historyModule);
 
 historyModule.controller('HistoryCtrl', function($scope, $http) {
+  $scope.title = 'History';
   $http.get('/api/history').
     success(function(data, status, headers, config) {
       $scope.entries = data['items'];
