7c66958b582419cd68560522e6ba7d6bce2039e1
Tiago Santos <camon.pt@gmail.com>
2015-08-05 20:37:16 +0100

    Update subtitles_subliminal.py
    Updated to work with the current stable version of subliminal (1.0.1)
    
    This solves this problem http://flexget.com/ticket/3047

--- flexget/plugins/output/subtitles_subliminal.py
+++ flexget/plugins/output/subtitles_subliminal.py
@@ -85,11 +85,12 @@ class PluginSubliminal(object):
         from babelfish import Language
         from dogpile.cache.exception import RegionAlreadyConfigured
         import subliminal
+        from subliminal import cli
         try:
-            subliminal.cache_region.configure('dogpile.cache.dbm', 
+            subliminal.region.configure('dogpile.cache.dbm', 
                                               arguments={'filename': os.path.join(tempfile.gettempdir(),
                                                                                   'cachefile.dbm'),
-                                              'lock_factory': subliminal.MutexLock})
+                                              'lock_factory': subliminal.cli.MutexLock})
         except RegionAlreadyConfigured:
             pass
         logging.getLogger("subliminal").setLevel(logging.CRITICAL)
