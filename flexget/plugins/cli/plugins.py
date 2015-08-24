94096d118885ca5dd266943e3cc85e7098272f2e
sean797 <seanokeeffe797@gmail.com>
2015-08-24 23:28:06 +0100

    added --porcelain option to various output

--- flexget/plugins/cli/plugins.py
+++ flexget/plugins/cli/plugins.py
@@ -11,9 +11,12 @@ log = logging.getLogger('plugins')
 
 @event('manager.subcommand.plugins')
 def plugins_summary(manager, options):
-    console('-' * 79)
-    console('%-20s%-30s%s' % ('Name', 'Roles (priority)', 'Info'))
-    console('-' * 79)
+    if options.porcelain:
+        console('%-30s%-s%-30s%-s%s' % ('Name', '|', 'Roles (priority)', '|', 'Info'))
+    else:
+        console('-' * 79)
+        console('%-30s%-30s%-s' % ('Name', 'Roles (priority)', 'Info'))
+        console('-' * 79)
 
     # print the list
     for plugin in sorted(get_plugins(phase=options.phase, group=options.group)):
@@ -29,9 +32,15 @@ def plugins_summary(manager, options):
             flags.append('debug')
         handlers = plugin.phase_handlers
         roles = ', '.join('%s(%s)' % (phase, handlers[phase].priority) for phase in handlers)
-        console('%-20s%-30s%s' % (plugin.name, roles, ', '.join(flags)))
-
-    console('-' * 79)
+        tab = '|'
+        if options.porcelain:
+            console('%-30s%-s%-30s%-s%s' % (plugin.name, '|', roles, '|', ', '.join(flags)))
+        else:
+            console('%-30s%-30s%-s' % (plugin.name, roles, ', '.join(flags)))
+    if options.porcelain:
+        pass
+    else:
+        console('-' * 79)
 
 
 @event('options.register')
@@ -39,3 +48,4 @@ def register_parser_arguments():
     plugins_subparser = options.register_command('plugins', plugins_summary, help='print registered plugin summaries')
     plugins_subparser.add_argument('--group', help='show plugins belonging to this group')
     plugins_subparser.add_argument('--phase', help='show plugins that act on this phase')
+    plugins_subparser.add_argument('--porcelain' , action='store_true', help='make the output parseable')
