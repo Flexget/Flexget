1eca174cff3eeb90d7fa1756d2b6adfa0c668e77
Steve <stevezau@gmail.com>
2015-09-20 20:22:44 +1000

    Adding UI authentication

--- flexget/ui/plugins/authentication/__init__.py
+++ flexget/ui/plugins/authentication/__init__.py
@@ -1,2 +1,9 @@
 from __future__ import unicode_literals, division, absolute_import
-from .authentication import *
+
+from flexget.ui import register_plugin, register_js, Blueprint
+
+authentication = Blueprint('authentication', __name__)
+register_plugin(authentication)
+
+register_js('authentication', 'js/authentication.js', bp=authentication)
+register_js('http-auth-interceptor', 'js/http-auth-interceptor.js', bp=authentication)
---
+++ flexget/ui/plugins/authentication/static/js/authentication.js
@@ -0,0 +1,42 @@
+'use strict';
+
+app.requires.push('http-auth-interceptor');
+
+app.controller('LoginController', function ($scope, $http, $mdDialog, authService) {
+  $scope.error = "";
+
+  var doLogin = function() {
+    var LoginModalController = function($scope) {
+      $scope.error = "";
+      $scope.credentials = {
+        username: '',
+        password: ''
+      };
+      $scope.login = function () {
+        $http.post('/api/login/', $scope.credentials).success(function() {
+          authService.loginConfirmed();
+        }).error(function(data) {
+          $scope.credentials.password = '';
+          if ('message' in data) {
+            $scope.error = data.message;
+          } else {
+            $scope.error = "Error during authentication";
+          }
+        });
+      }
+    };
+    $mdDialog.show({
+      templateUrl: 'static/authentication/login.html',
+      parent: angular.element(document.body),
+      controller: LoginModalController
+    });
+
+  };
+
+  var closeLogin = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.$on('event:auth-loginRequired', doLogin);
+  $scope.$on('event:auth-loginConfirmed', closeLogin)
+});
