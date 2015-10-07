1347060be16b170cfdba2b55e5f14166ca4cdf00
Steve <stevezau@gmail.com>
2015-10-08 09:45:08 +1100

    Removed register_route and register_menu and no longer required

--- flexget/ui/js/utils.js
+++ flexget/ui/js/utils.js
@@ -4,23 +4,6 @@ String.prototype.startsWith = function(prefix) {
   return this.indexOf(prefix) === 0;
 };
 
-function registerFlexModule(module) {
-  angular.module('flexgetApp').requires.push(module.name);
-}
-
-app.menu = [];
-app.routes = [];
-
-function register_menu(href, caption, icon, order) {
-  href = '/ui/#' + href;
-  app.menu.push({href: href, caption: caption, icon: icon, order: order})
-}
-
-function register_route(name, url, controller, template) {
-  app.routes.push({
-    name: name,
-    url: url,
-    controller: controller,
-    template: template
-  });
+function registerModule(module) {
+  angular.module('flexget').requires.push(module.name);
 }
