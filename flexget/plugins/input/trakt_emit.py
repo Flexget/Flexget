1fac88a6f8286b9db4f3ac1de1a218d41923320f
Christopher Thatcher <crawl45@gmail.com>
2015-10-11 03:34:25 -0500

    Check for json key before assigning function. Fix #3075

--- flexget/plugins/input/trakt_emit.py
+++ flexget/plugins/input/trakt_emit.py
@@ -59,16 +59,19 @@ class TraktEmit(object):
                 log.warning('The list "%s" is empty.' % config['list'])
                 return
             for item in data:
-                if item['show'] is not None:
-                    if not item['show']['title']:
-                        # Seems we can get entries with a blank show title sometimes
-                        log.warning('Found trakt list show with no series name.')
-                        continue
-                    trakt_id = item['show']['ids']['trakt']
-                    listed_series[trakt_id] = {
-                        'series_name': item['show']['title'],
-                        'trakt_id': trakt_id,
-                        'tvdb_id': item['show']['ids']['tvdb']}
+                if getattr(data, 'show'):
+                    if item['show'] is not None:
+                        if not item['show']['title']:
+                            # Seems we can get entries with a blank show title sometimes
+                            log.warning('Found trakt list show with no series name.')
+                            continue
+                        trakt_id = item['show']['ids']['trakt']
+                        listed_series[trakt_id] = {
+                            'series_name': item['show']['title'],
+                            'trakt_id': trakt_id,
+                            'tvdb_id': item['show']['ids']['tvdb']}
+                    else:
+                        log.error('Trakt input returned invalid Data for trakt list')
         context = config['context']
         if context == 'collected':
             context = 'collection'
