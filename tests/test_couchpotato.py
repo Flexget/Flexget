8d5e9a6fdc4f626b849e734b2bfeb58f164449c7
Or Carmi <or.carmi82@gmail.com>
2015-10-26 10:42:32 +0200

    Fixed sanity test

--- tests/test_couchpotato.py
+++ tests/test_couchpotato.py
@@ -5,29 +5,27 @@ from tests import FlexGetBase
 
 movie_list_file = 'couchpotoato_test_reponse.json'
 with open(movie_list_file, "r") as data:
-         movie_list_response = json.load(data)
+    movie_list_response = json.load(data)
 
 
 class TestCouchpotato(FlexGetBase):
-
     __yaml__ = """
         tasks:
           couch:
-            base_url: 'http://test.url.com'
-            port: 5050
-            api_key: '123abc'
+            couchpotato:
+              base_url: 'http://test.url.com'
+              port: 5050
+              api_key: '123abc'
         """
 
     @mock.patch('flexget.plugins.input.couchpotato.CouchPotato.get_json')
-    def test_couchpotato(self, mock_get):
-
-        mock_response = mock.Mock()
-        mock_response.return_value = movie_list_response
-        mock_get.return_value = mock_response
+    def test_couchpotato_sanity(self, mock_get):
+        mock_get.return_value = movie_list_response
 
         self.execute_task('couch')
 
-        assert mock_get.called
-
-
-
+        assert mock_get.called, 'Did not access Couchpotato results.'
+        assert len(self.task._all_entries) == 31, 'Did not produce 31 entries'
+        for entry in self.task._all_entries:
+            assert entry.store['quality_req'] == '', 'Quality for entry {} should be empty, instead its {}'.format(
+                entry['title'], entry.store['quality_req'])
