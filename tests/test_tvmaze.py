12f16bdb2fbc19b006ce759d4258641b51c47d5e
Or Carmi <or.carmi82@gmail.com>
2015-11-23 16:27:01 +0200

    Added tests

---
+++ tests/test_tvmaze.py
@@ -0,0 +1,109 @@
+from __future__ import unicode_literals, division, absolute_import
+
+from flexget.manager import Session
+from flexget.plugins.api_tvmaze import APITVMaze, TVMazeLookup, TVMazeSeries
+from tests import FlexGetBase, use_vcr
+
+lookup_series = APITVMaze.series_lookup
+
+
+class TestTVMazeShowLookup(FlexGetBase):
+    __yaml__ = """
+        templates:
+          global:
+            tvmaze_lookup: yes
+            # Access a tvdb field to cause lazy loading to occur
+            set:
+              afield: "{{tvdb_id}}{{tvmaze_episode_name}}"
+        tasks:
+          test:
+            mock:
+              - {title: 'House.S01E02.HDTV.XViD-FlexGet'}
+              - {title: 'Doctor.Who.2005.S02E03.PDTV.XViD-FlexGet'}
+            series:
+              - House
+              - Doctor Who 2005
+          test_unknown_series:
+            mock:
+              - {title: 'Aoeu.Htns.S01E01.htvd'}
+            series:
+              - Aoeu Htns
+          test_date:
+            mock:
+              - title: the daily show 2012-6-6
+            series:
+              - the daily show (with jon stewart)
+          test_search_result:
+            mock:
+              - {title: 'Shameless.2011.S01E02.HDTV.XViD-FlexGet'}
+              - {title: 'Shameless.2011.S03E02.HDTV.XViD-FlexGet'}
+            series:
+              - Shameless (2011)
+    """
+
+    @use_vcr
+    def test_lookup_name(self):
+        """tvmaze: Test Lookup (ONLINE)"""
+        self.execute_task('test')
+        entry = self.task.find_entry(title='House.S01E02.HDTV.XViD-FlexGet')
+        assert entry['tvmaze_series_id'] == 118, \
+            'Tvmaze_ID should be 118 is %s for %s' % (entry['tvmaze_series_name'], entry['series_name'])
+        assert entry['tvmaze_series_status'] == 'Ended', 'Series Status should be "ENDED" returned %s' \
+                                                         % (entry['tvmaze_series_status'])
+
+    @use_vcr
+    def test_lookup(self):
+        """tvmaze: Test Lookup (ONLINE)"""
+        self.execute_task('test')
+        entry = self.task.find_entry(title='House.S01E02.HDTV.XViD-FlexGet')
+        assert entry['tvmaze_episode_name'] == 'Paternity', \
+            '%s tvmaze_episode_name should be Paternity, is actually %s' % (
+                entry['title'], entry['tvmaze_episode_name'])
+        assert entry['tvmaze_series_status'] == 'Ended', \
+            'status for %s is %s, should be "ended"' % (entry['title'], entry['tvmaze_series_status'])
+        assert entry['afield'] == '73255Paternity', 'afield was not set correctly'
+        assert self.task.find_entry(tvmaze_episode_name='School Reunion'), \
+            'Failed imdb lookup Doctor Who 2005 S02E03'
+
+    @use_vcr
+    def test_unknown_series(self):
+        # Test an unknown series does not cause any exceptions
+        self.execute_task('test_unknown_series')
+        # Make sure it didn't make a false match
+        entry = self.task.find_entry('accepted', title='Aoeu.Htns.S01E01.htvd')
+        assert entry.get('tvdb_id') is None, 'should not have populated tvdb data'
+
+    @use_vcr
+    def test_search_results(self):
+        self.execute_task('test_search_result')
+        entry = self.task.entries[0]
+        print entry['tvmaze_series_name'].lower()
+        assert entry['tvmaze_series_name'].lower() == 'Shameless'.lower(), 'lookup failed'
+        with Session() as session:
+            assert self.task.entries[1]['tvmaze_series_name'].lower() == 'Shameless'.lower(), 'second lookup failed'
+
+            assert len(session.query(TVMazeLookup).all()) == 1, 'should have added 1 show to search result'
+
+            assert len(session.query(TVMazeSeries).all()) == 1, 'should only have added one show to show table'
+            assert session.query(
+                TVMazeSeries).first().name == 'Shameless', 'should have added Shameless and not Shameless (2011)'
+            # change the search query
+            session.query(TVMazeLookup).update({'search_name': "Shameless.S01E03.HDTV-FlexGet"})
+            session.commit()
+
+            lookupargs = {'title': "Shameless.S01E03.HDTV-FlexGet"}
+            series = APITVMaze.series_lookup(**lookupargs)
+
+            assert series.tvdb_id == entry['tvdb_id'], 'tvdb id should be the same as the first entry'
+            assert series.tvmaze_id == entry['tvmaze_series_id'], 'tvmaze id should be the same as the first entry'
+            assert series.name.lower() == entry['tvmaze_series_name'].lower(), 'series name should match first entry'
+
+    @use_vcr
+    def test_date(self):
+        self.execute_task('test_date')
+        entry = self.task.find_entry(title='the daily show 2012-6-6')
+        # Make sure show data got populated
+        assert entry.get('tvmaze_series_id') == 249, 'expected tvmaze_series_id 249, got %s' % entry.get(
+            'tvmaze_series_id')
+        assert entry.get('tvmaze_episode_id') == 20471, 'episode id should be 20471, is actually %s' % entry.get(
+            'tvmaze_episode_id')
