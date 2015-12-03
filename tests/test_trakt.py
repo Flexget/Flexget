8e54b85e3459beb0f05f9bb0ad60a613af4dbd5c
crawl45 <crawl45@gmail.com>
2015-12-02 22:09:14 -0600

    Reworked trakt_api scoring comparison. Added new test for low scoring matches to fail.

--- tests/test_trakt.py
+++ tests/test_trakt.py
@@ -45,6 +45,11 @@ class TestTraktShowLookup(FlexGetBase):
               - {title: 'Shameless.2011.S03E02.HDTV.XViD-FlexGet'}
             series:
               - Shameless (2011)
+          test_search_fail:
+            mock:
+              - {title: 'Baking.Around.S01E01.HDTV.XViD-FlexGet'}
+            series:
+              - Baking Around
     """
 
     @use_vcr
@@ -102,6 +107,11 @@ class TestTraktShowLookup(FlexGetBase):
             assert series.tvdb_id == entry['tvdb_id'], 'tvdb id should be the same as the first entry'
             assert series.id == entry['trakt_show_id'], 'trakt id should be the same as the first entry'
             assert series.title.lower() == entry['trakt_series_name'].lower(), 'series name should match first entry'
+    @use_vcr
+    def test_search_fail(self):
+        self.execute_task('test_search_fail')
+        entry = self.task.find_entry('accepted', title='Baking.Around.S01E01.HDTV.XViD-FlexGet')
+        assert entry.get('trakt_show_id') is None, 'Should not have returned trakt id'
 
     @use_vcr
     def test_date(self):
