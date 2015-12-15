5e83a5f0546329826a2f4a6e21290749205df22d
Daniel Liabeuf <lildadou@gmail.com>
2015-12-15 07:46:59 +0100

    Adding a lookup plugin for t411

--- flexget/plugins/plugin_torrent411.py
+++ flexget/plugins/plugin_torrent411.py
@@ -19,6 +19,7 @@ class T411InputPlugin(object):
       terms: <see available terms on "flexget t411 list-terms --category <category name>"
       max_resutls: XXX
     """
+
     def __init__(self):
         self.schema = {
             'type': 'object',
@@ -65,6 +66,38 @@ class T411InputPlugin(object):
 
         return entries
 
+
+class T411LookupPlugin:
+    schema = {'type': 'boolean'}
+
+    torrent_details_map = {
+        't411_terms': lambda details: [term.name for term in details.terms],
+    }
+
+    @staticmethod
+    def lazy_lookup(entry):
+        torrent_id = entry.get('t411_torrent_id')
+        if torrent_id:
+            log.debug('Lookup for %s' % entry['t411_torrent_id'])
+        else:
+            raise plugin.PluginError('looking up T411 for entry failed, no t411_torrent_id passed.')
+
+        proxy = T411Proxy()
+        proxy.set_credential()
+        details = proxy.details(torrent_id)
+        entry.update_using_map(T411LookupPlugin.torrent_details_map, details)
+
+    # Run after series and metainfo series
+    @plugin.priority(110)
+    def on_task_metainfo(self, task, config):
+        for entry in task.entries:
+            if entry.get('t411_torrent_id') is None:
+                continue
+
+            entry.register_lazy_func(T411LookupPlugin.lazy_lookup, T411LookupPlugin.torrent_details_map)
+
+
 @event('plugin.register')
 def register_plugin():
-    plugin.register(T411InputPlugin, 't411', groups=['search','input'], api_ver=2)
+    plugin.register(T411InputPlugin, 't411', groups=['search', 'input'], api_ver=2)
+    plugin.register(T411LookupPlugin, 't411_lookup', api_ver=2)
