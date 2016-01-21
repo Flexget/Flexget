f3e8ec30a82f3a8ba3cc5146e88672a3479cb50a
Or Carmi <or.carmi82@gmail.com>
2016-01-21 15:07:57 +0200

    [fix][tvdb REST API] Added return message for show not found

--- flexget/plugins/api_tvdb.py
+++ flexget/plugins/api_tvdb.py
@@ -557,16 +557,21 @@ tvdb_api = api.namespace('tvdb', description='TheTVDB Shows')
 @api.doc(params={'search': 'TV Show name or TVDB ID'})
 class TVDBSearchApi(APIResource):
     @api.response(200, 'Successfully found show')
-    @api.response(400, 'Missing search parameter')
+    @api.response(404, 'No show found')
     def get(self, search, session=None):
         try:
             tvdb_id = int(search)
         except ValueError:
             tvdb_id = None
 
-        if tvdb_id:
-            result = lookup_series(tvdb_id=tvdb_id, session=session)
-        else:
-            result = lookup_series(name=search, session=session)
+        try:
+            if tvdb_id:
+                result = lookup_series(tvdb_id=tvdb_id, session=session)
+            else:
+                result = lookup_series(name=search, session=session)
+        except LookupError as e:
+            return {'status': 'error',
+                    'message': e.args[0]
+                    }, 404
 
         return jsonify(result.to_dict())
