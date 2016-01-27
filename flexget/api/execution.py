1f23a9c29d497e45f79bb009c7946a4bb536efd8
Steven Adams <stevezau@gmail.com>
2016-01-27 19:02:01 +1100

    Refactor execute API

--- flexget/api/execution.py
+++ flexget/api/execution.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 from Queue import Queue, Empty
-
 import cherrypy
-
-from flask import request, jsonify, Response
-
-from flexget.options import get_parser
+from flask import jsonify, Response
+from flexget.task import task_phases
 from flexget.api import api, APIResource
 from flexget.utils import json
 from json import JSONEncoder
@@ -24,49 +21,70 @@ def _task_info_dict(task):
     }
 
 
-task_info_schema = {
+task_queue_schema = {
     'type': 'object',
     'properties': {
-        'id': {'type': 'integer'},
-        'name': {'type': 'string'},
-        'current_phase': {'type': 'string'},
-        'current_plugin': {'type': 'string'},
+        'tasks': {
+            'type': 'array',
+            'items': {
+                'id': {'type': 'integer'},
+                'name': {'type': 'string'},
+                'current_phase': {'type': 'string'},
+                'current_plugin': {'type': 'string'},
+            }
+        }
     }
 }
 
 execution_results_schema = {
     'type': 'object',
     'properties': {
-        'tasks': {
-            'type': 'array',
-            'items': task_info_schema,
+        'task': {
+            'type': 'object',
+            'properties': {
+                'id': {'type': 'integer'},
+                'name': {'type': 'string'},
+                'stream': {
+                    'type': 'array',
+                    'items': {
+                        'progress': {
+                            'type': 'object',
+                            'properties': {
+                                'status': {'type': 'string', 'enum': ['pending', 'running', 'complete']},
+                                'phase': {'type': 'string', 'enum': task_phases},
+                                'plugin': {'type': 'string'},
+                                'percent': {'type': 'float'}
+                            }
+                        },
+                        'summary': {
+                            'type': 'object',
+                            'properties': {
+                                'accepted': {'type': 'integer'},
+                                'rejected': {'type': 'integer'},
+                                'failed': {'type': 'integer'},
+                                'undecided': {'type': 'integer'},
+                                'aborted': {'type': 'boolean'},
+                                'abort_reason': {'type': 'string'},
+                            }
+                        },
+                        'entry_dump': {'type': 'array', 'items': {'type': 'object'}},
+                        'log': {'type': 'string'}
+                    }
+                }
+            }
         }
     }
 }
 
-
-execute_task_schema = {
-    'type': 'object',
-    'properties': {
-        'tasks': {
-            'type': 'array',
-            'items': {'type': 'string'}
-        },
-        'opt': {'type': 'string'},
-    }
-}
-
-
+execution_api_queue_schema = api.schema('execution_queue', task_queue_schema)
 execution_api_result_schema = api.schema('execution_result', execution_results_schema)
-execute_api_task_schema = api.schema('execute_task', execute_task_schema)
 
 
-@execution_api.route('/queue/')
+@execution_api.route('/')
 class ExecutionQueueAPI(APIResource):
-
-    @api.response(200, 'Show tasks in queue for execution', execution_api_result_schema)
+    @api.response(200, 'Show tasks in queue for execution', execution_api_queue_schema)
     def get(self, session=None):
-        """ List task executions """
+        """ List task(s) in queue for execution """
         tasks = [_task_info_dict(task) for task in self.manager.task_queue.run_queue.queue]
 
         if self.manager.task_queue.current_task:
@@ -75,32 +93,9 @@ class ExecutionQueueAPI(APIResource):
         return jsonify({'tasks': tasks})
 
 
-@execution_api.route('/execute/')
-@api.doc(description='Wildcards supported ie: TV* will execute all tasks with TV in the name')
-class ExecutionAPI(APIResource):
-    @api.validate(execute_api_task_schema)
-    @api.response(400, 'invalid options specified')
-    @api.response(200, 'List of tasks queued for execution')
-    def post(self, session=None):
-        """ Execute task(s) """
-        options = request.json or {}
-
-        options_string = options.pop('options_string', '')
-        if options_string:
-            try:
-                options['options'] = get_parser('execute').parse_args(options_string, raise_errors=True)
-            except ValueError as e:
-                return {'error': 'invalid options_string specified: %s' % e.message}, 400
-
-        tasks = [{'id': task_id, 'name': task_name}
-                 for task_id, task_name, task_event
-                 in self.manager.execute(options=options)]
-
-        return {'tasks': tasks}
-
-
-class ExecuteQueue(Queue):
+class ExecuteLog(Queue):
     """ Supports task log streaming by acting like a file object """
+
     def write(self, s):
         self.put(json.dumps({'log': s}))
 
@@ -110,47 +105,43 @@ stream_parser = api.parser()
 stream_parser.add_argument('progress', type=bool, required=False, default=True, help='Include task progress updates')
 stream_parser.add_argument('summary', type=bool, required=False, default=True, help='Include task summary')
 stream_parser.add_argument('log', type=bool, required=False, default=False, help='Include execution log')
-stream_parser.add_argument('entry_dump', type=bool, required=False, default=False, help='Include dump of entries including fields')
+stream_parser.add_argument('entry_dump', type=bool, required=False, default=False,
+                           help='Include dump of entries including fields')
 
 _streams = {}
 
 
-@execution_api.route('/execute/stream/')
+@execution_api.route('/execute/<task_name>/')
 @api.doc(description='Wildcards supported ie: TV* will execute all tasks with TV in the name')
-class ExecutionAPIStream(APIResource):
-    @api.validate(execute_api_task_schema)
-    @api.response(400, 'invalid options specified')
-    @api.response(200, 'Execution stream with task progress and/or log')
+class ExecutionAPI(APIResource):
+    @api.response(404, 'task not found')
+    @api.response(200, 'Execution task with optional progress and/or log stream', execution_api_result_schema)
     @api.doc(parser=stream_parser)
-    def post(self, session=None):
-        """ Execute task(s) and stream results """
-        options = request.json or {}
+    def get(self, task_name, session=None):
+        """ Execute task and stream results """
         args = stream_parser.parse_args()
 
-        options_string = options.pop('options_string', '')
-        if options_string:
-            try:
-                options['options'] = get_parser('execute').parse_args(options_string, raise_errors=True)
-            except ValueError as e:
-                return {'error': 'invalid options_string specified: %s' % e.message}, 400
+        if task_name.lower() not in [task.lower() for task in self.manager.user_config.get('tasks', {}).iterkeys()]:
+            return {'error': 'task does not exist'}, 404
 
-        queue = ExecuteQueue()
+        queue = ExecuteLog()
         output = queue if args['log'] else None
+        stream = True if any(
+            arg[0] in ['progress', 'summary', 'log', 'entry_dump'] for arg in args.iteritems() if arg[1]) else False
 
-        tasks_queued = []
+        task_id, __, task_event = self.manager.execute(options={'tasks': [task_name]}, output=output)[0]
 
-        for task_id, task_name, task_event in self.manager.execute(options=options, output=output):
-            tasks_queued.append({'id': task_id, 'name': task_name, 'event': task_event})
-            _streams[task_id] = {
-                'queue': queue,
-                'last_update': datetime.now(),
-                'args': args
-            }
+        if not stream:
+            return {'task': {'id': task_id, 'name': task_name}}
+
+        _streams[task_id] = {
+            'queue': queue,
+            'last_update': datetime.now(),
+            'args': args
+        }
 
         def stream_response():
-            # First return the tasks to execute
-            yield '{"stream": ['
-            yield json.dumps({'tasks': [{'id': task['id'], 'name': task['name']} for task in tasks_queued]}) + ',\n'
+            yield '{"task": {"id": "%s", "name": "%s", "stream": [' % (task_id, task_name)
 
             while True:
                 # If the server is shutting down then end the stream nicely
@@ -163,11 +154,11 @@ class ExecutionAPIStream(APIResource):
                 except Empty:
                     pass
 
-                if queue.empty() and all([task['event'].is_set() for task in tasks_queued]):
-                    for task in tasks_queued:
-                        del _streams[task['id']]
+                if queue.empty() and task_event.is_set():
+                    del _streams[task_id]
                     break
-            yield '{}]}'
+            yield '{}]}}'
+
         return Response(stream_response(), mimetype='text/event-stream')
 
 
@@ -181,6 +172,7 @@ class EntryDecoder(JSONEncoder):
         except TypeError:
             return str(o)
 
+
 _phase_percents = {
     'input': 5,
     'metainfo': 10,
@@ -193,7 +185,6 @@ _phase_percents = {
 
 
 def update_stream(task, status='pending'):
-
     if task.current_phase in _phase_percents:
         task.stream['percent'] = _phase_percents[task.current_phase]
 
@@ -204,42 +195,41 @@ def update_stream(task, status='pending'):
         'percent': task.stream.get('percent', 0)
     }
 
-    task.stream['queue'].put(json.dumps({'progress': {task.id: progress}}))
+    task.stream['queue'].put(json.dumps({'progress': progress}))
 
 
 @event('task.execute.started')
 def start_task(task):
     task.stream = _streams.get(task.id)
 
-    if task.stream:
+    if task.stream and task.stream['args']['progress']:
         update_stream(task, status='running')
 
 
 @event('task.execute.completed')
 def finish_task(task):
     if task.stream:
-        update_stream(task, status='complete')
+        if task.stream['args']['progress']:
+            update_stream(task, status='complete')
 
         if task.stream['args']['entry_dump']:
             entries = [entry.store for entry in task.entries]
-            task.stream['queue'].put(EntryDecoder().encode({'entry_dump': {task.id: entries}}))
+            task.stream['queue'].put(EntryDecoder().encode({'entry_dump': entries}))
 
         if task.stream['args']['summary']:
             task.stream['queue'].put(json.dumps({
                 'summary': {
-                    task.id: {
-                        'accepted': len(task.accepted),
-                        'rejected': len(task.rejected),
-                        'failed': len(task.failed),
-                        'undecided': len(task.undecided),
-                        'aborted': task.aborted,
-                        'abort_reason': task.abort_reason,
-                    }
+                    'accepted': len(task.accepted),
+                    'rejected': len(task.rejected),
+                    'failed': len(task.failed),
+                    'undecided': len(task.undecided),
+                    'aborted': task.aborted,
+                    'abort_reason': task.abort_reason,
                 }
             }))
 
 
 @event('task.execute.before_plugin')
 def track_progress(task, plugin_name):
-    if task.stream:
+    if task.stream and task.stream['args']['progress']:
         update_stream(task, status='running')
