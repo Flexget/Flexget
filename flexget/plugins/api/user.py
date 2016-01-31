8e9ff5011801e7db90c8a750df278ef54e761c53
Or Carmi <or.carmi82@gmail.com>
2016-01-31 12:10:06 +0200

    Added change password method

--- flexget/plugins/api/user.py
+++ flexget/plugins/api/user.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, division, absolute_import
 import datetime
 from math import ceil
 
+from flask.ext.login import login_user, LoginManager, current_user, current_app
 from flask import jsonify
 from flask import request
 from flask_restplus import inputs
@@ -10,6 +11,27 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from flexget.api import api, APIResource
 from flexget.plugins.filter import series
+from flexget.webserver import change_password
+import requests
 
 user_api = api.namespace('user', description='Manage user login credentials')
 
+user_password_input = {
+     'type': 'object',
+    'properties': {
+        'password': {'type': 'string'}
+    }
+}
+user_password_input_schema = api.schema('user_password_input', user_password_input)
+
+@user_api.route('/')
+@api.doc('Change user password')
+class UserManagementAPI(APIResource):
+    @api.validate(model=user_password_input_schema, description='test')
+    def put(self, session=None):
+        """ Change user password """
+        user = current_user
+        data = request.json
+        change_password(user=user, password=data.get('password'), session=session)
+        return {'status': 'success',
+                'message': 'Successfully changed user password'}
