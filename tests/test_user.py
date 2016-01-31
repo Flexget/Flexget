1b5d5db460459d27bc8d2c012cadc43495971807
Or Carmi <or.carmi82@gmail.com>
2016-01-31 16:16:41 +0200

    Added tests

---
+++ tests/test_user.py
@@ -0,0 +1,22 @@
+from flexget.utils import json
+from tests.test_api import APITest
+
+
+class TestUserAPI(APITest):
+    def test_change_password(self):
+        weak_password = {'password': 'weak'}
+        medium_password = {'password': 'a.better.password'}
+        strong_password = {'password': 'AVer123y$ron__g-=PaW[]rd'}
+
+        rsp = self.json_put('/user/', data=json.dumps(weak_password))
+        assert rsp.status_code == 400
+
+        rsp = self.json_put('/user/', data=json.dumps(medium_password))
+        assert rsp.status_code == 200
+
+        rsp = self.json_put('/user/', data=json.dumps(strong_password))
+        assert rsp.status_code == 200
+
+    def test_change_token(self):
+        rsp = self.get('user/token/')
+        assert rsp.status_code == 200
