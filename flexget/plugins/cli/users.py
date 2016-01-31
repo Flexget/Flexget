adc4245585e9138dab68fc1832123bec0aa42c30
Or Carmi <or.carmi82@gmail.com>
2016-01-31 12:44:22 +0200

    Removed unnecessary method from user operations

--- flexget/plugins/cli/users.py
+++ flexget/plugins/cli/users.py
@@ -18,34 +18,6 @@ def do_cli(manager, options, session=None):
     if hasattr(options, 'user'):
         options.user = options.user.lower()
 
-    if options.action == 'list':
-        users = get_users(session=session)
-        if users:
-            max_width = len(max([user.name for user in users], key=len)) + 4
-            console('_' * (max_width + 56 + 9))
-            console('| %-*s | %-*s |' % (max_width, 'Username', 56, 'API Token'))
-            if users:
-                for user in users:
-                    console('| %-*s | %-*s |' % (max_width, user.name, 56, user.token))
-        else:
-            console('No users found')
-
-    if options.action == 'add':
-        exists = user_exist(name=options.user, session=session)
-        if exists:
-            console('User %s already exists' % options.user)
-            return
-        user = add_user(name=options.user, password=options.password)
-        console('Added %s to the database with generated API Token: %s' % (user.name, user.token))
-
-    if options.action == 'delete':
-        user = user_exist(name=options.user, session=session)
-        if not user:
-            console('User %s does not exist' % options.user)
-            return
-        delete_user(user_name=user.name, session=session)
-        console('Deleted user %s' % options.user)
-
     if options.action == 'passwd':
         user = user_exist(name=options.user, session=session)
         if not user:
@@ -68,15 +40,6 @@ def do_cli(manager, options, session=None):
 def register_parser_arguments():
     parser = options.register_command('users', do_cli, help='Manage users providing access to the web server')
     subparsers = parser.add_subparsers(dest='action', metavar='<action>')
-
-    subparsers.add_parser('list', help='List users')
-
-    add_parser = subparsers.add_parser('add', help='add a new user')
-    add_parser.add_argument('user', metavar='<username>', help='Users login')
-    add_parser.add_argument('password', metavar='<password>', help='Users password')
-
-    del_parser = subparsers.add_parser('delete', help='delete a new user')
-    del_parser.add_argument('user', metavar='<username>', help='Login to delete')
 
     pwd_parser = subparsers.add_parser('passwd', help='change password for user')
     pwd_parser.add_argument('user', metavar='<username>', help='User to change password')
--- flexget/webserver.py
+++ flexget/webserver.py
@@ -213,27 +213,8 @@ class WebServer(threading.Thread):
 
 
 @with_session
-def get_users(session=None):
-    return session.query(User).all()
-
-
-@with_session
 def user_exist(name, session=None):
     return session.query(User).filter(User.name == name).first()
-
-
-@with_session
-def add_user(name, password, session=None):
-    user = User(name=name, password=password)
-    session.add(user)
-    session.commit()
-    return user
-
-
-@with_session
-def delete_user(user_name, session=None):
-    user = user_exist(name=user_name, session=session)
-    session.delete(user)
 
 
 @with_session
