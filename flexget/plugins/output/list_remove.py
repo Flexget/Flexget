7fa1972cd98ddbcae84046dc95d190c9fe99e9d2
Chase Sterling <chase.sterling@gmail.com>
2016-02-09 18:57:06 -0500

    Add list_remove and list_sync plugins

---
+++ flexget/plugins/output/list_remove.py
@@ -0,0 +1,34 @@
+import logging
+
+from flexget import plugin
+from flexget.event import event
+
+log = logging.getLogger('list_remove')
+
+
+class ListRemove(object):
+    schema = {
+        'type': 'array',
+        'items': {
+            'allOf': [
+                {'$ref': '/schema/plugins?group=list'},
+                {
+                    'maxProperties': 1,
+                    'error_maxProperties': 'Plugin options within list_remove plugin must be indented 2 more spaces '
+                                           'than the first letter of the plugin name.',
+                    'minProperties': 1
+                }
+            ]
+        }
+    }
+
+    def on_task_output(self, task, config):
+        for item in config:
+            for plugin_name, plugin_config in item.iteritems():
+                thelist = plugin.get_plugin_by_name(plugin_name).instance.get_list(plugin_config)
+                thelist -= task.accepted
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(ListRemove, 'list_remove', api_ver=2)
