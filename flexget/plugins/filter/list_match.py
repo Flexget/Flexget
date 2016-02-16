78de0f549a54a26b296d6c574dcd0920a462e061
Or Carmi <or.carmi82@gmail.com>
2016-02-16 18:03:00 +0200

    Added remove_on_match flag to list_match

--- flexget/plugins/filter/list_match.py
+++ flexget/plugins/filter/list_match.py
@@ -5,25 +5,40 @@ from flexget.event import event
 
 log = logging.getLogger('list_match')
 
+ITEMS_SCHEMA = {'type': 'array',
+                'items':
+                    {'allOf': [
+                        {'$ref': '/schema/plugins?group=list'},
+                        {
+                            'maxProperties': 1,
+                            'error_maxProperties': 'Plugin options within list_queue plugin must be indented 2 more spaces '
+                                                   'than the first letter of the plugin name.',
+                            'minProperties': 1
+                        }
+                    ]
+                    }
+                }
+
 
 class ListMatch(object):
-    schema = {
-        'type': 'array',
-        'items': {
-            'allOf': [
-                {'$ref': '/schema/plugins?group=list'},
-                {
-                    'maxProperties': 1,
-                    'error_maxProperties': 'Plugin options within list_queue plugin must be indented 2 more spaces '
-                                           'than the first letter of the plugin name.',
-                    'minProperties': 1
-                }
-            ]
-        }
-    }
+    schema = {'oneOf':
+                  [ITEMS_SCHEMA,
+                   {'type': 'object',
+                    'properties': {
+                        'items': ITEMS_SCHEMA,
+                        'remove_on_match': {'type': 'boolean'}
+                    }}
+                   ]
+              }
+
+    def prepare_config(self, config):
+        if isinstance(config, list):
+            config = {'items': config}
+            config.setdefault('remove_on_match', True)
 
     def on_task_filter(self, task, config):
-        for item in config:
+        config = self.prepare_config(config)
+        for item in config.get('items'):
             for plugin_name, plugin_config in item.iteritems():
                 thelist = plugin.get_plugin_by_name(plugin_name).instance.get_list(plugin_config)
                 for entry in task.all_entries:
@@ -31,7 +46,10 @@ class ListMatch(object):
                         entry.accept()
 
     def on_task_learn(self, task, config):
-        for item in config:
+        config = self.prepare_config(config)
+        if not config.get('remove_on_match'):
+            return
+        for item in config.get('items'):
             for plugin_name, plugin_config in item.iteritems():
                 thelist = plugin.get_plugin_by_name(plugin_name).instance.get_list(plugin_config)
                 thelist -= task.accepted
