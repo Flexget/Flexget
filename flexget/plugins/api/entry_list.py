50a1261c5e541713f202faf66f39d505d557de07
Or Carmi <or.carmi82@gmail.com>
2016-02-21 16:17:06 +0200

    Added initial entry_list API get method

---
+++ flexget/plugins/api/entry_list.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals, division, absolute_import
+
+import copy
+import logging
+
+from flask import jsonify
+
+from flexget.api import api, APIResource
+from flexget.plugins.list.entry_list import EntryList as el
+
+log = logging.getLogger('entry_list')
+
+entry_list_api = api.namespace('entry_list', description='Entry List operations')
+
+base_entry_model = {
+    'type': 'object',
+    'properties': {
+        'title': {'type': 'string'},
+        'url': {'type': 'string'},
+        'original_url': {'type': 'string'}
+    },
+    'additionalProperties': True
+}
+entry_return_model = copy.deepcopy(base_entry_model)
+entry_return_model['task'] = {'type': 'string'}
+entry_return_model['accepted_by'] = {'type': 'string'}
+
+entry_list_return_model = {
+    'type': 'object',
+    'properties': {
+        'entries': {'type': 'array', 'items': entry_return_model},
+        'number_of_entries': {'type': 'integer'},
+        'list_name': {'type': 'string'}
+    }
+}
+
+entry_list_return_model = api.schema('entry_list_return_model', entry_list_return_model)
+
+
+@entry_list_api.route('/<string:list_name>')
+@api.doc(params={'list_name': 'Name of the list'})
+class EntryListAPI(APIResource):
+    @api.response(200, model=entry_list_return_model)
+    def get(self, list_name, session=None):
+        ''' Get Entry list entries '''
+        entries = [entry.to_dict() for entry in el.get_list(list_name)]
+        return jsonify({'entries': entries,
+                        'number_of_entries': len(entries),
+                        'list_name': list_name})
