97a8f0f694b217ac1c741f572c1b699d109c25e4
Or Carmi <or.carmi82@gmail.com>
2016-02-22 10:42:41 +0200

    Fixed schema validation

--- flexget/plugins/api/movie_list.py
+++ flexget/plugins/api/movie_list.py
@@ -2,9 +2,10 @@ from __future__ import unicode_literals, division, absolute_import
 
 import copy
 import logging
+
 from flask import jsonify, request
+
 from flexget.api import api, APIResource
-from flexget.entry import Entry
 from flexget.plugins.api.movie_queue import empty_response
 from flexget.plugins.list.movie_list import PluginMovieList as ml
 
@@ -21,36 +22,39 @@ base_movie_entry = {
         'movie_year': {'type': 'integer'}
     },
     'additionalProperties': True,
-    'allOf': [
-        {'required': ['url']},
-        {'oneOf': [
-            {'required': ['title']},
-            {'required': ['movie_name', 'movie_year']}
-        ]}
+    'required': ['url'],
+    'anyOf': [
+        {'required': ['title']},
+        {'required': ['movie_name', 'movie_year']}
     ]
+
 }
 
 return_movie_entry = copy.deepcopy(base_movie_entry)
 return_movie_entry['properties']['id'] = {'type': 'integer'}
 return_movie_entry['properties']['list_name'] = {'type': 'string'}
 
-movie_list_return_model = {
-   'type': 'object',
+return_movie_list = {
+    'type': 'object',
     'properties': {
-        'entries': {'type': 'array', 'items': return_movie_entry},
-        'number_of_entries': {'type': 'integer'},
+        'movies': {
+            'type': 'array',
+            'items': return_movie_entry
+        },
+        'number_of_movies': {'type': 'integer'},
         'list_name': {'type': 'string'}
     }
 }
 
-base_movie_entry = api.schema('base_movie_entry', base_movie_entry)
-return_movie_entry = api.schema('base_movie_entry', return_movie_entry)
-movie_list_return_model = api.schema('movie_list_return_model', movie_list_return_model)
+base_movie_entry_schema = api.schema('base_movie_entry', base_movie_entry)
+return_movie_entry_schema = api.schema('return_movie_entry_schema', return_movie_entry)
+movie_list_return_schema = api.schema('movie_list_return_model', return_movie_list)
+
 
 @movie_list_api.route('/<string:list_name>')
 @api.doc(params={'list_name': 'Name of the list'})
 class MovieListAPI(APIResource):
-    @api.response(200, model=movie_list_return_model)
+    @api.response(code_or_apierror=200, model=movie_list_return_schema)
     def get(self, list_name, session=None):
         ''' Get Movie list entries '''
         # TODO Pagination
@@ -59,8 +63,8 @@ class MovieListAPI(APIResource):
                         'number_of_entries': len(movies),
                         'list_name': list_name})
 
-    @api.validate(base_movie_entry)
-    @api.response(201, model=return_movie_entry)
+    @api.validate(base_movie_entry_schema)
+    @api.response(201, model=return_movie_entry_schema)
     @api.doc(description="This will create a new list if list name doesn't exist")
     def post(self, list_name, session=None):
         ''' Adds a movie to the list. '''
@@ -70,7 +74,7 @@ class MovieListAPI(APIResource):
         movie = movies.add(data, session=session)
         return movie, 201
 
-    @api.validate(base_movie_entry)
+    @api.validate(base_movie_entry_schema)
     @api.response(200, model=empty_response)
     def delete(self, list_name, session=None):
         ''' Remove an movie from the list '''
