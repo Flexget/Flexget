0b8dde74fe30f5b77c1562084611e1f438a2ce1f
Or Carmi <or.carmi82@gmail.com>
2016-02-23 15:26:05 +0200

    Tweaked return objects

--- flexget/plugins/api/movie_list.py
+++ flexget/plugins/api/movie_list.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, division, absolute_import
 
+import copy
 import logging
 
 from flask import jsonify
@@ -37,16 +38,20 @@ input_movie_entry = {
         {'required': ['movie_name', 'movie_year']}
     ]
 }
-
-movie_list_id_object = {
+input_movie_list_id_object = {
     'type': 'object',
     'properties': {
-        'id': {'type': 'integer'},
         'id_name': {'type': 'string'},
         'id_value': {'type': 'string'},
-        'movie_id': {'type': 'integer'}
-    }
+    },
+    'required': ['id_name', 'id_value'],
+    'additionalProperties': False
 }
+
+return_movie_list_id_object = copy.deepcopy(input_movie_list_id_object)
+return_movie_list_id_object['properties']['id'] = {'type': 'integer'}
+return_movie_list_id_object['properties']['movie_id'] = {'type': 'integer'}
+
 
 movie_object = {
     'type': 'object',
@@ -56,7 +61,7 @@ movie_object = {
         'list_id': {'type': 'integer'},
         'movie_list_ids': {
             'type': 'array',
-            'items': movie_list_id_object
+            'items': return_movie_list_id_object
         },
     }
 }
@@ -82,7 +87,9 @@ return_movies = {
 return_lists = {'type': 'array', 'items': list_object}
 
 input_movie_entry_schema = api.schema('input_movie_entry', input_movie_entry)
-movie_list_id_object_schema = api.schema('movie_list_id_object', movie_list_id_object)
+input_movie_list_id_object = api.schema('input_movie_list_id_object', input_movie_list_id_object)
+
+movie_list_id_object_schema = api.schema('movie_list_id_object', return_movie_list_id_object)
 movie_object_schema = api.schema('movie_object', movie_object)
 list_object_schema = api.schema('list_object', list_object)
 return_lists_schema = api.schema('return_lists', return_lists)
@@ -118,7 +125,3 @@ class MovieListMoviesAPI(APIResource):
                     'message': 'list_id %d does not exist' % list_id}, 404
         movies = [movie.to_dict() for movie in list.movies]
         return jsonify({'movies': movies})
-
-
-
-
