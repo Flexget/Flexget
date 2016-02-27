236761bc22aa6f5d79860a663cfbf5d664ae0f07
Kristen Mills <kristen@kristen-mills.com>
2016-02-27 11:06:38 -0800

    Pulled data in seen controller

--- flexget/ui/src/plugins/seen/seen.controller.js
+++ flexget/ui/src/plugins/seen/seen.controller.js
@@ -5,7 +5,17 @@
     .module('flexget.plugins.seen')
     .controller('seenController', seenController);
 
-  function seenController($scope) {
+  function seenController($http) {
     var vm = this;
+
+    vm.title = 'Seen';
+
+    $http.get('/api/seen')
+      .success(function handleSeen(data) {
+        vm.entries = data.seen_entries;
+      })
+      .error(function handlerSeenError(data) {
+        // log error
+      });
   }
 })();
