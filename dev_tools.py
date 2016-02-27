ac556dcc07c4d9e4ac7d7a52b8e905ff6f965f26
Chase Sterling <chase.sterling@gmail.com>
2016-02-27 17:36:01 -0500

    Add dev_tools.py upgrade_deps command.

--- dev_tools.py
+++ dev_tools.py
@@ -3,7 +3,6 @@ import fileinput
 import os
 import shutil
 import subprocess
-import sys
 
 import click
 
@@ -81,6 +80,15 @@ def build_webui():
     # Build the ui
     click.echo('running `gulp buildapp`')
     subprocess.check_call('gulp buildapp', cwd=cwd, shell=True)
+
+
+@cli.command()
+def upgrade_deps():
+    try:
+        import pip
+    except ImportError:
+        raise click.ClickException('FATAL: Unable to import pip, please install it and run this again!')
+    pip.main(['install', '--upgrade', '-r', 'requirements.txt'])
 
 
 if __name__ == '__main__':
