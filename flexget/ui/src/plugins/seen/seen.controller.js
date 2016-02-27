5b20d6fa7e81ce96e11fe06b8e290afc900d1a76
Kristen Mills <kristen@kristen-mills.com>
2016-02-26 21:21:13 -0800

    Start of the seen page

---
+++ flexget/ui/src/plugins/seen/seen.controller.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('flexget.plugins.seen')
+    .controller('seenController', seenController);
+
+  function seenController($scope) {
+    var vm = this;
+  }
+})();
