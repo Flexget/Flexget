1ea89f6ac5fe29ffcbdaa2130d37cd96960e3fc6
tarzasai <ggelardi@gmail.com>
2016-02-28 23:48:23 +0100

    One less stupid issue

--- flexget/plugins/output/uoccin_processors.py
+++ flexget/plugins/output/uoccin_processors.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals, division, absolute_import
-from datetime import datetime
 import logging
 import os
 import re
 import shutil
+import time
 
 from flexget import plugin
 from flexget.entry import Entry
@@ -234,7 +234,7 @@ class UoccinWriter(object):
         if not os.path.exists(my_folder):
             os.makedirs(my_folder)
         # define the filename for the outgoing diff file
-        ts = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1000)
+        ts = int(time.time()*1000)
         fn = '%d.%s.diff' % (ts, config['uuid'])
         UoccinWriter.out_queue = os.path.join(my_folder, fn)
     
@@ -254,7 +254,7 @@ class UoccinWriter(object):
             os.remove(UoccinWriter.out_queue)
     
     def append_command(self, target, title, field, value):
-        ts = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1000)
+        ts = int(time.time()*1000)
         line = '%d|%s|%s|%s|%s\n' % (ts, target, title, field, value)
         with open(UoccinWriter.out_queue, 'a') as f:
             f.write(line)
