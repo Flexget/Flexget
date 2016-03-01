7f3c82632ad88e9f943e3f509584354d8ab7ea22
Tom Matheussen <tommatheussen5@hotmail.com>
2016-03-01 19:38:41 +0100

    [update] Pagination
    Updated pagination directive, fixing the issue of too much buttons
    showing up. Right now it's hardcoded to 5 buttons, will prob be
    configurable later on.

--- flexget/ui/src/directives/material-pagination/material-pagination.directive.js
+++ flexget/ui/src/directives/material-pagination/material-pagination.directive.js
@@ -23,23 +23,16 @@
   }
 
   function pagingLink(scope, element, attributes) {
-    scope.$watchCollection('[total, pageSize]', function(newValue, oldValue) {
-      if(newValue[0] && newValue[1] && newValue != oldValue) {
-        init(scope, attributes);
+    scope.$watch('page', function(newValue, oldValue) {
+      if(newValue && newValue != oldValue) {
+        updateButtons(scope, attributes);
       }
     })
   }
 
-  function init(scope, attrs) {
-    if(!scope.pageSize || scope.pageSize <= 0) {
-      scope.pageSize = 1;
-    }
-
-    var pageCount = Math.ceil(scope.total / scope.pageSize);
-
-    scope.stepList = [];
-
-    for(var i = 1; i <= pageCount; i++) {
+  function addRange(start, end, scope) {
+    var i = 0;
+    for(i = start; i <= end; i++) {
       scope.stepList.push({
         value: i,
         activeClass: scope.activeClass,
@@ -58,6 +51,34 @@
     scope.pagingAction({
       index: page
     });
+  }
+
+  function updateButtons(scope) {
+    var pageCount = Math.ceil(scope.total / scope.pageSize);
+
+    scope.stepList = [];
+
+    var cutOff = 5;
+
+    if(pageCount <= cutOff) {
+      addRange(1, pageCount, scope);
+    } else {
+      // Check if page is in the first 3
+      // Then we don't have to shift the numbers left, otherwise we get 0 and -1 values
+      if(scope.page - 2 < 2) {
+        addRange(1, 5, scope);
+
+      // Check if page is in the last 3
+      // Then we don't have to shift the numbers right, otherwise we get higher values without any results
+      } else if(scope.page + 2 > pageCount) {
+        addRange(pageCount - 4, pageCount, scope);
+
+      // If page is not in the start of end
+      // Then we add 2 numbers to each side of the current page
+      } else {
+        addRange(scope.page - 2, scope.page + 2, scope);
+      }
+    }
   }
 
 })();
