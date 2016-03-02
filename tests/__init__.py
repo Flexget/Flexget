57fc3d3006bb8a0f9b8acbe2973d283d41e3c7f2
Chase Sterling <chase.sterling@gmail.com>
2016-03-01 19:14:32 -0500

    Convert use_vcr to a fixture

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,20 +9,12 @@ import logging
 
 import mock
 from nose.plugins.attrib import attr
-from vcr import VCR
 
 import flexget.logger
 from flexget.task import Task, TaskAbort
 from tests import util
 
 log = logging.getLogger('tests')
-
-VCR_CASSETTE_DIR = os.path.join(os.path.dirname(__file__), 'cassettes')
-VCR_RECORD_MODE = os.environ.get('VCR_RECORD_MODE', 'once')
-
-vcr = VCR(cassette_library_dir=VCR_CASSETTE_DIR, record_mode=VCR_RECORD_MODE)
-test_arguments = None
-plugins_loaded = False
 
 
 def setup_logging_level():
@@ -36,44 +28,6 @@ def setup_logging_level():
 
     logging.getLogger().setLevel(level)
     return level
-
-
-def use_vcr(func=None, **kwargs):
-    """
-    Decorator for test functions which go online. A vcr cassette will automatically be created and used to capture and
-    play back online interactions. The nose 'vcr' attribute will be set, and the nose 'online' attribute will be set on
-    it based on whether it might go online.
-
-    The record mode of VCR can be set using the VCR_RECORD_MODE environment variable when running tests. Depending on
-    the record mode, and the existence of an already recorded cassette, this decorator will also dynamically set the
-    nose 'online' attribute.
-
-    Keyword arguments to :func:`vcr.VCR.use_cassette` can be supplied.
-    """
-    if func is None:
-        # When called with kwargs, e.g. @use_vcr(inject_cassette=True)
-        return functools.partial(use_vcr, **kwargs)
-    module = func.__module__.split('tests.')[-1]
-    class_name = inspect.stack()[1][3]
-    cassette_name = '.'.join([module, class_name, func.__name__])
-    kwargs.setdefault('path', cassette_name)
-    cassette_path = os.path.join(VCR_CASSETTE_DIR, cassette_name)
-    online = True
-    # Set our nose online attribute based on the VCR record mode
-    if vcr.record_mode == 'none':
-        online = False
-    elif vcr.record_mode == 'once':
-        online = not os.path.exists(cassette_path)
-    func = attr(online=online, vcr=True)(func)
-    # If we are not going online, disable domain limiting during test
-    if not online:
-        func = mock.patch('flexget.utils.requests.limit_domains', new=mock.MagicMock())(func)
-    if VCR_RECORD_MODE == 'off':
-        return func
-    else:
-        return vcr.use_cassette(**kwargs)(func)
-
-
 
 
 def build_parser_function(parser_name):
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ import sys
 import yaml
 from contextlib import contextmanager
 
+import mock
 import pytest
+from vcr import VCR
 
 import flexget.logger
 from flexget.manager import Manager
@@ -127,3 +129,46 @@ class MockManager(Manager):
         # We don't want to silently swallow crash reports during unit tests
         log.error('Crash Report Traceback:', exc_info=True)
         raise CrashReport('Crash report created during unit test, check log for traceback.')
+
+
+VCR_CASSETTE_DIR = os.path.join(os.path.dirname(__file__), 'cassettes')
+VCR_RECORD_MODE = os.environ.get('VCR_RECORD_MODE', 'once')
+
+vcr = VCR(cassette_library_dir=VCR_CASSETTE_DIR, record_mode=VCR_RECORD_MODE)
+
+
+@pytest.yield_fixture()
+def use_vcr(request):
+    """
+    Decorator for test functions which go online. A vcr cassette will automatically be created and used to capture and
+    play back online interactions. The nose 'vcr' attribute will be set, and the nose 'online' attribute will be set on
+    it based on whether it might go online.
+
+    The record mode of VCR can be set using the VCR_RECORD_MODE environment variable when running tests. Depending on
+    the record mode, and the existence of an already recorded cassette, this decorator will also dynamically set the
+    nose 'online' attribute.
+
+    Keyword arguments to :func:`vcr.VCR.use_cassette` can be supplied.
+    """
+    module = request.module.__name__.split('tests.')[-1]
+    class_name = request.cls.__name__
+    cassette_name = '.'.join([module, class_name, request.function.__name__])
+    cassette_path = os.path.join(VCR_CASSETTE_DIR, cassette_name)
+    online = True
+    # Set our nose online attribute based on the VCR record mode
+    if vcr.record_mode == 'none':
+        online = False
+    elif vcr.record_mode == 'once':
+        online = not os.path.exists(cassette_path)
+    # TODO: Don't know if this is right, do some testing.
+    pytest.mark.online(request)
+    # If we are not going online, disable domain limiting during test
+    if not online:
+        request.function = mock.patch('flexget.utils.requests.limit_domains', new=mock.MagicMock())(request.function)
+    if VCR_RECORD_MODE == 'off':
+        yield None
+    else:
+        with vcr.use_cassette(path=cassette_path) as cassette:
+            yield cassette
+
+
