c143b1f03f940a1707b5e25d5f085cb2957d32d7
Steven Adams <stevezau@gmail.com>
2016-03-04 07:26:16 +1100

    fix test_uoccin

--- tests/test_uoccin.py
+++ tests/test_uoccin.py
@@ -1,11 +1,10 @@
 from __future__ import unicode_literals, division, absolute_import
-from tests import FlexGetBase
 import os
 
 
-class TestUoccinReader(FlexGetBase):
+class TestUoccinReader(object):
 
-    __yaml__ = """
+    config = """
         templates:
           global:
             disable: [seen]
@@ -14,7 +13,7 @@ class TestUoccinReader(FlexGetBase):
           test_sync:
             filesystem:
               path:
-                - 'uoccin/device.flexget_test'
+                - '__tmp__'
               regexp: '.*\.diff$'
             uoccin_reader:
               uuid: flexget_test
@@ -38,42 +37,34 @@ class TestUoccinReader(FlexGetBase):
 1431198108565|series|272135.2.5|subtitles|eng,ita
     """
 
-    def __init__(self):
-        super(TestUoccinReader, self).__init__()
-        dir = os.path.join('uoccin', 'device.flexget_test')
-        try:
-            os.makedirs(dir)
-        except:
-            pass
-        with open(os.path.join(dir, '1431093328970.uoccin_test.diff'), 'w') as txt:
+    def test_read(self, execute_task, tmpdir):
+        with open(os.path.join(tmpdir.strpath, '1431093328970.uoccin_test.diff'), 'w') as txt:
             txt.write(TestUoccinReader.__diff__.strip())
-
-    def test_read(self):
-        self.execute_task('test_sync')
-        self.execute_task('test_sync_chk')
-        entry = self.task.find_entry(imdb_id='tt0047034')
+        execute_task('test_sync')
+        task = execute_task('test_sync_chk')
+        entry = task.find_entry(imdb_id='tt0047034')
         assert entry.get('uoccin_watchlist', False), \
             'Expected uoccin_watchlist:True for movie "Godzilla", found %s instead.' % entry.get('uoccin_watchlist')
-        entry = self.task.find_entry(imdb_id='tt0057012')
+        entry = task.find_entry(imdb_id='tt0057012')
         assert entry.get('uoccin_collected', False), \
             'Expected uoccin_collected:True for movie "Dr. Strangelove", found %s instead.' % entry.get('uoccin_collected')
-        entry = self.task.find_entry(tvdb_id='80379', series_season=8, series_episode=24)
+        entry = task.find_entry(tvdb_id='80379', series_season=8, series_episode=24)
         assert entry.get('uoccin_watchlist', False), \
             'Expected uoccin_watchlist:True for series "The Big Bang Theory", found %s instead.' % entry.get('uoccin_watchlist')
         assert 'pippo' in entry.get('uoccin_tags', None), \
             'Expected "pippo" in uoccin_tags for series "The Big Bang Theory", found %s instead.' % entry.get('uoccin_tags')
         assert entry.get('uoccin_watched', False), \
             'Expected uoccin_watched:True for episode "The Big Bang Theory S08E24", found %s instead.' % entry.get('uoccin_watched')
-        entry = self.task.find_entry(tvdb_id='272135', series_season=2, series_episode=5)
+        entry = task.find_entry(tvdb_id='272135', series_season=2, series_episode=5)
         assert entry.get('uoccin_collected', False), \
             'Expected uoccin_collected:True for episode "TURN S08E24", found %s instead.' % entry.get('uoccin_collected')
         assert 'ita' in entry.get('uoccin_subtitles', None), \
             'Expected "ita" in uoccin_subtitles for episode "TURN S02E05", found %s instead.' % entry.get('uoccin_subtitles')
 
 
-class TestUoccinWriters(FlexGetBase):
+class TestUoccinWriters(object):
 
-    __yaml__ = """
+    config = """
         templates:
           global:
             disable: [seen]
@@ -98,18 +89,18 @@ class TestUoccinWriters(FlexGetBase):
             uoccin_lookup: uoccin
     """
 
-    def test_write(self):
-        self.execute_task('test_del')
-        self.execute_task('test_chk')
-        entry = self.task.find_entry(imdb_id='tt0047034')
+    def test_write(self, execute_task):
+        execute_task('test_del')
+        task = execute_task('test_chk')
+        entry = task.find_entry(imdb_id='tt0047034')
         assert entry is None or entry.get('uoccin_watchlist', True) == False, \
             'Expected uoccin_watchlist:False for movie "Godzilla", found %s instead.' % entry.get('uoccin_watchlist')
-        entry = self.task.find_entry(imdb_id='tt0057012')
+        entry = task.find_entry(imdb_id='tt0057012')
         assert entry is None or entry.get('uoccin_collected', True) == False, \
             'Expected uoccin_collected:False for movie "Dr. Strangelove", found %s instead.' % entry.get('uoccin_collected')
-        entry = self.task.find_entry(tvdb_id='80379', series_season=8, series_episode=24)
+        entry = task.find_entry(tvdb_id='80379', series_season=8, series_episode=24)
         assert entry is None or entry.get('uoccin_watched', True) == False, \
             'Expected uoccin_watched:False for episode "The Big Bang Theory S08E24", found %s instead.' % entry.get('uoccin_watched')
-        entry = self.task.find_entry(tvdb_id='272135', series_season=2, series_episode=5)
+        entry = task.find_entry(tvdb_id='272135', series_season=2, series_episode=5)
         assert entry is None or entry.get('uoccin_collected', True) == False, \
             'Expected uoccin_collected:False for episode "TURN S02E05", found %s instead.' % entry.get('uoccin_collected')
