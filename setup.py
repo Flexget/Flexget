c13e0ddb0fb435ceda043d4098036b9001e75ae8
Chase Sterling <chase.sterling@gmail.com>
2016-03-04 00:23:33 -0500

    [change] Python 2.6 is no longer supported

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
