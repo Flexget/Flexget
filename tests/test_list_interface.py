33f50851580d5e849f1583512349489ed81dee6d
Or Carmi <or.carmi82@gmail.com>
2016-03-08 12:33:26 +0200

    Added tests

--- tests/test_list_interface.py
+++ tests/test_list_interface.py
@@ -11,6 +11,12 @@ class TestListInterface(object):
           list_get:
             entry_list: test_list
 
+          list_1_get:
+            entry_list: list 1
+
+          list_2_get:
+            entry_list: list 2
+
           test_list_add:
             mock:
               - {title: 'title 1', url: "http://mock.url/file1.torrent"}
@@ -18,6 +24,30 @@ class TestListInterface(object):
             accept_all: yes
             list_add:
               - entry_list: test_list
+
+          list_1_add:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+            accept_all: yes
+            list_add:
+              - entry_list: list 1
+
+          list_2_add:
+            mock:
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            accept_all: yes
+            list_add:
+              - entry_list: list 2
+
+          test_multiple_list_add:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+            accept_all: yes
+            list_add:
+              - entry_list: list 1
+              - entry_list: list 2
 
           test_list_accept_with_remove:
             mock:
@@ -35,6 +65,26 @@ class TestListInterface(object):
             list_accept:
               lists:
                 - entry_list: test_list
+              remove_on_accept: no
+
+          test_multiple_list_accept_with_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              - entry_list: list 1
+              - entry_list: list 2
+
+          test_multiple_list_accept_without_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              lists:
+                - entry_list: list 1
+                - entry_list: list 2
               remove_on_accept: no
 
           test_list_remove:
@@ -57,6 +107,16 @@ class TestListInterface(object):
         assert len(task.entries) == 2
 
         task = execute_task('list_get')
+        assert len(task.entries) == 2
+
+    def test_multiple_list_add(self, execute_task):
+        task = execute_task('test_multiple_list_add')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_1_get')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_get')
         assert len(task.entries) == 2
 
     def test_list_accept_with_remove(self, execute_task):
@@ -87,6 +147,50 @@ class TestListInterface(object):
         task = execute_task('list_get')
         assert len(task.entries) == 2
 
+    def test_multiple_list_accept_with_remove(self, execute_task):
+        task = execute_task('list_1_add')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_add')
+        assert len(task.entries) == 1
+
+        task = execute_task('list_1_get')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_get')
+        assert len(task.entries) == 1
+
+        task = execute_task('test_multiple_list_accept_with_remove')
+        assert len(task.accepted) == 3
+
+        task = execute_task('list_1_get')
+        assert len(task.entries) == 0
+
+        task = execute_task('list_2_get')
+        assert len(task.entries) == 0
+
+    def test_multiple_list_accept_without_remove(self, execute_task):
+        task = execute_task('list_1_add')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_add')
+        assert len(task.entries) == 1
+
+        task = execute_task('list_1_get')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_get')
+        assert len(task.entries) == 1
+
+        task = execute_task('test_multiple_list_accept_without_remove')
+        assert len(task.accepted) == 3
+
+        task = execute_task('list_1_get')
+        assert len(task.entries) == 2
+
+        task = execute_task('list_2_get')
+        assert len(task.entries) == 1
+
     def test_list_remove(self, execute_task):
         task = execute_task('test_list_add')
         assert len(task.entries) == 2
@@ -109,4 +213,3 @@ class TestListInterface(object):
 
         task = execute_task('test_list_reject')
         assert len(task.rejected) == 1
-
