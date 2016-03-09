5c3a1d6121f30814479b42bb1162b684daac5bf2
Tom Matheussen <Tommatheussen5@hotmail.com>
2016-03-09 12:40:41 +0100

    [fix][WebUI] Fixed forget series button #935
    Updated the forgetting of series in the webui, which was broken after
    the component refactoring

--- flexget/ui/src/plugins/series/components/series-show/series-show.component.js
+++ flexget/ui/src/plugins/series/components/series-show/series-show.component.js
@@ -9,16 +9,28 @@
       controller: seriesShowController,
       bindings: {
         show: '<',
-        forgetSeries: '<',
+        confirmDelete: '&'
       },
     });
 
-    function seriesShowController($state) {
+    function seriesShowController($state, $mdDialog) {
       var vm = this;
 
       vm.gotoEpisodes = function() {
         $state.go('flexget.episodes', { id: vm.show.show_id });
       };
+
+      vm.forgetSeries = function() {
+        var confirm = $mdDialog.confirm()
+          .title('Confirm forgetting show.')
+          .htmlContent("Are you sure you want to completely forget <b>" + vm.show.show_name + "</b>?")
+          .ok("Forget")
+          .cancel("No");
+
+        $mdDialog.show(confirm).then(function() {
+          vm.confirmDelete();
+        });
+      }
 
     }
 })();
--- flexget/ui/src/plugins/series/series.component.js
+++ flexget/ui/src/plugins/series/series.component.js
@@ -9,7 +9,7 @@
       controller: seriesController,
     });
 
-  function seriesController($http, $state, $mdDialog) {
+  function seriesController($http, $mdDialog) {
     var vm = this;
 
     var options = {
@@ -33,6 +33,17 @@
       });
     }
 
+    vm.confirmDelete = function(show) {
+      $http.delete('/api/series/' + show.show_id)
+        .success(function(data) {
+          var index = vm.series.indexOf(show);
+          vm.series.splice(index, 1);
+        })
+        .error(function(error) {
+          
+        })
+    }
+
     //Call from the pagination to update the page to the selected page
     vm.updateListPage = function(index) {
       options.page = index;
@@ -47,29 +58,7 @@
       });
     }
 
-    vm.forgetSeries = function(show) {
-      var confirm = $mdDialog.confirm()
-      .title('Confirm forgetting show.')
-      .htmlContent("Are you sure you want to completely forget <b>" + show.show_name + "</b>?")
-      .ok("Forget")
-      .cancel("No");
-
-      $mdDialog.show(confirm).then(function() {
-        $http.delete('/api/series/' + show.show_id)
-        .success(function(data) {
-          var index = vm.series.indexOf(show);
-          vm.series.splice(index, 1);
-        })
-        .error(function(error) {
-          var errorDialog = $mdDialog.alert()
-          .title("Something went wrong")
-          .htmlContent("Oops, something went wrong when trying to forget <b>" + show.show_name + "</b>:\n" + error.message)
-          .ok("Ok");
-
-          $mdDialog.show(errorDialog);
-        })
-      });
-    }
+    
 
     //Load initial list of series
     getSeriesList();
