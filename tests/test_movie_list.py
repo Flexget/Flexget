8c7cf0af5a87d52c65ed77f52bf301404053e663
Or Carmi <or.carmi82@gmail.com>
2016-03-09 11:49:15 +0200

    Started movie_list test

--- tests/test_movie_list.py
+++ tests/test_movie_list.py
@@ -1,112 +1,215 @@
 from __future__ import unicode_literals, division, absolute_import
 
-from flexget.utils import json
 
+class TestListInterface(object):
+    config = """
+        templates:
+          global:
+            disable: [seen]
 
-class TestMovieListAPI(object):
-    config = 'tasks: {}'
+        tasks:
+          list_get:
+            movie_list: test_list
 
-    def test_movie_list_list(self, api_client):
-        # No params
-        rsp = api_client.get('/movie_list/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+          list_1_get:
+            movie_list: list 1
 
-        # Named param
-        rsp = api_client.get('/movie_list/?name=name')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+          list_2_get:
+            movie_list: list 2
 
-        payload = {'name': 'name'}
+          test_list_add:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+            accept_all: yes
+            list_add:
+              - movie_list: test_list
 
-        # Create list
-        rsp = api_client.json_post('/movie_list/', data=json.dumps(payload))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
+          list_1_add:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+            accept_all: yes
+            list_add:
+              - movie_list: list 1
 
-    def test_movie_list_list_id(self, api_client):
-        payload = {'name': 'name'}
+          list_2_add:
+            mock:
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            accept_all: yes
+            list_add:
+              - movie_list: list 2
 
-        # Create list
-        rsp = api_client.json_post('/movie_list/', data=json.dumps(payload))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
+          test_multiple_list_add:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+            accept_all: yes
+            list_add:
+              - movie_list: list 1
+              - movie_list: list 2
 
-        # Get list
-        rsp = api_client.get('/movie_list/1/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+          test_list_accept_with_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              - movie_list: test_list
 
-        # Delete list
-        rsp = api_client.delete('/movie_list/1/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+          test_list_accept_without_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              lists:
+                - movie_list: test_list
+              remove_on_accept: no
 
-    def test_movie_list_movies(self, api_client):
-        # Get non existent list
-        rsp = api_client.get('/movie_list/1/movies/')
-        assert rsp.status_code == 404, 'Response code is %s' % rsp.status_code
+          test_multiple_list_accept_with_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              - movie_list: list 1
+              - movie_list: list 2
 
-        payload = {'name': 'name'}
+          test_multiple_list_accept_without_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 2', url: "http://mock.url/file2.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_accept:
+              lists:
+                - movie_list: list 1
+                - movie_list: list 2
+              remove_on_accept: no
 
-        # Create list
-        rsp = api_client.json_post('/movie_list/', data=json.dumps(payload))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
+          test_list_remove:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+            accept_all: yes
+            list_remove:
+              - movie_list: test_list
 
-        identifier = {'imdb_id': 'tt1234567'}
-        movie_data = {'title': 'title',
-                      'original_url': 'http://test.com',
-                      'movie_identifiers': [identifier]}
+          test_list_reject:
+            mock:
+              - {title: 'title 1', url: "http://mock.url/file1.torrent"}
+              - {title: 'title 3', url: "http://mock.url/file3.torrent"}
+            list_reject:
+              - movie_list: test_list
+    """
 
-        # Add movie to list
-        rsp = api_client.json_post('/movie_list/1/movies/', data=json.dumps(movie_data))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
+    def test_list_add(self, execute_task):
+        task = execute_task('test_list_add')
+        assert len(task.entries) == 2
 
-        # Get movies from list
-        rsp = api_client.get('/movie_list/1/movies/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        task = execute_task('list_get')
+        assert len(task.entries) == 2
 
-        returned_identifier = json.loads(rsp.data)['movies'][0]['movies_list_ids'][0]
-        assert returned_identifier['id_name'], returned_identifier['id_value'] == identifier.items()[0]
-
-    def test_movie_list_movie(self, api_client):
-        payload = {'name': 'name'}
-
-        # Create list
-        rsp = api_client.json_post('/movie_list/', data=json.dumps(payload))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
-
-        identifier = {'imdb_id': 'tt1234567'}
-        movie_data = {'title': 'title',
-                      'original_url': 'http://test.com',
-                      'movie_identifiers': [identifier]}
-
-        # Add movie to list
-        rsp = api_client.json_post('/movie_list/1/movies/', data=json.dumps(movie_data))
-        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
-
-        # Get movies from list
-        rsp = api_client.get('/movie_list/1/movies/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
-        returned_identifier = json.loads(rsp.data)['movies'][0]['movies_list_ids'][0]
-        assert returned_identifier['id_name'], returned_identifier['id_value'] == identifier.items()[0]
-
-        # Get specific movie from list
-        rsp = api_client.get('/movie_list/1/movies/1/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
-        returned_identifier = json.loads(rsp.data)['movies_list_ids'][0]
-        assert returned_identifier['id_name'], returned_identifier['id_value'] == identifier.items()[0]
-
-        identifiers = [{'trakt_movie_id': '12345'}]
-
-        # Change specific movie from list
-        rsp = api_client.json_put('/movie_list/1/movies/1/', data=json.dumps(identifiers))
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
-        returned_identifier = json.loads(rsp.data)['movies_list_ids'][0]
-        assert returned_identifier['id_name'], returned_identifier['id_value'] == identifiers[0].items()
-
-        # Delete specific movie from list
-        rsp = api_client.delete('/movie_list/1/movies/1/')
-        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
-
-        # Get non existent movie from list
-        rsp = api_client.get('/movie_list/1/movies/1/')
-        assert rsp.status_code == 404, 'Response code is %s' % rsp.status_code
-
-        # Delete non existent movie from list
-        rsp = api_client.delete('/movie_list/1/movies/1/')
-        assert rsp.status_code == 404, 'Response code is %s' % rsp.status_code
+    # def test_multiple_list_add(self, execute_task):
+    #     task = execute_task('test_multiple_list_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_1_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_get')
+    #     assert len(task.entries) == 2
+    # 
+    # def test_list_accept_with_remove(self, execute_task):
+    #     task = execute_task('test_list_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('test_list_accept_with_remove')
+    #     assert len(task.all_entries) == 3
+    #     assert len(task.accepted) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 0
+    # 
+    # def test_list_accept_without_remove(self, execute_task):
+    #     task = execute_task('test_list_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('test_list_accept_without_remove')
+    #     assert len(task.all_entries) == 3
+    #     assert len(task.accepted) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 2
+    # 
+    # def test_multiple_list_accept_with_remove(self, execute_task):
+    #     task = execute_task('list_1_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_add')
+    #     assert len(task.entries) == 1
+    # 
+    #     task = execute_task('list_1_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_get')
+    #     assert len(task.entries) == 1
+    # 
+    #     task = execute_task('test_multiple_list_accept_with_remove')
+    #     assert len(task.accepted) == 3
+    # 
+    #     task = execute_task('list_1_get')
+    #     assert len(task.entries) == 0
+    # 
+    #     task = execute_task('list_2_get')
+    #     assert len(task.entries) == 0
+    # 
+    # def test_multiple_list_accept_without_remove(self, execute_task):
+    #     task = execute_task('list_1_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_add')
+    #     assert len(task.entries) == 1
+    # 
+    #     task = execute_task('list_1_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_get')
+    #     assert len(task.entries) == 1
+    # 
+    #     task = execute_task('test_multiple_list_accept_without_remove')
+    #     assert len(task.accepted) == 3
+    # 
+    #     task = execute_task('list_1_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_2_get')
+    #     assert len(task.entries) == 1
+    # 
+    # def test_list_remove(self, execute_task):
+    #     task = execute_task('test_list_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('test_list_remove')
+    #     assert len(task.accepted) == 1
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 1
+    # 
+    # def test_list_reject(self, execute_task):
+    #     task = execute_task('test_list_add')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('list_get')
+    #     assert len(task.entries) == 2
+    # 
+    #     task = execute_task('test_list_reject')
+    #     assert len(task.rejected) == 1
