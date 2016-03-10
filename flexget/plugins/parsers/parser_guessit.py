885741da73a80d8b9ff4e90b7442e832c880b063
Steven Adams <stevezau@gmail.com>
2016-03-11 10:51:39 +1100

    Use future.utils.native before sending to guessit as it expects unicode on PY2 and str on PY3

--- flexget/plugins/parsers/parser_guessit.py
+++ flexget/plugins/parsers/parser_guessit.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals, division, absolute_import
 from builtins import str
 from builtins import object
 
+from future.utils import native
+
 import datetime
 import logging
 import re
@@ -293,7 +295,8 @@ class ParserGuessit(object):
         guessit_options = self._guessit_options(kwargs)
         guessit_options['type'] = 'movie'
         guess_result = guessit_api.guessit(data, options=guessit_options)
-        parsed = GuessitParsedMovie(data, kwargs.pop('name', None), guess_result, **kwargs)
+        # NOTE: Guessit expects str on PY3 and unicode on PY2 hence the use of future.utils.native
+        parsed = GuessitParsedMovie(native(data), kwargs.pop('name', None), guess_result, **kwargs)
         end = time.clock()
         log.debug('Parsing result: %s (in %s ms)', parsed, (end - start) * 1000)
         return parsed
@@ -314,7 +317,9 @@ class ParserGuessit(object):
         parse_type = 'episode' if kwargs.get('name') else None
         if parse_type:
             guessit_options['type'] = parse_type
-        guess_result = guessit_api.guessit(data, options=guessit_options)
+
+        # NOTE: Guessit expects str on PY3 and unicode on PY2 hence the use of future.utils.native
+        guess_result = guessit_api.guessit(native(data), options=guessit_options)
         parsed = GuessitParsedSerie(data, kwargs.pop('name', None), guess_result, **kwargs)
         end = time.clock()
         log.debug('Parsing result: %s (in %s ms)', parsed, (end - start) * 1000)
