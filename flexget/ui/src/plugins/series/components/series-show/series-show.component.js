23b8c94160bb7719d4293e516e104064f12600b7
Tom Matheussen <Tommatheussen5@hotmail.com>
2016-03-16 16:01:12 +0100

    [Add] Series begin in webUI
    Added the option to set series begin in the cards. Will need some more
    validation and handling.

--- flexget/ui/src/plugins/series/components/series-show/series-show.component.js
+++ flexget/ui/src/plugins/series/components/series-show/series-show.component.js
@@ -19,5 +19,20 @@
       vm.gotoEpisodes = function() {
         $state.go('flexget.episodes', { id: vm.show.show_id });
       };
+
+      vm.setBegin = function(ev) {
+        $mdDialog.show({
+          controller: 'seriesBeginController',
+          controllerAs: 'vm',
+          templateUrl: 'plugins/series/components/series-begin/series-begin.tmpl.html',
+          locals: {
+            showId: vm.show.show_id
+          }
+        }).then(function(data) {
+          vm.show.begin_episode = data;
+        }, function(err) {
+          console.log(err);
+        });
+      }
     }
 })();
