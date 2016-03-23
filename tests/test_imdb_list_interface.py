8e9a11eff0cd382ca9e9b44465e2f45b6dec424c
Or Carmi <or.carmi82@gmail.com>
2016-03-23 12:06:18 +0200

    Added imdb_list remove test

--- tests/test_imdb_list_interface.py
+++ tests/test_imdb_list_interface.py
@@ -27,3 +27,19 @@ class TestIMDBList(object):
         imdb_set.add(entry)
 
         assert entry in imdb_set
+
+    def test_imdb_list_remove(self):
+        imdb_set = ImdbEntrySet(self.imdb_config)
+        # Clearing existing list
+        imdb_set.clear()
+
+        entry = Entry(title='the matrix', imdb_id='tt0133093')
+
+        assert entry not in imdb_set
+        imdb_set.add(entry)
+
+        assert entry in imdb_set
+
+        imdb_set.remove(entry)
+        assert entry not in imdb_set
+
