6927d400ee0d887fce5b7be75ddbc73b3a156783
Tom Matheussen <tommatheussen5@hotmail.com>
2016-03-25 17:47:33 +0100

    Added dialog for more details to error toast

--- flexget/ui/src/components/error/error.service.js
+++ flexget/ui/src/components/error/error.service.js
@@ -5,40 +5,43 @@
         .factory('errorService', errorService);
 
     function errorService($mdToast, $mdDialog) {
+        var toast = {
+            templateUrl: 'components/error/toast.tmpl.html',
+            position: 'bottom right',
+            controller: toastController,
+            controllerAs: 'vm'
+        }
+
+        var dialog = {
+            templateUrl: 'components/error/dialog.tmpl.html',
+            controller: dialogController,
+            controllerAs: 'vm'
+        }
+
         return {
-            showToast: function(vars) {
-                var toast = {
-                    templateUrl: 'components/error/toast.tmpl.html',
-                    position: 'bottom right',
-                    controller: function() {
-                        var vm = this;
-
-                        vm.text = "Damnit Flexget, you had one job!";
-                        /*vm.test = vars.text;
-
-                        vm.openMoreInfo = function(e) {
-                            $mdDialog
-                              .show($mdDialog
-                                .alert()
-                                .title('More info goes here.')
-                                .textContent('Something witty.')
-                                .ariaLabel('More info')
-                                .ok('Got it')
-                                .targetEvent(e)
-                              )
-                              .then(function() {
-                                isDlgOpen = false;
-                              })
-                        };*/
-
-
-                    },
-                    controllerAs: 'vm'
-                }
-
+            showToast: function() {           
                 $mdToast.show(toast);
             }
         }
+
+        
+        function dialogController() {
+            var vm = this;
+
+            vm.close = function() {
+                $mdDialog.hide();
+            }
+        }
+
+        function toastController() {
+            var vm = this;
+
+            vm.text = "Damnit Flexget, you had one job!";
+
+            vm.openDetails = function() {
+                $mdDialog.show(dialog);
+            }
+        };
     }
 
 })();
