cf382fd7e837a1e9ce3af1ea3b982a01da86ee70
cvium <clausvium@gmail.com>
2016-03-31 20:30:56 +0200

    changed filesystem fields to datetime obj and improved the login in age

--- flexget/plugins/filter/age.py
+++ flexget/plugins/filter/age.py
@@ -18,7 +18,7 @@ class Age(object):
         Example:
 
           age:
-            field: 'access'  # 'access' is a field set from filesystem plugin
+            field: 'accessed'  # 'accessed' is a field set from filesystem plugin
             age: '7 days'
             action: 'accept'
     """
@@ -41,17 +41,31 @@ class Age(object):
                 entry.fail('Field {0} does not exist'.format(field))
                 continue
 
-            val = entry[field]
+            field_value = entry[field]
+
+            if isinstance(field_value, datetime):
+                field_date = field_value
+            elif isinstance(field_value, float):
+                field_date = datetime.fromtimestamp(field_value)
+            elif isinstance(field_value, str):
+                try:
+                    field_date = dateutil_parse(entry[field])
+                except ValueError as e:
+                    log.warning('Entry %s ignored: %s is not a valid date', entry['title'], field_value)
+                    continue
+            else:
+                log.warning('Entry %s ignored: %s is not a valid date', entry['title'], field_value)
+                continue
+
             age_cutoff = datetime.now() - parse_timedelta(config['age'])
 
-            field_date = datetime.fromtimestamp(val) if isinstance(val, float) else dateutil_parse(val)
             if field_date < age_cutoff:
-                info_string = 'Date in field {0} is older than {1}'.format(field, config['age'])
+                info_string = 'Date in field `{0}` is older than {1}'.format(field, config['age'])
                 if config['action'] == 'accept':
                     entry.accept(info_string)
                 else:
                     entry.reject(info_string)
-                log.debug('Entry %s was %sed because date in field %s is older than %s', entry['title'],
+                log.debug('Entry %s was %sed because date in field `%s` is older than %s', entry['title'],
                           config['action'], field, config['age'])
 
 
--- flexget/plugins/input/filesystem.py
+++ flexget/plugins/input/filesystem.py
@@ -119,9 +119,9 @@ class Filesystem(object):
         except Exception as e:
             log.warning('Error setting timestamp for %s: %s' % (filepath, e))
             entry['timestamp'] = None
-        entry['access'] = filepath.getatime()
-        entry['modified'] = filepath.getmtime()
-        entry['created'] = filepath.getctime()
+        entry['accessed'] = 10 #datetime.fromtimestamp(filepath.getatime())
+        entry['modified'] = datetime.fromtimestamp(filepath.getmtime())
+        entry['created'] = datetime.fromtimestamp(filepath.getctime())
         if entry.isvalid():
             if test_mode:
                 log.info("Test mode. Entry includes:")
