54cee007c593d29f7e4f4b81d295ac5aab3246a3
Roland Bortnyák <antivirtel@gmail.com>
2016-04-02 12:58:32 +0200

    Lower loglevels of movie-queue IMDB/TMDB lookup fails to Verbose, avoiding logspam.

--- flexget/plugins/filter/movie_queue.py
+++ flexget/plugins/filter/movie_queue.py
@@ -164,7 +164,7 @@ class MovieQueue(queue_base.FilterQueueBase):
             if conditions:
                 break
         if not conditions:
-            log_once('IMDB and TMDB lookups failed for %s.' % entry['title'], log, logging.WARN)
+            log_once('IMDB and TMDB lookups failed for %s.' % entry['title'], log, logging.VERBOSE)
             return
 
         quality = entry.get('quality', qualities.Quality())
