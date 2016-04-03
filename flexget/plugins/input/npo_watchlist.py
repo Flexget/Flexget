2a72d39b14aa447bf13b1fe9b437dbb680d69507
Or Carmi <or.carmi82@gmail.com>
2016-04-03 13:39:51 +0300

    Added npo to list group

--- flexget/plugins/input/npo_watchlist.py
+++ flexget/plugins/input/npo_watchlist.py
@@ -133,4 +133,4 @@ class NPOWatchlist(object):
             
 @event('plugin.register')
 def register_plugin():
-    plugin.register(NPOWatchlist, 'npo_watchlist', api_ver=2)
+    plugin.register(NPOWatchlist, 'npo_watchlist', api_ver=2, groups=['list'])
