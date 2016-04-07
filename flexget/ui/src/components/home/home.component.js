adec651c87101143111cd2224127805ec3145e9f
Tom Matheussen <tommatheussen5@hotmail.com>
2016-04-07 22:03:14 +0200

    Removed testing buttons

--- flexget/ui/src/components/home/home.component.js
+++ flexget/ui/src/components/home/home.component.js
@@ -3,22 +3,6 @@
   angular
     .module('flexget.home')
     .component('home', {
-      templateUrl: 'components/home/home.tmpl.html',
-      controllerAs: 'vm',
-      controller: homeController,
+      templateUrl: 'components/home/home.tmpl.html'
     })
-
-    function homeController(errorService) {
-    	var vm = this;
-
-    	vm.openToast = function() {
-    		console.log(errorService);
-
-    		var params = {
-    			text: "Test"
-    		}
-
-    		errorService.showToast(params);
-    	}
-    }
 })();
