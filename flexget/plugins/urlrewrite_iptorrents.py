4c680e5cceb4669642af2642a0cb7693f80e4927
Ian Easton <ijeaston@gmail.com>
2016-04-17 19:51:37 -0400

    Nested td tags make it difficult to pinpoint the element where the size data is. Hopefully the bounding on the regular expressions will prevent any false positives.

--- flexget/plugins/urlrewrite_iptorrents.py
+++ flexget/plugins/urlrewrite_iptorrents.py
@@ -151,8 +151,8 @@ class UrlRewriteIPTorrents(object):
                 entry['search_sort'] = torrent_availability(entry['torrent_seeds'],
                                                             entry['torrent_leeches'])
 
-                size = torrent.findNext("td", text=re.compile('([\.\d]+) ([GMK]?)B')).text
-                size = re.search('([\.\d]+) ([GMK]?)B', size)
+                size = torrent.findNext(text=re.compile('^([\.\d]+) ([GMK]?)B$'))
+                size = re.search('^([\.\d]+) ([GMK]?)B$', size)
                 if size:
                     if size.group(2) == 'G':
                         entry['content_size'] = int(float(size.group(1)) * 1000 ** 3 / 1024 ** 2)
