b399db4657bcc38de03ad2527842cd5095518dbb
Steven Adams <stevezau@gmail.com>
2016-04-30 14:31:57 +1000

    cleanup imports

--- flexget/plugins/parsers/__init__.py
+++ flexget/plugins/parsers/__init__.py
@@ -1,3 +1,1 @@
-from __future__ import unicode_literals, division, absolute_import
-from builtins import *  # pylint: disable=unused-import, redefined-builtin
 from .parser_common import ParseWarning, SERIES_ID_TYPES
