43c7614e68be8fafae1e4762e68f5f5fc7a40707
Tom Matheussen <tmatheussen@avia-gis.com>
2016-05-24 16:59:36 +0200

    Added serving of specs in the browser, still needs reloading and autorerunning functions

---
+++ flexget/ui/test-helpers/mockModule.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular
+  .module('flexget', [
+    'ui.router',
+    'ngMaterial',
+    'ngCookies',
+    'ngMessages',
+    'angular-loading-bar',
+    'flexget.components',
+    'flexget.directives',
+    'flexget.services',
+    'angular-cache'
+  ]);
+})();
