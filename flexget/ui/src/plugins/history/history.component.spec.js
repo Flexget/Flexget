522713964a29b696c32b3104af3f280b821d5a7b
Tom Matheussen <tmatheussen@avia-gis.com>
2016-05-24 15:34:46 +0200

    Updated the history tests, checking the HTTP requests now, returning mock data

--- flexget/ui/src/plugins/history/history.component.spec.js
+++ flexget/ui/src/plugins/history/history.component.spec.js
@@ -1,44 +1,55 @@
-describe("Plugin: History.component", function() {
-   var controller;
-   var routeObj;
-   
-   beforeEach(function() {
-        bard.appModule('flexget.plugins.history', function($provide) {
-            $provide.provider('route', function() {
-                this.$get = function() {
-                    //var register  = sinon.spy('register');
+describe("Plugin: History.component", function () {
+	var controller;
+	var history = mockHistoryData.getMockHistory();
 
-                    function register() {};                   
+	beforeEach(function () {
+		bard.appModule('flexget.plugins.history', function ($provide) {
+			$provide.provider('route', function () {
+				this.$get = function () {
 
-                    return {
-                        register: register
-                    }
-                }
-            });
-            
-            $provide.service('sideNav', function() {
-                this.register = function() {};
-            });
-        });
-        
-        bard.inject('$componentController', '$rootScope', '$http', '$httpBackend', 'route', 'sideNav');
-   });
-   
-   beforeEach(function() {
-       $httpBackend.expectGET('/api/history/').respond(200, { entries: "test" });
-       
-       controller = $componentController('historyView');
-   });   
-   
-   it("should exist", function() {
-       expect(controller).to.exist;
-   });
-   
-   describe("activation", function() {
-       it("should get entries", function() {
-            $httpBackend.flush();
-            
-            expect(controller.entries).to.exist;
-       });
-   })
+					function register() { };
+
+					return {
+						register: register
+					}
+				}
+			});
+
+			$provide.service('sideNav', function () {
+				this.register = function () { };
+			});
+		});
+
+		bard.inject('$componentController', '$rootScope', '$http', '$httpBackend', 'route', 'sideNav', '$q');
+	});
+
+	beforeEach(function () {
+		controller = $componentController('historyView');
+	});
+
+	it("should exist", function () {
+		expect(controller).to.exist;
+	});
+
+	describe("activation", function () {
+		beforeEach(function () {
+			controller.$onInit();
+		});
+
+		it("should get entries", function () {
+			$httpBackend.expectGET('/api/history/').respond(200, history);
+
+			$httpBackend.flush();
+
+			expect(controller.entries).to.exist;
+		});
+
+		it("should pass when the API call fails", function () {
+			$httpBackend.expectGET('/api/history/').respond(400);
+
+			$httpBackend.flush();
+
+			expect(true).to.be.true;
+		});
+	});
 });
