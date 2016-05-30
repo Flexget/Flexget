bdad8a43ccd273f20a8cc708f02aa392e8348f57
Tom Matheussen <tommatheussen5@hotmail.com>
2016-05-30 18:10:28 +0200

    Made the releases dialog prettier, also added close option other than the ESC button

--- flexget/ui/src/plugins/series/components/episode-releases/episode-releases.component.js
+++ flexget/ui/src/plugins/series/components/episode-releases/episode-releases.component.js
@@ -2,32 +2,33 @@
     'use strict';
 
     angular
-    .module('flexget.plugins.series')
-    .component('episodeReleases', {
-        templateUrl: 'plugins/series/components/episode-releases/episode-releases.tmpl.html',
-        controllerAs: 'vm',
-        controller: episodesReleasesController,
-        bindings: {
-            show: '<',
-            episode: '<',
-            releases: '<'
-        },
-    });
+		.module('flexget.plugins.series')
+		.component('episodeReleases', {
+			templateUrl: 'plugins/series/components/episode-releases/episode-releases.tmpl.html',
+			controllerAs: 'vm',
+			controller: episodesReleasesController,
+			bindings: {
+				show: '<',
+				episode: '<',
+				releases: '<'
+			},
+		});
 
 
     function episodesReleasesController($http, $filter, $mdDialog, seriesService) {
         var vm = this;
+		vm.cancel = cancel;
 
         loadReleases();
 
         //Call from a release item, to reset the release
-        vm.resetRelease = function(release) {
+        vm.resetRelease = function (release) {
 
             var confirm = $mdDialog.confirm()
-            .title('Confirm resetting a release')
-            .htmlContent("Are you sure you want to reset the release <b>" + release.release_title + "</b>?")
-            .ok("reset")
-            .cancel("No");
+				.title('Confirm resetting a release')
+				.htmlContent("Are you sure you want to reset the release <b>" + release.release_title + "</b>?")
+				.ok("reset")
+				.cancel("No");
 
             $mdDialog.show(confirm).then(function () {
                 seriesService.resetRelease(vm.show, vm.episode, release).then(function (data) {
@@ -39,12 +40,12 @@
         }
 
         //Call from a release item, to forget the release
-        vm.forgetRelease = function(release) {
+        vm.forgetRelease = function (release) {
             var confirm = $mdDialog.confirm()
-            .title('Confirm forgetting a release')
-            .htmlContent("Are you sure you want to delete the release <b>" + release.release_title + "</b>?")
-            .ok("Forget")
-            .cancel("No");
+				.title('Confirm forgetting a release')
+				.htmlContent("Are you sure you want to delete the release <b>" + release.release_title + "</b>?")
+				.ok("Forget")
+				.cancel("No");
 
             $mdDialog.show(confirm).then(function () {
                 seriesService.forgetRelease(vm.show, vm.episode, release).then(function (data) {
@@ -53,22 +54,25 @@
                     vm.releases.splice(index, 1);
 
                     vm.episode.episode_number_of_releases -= 1;
-                    if(vm.releases.length == 0) {
+                    if (vm.releases.length == 0) {
                         vm.releases = undefined;
                     }
                 })
             })
         }
 
-
         function loadReleases() {
             $http.get('/api/series/' + vm.show.show_id + '/episodes/' + vm.episode.episode_id + '/releases')
-            .success(function(data) {
-                vm.releases = data.releases;
+				.success(function (data) {
+					vm.releases = data.releases;
 
-            }).error(function(error) {
-                console.log(error);
-            });
+				}).error(function (error) {
+					console.log(error);
+				});
         }
+
+		function cancel() {
+			$mdDialog.cancel();
+		}
     }
 })();
