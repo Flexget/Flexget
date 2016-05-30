b4900928951e3db84c02ddc2132df23b824ca07c
Tom Matheussen <tommatheussen5@hotmail.com>
2016-05-30 18:55:55 +0200

    Added createList method to tests

--- flexget/ui/src/components/exception/exception.service.js
+++ flexget/ui/src/components/exception/exception.service.js
@@ -5,14 +5,14 @@
     .module('flexget.components.exception')
     .factory('exception', exception);
 
-  function exception($q) {
+  function exception($q, $log) {
     var service = {
       catcher: catcher
     };
     return service;
 
     function catcher(error) {
-		console.log(error.data.message);
+		$log.log(error.data.message);
 
 //TODO: Make this better: show toasts with the message, and popup with more info
 // Also, when this gets implemented, stub out the functions in the tests so it doesn't call all the log or toast functions		
