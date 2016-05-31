c44172a44f7000aabf4cd197c3de516534ed2f99
Tom Matheussen <tommatheussen5@hotmail.com>
2016-05-31 21:42:16 +0200

    Added tests for the urlInterceptor block

--- flexget/ui/src/blocks/urlInterceptor/urlInterceptor.service.spec.js
+++ flexget/ui/src/blocks/urlInterceptor/urlInterceptor.service.spec.js
@@ -2,12 +2,36 @@ describe("Blocks: UrlInterceptor", function () {
 	beforeEach(function () {
 		bard.appModule('blocks.urlInterceptor');
 
-		bard.inject('urlInterceptor');
+		bard.inject('urlInterceptor', '$httpBackend');
 
 		//sinon.spy(exception, 'catcher');
 	});
 
 	it("should exist", function () {
 		expect(urlInterceptor).to.exist;
+	});
+
+	describe('request()', function () {
+		it("should exist", function () {
+			expect(urlInterceptor.request).to.exist;
+		});
+
+		it('should append a trailing slash to the request', function () {
+			var config = urlInterceptor.request({ url: '/api/movie_list' });
+
+			expect(config.url).to.equal('/api/movie_list/');
+		});
+
+		it('should not append a trailing slash when one is present already', function () {
+			var config = urlInterceptor.request({ url: '/api/movie_list/' });
+
+			expect(config.url).to.equal('/api/movie_list/');
+		});
+
+		it('should not append a trailing slash to resource requests', function () {
+			var config = urlInterceptor.request({ url: '/movies.tmpl.html' });
+
+			expect(config.url).to.equal('/movies.tmpl.html');
+		});
 	});
 });
