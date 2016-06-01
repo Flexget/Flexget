b71e14bea14e973a64552616d07e6f9a3c6a12e5
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-01 23:41:55 +0200

    Added toast message in the exception catcher, also added tests for the exception block

--- flexget/ui/src/blocks/exception/exception.service.spec.js
+++ flexget/ui/src/blocks/exception/exception.service.spec.js
@@ -1,10 +1,15 @@
 describe("Blocks: Exception", function () {
+	var mockErrorResponse = {
+		status: 500,
+		data: {
+			message: "Failure"
+		}
+	}
+	
 	beforeEach(function () {
 		bard.appModule('blocks.exception');
 
-		bard.inject('exception');
-
-		//sinon.spy(exception, 'catcher');
+		bard.inject('exception', 'errorService', '$rootScope');
 	});
 
 	it("should exist", function () {
@@ -14,6 +19,18 @@ describe("Blocks: Exception", function () {
 	describe('catcher()', function () {
 		it('should exist', function () {
 			expect(exception.catcher).to.exist;
+		});
+
+		it('should tell the errorService to open a toast', function () {
+			sinon.stub(errorService, 'showToast');
+
+			exception.catcher(mockErrorResponse).catch(function (err) {
+				expect(err).to.equal(mockErrorResponse);
+			});
+
+			$rootScope.$apply();
+
+			expect(errorService.showToast).to.have.been.calledOnce;
 		});
 	});
 });
