064c9c310d73af7c2ec91c54aba7ee228ddcc516
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-03 19:50:25 +0200

    Fixed tests for the 404 Router

--- flexget/ui/src/components/404/404.route.spec.js
+++ flexget/ui/src/components/404/404.route.spec.js
@@ -1,40 +1,35 @@
 describe('404 Routes: ', function () {
 	
     beforeEach(function () {
-		//Create abstract parent state first
-		//TODO: create funcion for this, so we can just call the function and not need to inject the entire block everywhere
-		module('ui.router', function ($stateProvider) {
-			$stateProvider.state('flexget', { abstract: true });
-		});
 		module('components.404');
 		bard.inject('$state', '$templateCache', '$rootScope', '$location');
     });
 
     it("should map state 'flexget.404' to url #/", function () {
-		expect($state.href('flexget.404', {})).to.equal('#/404');
+		expect($state.href('404', {})).to.equal('#/404');
     });
 	
 	it("should map state route to the 'notFound' component", function () {
-		expect($state.get('flexget.404').component).to.equal('notFound');
+		expect($state.get('404').component).to.equal('notFound');
 	});
 	
 	describe("Transitions", function() {
 		it("should work with $state.go", function () {
-			$state.go('flexget.404');
+			$state.go('404');
 			$rootScope.$apply();
-			expect($state.is('flexget.404')).to.be.true;
+			expect($state.is('404')).to.be.true;
 		});
 		
 		it("should work with '/404' path", function() {
 			$location.path('/404');
 			$rootScope.$apply();
-			expect($state.is('flexget.404')).to.be.true;
+			expect($state.is('404')).to.be.true;
 		});
 		
 		it("should work with '/unkown' path", function() {
 			$location.path('/unkown');
 			$rootScope.$apply();
-			expect($state.is('flexget.404')).to.be.true;
+			expect($state.is('404')).to.be.true;
 		});
 	});
 });
