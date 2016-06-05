e2b0fc84b6d57742f96d8f46aa79734b83e62973
Or Carmi <or.carmi82@gmail.com>
2016-06-05 13:21:11 +0300

    Added check to see if obj is None to avoid crash

--- flexget/utils/json.py
+++ flexget/utils/json.py
@@ -23,7 +23,6 @@ except ImportError:
         except ImportError:
             raise DependencyError(missing='simplejson')
 
-
 DATE_FMT = '%Y-%m-%d'
 ISO8601_FMT = '%Y-%m-%dT%H:%M:%SZ'
 
@@ -32,6 +31,9 @@ class DTDecoder(json.JSONDecoder):
     def decode(self, obj, **kwargs):
         # The built-in `json` library will `unicode` strings, except for empty strings. patch this for
         # consistency so that `unicode` is always returned.
+        if obj is None:
+            return
+
         if obj == b'':
             return ''
 
