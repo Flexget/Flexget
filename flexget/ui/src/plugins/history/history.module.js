ff8808f836084b26e6f144b5dcd6ecd5a999da55
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-06 20:55:37 +0200

    Added a route for all plugins, also refactored plugins modules

--- flexget/ui/src/plugins/history/history.module.js
+++ flexget/ui/src/plugins/history/history.module.js
@@ -1,13 +1,9 @@
 (function () {
     'use strict';
 
-    var historyModule = angular.module("flexget.plugins.history", ['angular.filter', 'flexget.components', 'flexget']);
-
-    registerPlugin(historyModule);
-
-    /*historyModule.run(function (route, sideNav) {
-        route.register('history', '/history', 'history-view');
-        sideNav.register('/history', 'History', 'fa fa-history', 30);
-    });*/
-
+    angular
+		.module("plugins.history", [
+			'blocks.router',
+			'angular.filter'
+		]);
 })();
--- flexget/ui/src/plugins/movies/movies.module.js
+++ flexget/ui/src/plugins/movies/movies.module.js
@@ -1,13 +1,6 @@
 (function () {
     'use strict';
 
-    var moviesModule = angular.module("flexget.plugins.movies", []);
-
-    registerPlugin(moviesModule);
-
-    /*moviesModule.run(function (route, sideNav) {
-        route.register('movies', '/movies', 'movies-view');
-        sideNav.register('/movies', 'Movies', 'fa fa-film', 50);
-    });*/
-
+    angular
+		.module("plugins.movies", []);
 })();
