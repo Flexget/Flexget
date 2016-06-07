71f278b05da94d287e49222476da9a5e66c90e19
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-07 22:07:22 +0200

    Added tests for the seen plugin

---
+++ flexget/ui/src/plugins/seen/components/seen-entry/seen-entry.component.spec.js
@@ -0,0 +1,17 @@
+describe("Plugin: Seen-entry.component", function () {
+	var controller;
+
+	beforeEach(function () {
+		bard.appModule('plugins.seen');
+
+		bard.inject('$componentController', '$rootScope');
+	});
+
+	beforeEach(function () {
+		controller = $componentController('seenEntry');
+	});
+
+	it("should exist", function () {
+		expect(controller).to.exist;
+	});
+});
