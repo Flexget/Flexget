7c6434990ce3a9a19970b8dc37dc41aed66b1a49
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-09 21:24:04 +0200

    Added tests for movie-entry

--- flexget/ui/tests-mock-data/movie_list.js
+++ flexget/ui/tests-mock-data/movie_list.js
@@ -4,7 +4,8 @@ var mockMovieListData = (function () {
 		getMovieListById: getMovieListById,
 		getMovieListMovies: getMovieListMovies,
 		getMovieListMovieById: getMovieListMovieById,
-		createMovieList: createMovieList
+		createMovieList: createMovieList,
+		getMovieMetadata: getMovieMetadata
 	};
 
 	function getMovieLists() {
@@ -201,6 +202,57 @@ var mockMovieListData = (function () {
 			"added_on": "Mon, 09 Jun 2016 09:32:42 GMT",
 			"id": 33,
 			"name": "New List"
+		}
+	}
+
+	function getMovieMetadata() {
+		return {
+			"cached_at": "Thu, 09 Jun 2016 21:22:10 GMT",
+			"genres": [
+				"action",
+				"adventure",
+				"fantasy"
+			],
+			"homepage": "http://www.warcraft-themovie.com/",
+			"id": 50145,
+			"images": {
+				"banner": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/banners/original/186ef347c8.jpg"
+				},
+				"clearart": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/cleararts/original/8ee8691096.png"
+				},
+				"fanart": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/fanarts/original/e8785015d5.jpg",
+					"medium": "https://walter.trakt.us/images/movies/000/050/145/fanarts/medium/e8785015d5.jpg",
+					"thumb": "https://walter.trakt.us/images/movies/000/050/145/fanarts/thumb/e8785015d5.jpg"
+				},
+				"logo": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/logos/original/41c3ac6bad.png"
+				},
+				"poster": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/posters/original/f327360575.jpg",
+					"medium": "https://walter.trakt.us/images/movies/000/050/145/posters/medium/f327360575.jpg",
+					"thumb": "https://walter.trakt.us/images/movies/000/050/145/posters/thumb/f327360575.jpg"
+				},
+				"thumb": {
+					"full": "https://walter.trakt.us/images/movies/000/050/145/thumbs/original/017439146d.jpg"
+				}
+			},
+			"imdb_id": "tt0803096",
+			"language": "en",
+			"overview": "The peaceful realm of Azeroth stands on the brink of war as its civilization faces a fearsome race of invaders: orc warriors fleeing their dying home to colonize another. As a portal opens to connect the two worlds, one army faces destruction and the other faces extinction. From opposing sides, two heroes are set on a collision course that will decide the fate of their family, their people, and their home.",
+			"rating": 7.36667,
+			"released": null,
+			"runtime": 123,
+			"slug": "warcraft-2016",
+			"tagline": "Two worlds. One home.",
+			"title": "Warcraft",
+			"tmdb_id": 68735,
+			"trailer": "http://youtube.com/watch?v=2Rxoz13Bthc",
+			"updated_at": "Sun, 05 Jun 2016 08:51:15 GMT",
+			"votes": 330,
+			"year": 2016
 		}
 	}
 })();
