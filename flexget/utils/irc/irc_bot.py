c69cf0b4b64a99d3e3e3523b8ce4eed55c3ceb9e
cvium <clausvium@gmail.com>
2016-06-15 10:11:08 +0200

    temporarily fix py3

--- flexget/utils/irc/irc_bot.py
+++ flexget/utils/irc/irc_bot.py
@@ -160,7 +160,8 @@ class IRCBot(asynchat.async_chat):
 
     def handle_error(self):
         exc_info = sys.exc_info()
-        raise exc_info[0], exc_info[1], exc_info[2]
+        if exc_info:
+            raise (exc_info[0], exc_info[1], exc_info[2])
         delay = min(self.connection_attempts ** 2, self.max_connection_delay)
         log.error('Unknown error occurred. Attempting to restart connection in %s seconds.', delay)
         self.schedule.queue_command(delay, functools.partial(self.reconnect), 'reconnect')
