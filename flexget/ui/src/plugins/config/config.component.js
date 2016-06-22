e7a7a56cfca6deb505a534eda1e4ec99a1916505
Tom Matheussen <tommatheussen5@hotmail.com>
2016-06-22 19:38:38 +0200

    Added saving and error showing of the config editor, also added theme selection for the config editor

--- flexget/ui/src/plugins/config/config.component.js
+++ flexget/ui/src/plugins/config/config.component.js
@@ -8,26 +8,41 @@
 			controller: configController,
 		});
 
-	function configController($http, base64) {
+	function configController($http, base64, $mdDialog) {
 		var vm = this;
-
+		
 		vm.aceOptions = {
-			mode: 'yaml'
+			mode: 'yaml',
+			theme: 'chrome'
 		};
+
+		var themelist = ace.require('ace/ext/themelist');
+		vm.themes = themelist.themes;
 
 		$http.get('/api/server/raw_config')
 			.then(function (response) {
 				var encoded = response.data.raw_config;
 				vm.config = base64.decode(encoded);
+				vm.origConfig = angular.copy(vm.config);
 			}, function (error) {
 				// log error
 				console.log(error);
 			});
 
 		vm.save = function () {
-			console.log(vm.config);
+			var encoded = base64.encode(vm.config);
+			$http.post('/api/server/raw_config', { raw_config: encoded })
+				.then(function (data) {
+					var dialog = $mdDialog.alert()
+						.title("Update success")
+						.ok("Ok")
+						.textContent("Your config file has been successfully updated")
 
+					$mdDialog.show(dialog);
+				}, function (error) {
+					vm.errors = error.data.errors;
+				});
 		}
-	}
+	};
 
 })();
