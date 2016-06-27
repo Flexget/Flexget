3119e26830cb515b106b44511052c6633bd96ecd
Or Carmi <or.carmi82@gmail.com>
2016-06-27 15:23:39 +0300

    [tweak] raw_config - Changed API return codes

--- flexget/api/server.py
+++ flexget/api/server.py
@@ -22,7 +22,7 @@ from pyparsing import nums, alphanums, printables
 from yaml.error import YAMLError
 
 from flexget._version import __version__
-from flexget.api import api, APIResource, ApiError, __version__ as __api_version__
+from flexget.api import api, APIResource, ApiError, __version__ as __api_version__, BadRequest
 
 log = logging.getLogger('api.server')
 
@@ -155,16 +155,17 @@ class ServerConfigAPI(APIResource):
 
     @api.validate(raw_config_schema)
     @api.response(200, description='Successfully updated config')
-    @api.response(501, model=yaml_error_schema, description='YAML syntax error')
-    @api.response(502, model=config_validation_schema, description='Config validation error')
+    @api.response(BadRequest)
+    @api.response(ApiError)
+    @api.doc(description='Config file must be base64 encoded. A backup will be created, and if successful config will'
+                         ' be loaded and saved to original file.')
     def post(self, session=None):
         """ Update config """
         data = request.json
         try:
             raw_config = base64.b64decode(data['raw_config'])
         except (TypeError, binascii.Error):
-            return {'status': 'error',
-                    'message': 'payload was not a valid base64 encoded string'}, 400
+            raise BadRequest(message='payload was not a valid base64 encoded string')
 
         try:
             config = yaml.safe_load(raw_config)
@@ -177,7 +178,7 @@ class ServerConfigAPI(APIResource):
                     error.update({'line': e.context_mark.line, 'column': e.context_mark.column})
                 if e.problem_mark is not None:
                     error.update({'line': e.problem_mark.line, 'column': e.problem_mark.column})
-                raise ApiError(message='Invalid YAML syntax', payload=error)
+                raise BadRequest(message='Invalid YAML syntax', payload=error)
 
         try:
             backup_path = self.manager.update_config(config)
@@ -186,7 +187,7 @@ class ServerConfigAPI(APIResource):
             for er in e.errors:
                 errors.append({'error': er.message,
                                'config_path': er.json_pointer})
-            raise ApiError(message='Error loading config: %s' % e.args[0], payload={'errors': errors})
+            raise BadRequest(message='Error loading config: %s' % e.args[0], payload={'errors': errors})
 
         try:
             self.manager.backup_config()
