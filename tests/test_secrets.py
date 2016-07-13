7db721172089b91309f4d5a506dc80d61a115d87
Or Carmi <or.carmi82@gmail.com>
2016-07-13 14:55:15 +0300

    Fixed test

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,14 +1,13 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
-from mock import patch
-
-from flexget.plugins.modify import config_secrets
+import pytest
 
 
+@pytest.mark.usefixtures('tmpdir')
 class TestSecrets(object):
     config = """
-        secrets: test_secret.yml
+        secrets: __tmp__/secret.yml
         tasks:
           test_secret:
             mock:
@@ -17,10 +16,7 @@ class TestSecrets(object):
               - '{{ secrets.test_secret }}': accept
     """
 
-    @patch.object(config_secrets, 'secrets_from_file')
-    def test_secret_from_file(self, mock_secret_file, execute_task):
-        mock_secret_file.return_value = {'test_secret': True}
-
+    @pytest.mark.filecopy('secret.yml', '__tmp__/secret.yml')
+    def test_secret_from_file(self, execute_task):
         task = execute_task('test_secret')
-        assert mock_secret_file.called
         assert len(task.accepted) == 1
