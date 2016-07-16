fef6d308870386fd931bee95be4c9aeece23492e
nitz <nitz.raz@gmail.com>
2016-07-16 13:05:25 +0300

    Fixed trakt episode collection
    Trakt episode collection is now being done directly using the "episodes"
    and not under "shows", which didn't work for me

--- tests/test_trakt_list_interface.py
+++ tests/test_trakt_list_interface.py
@@ -95,8 +95,8 @@ class TestTraktList(object):
         assert set(titles) == set(('The Walking Dead', 'Deadpool', 'Castle S08E15 Fidelis Ad Mortem'))
 
     def test_trakt_add(self):
-        trakt_set = TraktSet(self.trakt_config)
         # Initialize trakt set
+        trakt_set = TraktSet(self.trakt_config)
         trakt_set.clear()
 
         entry = Entry(title='White collar', series_name='White Collar (2009)')
@@ -106,6 +106,21 @@ class TestTraktList(object):
         trakt_set.add(entry)
         time.sleep(5)
         assert entry in trakt_set
+
+    def test_trakt_add_episode(self):
+        episode_config = self.trakt_config.copy()
+        episode_config['type'] = 'episodes'
+        trakt_set = TraktSet(episode_config)
+        # Initialize trakt set
+        trakt_set.clear()
+
+        entry = Entry(**{u'trakt_show_slug': u'game-of-thrones', u'original_url': u'http://trakt.tv/shows/game-of-thrones/seasons/4/episodes/5', u'url': u'http://trakt.tv/shows/game-of-thrones/seasons/4/episodes/5', u'series_season': 4, u'tvdb_id': 121361, u'series_name': u'Game of Thrones (2011)', u'imdb_id': u'tt0944947', u'series_id': u'S04E05', u'series_episode': 5, u'trakt_episode_id': 73674, u'title': u'Game of Thrones (2011) S04E05 First of His Name', u'trakt_show_id': 1390, u'trakt_ep_name': u'First of His Name', u'tvrage_id': 24493})
+
+        assert entry not in trakt_set
+
+        trakt_set.add(entry)
+        assert entry in trakt_set
+
 
     def test_trakt_remove(self):
         trakt_set = TraktSet(self.trakt_config)
