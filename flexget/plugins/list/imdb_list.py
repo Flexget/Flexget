4b2d9c2f44b02dda367a058ee5a132a1a3862796
Or Carmi <or.carmi82@gmail.com>
2016-07-18 13:49:50 +0300

    Even more logs

--- flexget/plugins/list/imdb_list.py
+++ flexget/plugins/list/imdb_list.py
@@ -293,7 +293,8 @@ class ImdbEntrySet(MutableSet):
             'ref_tag': 'title'
         }
         log.debug('adding title %s with ID %s to imdb %s', entry['title'], entry['imdb_id'], self.list_id)
-        self.session.post('http://www.imdb.com/list/_ajax/edit', data=data, cookies=self.cookies)
+        resp = self.session.post('http://www.imdb.com/list/_ajax/edit', data=data, cookies=self.cookies)
+        log.debug('imdb response" %s, status code: %s', resp.content, resp.status_code)
         # Invalidate cache so that new movie info will be grabbed
         self.invalidate_cache()
 
