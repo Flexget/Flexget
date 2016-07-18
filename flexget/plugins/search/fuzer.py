b4ed057caaf93f384bc1c1a305317721d4e42b47
Or Carmi <or.carmi82@gmail.com>
2016-07-18 10:55:34 +0300

    [change] fuzer -Refactor and added support for search via imdb_id

--- flexget/plugins/search/fuzer.py
+++ flexget/plugins/search/fuzer.py
@@ -66,12 +66,66 @@ class UrlRewriteFuzer(object):
         'additionalProperties': False
     }
 
+    @staticmethod
+    def get_fuzer_soup(search_term, categories_list):
+        params = {'matchquery': 'any'}
+        page = requests.get(
+            'https://www.fuzer.me/browse.php?ref_=advanced&query={}&{}'.format(search_term, '&'.join(categories_list)),
+            params=params)
+        log.debug('Using %s as fuzer search url' % page.url)
+        return get_soup(page.content)
+
+    def extract_entry_from_soup(self, soup):
+        table = soup.find('div', {'id': 'main_table'}).find('table', {'class': 'table_info'})
+        if len(table.find_all('tr')) == 1:
+            log.debug('No search results were returned, continuing')
+            return
+        entries = set()
+        for tr in table.find_all("tr"):
+            if 'colhead_dark' in tr.get('class'):
+                continue
+            name = tr.find('div', {'class': 'main_title'}).find('a').text
+            torrent_name = re.search('\\r\\n(.*)',
+                                     tr.find('div', {'id': 'attachment_dl'}).find('a')['title']).group(1)
+            attachment_link = tr.find('div', {'id': 'attachment_dl'}).find('a')['href']
+            attachment_id = re.search('attachmentid\=(\d+)', attachment_link).group(1)
+            raw_size = tr.find_all('td', {'class': 'inline_info'})[0].text.strip()
+            seeders = int(tr.find_all('td', {'class': 'inline_info'})[2].text)
+            leechers = int(tr.find_all('td', {'class': 'inline_info'})[3].text)
+
+            e = Entry()
+            e['title'] = name
+            final_url = 'https://www.fuzer.me/rss/torrent.php/{}/{}/{}/{}'.format(attachment_id, self.user_id,
+                                                                                  self.rss_key, torrent_name)
+
+            log.debug('RSS-ified download link: %s' % final_url)
+            e['url'] = final_url
+
+            e['torrent_seeds'] = seeders
+            e['torrent_leeches'] = leechers
+            e['search_sort'] = torrent_availability(e['torrent_seeds'], e['torrent_leeches'])
+
+            size = re.search('(\d+.?\d+)([TGMK]?)B', raw_size)
+            if size:
+                if size.group(2) == 'T':
+                    e['content_size'] = int(float(size.group(1)) * 1000 ** 4 / 1024 ** 2)
+                elif size.group(2) == 'G':
+                    e['content_size'] = int(float(size.group(1)) * 1000 ** 3 / 1024 ** 2)
+                elif size.group(2) == 'M':
+                    e['content_size'] = int(float(size.group(1)) * 1000 ** 2 / 1024 ** 2)
+                elif size.group(2) == 'K':
+                    e['content_size'] = int(float(size.group(1)) * 1000 / 1024 ** 2)
+                else:
+                    e['content_size'] = int(float(size.group(1)) / 1024 ** 2)
+            entries.add(e)
+        return entries
+
     @plugin.internet(log)
     def search(self, task, entry, config=None):
         """
         Search for name from fuzer.
         """
-        rss_key = config['rss_key']
+        self.rss_key = config['rss_key']
         username = config['username']
         password = hashlib.md5(config['password'].encode('utf-8')).hexdigest()
 
@@ -95,7 +149,7 @@ class UrlRewriteFuzer(object):
         if any(phrase in login.text for phrase in login_check_phrases):
             raise PluginError('Login to Fuzer failed, check credentials')
 
-        user_id = requests.cookies.get('fzr2userid')
+        self.user_id = requests.cookies.get('fzr2userid')
         category = config.get('category', [0])
         # Make sure categories is a list
         if not isinstance(category, list):
@@ -104,65 +158,22 @@ class UrlRewriteFuzer(object):
         # If there are any text categories, turn them into their id number
         categories = [c if isinstance(c, int) else CATEGORIES[c] for c in category]
 
-        params = {'matchquery': 'any'}
         c_list = []
         for c in categories:
-            c_list.append('c{}={}'.format(quote_plus('[]'),c))
+            c_list.append('c{}={}'.format(quote_plus('[]'), c))
 
         entries = set()
-        for search_string in entry.get('search_strings', [entry['title']]):
-            query = normalize_unicode(search_string).replace(":", "")
-            text = quote_plus(query.encode('windows-1255'))
-
-            page = requests.get(
-                'https://www.fuzer.me/browse.php?ref_=advanced&query={}&{}'.format(text, '&'.join(c_list), params=params),
-                params=params)
-            log.debug('Using %s as fuzer search url' % page.url)
-            soup = get_soup(page.content)
-
-            table = soup.find('div', {'id': 'main_table'}).find('table', {'class': 'table_info'})
-            if len(table.find_all('tr')) == 1:
-                log.debug('No search results were returned, continuing')
-                continue
-            for tr in table.find_all("tr"):
-                if 'colhead_dark' in tr.get('class'):
-                    continue
-                name = tr.find('div', {'class': 'main_title'}).find('a').text
-                torrent_name = re.search('\\r\\n(.*)',
-                                         tr.find('div', {'id': 'attachment_dl'}).find('a')['title']).group(1)
-                attachment_link = tr.find('div', {'id': 'attachment_dl'}).find('a')['href']
-                attachment_id = re.search('attachmentid\=(\d+)', attachment_link).group(1)
-                raw_size = tr.find_all('td', {'class': 'inline_info'})[0].text.strip()
-                seeders = int(tr.find_all('td', {'class': 'inline_info'})[2].text)
-                leechers = int(tr.find_all('td', {'class': 'inline_info'})[3].text)
-
-                e = Entry()
-                e['title'] = name
-                final_url = 'https://www.fuzer.me/rss/torrent.php/{}/{}/{}/{}'.format(attachment_id, user_id,
-                                                                                      rss_key, torrent_name)
-
-                log.debug('RSS-ified download link: %s' % final_url)
-                e['url'] = final_url
-
-                e['torrent_seeds'] = seeders
-                e['torrent_leeches'] = leechers
-                e['search_sort'] = torrent_availability(e['torrent_seeds'], e['torrent_leeches'])
-
-                size = re.search('(\d+.?\d+)([TGMK]?)B', raw_size)
-                if size:
-                    if size.group(2) == 'T':
-                        e['content_size'] = int(float(size.group(1)) * 1000 ** 4 / 1024 ** 2)
-                    elif size.group(2) == 'G':
-                        e['content_size'] = int(float(size.group(1)) * 1000 ** 3 / 1024 ** 2)
-                    elif size.group(2) == 'M':
-                        e['content_size'] = int(float(size.group(1)) * 1000 ** 2 / 1024 ** 2)
-                    elif size.group(2) == 'K':
-                        e['content_size'] = int(float(size.group(1)) * 1000 / 1024 ** 2)
-                    else:
-                        e['content_size'] = int(float(size.group(1)) / 1024 ** 2)
-                entries.add(e)
-
-        return sorted(entries, reverse=True, key=lambda x: x.get('search_sort'))
+        if entry.get('imdb_id'):
+            log.debug('imdb_id {} detected, using in search.'.format(entry['imdb_id']))
+            soup = self.get_fuzer_soup(entry['imdb_id'], c_list)
+            entries = self.extract_entry_from_soup(soup)
+        else:
+            for search_string in entry.get('search_strings', [entry['title']]):
+                query = normalize_unicode(search_string).replace(":", "")
+                text = quote_plus(query.encode('windows-1255'))
+                soup = self.get_fuzer_soup(text, c_list)
+                entries = self.extract_entry_from_soup(soup)
+        return sorted(entries, reverse=True, key=lambda x: x.get('search_sort')) if entries else []
 
 
 @event('plugin.register')
