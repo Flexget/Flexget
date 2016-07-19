17589d16f9bf88a1491e539db222719ca08b7889
Tom Matheussen <tommatheussen5@hotmail.com>
2016-07-19 13:54:48 +0200

    Removed specific plugins, should load them all now

--- flexget/ui/karma.conf.js
+++ flexget/ui/karma.conf.js
@@ -3,11 +3,6 @@
 
 module.exports = function(config) {
   config.set({
-  	
-  	plugins: [
-  	"karma-coverage",
-  	"karma-wiredep"
-  	],
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
