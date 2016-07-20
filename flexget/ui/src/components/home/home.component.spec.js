a10fa03218193c96f5985677d0183e4162867391
Tom Matheussen <tommatheussen5@hotmail.com>
2016-07-20 12:18:31 +0200

    Code style improvements for home component

--- flexget/ui/src/components/home/home.component.spec.js
+++ flexget/ui/src/components/home/home.component.spec.js
@@ -1,8 +1,11 @@
+/* global bard */
 describe('Home Component:', function () {
 	var component;
 
 	beforeEach(function () {
 		bard.appModule('components.home');
+
+		/* global $componentController */
 		bard.inject('$componentController');
 	});
 
@@ -10,7 +13,7 @@ describe('Home Component:', function () {
 		component = $componentController('home');
 	});
 
-	it("should exist", function () {
+	it('should exist', function () {
 		expect(component).to.exist;
 	});
 });
