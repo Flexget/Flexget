dbea7b736ba0d5c62cd66632751e8ce078669ca5
Steven Adams <stevezau@gmail.com>
2016-07-22 16:08:16 +1000

    forgot to import pytest

--- tests/test_feed_control.py
+++ tests/test_feed_control.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
+import pytest
+
 
 class TestOnlyTask(object):
     """
