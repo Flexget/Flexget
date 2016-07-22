2bee060bc2af34c73a6b211d65bc15eae1ca318a
Steven Adams <stevezau@gmail.com>
2016-07-22 23:56:25 +1000

    allow extending string

--- flexget/ui/src/app.utils.js
+++ flexget/ui/src/app.utils.js
@@ -1,12 +1,12 @@
 'use strict';
 
-if (typeof String.prototype.startsWith !== 'function') {
+if (typeof String.prototype.startsWith !== 'function') { // eslint-disable-line no-extend-native
     String.prototype.startsWith = function (prefix) {
         return this.indexOf(prefix) === 0;
     };
 }
 
-if (typeof String.prototype.endsWith !== 'function') {
+if (typeof String.prototype.endsWith !== 'function') { // eslint-disable-line no-extend-native
     String.prototype.endsWith = function (suffix) {
         return this.indexOf(suffix, this.length - suffix.length) !== -1;
     };
