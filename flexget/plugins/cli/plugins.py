f0df37a03c2e02af0e3168dabdf368c69dcf814a
paranoidi <marko.koivusalo@gmail.com>
2016-08-05 17:55:16 +0300

    Use tables with command

--- flexget/plugins/cli/plugins.py
+++ flexget/plugins/cli/plugins.py
@@ -3,51 +3,52 @@ from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
 import logging
 
+
 from flexget import options
 from flexget.event import event
 from flexget.logger import console
 from flexget.plugin import get_plugins
+from flexget.options import CLITable, table_parser, CLITableError
 
 log = logging.getLogger('plugins')
 
 
-@event('manager.subcommand.plugins')
 def plugins_summary(manager, options):
-    if options.porcelain:
-        console('%-30s%-s%-30s%-s%s' % ('Name', '|', 'Roles (priority)', '|', 'Info'))
-    else:
-        console('-' * 79)
-        console('%-30s%-30s%-s' % ('Name', 'Roles (priority)', 'Info'))
-        console('-' * 79)
-
-    # print the list
+    header = ['Keyword', 'Phases', 'Flags']
+    table_data = [header]
     for plugin in sorted(get_plugins(phase=options.phase, group=options.group)):
-        # do not include test classes, unless in debug mode
-        if plugin.get('debug_plugin', False) and not options.debug:
-            continue
         flags = []
         if plugin.instance.__doc__:
-            flags.append('--doc')
+            flags.append('doc')
         if plugin.builtin:
             flags.append('builtin')
         if plugin.debug:
-            flags.append('debug')
+            if not options.debug:
+                continue
+            flags.append('developers')
         handlers = plugin.phase_handlers
-        roles = ', '.join('%s(%s)' % (phase, handlers[phase].priority) for phase in handlers)
+        roles = []
+        for phase in handlers:
+            prio = handlers[phase].priority
+            roles.append('{0}({1})'.format(phase, prio))
 
-        if options.porcelain:
-            console('%-30s%-s%-30s%-s%s' % (plugin.name, '|', roles, '|', ', '.join(flags)))
+        if options.table_type == 'porcelain':
+            jc = ', '
         else:
-            console('%-30s%-30s%-s' % (plugin.name, roles, ', '.join(flags)))
-    if options.porcelain:
-        pass
-    else:
-        console('-' * 79)
+            jc = '\n'
+
+        table_data.append([plugin.name, jc.join(roles), jc.join(flags)])
+
+    table = CLITable(options.table_type, table_data)
+    try:
+        console(table.output)
+    except CLITableError as e:
+        console('ERROR: %s' % str(e))
 
 
 @event('options.register')
 def register_parser_arguments():
-    plugins_subparser = options.register_command('plugins', plugins_summary, help='print registered plugin summaries')
-    plugins_subparser.add_argument('--group', help='show plugins belonging to this group')
-    plugins_subparser.add_argument('--phase', help='show plugins that act on this phase')
-    plugins_subparser.add_argument('--porcelain', action='store_true', help='make the output parseable')
+    parser = options.register_command('plugins', plugins_summary, help='Print registered plugin summaries', 
+                                      parents=[table_parser])
+    parser.add_argument('--group', help='Show plugins belonging to this group')
+    parser.add_argument('--phase', help='Show plugins that act on this phase')
