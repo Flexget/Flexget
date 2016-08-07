cf86e15c48a99b7a2e0f88648da389dd90baf618
Or Carmi <or.carmi82@gmail.com>
2016-08-07 16:48:40 +0300

    Readded missing import

--- flexget/options.py
+++ flexget/options.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
+import sys
 import copy
 import random
 import string
