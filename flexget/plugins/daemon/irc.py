59427bd2264f056e861a41e0297e678bf4a4f5e5
cvium <clausvium@gmail.com>
2016-08-12 15:42:04 +0200

    [fix] irc: Fixed KeyError when extracting tags from announcement

--- flexget/plugins/daemon/irc.py
+++ flexget/plugins/daemon/irc.py
@@ -459,11 +459,13 @@ class IRCConnection(IRCBot):
         :return:
         """
 
-        parsed_fields = {}
         ignore_optionals = []
 
         if rules is None:
             rules = self.tracker_config.find('parseinfo/linematched')
+
+        # Make sure all irc fields from entry are in `fields`
+        fields = {key: val for key, val in entry.items() if key.startswith('irc_')}
 
         for rule in rules:
             log.debug('Processing rule %s' % rule.tag)
@@ -476,8 +478,8 @@ class IRCConnection(IRCBot):
                         result += element.get('value')
                     elif element.tag in ['var', 'varenc']:
                         varname = element.get('name')
-                        if irc_prefix(varname) in entry:
-                            value = entry[irc_prefix(varname)]
+                        if irc_prefix(varname) in fields:
+                            value = fields[irc_prefix(varname)]
                         elif self.config.get(varname):
                             value = self.config.get(varname)
                         else:
@@ -492,7 +494,7 @@ class IRCConnection(IRCBot):
                 else:
                     # Only set the result if we processed all elements
                     log.debug('Result for rule %s: %s=%s', rule.tag, rule.get('name'), result)
-                    parsed_fields[irc_prefix(rule.get('name'))] = result
+                    fields[irc_prefix(rule.get('name'))] = result
 
             # Var Replace - replace text in a var
             elif rule.tag == 'varreplace':
@@ -500,16 +502,16 @@ class IRCConnection(IRCBot):
                 target_var = irc_prefix(rule.get('name'))
                 regex = rule.get('regex')
                 replace = rule.get('replace')
-                if source_var and target_var and regex is not None and replace is not None and source_var in entry:
-                    parsed_fields[target_var] = re.sub(regex, replace, entry[source_var])
-                    log.debug('varreplace: %s=%s', target_var, parsed_fields[target_var])
+                if source_var and target_var and regex is not None and replace is not None and source_var in fields:
+                    fields[target_var] = re.sub(regex, replace, fields[source_var])
+                    log.debug('varreplace: %s=%s', target_var, fields[target_var])
                 else:
                     log.error('Invalid varreplace options, skipping rule')
 
             # Extract - create multiple vars from a single regex
             elif rule.tag == 'extract':
                 source_var = irc_prefix(rule.get('srcvar'))
-                if source_var not in entry:
+                if source_var not in fields:
                     if rule.get('optional', 'false') == 'false':
                         log.error('Error processing extract rule, non-optional value %s missing!', source_var)
                     ignore_optionals.append(source_var)
@@ -520,9 +522,9 @@ class IRCConnection(IRCBot):
                     log.error('Regex option missing on extract rule, skipping rule')
                     continue
                 group_names = [irc_prefix(x.get('name')) for x in rule.find('vars') if x.tag == 'var']
-                match = re.search(regex, entry[source_var])
+                match = re.search(regex, fields[source_var])
                 if match:
-                    parsed_fields.update(dict(zip(group_names, match.groups())))
+                    fields.update(dict(zip(group_names, match.groups())))
                 else:
                     log.debug('No match found for rule extract')
 
@@ -532,7 +534,7 @@ class IRCConnection(IRCBot):
                 split = rule.get('split')
                 if source_var in ignore_optionals:
                     continue
-                values = [strip_whitespace(x) for x in entry[source_var].split(split)]
+                values = [strip_whitespace(x) for x in fields[source_var].split(split)]
                 for element in rule:
                     if element.tag == 'setvarif':
                         target_var = irc_prefix(element.get('varName'))
@@ -544,13 +546,13 @@ class IRCConnection(IRCBot):
                             for val in values:
                                 match = re.match(regex, val)
                                 if match:
-                                    parsed_fields[target_var] = val
+                                    fields[target_var] = val
                                     found_match = True
                             if not found_match:
                                 log.debug('No matches found for regex %s', regex)
                         elif value is not None and new_value is not None:
                             if value in values:
-                                parsed_fields[target_var] = new_value
+                                fields[target_var] = new_value
                             else:
                                 log.debug('No match found for value %s in %s', value, source_var)
                         else:
@@ -571,9 +573,9 @@ class IRCConnection(IRCBot):
                         else:
                             log.error('No variable bindings found in extract rule, skipping.')
                             continue
-                        match = re.match(regex, entry.get(source_var, ''))
+                        match = re.match(regex, fields.get(source_var, ''))
                         if match:
-                            parsed_fields.update(dict(zip(vars, match.groups())))
+                            fields.update(dict(zip(vars, match.groups())))
                         else:
                             log.debug('No match for extract with regex: %s', regex)
                     else:
@@ -586,8 +588,8 @@ class IRCConnection(IRCBot):
                 target_var = irc_prefix(rule.get('varName'))
                 target_val = rule.get('newValue')
                 if source_var and regex and target_var and target_val:
-                    if source_var in entry and re.match(regex, entry[source_var]):
-                        parsed_fields[target_var] = target_val
+                    if source_var in fields and re.match(regex, fields[source_var]):
+                        fields[target_var] = target_val
                 else:
                     log.error('Option missing on setregex, skipping rule')
 
@@ -597,15 +599,15 @@ class IRCConnection(IRCBot):
                 regex = rule.get('regex')
 
                 if source_var and regex:
-                    if source_var in entry and re.match(regex, entry[source_var]):
-                        parsed_fields.update(self.process_tracker_config_rules(entry, rule))
+                    if source_var in fields and re.match(regex, fields[source_var]):
+                        fields.update(self.process_tracker_config_rules(fields, rule))
                 else:
                     log.error('Option missing for if statement, skipping rule')
 
             else:
                 log.warning('Unsupported linematched tag: %s', rule.tag)
 
-        return parsed_fields
+        return fields
 
     def on_privmsg(self, msg):
         """
