1647149430114cf877609afa436d62c94719d45b
Or Carmi <or.carmi82@gmail.com>
2016-08-16 17:56:27 +0300

    Added colors and a builtin flag to plugins cli

--- flexget/plugins/cli/plugins.py
+++ flexget/plugins/cli/plugins.py
@@ -3,11 +3,10 @@ from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
 import logging
 
-
 from flexget import options
 from flexget.event import event
 from flexget.plugin import get_plugins
-from flexget.terminal import TerminalTable, CLITableError, table_parser, console
+from flexget.terminal import TerminalTable, CLITableError, table_parser, console, colorize
 
 log = logging.getLogger('plugins')
 
@@ -16,6 +15,8 @@ def plugins_summary(manager, options):
     header = ['Keyword', 'Phases', 'Flags']
     table_data = [header]
     for plugin in sorted(get_plugins(phase=options.phase, group=options.group)):
+        if options.builtins and not plugin.builtin:
+            continue
         flags = []
         if plugin.instance.__doc__:
             flags.append('doc')
@@ -35,19 +36,22 @@ def plugins_summary(manager, options):
             jc = ', '
         else:
             jc = '\n'
-
-        table_data.append([plugin.name, jc.join(roles), jc.join(flags)])
+        name = colorize('green', plugin.name) if 'builtin' in flags else plugin.name
+        table_data.append([name, jc.join(roles), jc.join(flags)])
 
     table = TerminalTable(options.table_type, table_data)
     try:
         console(table.output)
     except CLITableError as e:
         console('ERROR: %s' % str(e))
+        return
+    console(colorize('green', ' Built-in plugins'))
 
 
 @event('options.register')
 def register_parser_arguments():
-    parser = options.register_command('plugins', plugins_summary, help='Print registered plugin summaries', 
+    parser = options.register_command('plugins', plugins_summary, help='Print registered plugin summaries',
                                       parents=[table_parser])
     parser.add_argument('--group', help='Show plugins belonging to this group')
     parser.add_argument('--phase', help='Show plugins that act on this phase')
+    parser.add_argument('--builtins', action='store_true', help='Show just builtin plugins')
