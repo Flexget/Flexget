68529cfc4535fb80b8b9021468ceb9f15e05f566
cvium <clausvium@gmail.com>
2016-08-30 09:54:37 +0200

    changed test based on renaming

--- flexget/tests/test_reorder_quality.py
+++ flexget/tests/test_reorder_quality.py
@@ -5,11 +5,11 @@ from builtins import *  # pylint: disable=unused-import, redefined-builtin
 class TestQualityPriority(object):
     config = """
         tasks:
-          test_quality_priority:
+          test_reorder_quality:
             mock:
               - {title: 'Some Show S01E01 WEBRip'}
               - {title: 'Some Show S01E01 HDTV'}
-            quality_priority:
+            reorder_quality:
               webrip:
                 above: hdtv
             series:
@@ -24,8 +24,8 @@ class TestQualityPriority(object):
                   identified_by: ep
     """
 
-    def test_quality_priority(self, execute_task):
-        task = execute_task('test_quality_priority')
+    def test_reorder_quality(self, execute_task):
+        task = execute_task('test_reorder_quality')
 
         assert task.find_entry('accepted', title='Some Show S01E01 WEBRip'), 'WEBRip should have been accepted'
 
