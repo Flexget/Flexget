752ed2441cd7b148a3610b38bc0233613bdeb5a8
cvium <clausvium@gmail.com>
2016-09-04 17:55:55 +0200

    fixed exception message in py3

--- flexget/api/cached.py
+++ flexget/api/cached.py
@@ -28,7 +28,7 @@ class CachedResource(APIResource):
         try:
             file_path, mime_type = cached_resource(url, force)
         except RequestException as e:
-            raise BadRequest('Request Error: {}'.format(e.message))
+            raise BadRequest('Request Error: {}'.format(e.args[0]))
         except OSError as e:
             raise ApiError(e)
         return send_file(file_path, mimetype=mime_type)
