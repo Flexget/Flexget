9e3d36880d563565a4ff045f436cd0da9e27ce81
Or Carmi <or.carmi82@gmail.com>
2016-09-08 12:28:48 +0300

    Tweaked history

--- flexget/plugins/api/history.py
+++ flexget/plugins/api/history.py
@@ -7,7 +7,7 @@ from math import ceil
 from flask import jsonify
 from sqlalchemy import desc
 
-from flexget.api import api, APIResource
+from flexget.api import api, APIResource, BadRequest
 from flexget.plugins.output.history import History
 
 log = logging.getLogger('history')
@@ -38,16 +38,6 @@ history_api_schema = {
 
 history_api_schema = api.schema('history.list', history_api_schema)
 
-default_error_schema = {
-    'type': 'object',
-    'properties': {
-        'status': {'type': 'string'},
-        'message': {'type': 'string'}
-    }
-}
-
-default_error_schema = api.schema('default_error_schema', default_error_schema)
-
 history_parser = api.parser()
 history_parser.add_argument('page', type=int, required=False, default=1, help='Page number')
 history_parser.add_argument('max', type=int, required=False, default=50, help='Results per page')
@@ -58,7 +48,7 @@ history_parser.add_argument('task', type=str, required=False, default=None, help
 @api.doc(parser=history_parser)
 class HistoryAPI(APIResource):
 
-    @api.response(404, description='Page does not exist', model=default_error_schema)
+    @api.response(BadRequest)
     @api.response(200, model=history_api_schema)
     def get(self, session=None):
         """ List of previously accepted entries """
@@ -78,7 +68,7 @@ class HistoryAPI(APIResource):
         pages = int(ceil(count / float(max_results)))
 
         if page > pages:
-            return {'error': 'page %s does not exist' % page}, 404
+            raise BadRequest('page %s does not exist' % page)
 
         start = (page - 1) * max_results
         finish = start + max_results
