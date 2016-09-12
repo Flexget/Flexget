33f45077df38f8d2048d9ddc34f459f8944cac12
Or Carmi <or.carmi82@gmail.com>
2016-09-12 12:42:26 +0300

    Refactored execute test

--- flexget/tests/api_tests/test_execute_api.py
+++ flexget/tests/api_tests/test_execute_api.py
@@ -3,6 +3,9 @@ from builtins import *  # pylint: disable=unused-import, redefined-builtin
 
 import json
 
+from flexget.api import base_message
+from flexget.api.tasks import ObjectsContainer as OC
+
 
 class TestExecuteAPI(object):
     @staticmethod
@@ -26,18 +29,22 @@ class TestExecuteAPI(object):
                 - reject
         """
 
-    def test_execute(self, api_client, manager):
+    def test_execute(self, api_client, manager, schema_match):
         # Minimal payload
         payload = {'tasks': ['test_task']}
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
 
         assert len(task.accepted) == 1
 
-    def test_inject_plain(self, api_client, manager):
+    def test_inject_plain(self, api_client, manager, schema_match):
         entry = {
             'title': "injected",
             'url': 'http://test.com'
@@ -49,6 +56,10 @@ class TestExecuteAPI(object):
         }
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -56,7 +67,7 @@ class TestExecuteAPI(object):
         assert len(task.all_entries) == 1
         assert len(task.accepted) == 0
 
-    def test_inject_accept(self, api_client, manager):
+    def test_inject_accept(self, api_client, manager, schema_match):
         entry = {
             'title': "injected",
             'url': 'http://test.com',
@@ -70,6 +81,10 @@ class TestExecuteAPI(object):
         }
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -77,7 +92,7 @@ class TestExecuteAPI(object):
         assert len(task.all_entries) == 1
         assert len(task.accepted) == 1
 
-    def test_inject_force(self, api_client, manager):
+    def test_inject_force(self, api_client, manager, schema_match):
         entry = {
             'title': "accept",
             'url': 'http://test.com',
@@ -89,6 +104,10 @@ class TestExecuteAPI(object):
         }
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -98,6 +117,10 @@ class TestExecuteAPI(object):
 
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -111,6 +134,10 @@ class TestExecuteAPI(object):
 
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -118,7 +145,7 @@ class TestExecuteAPI(object):
         assert len(task.all_entries) == 1
         assert len(task.accepted) == 1
 
-    def test_inject_with_fields(self, api_client, manager):
+    def test_inject_with_fields(self, api_client, manager, schema_match):
         fields = {'imdb_id': "tt1234567",
                   'tmdb_id': "1234567"}
         entry = {
@@ -135,6 +162,10 @@ class TestExecuteAPI(object):
 
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -146,7 +177,7 @@ class TestExecuteAPI(object):
         assert entry['imdb_id'] == "tt1234567"
         assert entry['tmdb_id'] == "1234567"
 
-    def test_multiple_entries(self, api_client, manager):
+    def test_multiple_entries(self, api_client, manager, schema_match):
         entry1 = {
             'title': "entry1",
             'url': 'http://test.com',
@@ -164,6 +195,10 @@ class TestExecuteAPI(object):
         }
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -171,7 +206,7 @@ class TestExecuteAPI(object):
         assert len(task.all_entries) == 2
         assert len(task.accepted) == 2
 
-    def test_2nd_endpoint(self, api_client, manager):
+    def test_2nd_endpoint(self, api_client, manager, schema_match):
         entry = {
             'title': "injected",
             'url': 'http://test.com',
@@ -184,6 +219,10 @@ class TestExecuteAPI(object):
         }
         rsp = api_client.json_post('/inject/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
 
         task = self.get_task_queue(manager)
         task.execute()
@@ -205,14 +244,26 @@ class TestExecuteMultipleTasks(object):
             accept_all: yes
         """
 
-    def test_execute_multiple_tasks(self, api_client, manager):
+    def test_execute_multiple_tasks(self, api_client, manager, schema_match):
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps({}))
         assert rsp.status_code == 422
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(base_message, data)
+        assert not errors
 
         payload = {'tasks': ['non_existing_test_task']}
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 404
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(base_message, data)
+        assert not errors
 
         payload = {'tasks': ['test_task1', 'test_task2']}
         rsp = api_client.json_post('/tasks/execute/', data=json.dumps(payload))
         assert rsp.status_code == 200
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.task_execution_results_schema, data)
+        assert not errors
