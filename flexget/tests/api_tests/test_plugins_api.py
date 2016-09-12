f5e96572229887b2cf89159483087319bd2a7187
Or Carmi <or.carmi82@gmail.com>
2016-09-12 10:59:51 +0300

    Used json schema in plugins test

--- flexget/tests/api_tests/test_plugins_api.py
+++ flexget/tests/api_tests/test_plugins_api.py
@@ -1,34 +1,68 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # pylint: disable=unused-import, redefined-builtin
+from flexget.api import base_message
 
 from flexget.utils import json
+from flexget.api.plugins import ObjectsContainer as OC
 
 
 class TestPluginsAPI(object):
     config = 'tasks: {}'
 
-    def test_plugins_api(self, api_client):
+    def test_plugins_api(self, api_client, schema_match):
         rsp = api_client.get('/plugins/')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_list_reply, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/?include_schema=true')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_list_reply, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/?group=search')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_list_reply, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/?group=fgfg')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
-        assert json.loads(rsp.get_data(as_text=True))['plugin_list'] == []
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_list_reply, data)
+        assert not errors
+        assert data['plugin_list'] == []
 
         rsp = api_client.get('/plugins/?phase=fgfg')
         assert rsp.status_code == 400, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(base_message, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/?phase=input')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_list_reply, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/bla/')
         assert rsp.status_code == 400, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(base_message, data)
+        assert not errors
 
         rsp = api_client.get('/plugins/seen/')
         assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.plugin_object, data)
+        assert not errors
