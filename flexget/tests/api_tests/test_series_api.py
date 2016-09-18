0a06ba1862e0cac74a0a0d877a60a9449654c345
Or Carmi <or.carmi82@gmail.com>
2016-09-18 14:31:42 +0300

    Added series configured param tests

--- flexget/tests/api_tests/test_series_api.py
+++ flexget/tests/api_tests/test_series_api.py
@@ -39,5 +39,75 @@ class TestSeriesAPI(object):
         errors = schema_match(OC.series_list_schema, data)
         assert not errors
 
-        errors = schema_match(OC.show_details_schema, data['shows'][0])
+        show = data['shows'][0]
+        errors = schema_match(OC.show_details_schema, show)
         assert not errors
+
+        assert show['show_name'] == 'test series'
+
+    def test_series_configured_param(self, api_client, schema_match):
+        with Session() as session:
+            series = Series()
+            series.name = 'test series'
+            session.add(series)
+            session.commit()
+
+        # Default is configured series, no results
+        rsp = api_client.get('/series/')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.series_list_schema, data)
+        assert not errors
+
+        assert data['shows'] == []
+
+        # Get unconfigured series
+        rsp = api_client.get('/series/?in_config=unconfigured')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.series_list_schema, data)
+        assert not errors
+
+        show = data['shows'][0]
+        errors = schema_match(OC.show_details_schema, show)
+        assert not errors
+
+        assert len(data['shows']) == 1
+        assert show['show_name'] == 'test series'
+
+        # Add a configured series
+        with Session() as session:
+            series = Series()
+            series.name = 'test series 2'
+            session.add(series)
+            session.commit()
+            task = SeriesTask('test task')
+            series.in_tasks = [task]
+            session.commit()
+
+        # Get only configures series
+        rsp = api_client.get('/series/')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.series_list_schema, data)
+        assert not errors
+
+        show = data['shows'][0]
+        errors = schema_match(OC.show_details_schema, show)
+        assert not errors
+
+        assert len(data['shows']) == 1
+        assert show['show_name'] == 'test series 2'
+
+        # Get all series
+        rsp = api_client.get('/series/?in_config=all')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        data = json.loads(rsp.get_data(as_text=True))
+
+        errors = schema_match(OC.series_list_schema, data)
+        assert not errors
+
+        assert len(data['shows']) == 2
