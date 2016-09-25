2c3086ee17baddfb0dda2f9198c29e13e8db4a85
cvium <clausvium@gmail.com>
2016-09-25 10:47:46 +0200

    Fixed imdb parser test

--- flexget/tests/test_imdb_parser.py
+++ flexget/tests/test_imdb_parser.py
@@ -30,14 +30,15 @@ class TestImdbParser(object):
         }, 'Actors not parsed correctly'
         assert parser.directors == {'nm0001741': 'Bryan Singer'}, 'Directors not parsed correctly'
         assert len(set(parser.genres).intersection([u'crime', u'drama', u'mystery', u'thriller'])) == \
-               len([u'crime', u'drama', u'mystery', u'thriller']), 'Genres not parsed correctly'
+            len([u'crime', u'drama', u'mystery', u'thriller']), 'Genres not parsed correctly'
         assert parser.imdb_id == 'tt0114814', 'ID not parsed correctly'
         assert len(set(parser.languages).intersection(
             ['english', 'hungarian', 'spanish', 'french'])) == 4, 'Languages not parsed correctly'
         assert parser.mpaa_rating == 'R', 'Rating not parsed correctly'
         assert parser.name == 'The Usual Suspects', 'Name not parsed correctly'
         assert (parser.photo ==
-                'http://ia.media-imdb.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@._V1_UX182_CR0,0,182,268_AL_.jpg'
+                'https://images-na.ssl-images-amazon.com/images/M/MV5BMzI1MjI5MDQyOV5BMl5BanBnXkFtZTcwNzE4Mjg3NA@@.'
+                '_V1_UX182_CR0,0,182,268_AL_.jpg'
                 ), 'Photo not parsed correctly'
         assert parser.plot_outline == (
             'Following a truck hijack in New York, five conmen are arrested and brought together for questioning. '
