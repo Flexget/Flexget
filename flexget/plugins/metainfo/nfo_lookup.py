b29b374c9b63dc376ea3f1ac0241c2abf914fb81
Darlan Cavalcante Moreira <darcamo@gmail.com>
2016-10-09 01:59:52 -0300

    Add the nfo_lookup plugin (metadata)

---
+++ flexget/plugins/metainfo/nfo_lookup.py
@@ -0,0 +1,241 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # pylint: disable=unused-import, redefined-builtin
+
+import logging
+import os
+import xml.etree.ElementTree as ET
+
+from flexget import plugin
+from flexget.event import event
+from flexget.plugins.metainfo.imdb_lookup import ImdbLookup
+# from flexget.utils.imdb import make_url as make_imdb_url
+# from flexget.utils.imdb import ImdbSearch
+
+log = logging.getLogger('nfo_lookup')
+
+
+class NfoLookup(object):
+    """
+    Retrieves information from a local '.nfo' info file.
+
+    The nfo file is used by Kodi.
+
+    Example:
+        nfo_lookup: yes
+
+    WARNING: This plugin will read a file with extension '.nfo' and the same
+    name as the entry filename as an XML file using xml.etree.ElementTree from
+    the standard python library. As such, it is vulnerable to XML
+    vulnerabilities described in the link below
+    https://docs.python.org/3/library/xml.html#xml-vulnerabilities
+
+    Use this only with nfo files you have created yourself.
+    """
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    # Fields that will be read from the nfo file. The corresponding field in
+    # the entry will be nfo_{0} for each field name in these two lists.
+
+    # Fields that appear only once in the nfo file
+    single_value_fields = ["title", "originaltitle", "sorttitle", "rating",
+                           "year", "votes", "plot", "runtime",
+                           "id", "filenameandpath", "trailer",
+                           # thumb has multiple values, but we will get the first one
+                           "thumb"]
+    # Fields that can appear multiple times in the nfo file
+    multiple_value_fields = [
+        # "thumb",
+        "genre", "director", "actor"]
+    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+    schema = {'type': 'boolean'}
+
+    # Run before the imdb_lookup plugin
+    @plugin.priority(150)
+    def on_task_metainfo(self, task, config):
+        # check if disabled (value set to false)
+        if not config:
+            # Config was set to 'no' instead of yes. Don't do anything then.
+            return
+
+        for entry in task.entries:
+            # If this entry was obtained from the filesystem plugin it should
+            # have a filename field. If it does not have one then there is
+            # nothing we can do in this plugin.
+            filename = entry.get('filename')
+            location = entry.get('location')
+
+            # If there is no 'filename' field there is also no nfo file
+            if filename is None or location is None:
+                log.warning("Entry '{0}' didn't come from the filesystem plugin".format(entry.get('title')))
+                nfo_filename = None
+            else:
+                nfo_filename = self.get_nfo_filename(entry)
+                # Stop if there is no nfo file
+                if nfo_filename is None:
+                    log.warning(
+                        "Entry '{0}' has no corresponding '.nfo' file".format(
+                            entry.get('title')))
+                    # continue
+
+            # Populate the fields from the information in the .nfo file
+            self.lookup(entry, nfo_filename)
+
+    def lookup(self, entry, nfo_filename):
+        # If there is already data from a previous parse then we don't need to
+        # do anything
+        if entry.get('nfo_id') is not None:
+            log.warning("Entry '{0}' was already parsed by nfo_lookup. "
+                        "I won't do anything.".format(entry.get('title')))
+            return
+
+        # In case there is no nfo file, `nfo_filename` will be None at this point
+        if nfo_filename is None:
+            # Setting the fields dictionary to be an empty dictionary here
+            # means that no new information will be added to the entry in this
+            # lookup function. We will still perform an IMDB lookup later, but
+            # it will be the same as if we had used the imdb_lookup plugin.
+            fields = {}
+        else:
+            # Get all values we can from the nfo file
+            fields = self.get_fields_from_nfo_file(nfo_filename)
+
+            # Update the entry with the just the imdb_id. This is enouth so that we
+            # can search with ImdbLookup and be sure to get the same movie pointed
+            # by the nfo file
+            if 'nfo_id' in fields:
+                entry.update({u'imdb_id': fields['nfo_id']})
+            else:
+                # We will perform an IMDB search later the fields add here might be
+                # overwritten
+                self.add_imdb_fields_to_dict(fields)
+
+        # Let's search metadata in IMDB. If we found an nfo file containing an
+        # ID we have already added the 'imdb_id' field and this will make the
+        # IMDB lookup below find the correct movie no matter what the title and
+        # filename are.
+        imdb_lookup = ImdbLookup()
+        imdb_lookup.lookup(entry)  # This will update the entry
+
+        # Add corresponding imdb fields to the dictionary We might have found
+        # more imdb metadata in the nfo file, such as the title, the plot,
+        # etc. We will replace these fields from the previous IMDB lookup with
+        # the ones from the nfo file.
+        self.add_imdb_fields_to_dict(fields)
+        entry.update(fields)
+
+    def get_fields_from_nfo_file(self, nfo_filename):
+        d = {}
+        if os.path.exists(nfo_filename):
+            tree = ET.parse(nfo_filename)
+            root = tree.getroot()
+
+            # TODO: Right now it only works for movies
+            if root.tag != 'movie':
+                return d
+
+            # TODO: Get more metadata from the nfo file
+
+            # Single value fields
+            for field_name in NfoLookup.single_value_fields:
+                nfo_field_name = u'nfo_{0}'.format(field_name)
+                value = self._extract_single_field(root, field_name)
+                if value is not None:
+                    d[nfo_field_name] = value
+
+            # Multiple value fields
+            for field_name in NfoLookup.multiple_value_fields:
+                nfo_field_name = u'nfo_{0}'.format(field_name)
+                values = self._extract_multiple_field(root, field_name)
+                if values is not None:
+                    d[nfo_field_name] = values
+
+        return d
+
+    @staticmethod
+    def _extract_single_field(root, name):
+        """
+        Use this method to get fields from the root XML tree that only appear once,
+        such as 'title', 'year', etc.
+        """
+        f = root.find(name)
+        if f is not None:
+            return f.text
+        else:
+            return None
+
+    @staticmethod
+    def _extract_multiple_field(root, name):
+        """
+        Use this method to get fields from the root XML tree that can appear more
+        than once, such as 'actor', 'genre', 'director', etc.
+        """
+        if name == 'actor':
+            values = []
+        else:
+            values = [i.text for i in root.findall(name)]
+
+        if len(values) > 0:
+            return values
+        else:
+            return None
+
+    def add_imdb_fields_to_dict(self, fields):
+        """
+        Take the dictionary of 'nfo fields' and add extra keys with 'imdb fields'
+        corresponding to the 'nfo fields'
+        """
+        single_field_mapping = {
+            # u'nfo_id': u'imdb_id',
+            u'nfo_title': u'imdb_name',
+            u'nfo_originaltitle': u'imdb_original_name',
+            # u'nfo_votes' u'imdb_votes',
+            # u'nfo_year': u'imdb_year',
+            u'nfo_plot': u'imdb_plot_outline',
+            # u'nfo_rating': u'imdb_score',
+            # u'nfo_thumb': u'imdb_photo'
+        }
+
+        # multiple_field_mapping = {
+        #     # List of imdb genres
+        #     u'nfo_genre': u'imdb_genres',
+        #     # Directors dictionary (imdbid, name)
+        #     u'nfo_director': u'imdb_directors',
+        #     # Actors dictionary (key: imdbid, value: name)
+        #     u'nfo_actor': u'imdb_actors',
+        # }
+
+        # extra_imdb_fields = ['imdb_url',
+        #                      # 'imdb_photo',
+        #                      'imdb_languages']
+
+        # Add imdb entries with single value
+        for nfo_name, imdb_name in single_field_mapping.items():
+            if nfo_name in fields:
+                fields[imdb_name] = fields[nfo_name]
+
+        # Add imdb fields with multiple values
+        if 'nfo_genre' in fields:
+            fields[u'imdb_genres'] = fields[u'nfo_genre']
+
+        # The nfo file can also have actors and directors, but there is no gain
+        # replacing the the values we got from the IMDB lookup.
+
+    def get_nfo_filename(self, entry):
+        location = entry.get('location')
+        # TODO: maybe create an option to set a different extension than just .nfo
+        nfo_full_filename = os.path.splitext(location)[0] + '.nfo'
+
+        if os.path.isfile(nfo_full_filename):
+            return nfo_full_filename
+        else:
+            return None
+
+    @property
+    def movie_identifier(self):
+        """Returns the plugin main identifier type"""
+        return 'nfo_id'
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(NfoLookup, 'nfo_lookup', api_ver=2, groups=['movie_metainfo'])
---
+++ flexget/tests/test_nfo_lookup.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # pylint: disable=unused-import, redefined-builtin
+
+import pytest
+
+
+@pytest.mark.online
+class TestNfoLookupWithMovies(object):
+    base = "nfo_lookup_test_dir/"
+    config = """
+        tasks:
+          test_1:  # Only ID
+            filesystem:
+              path: {0}/test_1
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_2:  # ID and Title
+            filesystem:
+              path: {0}/test_2
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_3:  # ID, Title and Original Title
+            filesystem:
+              path: {0}/test_3
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_4:  # ID, Title and Plot
+            filesystem:
+              path: {0}/test_4
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_5:  # ID and genres
+            filesystem:
+              path: {0}/test_5
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_6:  # No nfo file is provided
+            filesystem:
+              path: {0}/test_6
+              mask: '*.mkv'
+            nfo_lookup: yes
+          test_7:  # Use the nfo_lookup plugin with entries not from the filesystem plugin
+            mock:
+              - {{title: 'A Bela e a Fera'}}
+            nfo_lookup: yes
+          test_8:  # Call the plugin twice. The second time won't do anything
+            mock:
+              - {{title: 'A Bela e a Fera', filename: 'beast_beauty.mkv', nfo_id: 'tt2316801'}}
+            nfo_lookup: yes
+          test_9:  # Disabled configuration
+            filesystem:
+              path: {0}/test_9
+              mask: '*.mkv'
+            nfo_lookup: no
+    """.format(base)
+
+    def test_nfo_with_only_id(self, execute_task):
+        # run the task
+        task = execute_task('test_1')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == [u'nfo_id']
+
+            # Check that the 'nfo_id' field is set to the correct movie
+            assert entry['nfo_id'] == 'tt2316801'
+            assert entry['imdb_id'] == 'tt2316801'
+
+            # The IMDB search was able to get the correct movie even though
+            # there are many versions of (Beauty and the Beast). This
+            # particular version is french
+            assert entry['imdb_name'] == 'Beauty and the Beast'
+            assert entry['imdb_original_name'] == u"La belle et la bête"
+            assert entry['title'] == u'A Bela e a Fera'  # This will be == to filename
+            assert entry['imdb_year'] == 2014
+            assert entry['imdb_genres'] == [u'fantasy', u'romance']
+
+    def test_nfo_with_id_title(self, execute_task):
+        # run the task
+        task = execute_task('test_2')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == [u'nfo_id', u'nfo_title']
+
+            # Check that the 'nfo_id' field is set to the correct movie
+            assert entry['nfo_id'] == 'tt2316801'
+            assert entry['imdb_id'] == 'tt2316801'
+
+            # The IMDB search was able to get the correct movie even though
+            # there are many versions of (Beauty and the Beast). This
+            # particular version is french
+            assert entry['imdb_name'] == 'A Bela e a Fera'  # From title tag in nfo file
+            assert entry['imdb_original_name'] == u"La belle et la bête"
+            assert entry['title'] == u'Bela e Fera'  # This will be == to filename
+            assert entry['imdb_year'] == 2014
+            assert entry['imdb_genres'] == [u'fantasy', u'romance']
+
+    def test_nfo_with_id_title_originaltitle(self, execute_task):
+        # run the task
+        task = execute_task('test_3')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == [u'nfo_id', u'nfo_originaltitle', u'nfo_title']
+
+            # Check that the 'nfo_id' field is set to the correct movie
+            assert entry['nfo_id'] == 'tt2316801'
+            assert entry['imdb_id'] == 'tt2316801'
+
+            # The IMDB search was able to get the correct movie even though
+            # there are many versions of (Beauty and the Beast). This
+            # particular version is french
+            assert entry['imdb_name'] == 'A Bela e a Fera'  # From title tag in nfo file
+            assert entry['imdb_original_name'] == u"La belle et la bête (French)"
+            assert entry['title'] == u'A Bela e a Fera'  # This will be == to filename
+            assert entry['imdb_year'] == 2014
+            assert entry['imdb_genres'] == [u'fantasy', u'romance']
+
+    def test_nfo_with_id_title_plot(self, execute_task):
+        # run the task
+        task = execute_task('test_4')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == [u'nfo_id', u'nfo_plot', u'nfo_title']
+
+            # Check that the 'nfo_id' field is set to the correct movie
+            assert entry['nfo_id'] == 'tt2316801'
+            assert entry['imdb_id'] == 'tt2316801'
+
+            assert entry['nfo_title'] == 'A Bela e a Fera'
+
+            # The IMDB search was able to get the correct movie even though
+            # there are many versions of (Beauty and the Beast). This
+            # particular version is french
+            assert entry['imdb_name'] == 'A Bela e a Fera'  # From title tag in nfo file
+            assert entry['imdb_original_name'] == u"La belle et la bête" # From IMDB
+            assert entry['title'] == u'A Bela e a Fera'  # This will be == to filename
+            assert entry['imdb_year'] == 2014
+            assert entry['imdb_genres'] == [u'fantasy', u'romance']
+            assert entry['imdb_plot_outline'] == u"Um romance inesperado floresce depois que a filha mais nova de um mercador em dificuldades se oferece para uma misteriosa besta com a qual seu pai ficou endividado."
+
+    def test_nfo_with_id_genres(self, execute_task):
+        # run the task
+        task = execute_task('test_5')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == [u'nfo_genre', u'nfo_id']
+
+            # Check that the 'nfo_id' field is set to the correct movie
+            assert entry['nfo_id'] == 'tt2316801'
+            assert entry['imdb_id'] == 'tt2316801'
+            # Check the genres from the nfo file
+
+            assert entry['imdb_genres'] == ['Fantasia', 'Romance']
+            assert entry['nfo_genre'] == ['Fantasia', 'Romance']
+
+    def test_nfo_with_no_nfo_file(self, execute_task):
+        # run the task
+        task = execute_task('test_6')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == []
+
+            # Since there is no nfo file then an IMDB search is performed only
+            # with the filename. That means that we will get a different
+            # version of the "Beauty and the Beast" movie, with a different ID
+            assert entry['imdb_id'] != 'tt2316801'
+            # Filename (it is in portuguese)
+            assert entry['title'] == u'A Bela e a Fera'  # This will be == to filename
+            # IMDB is able to find the movie from the Portuguese title,
+            # although it is not the correct one
+            assert entry['imdb_name'] == 'Beauty and the Beast'
+
+    def test_nfo_lookup_without_filesystem(self, execute_task):
+        # run the task
+        task = execute_task('test_7')
+        # This is the same as not having an nfo file
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = sorted([i for i in entry.keys() if i[:3] == 'nfo'])
+            assert nfo_keys == []
+
+            # Since there is no nfo file then an IMDB search is performed only
+            # with the filename. That means that we will get a different
+            # version of the "Beauty and the Beast" movie, with a different ID
+            assert entry['imdb_id'] != 'tt2316801'
+            # Filename (it is in portuguese)
+            assert entry['title'] == u'A Bela e a Fera'  # This will be == to filename
+            # IMDB is able to find the movie from the Portuguese title,
+            # although it is not the correct one
+            assert entry['imdb_name'] == 'Beauty and the Beast'
+
+    def test_nfo_lookup_with_already_processed_entry(self, execute_task):
+        # run the task
+        task = execute_task('test_8')
+        for entry in task.entries:
+            # Since the entry was not processed again then no new fields
+            # besides the fields added by the mock configiration in test_8
+            # should be present.
+            keys = sorted(entry.keys())
+            assert entry['nfo_id'] == 'tt2316801'
+
+            # NOTE: The mock configuration in test_8 only specify the fields
+            # "title", "filename" and "nfo_id". The other fields in the assert
+            # below are added by the testing framework and the mock plugin. If
+            # this change in any future version make the necessary changes in
+            # the assert below.
+            assert keys == sorted([u'title', u'filename', u'nfo_id',
+                                   u'task', u'url', u'original_url', u'quality'])
+
+    def test_nfo_lookup_with_disabled_configuration(self, execute_task):
+        # run the task
+        task = execute_task('test_9')
+        for entry in task.entries:
+            # Get all 'nfo' keys in the entry
+            nfo_keys = [i for i in entry.keys() if i[:3] == 'nfo']
+            assert nfo_keys == []
+
+            imdb_keys = [i for i in entry.keys() if i[:4] == 'imdb']
+            assert imdb_keys == []
+
+
+# TODO: Test with series
