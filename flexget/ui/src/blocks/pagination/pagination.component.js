ab936bbd5c8243843b94ab3a550cfc00717af5e9
Tom Matheussen <tommatheussen5@hotmail.com>
2016-10-10 20:08:47 +0200

    Added some validation to the pagination, nothing enabled when no links are present

--- flexget/ui/src/blocks/pagination/pagination.component.js
+++ flexget/ui/src/blocks/pagination/pagination.component.js
@@ -34,15 +34,15 @@
         }
 
         function linkGroupFirst() {
-            var rightDebt = Math.max(0,
-                +vm.currentPage - (+vm.last.page - 2));
+            var rightDebt = vm.last ? Math.max(0,
+                +vm.currentPage - (+vm.last.page - 2)) : 0;
             return Math.max(1, +vm.currentPage - rightDebt - 2);
         }
         
         function linkGroupLast() {
             var leftDebt = Math.max(0,
                 1 + 2 - (+vm.currentPage));
-            return Math.min(+vm.last.page, +vm.currentPage + leftDebt + 2);
+            return Math.min(vm.last ? +vm.last.page : 0, +vm.currentPage + leftDebt + 2);
         }
 
         function setPage(page) {
