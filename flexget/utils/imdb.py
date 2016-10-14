64b20ab6e2eb11dd799e711df0cda3a27c488e49
Darlan Cavalcante Moreira <darcamo@gmail.com>
2016-10-14 17:22:18 -0300

    Raise an exception when the wrong type is passed
    Raise an exception when the wrong type (anything that is not a string or
    None) is passes in is_valid_imdb_title_id and is_valid_imdb_person_id

--- flexget/utils/imdb.py
+++ flexget/utils/imdb.py
@@ -37,22 +37,26 @@ def is_imdb_url(url):
     return re.match(r'https?://[^/]*imdb\.com/', url)
 
 
-def is_valid_imdb_title_id(value=None):
+def is_valid_imdb_title_id(value):
     """
     Return True if `value` is a valid IMDB ID for titles (movies, series, etc).
     """
-    if not isinstance(value, basestring):
+    if value is None:
         return False
+    if not isinstance(value, basestring):
+        raise TypeError("is_valid_imdb_title_id expects a string but got {0}".format(type(value)))
     # IMDB IDs for titles have 'tt' followed by 7 digits
     return re.match('tt[\d]{7}', value) is not None
 
 
-def is_valid_imdb_person_id(value=None):
+def is_valid_imdb_person_id(value):
     """
     Return True if `value` is a valid IMDB ID for a person.
     """
-    if not isinstance(value, basestring):
+    if value is None:
         return False
+    if not isinstance(value, basestring):
+        raise TypeError("is_valid_imdb_person_id expects a string but got {0}".format(type(value)))
     # An IMDB ID for a person is formed by 'nm' followed by 7 digits
     return re.match('nm[\d]{7}', value) is not None
 
