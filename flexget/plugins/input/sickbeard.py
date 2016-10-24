13bc81b46d7dea4c20fb9ee4108d6f2da98e45de
liiight <or.carmi82@gmail.com>
2016-10-25 00:27:57 +0300

    Fixed typo

--- flexget/plugins/input/sickbeard.py
+++ flexget/plugins/input/sickbeard.py
@@ -94,7 +94,7 @@ class Sickbeard(object):
                                      % (parsedurl.scheme, parsedurl.netloc, config.get('port'), parsedurl.path, e))
         entries = []
         for _, show in list(json['data'].items()):
-            log.deubg('processing show: {}'.format(show))
+            log.debug('processing show: {}'.format(show))
             fg_qualities = ''  # Initializes the quality parameter
             if show['paused'] and config.get('only_monitored'):
                 continue
