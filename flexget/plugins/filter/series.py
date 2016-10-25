f544e854d92e9463c2aef1e9a959d108b4fc20f4
Or Carmi <or.carmi82@gmail.com>
2016-10-25 16:51:57 +0300

    Moved series list to be a part of series plugin

--- flexget/plugins/filter/series.py
+++ flexget/plugins/filter/series.py
@@ -5,18 +5,13 @@ import argparse
 import logging
 import re
 import time
+from collections import MutableSet
 from copy import copy
 from datetime import datetime, timedelta
 
-from past.builtins import basestring
-from sqlalchemy import (Column, Integer, String, Unicode, DateTime, Boolean,
-                        desc, select, update, delete, ForeignKey, Index, func, and_, not_)
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.ext.hybrid import Comparator, hybrid_property
-from sqlalchemy.orm import relation, backref, object_session
-
 from flexget import db_schema, options, plugin
 from flexget.config_schema import one_or_more
+from flexget.entry import Entry
 from flexget.event import event, fire_event
 from flexget.manager import Session
 from flexget.plugin import get_plugin_by_name
@@ -27,6 +22,12 @@ from flexget.utils.log import log_once
 from flexget.utils.sqlalchemy_utils import (table_columns, table_exists, drop_tables, table_schema, table_add_column,
                                             create_index)
 from flexget.utils.tools import merge_dict_from_to, parse_timedelta, parse_episode_identifier
+from past.builtins import basestring
+from sqlalchemy import (Column, Integer, String, Unicode, DateTime, Boolean,
+                        desc, select, update, delete, ForeignKey, Index, func, and_, not_)
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.ext.hybrid import Comparator, hybrid_property
+from sqlalchemy.orm import relation, backref, object_session
 
 SCHEMA_VER = 14
 DEFAULT_SERIES_LIST_NAME = 'series_list'
@@ -162,6 +163,103 @@ def upgrade(ver, session):
         table_add_column('series', 'list_id', Integer, session, result.inserted_primary_key[0])
         ver = 14
     return ver
+
+
+class SeriesListList(Base):
+    __tablename__ = 'series_list_lists'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode, unique=True)
+    added = Column(DateTime, default=datetime.now)
+    series = relation('Series', backref='series_list', cascade='all, delete, delete-orphan', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'list_name': self.list_name,
+            'added_on': self.added
+        }
+
+
+class SeriesListSet(MutableSet):
+    def _db_list(self, session):
+        return session.query(SeriesListList).filter(SeriesListList.name == self.config).first()
+
+    def __init__(self, config):
+        self.config = config
+        with Session() as session:
+            if not self._db_list(session):
+                session.add(SeriesListList(name=self.config))
+
+    def _from_iterable(self, it):
+        return set(it)
+
+    def _find_entry(self, entry):
+        with Session() as session:
+            log.debug('trying to find series %s in DB', entry['series_name'])
+            series = session.query(Series).filter(Series.name == entry['series_name']).one_or_none()
+            if series:
+                log.debug('found series %s', series.name)
+            return series
+
+    def __contains__(self, entry):
+        return self._find_entry(entry) is not None
+
+    def __len__(self):
+        with Session() as session:
+            return len(self._db_list(session).series)
+
+    def __iter__(self):
+        with Session() as session:
+            return iter(
+                [Entry(title=series.name, url='', series_name=series.name) for series in self._db_list(session).series])
+
+    def add(self, entry):
+        with Session() as session:
+            if self.__contains__(entry):
+                log.debug('series %s already exist in DB, skipping', entry['series_name'])
+                return
+            db_list = self._db_list(session=session)
+            series = Series()
+            series.name = entry['series_name']
+            log.debug('adding series %s to list %s', series.name, db_list.name)
+            db_list.series.append(series)
+
+    def discard(self, entry):
+        with Session() as session:
+            series = self._find_entry(entry)
+            if series:
+                log.debug('deleting series %s from DB', series.name)
+                session.delete(series)
+
+    @property
+    def immutable(self):
+        return False
+
+    @property
+    def online(self):
+        """
+        Set the online status of the plugin, online plugin should be treated
+        differently in certain situations, like test mode
+        """
+        return False
+
+    def get(self, entry):
+        raise NotImplementedError
+
+
+class PluginSeriesList(object):
+    schema = {'type': 'string'}
+
+    @staticmethod
+    def get_list(config):
+        return SeriesListSet(config)
+
+    def on_task_input(self, task, config):
+        return list(SeriesListSet(config))
 
 
 @event('manager.db_cleanup')
@@ -1768,6 +1866,7 @@ def register_plugin():
     plugin.register(FilterSeries, 'series', api_ver=2)
     # This is a builtin so that it can update the database for tasks that may have had series plugin removed
     plugin.register(SeriesDBManager, 'series_db', builtin=True, api_ver=2)
+    plugin.register(PluginSeriesList, 'series_list', api_ver=2, groups=['list'])
 
 
 @event('options.register')
