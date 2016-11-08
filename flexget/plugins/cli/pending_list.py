eb5e6cf3961915fba177797c0cb034b93b508a16
Or Carmi <or.carmi82@gmail.com>
2016-11-08 11:29:23 +0200

    Renamed wait_list to pending_list

--- flexget/plugins/cli/pending_list.py
+++ flexget/plugins/cli/pending_list.py
@@ -8,8 +8,8 @@ from colorclass.toggles import disable_all_colors
 from flexget import options
 from flexget.event import event
 from flexget.manager import Session
-from flexget.plugins.list.wait_list import get_wait_entry_by_id, get_wait_entry_by_title, WaitListList, WaitListEntry
-from flexget.plugins.list.wait_list import get_wait_lists, get_list_by_exact_name, get_wait_entries_by_list_id
+from flexget.plugins.list.pending_list import get_wait_entry_by_id, get_entry_by_title, PendingListList, PendingListEntry
+from flexget.plugins.list.pending_list import get_pending_lists, get_list_by_exact_name, get_entries_by_list_id
 from flexget.terminal import TerminalTable, TerminalTableError, table_parser, console, colorize
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -29,23 +29,23 @@ def do_cli(manager, options):
         disable_all_colors()
 
     action_map = {
-        'all': wait_list_lists,
-        'list': wait_list_list,
-        'show': wait_list_show,
-        'approve': partial(wait_list_approve, approve=True),
-        'reject': partial(wait_list_approve, approve=False),
-        'del': wait_list_del,
-        'add': wait_list_add,
-        'purge': wait_list_purge
+        'all': pending_list_lists,
+        'list': pending_list_list,
+        'show': pending_list_show,
+        'approve': partial(pending_list_approve, approve=True),
+        'reject': partial(pending_list_approve, approve=False),
+        'del': pending_list_del,
+        'add': pending_list_add,
+        'purge': pending_list_purge
     }
 
     action_map[options.list_action](options)
 
 
-def wait_list_lists(options):
-    """ Show all wait lists """
+def pending_list_lists(options):
+    """ Show all pending lists """
     with Session() as session:
-        lists = get_wait_lists(session=session)
+        lists = get_pending_lists(session=session)
         header = ['#', 'List Name']
         table_data = [header]
         for entry_list in lists:
@@ -57,17 +57,17 @@ def wait_list_lists(options):
         console('ERROR: %s' % str(e))
 
 
-def wait_list_list(options):
-    """List wait entry list"""
+def pending_list_list(options):
+    """List pending list entries"""
     with Session() as session:
         try:
             wait_list = get_list_by_exact_name(options.list_name, session=session)
         except NoResultFound:
-            console('Could not find wait list with name {}'.format(options.list_name))
+            console('Could not find pending list with name {}'.format(options.list_name))
             return
         header = ['#', 'Title', '# of fields', 'Approved']
         table_data = [header]
-        for entry in get_wait_entries_by_list_id(wait_list.id, order_by='added', descending=True, session=session):
+        for entry in get_entries_by_list_id(wait_list.id, order_by='added', descending=True, session=session):
             approved = colorize('green', entry.approved) if entry.approved else entry.approved
             table_data.append([entry.id, entry.title, len(entry.entry), approved])
     table = TerminalTable(options.table_type, table_data)
@@ -77,26 +77,26 @@ def wait_list_list(options):
         console('ERROR: %s' % str(e))
 
 
-def wait_list_show(options):
+def pending_list_show(options):
     with Session() as session:
         try:
             wait_list = get_list_by_exact_name(options.list_name, session=session)
         except NoResultFound:
-            console('Could not find wait list with name {}'.format(options.list_name))
+            console('Could not find pending list with name {}'.format(options.list_name))
             return
 
         try:
             entry = get_wait_entry_by_id(wait_list.id, int(options.entry), session=session)
         except NoResultFound:
             console(
-                'Could not find matching wait entry with ID {} in list `{}`'.format(int(options.entry),
+                'Could not find matching pending entry with ID {} in list `{}`'.format(int(options.entry),
                                                                                     options.list_name))
             return
         except ValueError:
-            entry = get_wait_entry_by_title(wait_list.id, options.entry, session=session)
+            entry = get_entry_by_title(wait_list.id, options.entry, session=session)
             if not entry:
                 console(
-                    'Could not find matching wait entry with title `{}` in list `{}`'.format(options.entry,
+                    'Could not find matching pending entry with title `{}` in list `{}`'.format(options.entry,
                                                                                              options.list_name))
                 return
         header = ['Field name', 'Value']
@@ -111,31 +111,31 @@ def wait_list_show(options):
         console('ERROR: %s' % str(e))
 
 
-def wait_list_add(options):
+def pending_list_add(options):
     with Session() as session:
         try:
             wait_list = get_list_by_exact_name(options.list_name, session=session)
         except NoResultFound:
-            console('Could not find a wait list with name `{}`, creating'.format(options.list_name))
-            wait_list = WaitListList(name=options.list_name)
+            console('Could not find a pending list with name `{}`, creating'.format(options.list_name))
+            wait_list = PendingListList(name=options.list_name)
             session.add(wait_list)
         session.merge(wait_list)
         session.commit()
         title = options.entry_title
         entry = {'title': options.entry_title, 'url': options.url}
-        db_entry = get_wait_entry_by_title(list_id=wait_list.id, title=title, session=session)
+        db_entry = get_entry_by_title(list_id=wait_list.id, title=title, session=session)
         if db_entry:
             console("Entry with the title `{}` already exist with list `{}`. Will replace identifiers if given".format(
                 title, wait_list.name))
-            output = 'Successfully updated entry `{}` to wait list `{}` '.format(title, wait_list.name)
+            output = 'Successfully updated entry `{}` to pending list `{}` '.format(title, wait_list.name)
         else:
             console("Adding entry with title `{}` to list `{}`".format(title, wait_list.name))
-            db_entry = WaitListEntry(entry=entry, wait_list_id=wait_list.id)
+            db_entry = PendingListEntry(entry=entry, pending_list_id=wait_list.id)
             if options.approved:
                 console('marking entry as approved')
                 db_entry.approved = True
             session.add(db_entry)
-            output = 'Successfully added entry `{}` to wait list `{}` '.format(title, wait_list.name)
+            output = 'Successfully added entry `{}` to pending list `{}` '.format(title, wait_list.name)
         if options.attributes:
             console('Adding attributes to entry `{}`'.format(title))
             for identifier in options.attributes:
@@ -145,23 +145,23 @@ def wait_list_add(options):
         console(output)
 
 
-def wait_list_approve(options, approve=None):
+def pending_list_approve(options, approve=None):
     with Session() as session:
         try:
             entry_list = get_list_by_exact_name(options.list_name)
         except NoResultFound:
-            console('Could not find wait list with name `{}`'.format(options.list_name))
+            console('Could not find pending list with name `{}`'.format(options.list_name))
             return
         try:
             db_entry = get_wait_entry_by_id(entry_list.id, int(options.entry), session=session)
         except NoResultFound:
-            console('Could not find matching wait entry with ID {} in list `{}`'.format(int(options.entry),
+            console('Could not find matching entry with ID {} in list `{}`'.format(int(options.entry),
                                                                                         options.list_name))
             return
         except ValueError:
-            db_entry = get_wait_entry_by_title(entry_list.id, options.entry, session=session)
+            db_entry = get_entry_by_title(entry_list.id, options.entry, session=session)
             if not db_entry:
-                console('Could not find matching wait entry with title `{}` in list `{}`'.format(options.entry,
+                console('Could not find matching entry with title `{}` in list `{}`'.format(options.entry,
                                                                                                  options.list_name))
                 return
         approve_text = 'approved' if approve else 'rejected'
@@ -172,32 +172,32 @@ def wait_list_approve(options, approve=None):
         console('Successfully marked pending entry {} as {}'.format(db_entry.title, approve_text))
 
 
-def wait_list_del(options):
+def pending_list_del(options):
     with Session() as session:
         try:
             entry_list = get_list_by_exact_name(options.list_name)
         except NoResultFound:
-            console('Could not find wait list with name `{}`'.format(options.list_name))
+            console('Could not find pending list with name `{}`'.format(options.list_name))
             return
         try:
             db_entry = get_wait_entry_by_id(entry_list.id, int(options.entry), session=session)
         except NoResultFound:
             console(
-                'Could not find matching wait entry with ID {} in list `{}`'.format(int(options.entry),
+                'Could not find matching entry with ID {} in list `{}`'.format(int(options.entry),
                                                                                     options.list_name))
             return
         except ValueError:
-            db_entry = get_wait_entry_by_title(entry_list.id, options.entry, session=session)
+            db_entry = get_entry_by_title(entry_list.id, options.entry, session=session)
             if not db_entry:
                 console(
-                    'Could not find matching wait entry with title `{}` in list `{}`'.format(options.entry,
+                    'Could not find matching entry with title `{}` in list `{}`'.format(options.entry,
                                                                                              options.list_name))
                 return
-        console('Removing wait entry `{}` from list {}'.format(db_entry.title, options.list_name))
+        console('Removing entry `{}` from list {}'.format(db_entry.title, options.list_name))
         session.delete(db_entry)
 
 
-def wait_list_purge(options):
+def pending_list_purge(options):
     with Session() as session:
         try:
             entry_list = get_list_by_exact_name(options.list_name)
@@ -224,24 +224,24 @@ def register_parser_arguments():
                                         ' tmdb_id=XXX, etc')
     list_name_parser = ArgumentParser(add_help=False)
     list_name_parser.add_argument('list_name', nargs='?', default='wait_entries',
-                                  help='Name of wait list to operate on')
+                                  help='Name of pending list to operate on')
     # Register subcommand
-    parser = options.register_command('wait-list', do_cli, help='View and manage wait lists')
+    parser = options.register_command('pending-list', do_cli, help='View and manage pending lists')
     # Set up our subparsers
     subparsers = parser.add_subparsers(title='actions', metavar='<action>', dest='list_action')
-    subparsers.add_parser('all', help='Shows all existing wait lists', parents=[table_parser])
+    subparsers.add_parser('all', help='Shows all existing pending lists', parents=[table_parser])
     subparsers.add_parser('list', parents=[list_name_parser, table_parser],
-                          help='List pending entries from a wait list')
+                          help='List pending entries from a pending list')
     subparsers.add_parser('show', parents=[list_name_parser, global_entry_parser, table_parser],
                           help='Show entry fields.')
     add = subparsers.add_parser('add', parents=[list_name_parser, entry_parser, attributes_parser],
-                                help='Add a pending entry to a wait list')
+                                help='Add a entry to a pending list')
     add.add_argument('--approved', action='store_true', help='Add an entry as approved')
     subparsers.add_parser('approve', parents=[list_name_parser, global_entry_parser],
                           help="Mark a pending entry as approved")
     subparsers.add_parser('reject', parents=[list_name_parser, global_entry_parser],
                           help="Mark a pending entry as rejected")
     subparsers.add_parser('del', parents=[list_name_parser, global_entry_parser],
-                          help='Remove a pending entry from a wait list using its title or ID')
+                          help='Remove an entry from a pending list using its title or ID')
     subparsers.add_parser('purge', parents=[list_name_parser],
-                          help='Removes an entire wait list with all of its entries. Use this with caution')
+                          help='Removes an entire pending list with all of its entries. Use this with caution')
--- flexget/tests/test_pending_list.py
+++ flexget/tests/test_pending_list.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # pylint: disable=unused-import, redefined-builtin
+
 from flexget.manager import Session
-from flexget.plugins.list.wait_list import WaitListList
+from flexget.plugins.list.pending_list import PendingListList
 
 
 class TestListInterface(object):
@@ -12,35 +13,35 @@ class TestListInterface(object):
 
         tasks:
           list_get:
-            wait_list: test_list
+            pending_list: test_list
 
-          wait_list_add:
+          pending_list_add:
             mock:
               - {title: 'title 1', url: "http://mock.url/file1.torrent"}
               - {title: 'title 2', url: "http://mock.url/file2.torrent"}
             accept_all: yes
             list_add:
-              - wait_list: test_list
+              - pending_list: test_list
 
-          wait_list_match:
+          pending_list_match:
             mock:
               - {title: 'title 1', url: "http://mock.url/file1.torrent"}
               - {title: 'title 2', url: "http://mock.url/file2.torrent"}
               - {title: 'title 3', url: "http://mock.url/file3.torrent"}
             list_match:
               from:
-                - wait_list: test_list
+                - pending_list: test_list
     """
 
     def test_list_add(self, execute_task):
-        task = execute_task('wait_list_add')
+        task = execute_task('pending_list_add')
         assert len(task.entries) == 2
 
         task = execute_task('list_get')
         assert len(task.entries) == 0
 
         with Session() as session:
-            list = session.query(WaitListList).first()
+            list = session.query(PendingListList).first()
             assert list
             for entry in list.entries:
                 entry.approved = True
@@ -49,14 +50,14 @@ class TestListInterface(object):
         assert len(task.entries) == 2
 
     def test_list_match(self, execute_task):
-        task = execute_task('wait_list_add')
+        task = execute_task('pending_list_add')
         assert len(task.entries) == 2
 
-        task = execute_task('wait_list_match')
+        task = execute_task('pending_list_match')
         assert len(task.accepted) == 0
 
         with Session() as session:
-            list = session.query(WaitListList).first()
+            list = session.query(PendingListList).first()
             assert list
             for entry in list.entries:
                 entry.approved = True
@@ -64,7 +65,7 @@ class TestListInterface(object):
         task = execute_task('list_get')
         assert len(task.entries) == 2
 
-        task = execute_task('wait_list_match')
+        task = execute_task('pending_list_match')
         assert len(task.accepted) == 2
 
         task = execute_task('list_get')
