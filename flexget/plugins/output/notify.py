fbb6ddb4494a94326c5084530e0111cdd5857af2
Or Carmi <or.carmi82@gmail.com>
2016-11-15 10:17:41 +0200

    Removed phase support

--- flexget/plugins/output/notify.py
+++ flexget/plugins/output/notify.py
@@ -9,7 +9,7 @@ from flexget.event import event
 
 log = logging.getLogger('notify')
 
-HANDLED_PHASES = ['start', 'input', 'filter', 'output', 'exit']
+ENTRY_CONTAINERS = ['entries', 'accepted', 'rejected', 'failed', 'undecided']
 
 
 class Notify(object):
@@ -24,8 +24,7 @@ class Notify(object):
                                             '2 more spaces than the first letter of the plugin name.',
                      'minProperties': 1}]}},
             'scope': {'type': 'string', 'enum': ['task', 'entries']},
-            'what': one_or_more({'type': 'string', 'enum': ['entries', 'accepted', 'rejected', 'failed', 'undecided']}),
-            'phase': one_or_more({'type': 'string', 'enum': HANDLED_PHASES, 'default': 'output'})
+            'what': one_or_more({'type': 'string', 'enum': ENTRY_CONTAINERS})
         },
         'required': ['to'],
         'additionalProperties': False
@@ -33,30 +32,16 @@ class Notify(object):
 
     @staticmethod
     def prepare_config(config):
-        if not isinstance(config['phase'], list):
-            config['phase'] = [config['phase']]
         if not isinstance(config['what'], list):
             config['what'] = [config['what']]
         config.setdefault('what', ['accepted'])
         config.setdefault('scope', 'entries')
-        config.setdefault('phase', ['output'])
         return config
 
-    def send_notification(self, task, phase, config):
+    def on_task_exit(self, task, config):
         config = self.prepare_config(config)
 
-        if phase not in config['phase']:
-            log.debug('phase %s not configured', phase)
-            return
-
         scope = config['scope']
-
-        # In case the request notification scope is `task`, skip all phases other than exit in order not to send
-        # more than 1 notification
-        if scope == 'task' and phase != 'exit':
-            log.debug('skipping phase on_task_%s since scope is `task`', phase)
-            return
-
         what = config['what']
 
         # Build a list of entry containers or just uses task, depending on the scope
@@ -74,23 +59,6 @@ class Notify(object):
 
                 log.debug('sending a notification to %s', plugin_name)
                 notifier.notify(**kwargs)
-
-    def __getattr__(self, item):
-        """Creates methods to handle task phases."""
-        for phase in HANDLED_PHASES:
-            if item == plugin.phase_methods[phase]:
-                # A phase method we handle has been requested
-                break
-        else:
-            # We don't handle this phase
-            raise AttributeError(item)
-
-        def phase_handler(task, config):
-            self.send_notification(task, phase, config)
-
-        # Make sure we run after other plugins
-        phase_handler.priority = 255
-        return phase_handler
 
 
 @event('plugin.register')
