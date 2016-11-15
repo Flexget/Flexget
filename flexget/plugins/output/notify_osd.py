7c88df2859c07d8a77760c609457acde7f3c4dbd
Or Carmi <or.carmi82@gmail.com>
2016-11-15 14:30:10 +0200

    Removed unused import

--- flexget/plugins/output/notify_osd.py
+++ flexget/plugins/output/notify_osd.py
@@ -5,7 +5,7 @@ import logging
 
 from flexget import plugin
 from flexget.event import event
-from flexget.utils.template import RenderError, render_from_task
+from flexget.utils.template import RenderError
 
 log = logging.getLogger('notify_osd')
 
