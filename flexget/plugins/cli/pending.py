b08b7d63a523462efed7bd8e8694113ac24c05a6
Or Carmi <or.carmi82@gmail.com>
2016-11-23 12:41:46 +0200

    [add] pending CLI - Added `clear` action to deleted all pending entries

--- flexget/plugins/cli/pending.py
+++ flexget/plugins/cli/pending.py
@@ -7,7 +7,7 @@ from colorclass.toggles import disable_all_colors
 from flexget import options
 from flexget.event import event
 from flexget.manager import Session
-from flexget.plugins.filter.pending_approval import list_pending_entries, get_entry_by_id
+from flexget.plugins.filter.pending_approval import list_pending_entries, get_entry_by_id, PendingEntry
 from flexget.terminal import TerminalTable, TerminalTableError, table_parser, console, colorize
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -31,6 +31,8 @@ def do_cli(manager, options):
         manage_entries(options, options.selection, True)
     elif options.action == 'reject':
         manage_entries(options, options.selection, False)
+    elif options.action == 'clear':
+        clear_entries(options)
 
 
 def list_entries(options):
@@ -84,17 +86,31 @@ def manage_entries(options, selection, approved):
                 entry.approved = approved
 
 
+def clear_entries(options):
+    """Clear pending entries"""
+    with Session() as session:
+        query = session.query(PendingEntry).filter(PendingEntry.approved == False)
+        if options.task_name:
+            query = query.filter(PendingEntry.task_name == options.task_name)
+        deleted = query.delete()
+        console('Successfully deleted %i pending entries' % deleted)
+
+
 @event('options.register')
 def register_parser_arguments():
     selection_parser = ArgumentParser(add_help=False)
     selection_parser.add_argument('selection', type=valid_entry,
                                   help='Entity ID or \'all\' to approve all pending entries')
 
-    parser = options.register_command('pending', do_cli, help='View and pending')
+    filter_parser = ArgumentParser(add_help=False)
+    filter_parser.add_argument('--task-name', help='Filter by task name')
+
+    parser = options.register_command('pending', do_cli, help='View and manage pending entries')
     subparsers = parser.add_subparsers(title='actions', metavar='<action>', dest='action')
 
-    list_parser = subparsers.add_parser('list', help='Shows all existing pending entries', parents=[table_parser])
-    list_parser.add_argument('--task-name', help='Filter by task name')
+    list_parser = subparsers.add_parser('list', help='Shows all existing pending entries',
+                                        parents=[table_parser, filter_parser])
+
     list_group = list_parser.add_mutually_exclusive_group()
     list_group.add_argument('--pending', action='store_false', help='Show only pending entries', dest='approved',
                             default=None)
@@ -103,3 +119,4 @@ def register_parser_arguments():
 
     subparsers.add_parser('approve', help='Approve pending entries', parents=[selection_parser])
     subparsers.add_parser('reject', help='Reject pending entries', parents=[selection_parser])
+    subparsers.add_parser('clear', help='Clear all pending entries', parents=[filter_parser])
