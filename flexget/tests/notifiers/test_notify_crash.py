f1d3fde4740f1154f98020c70a2a49bfb2f259e6
Or Carmi <or.carmi82@gmail.com>
2016-12-05 17:20:58 +0200

    Fixed test_abort

--- flexget/tests/notifiers/test_notify_crash.py
+++ flexget/tests/notifiers/test_notify_crash.py
@@ -4,31 +4,31 @@ from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 import mock
 
 @mock.patch('requests.Session.request')
-class TestNotifyCrash(object):
+class TestNotifyAbort(object):
     config = """
         tasks:
-          test_crash:
+          test_abort:
             # causes on_task_abort to be called
             disable: builtins
 
             # causes abort
             abort: yes
 
-            notify_crash:
+            notify_abort:
               to:
                 - pushover:
                     user_key: user_key
           no_crash:
             disable: builtins
-            notify_crash:
+            notify_abort:
               to:
                 - pushover:
                     user_key: user_key
     """
 
     def test_abort(self, mocked_request, execute_task):
-        execute_task('test_crash', abort=True)
-        data = {'message': 'Reason: abort plugin', 'title': 'Task test_crash has crashed!'}
+        execute_task('test_abort', abort=True)
+        data = {'message': 'Reason: abort plugin', 'title': 'Task test_abort has aborted!'}
 
         assert mocked_request.called
 
