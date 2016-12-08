f37d1ea48c859a6f011eb7241ec7635a200f99b3
Or Carmi <or.carmi82@gmail.com>
2016-12-08 13:40:43 +0200

    [fix] pushbullet - Correctly catch and handle RequestException when its Response is None

--- flexget/plugins/notifiers/pushbullet.py
+++ flexget/plugins/notifiers/pushbullet.py
@@ -115,12 +115,15 @@ class PushbulletNotifier(object):
         try:
             response = requests.post(PUSHBULLET_URL, headers=headers, json=notification)
         except RequestException as e:
-            if e.response.status_code == 429:
-                reset_time = datetime.datetime.fromtimestamp(
-                    int(e.response.headers['X-Ratelimit-Reset'])).strftime('%Y-%m-%d %H:%M:%S')
-                message = 'Monthly Pushbullet database operations limit reached. Next reset: %s' % reset_time
+            if e.response is not None:
+                if e.response.status_code == 429:
+                    reset_time = datetime.datetime.fromtimestamp(
+                        int(e.response.headers['X-Ratelimit-Reset'])).strftime('%Y-%m-%d %H:%M:%S')
+                    message = 'Monthly Pushbullet database operations limit reached. Next reset: %s' % reset_time
+                else:
+                    message = e.response.json()['error']['message']
             else:
-                message = e.response.json()['error']['message']
+                message = str(e)
             raise PluginWarning(message)
 
         reset_time = datetime.datetime.fromtimestamp(
