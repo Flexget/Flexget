ca196ffe41a287d663f6bec31c8e6c87f7efb27f
Chase Sterling <chase.sterling@gmail.com>
2016-12-10 02:11:10 -0500

    Add basic notify_task tests.

--- flexget/tests/notifiers/conftest.py
+++ flexget/tests/notifiers/conftest.py
@@ -1,3 +1,6 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
+
 import pytest
 
 from flexget import plugin
