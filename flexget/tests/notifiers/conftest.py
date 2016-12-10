33b73c74fd22c9e85280955c7186c0234e241b75
Chase Sterling <chase.sterling@gmail.com>
2016-12-10 01:56:12 -0500

    Add a debug notifier output to use with tests. Use it to test notify_abort.

---
+++ flexget/tests/notifiers/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+from flexget import plugin
+from flexget.event import event
+from flexget.plugin import get_plugin_by_name
+
+
+class DebugNotification(object):
+    schema = {'type': 'object'}
+
+    def __init__(self):
+        self.notifications = []
+
+    def notify(self, title, message, config):
+        self.notifications.append((title, message, config))
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(DebugNotification, 'debug_notification', groups=['notifiers'], api_ver=2, debug=True)
+
+
+@pytest.fixture()
+def debug_notifications(manager):
+    notifications = get_plugin_by_name('debug_notification').instance.notifications = []
+    return notifications
--- flexget/tests/notifiers/test_notify_abort.py
+++ flexget/tests/notifiers/test_notify_abort.py
@@ -1,9 +1,7 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
-import mock
 
-@mock.patch('requests.Session.request')
 class TestNotifyAbort(object):
     config = """
         tasks:
@@ -16,25 +14,27 @@ class TestNotifyAbort(object):
 
             notify_abort:
               to:
-                - pushover:
+                - debug_notification:
                     user_key: user_key
           no_crash:
             disable: builtins
             notify_abort:
               to:
-                - pushover:
+                - debug_notification:
                     user_key: user_key
     """
 
-    def test_notify_abort(self, mocked_request, execute_task):
+    def test_notify_abort(self, execute_task, debug_notifications):
         execute_task('test_abort', abort=True)
-        data = {'message': 'Reason: abort plugin', 'title': 'Task test_abort has aborted!'}
+        data = (
+            'Task test_abort has aborted!',
+            'Reason: abort plugin',
+            {'user_key': 'user_key'}
+        )
 
-        assert mocked_request.called
+        assert debug_notifications[0] == data
+        assert len(debug_notifications) == 1
 
-        for k, v in data.items():
-            assert mocked_request.call_args[1]['data'][k] == v
-
-    def test_no_crash(self, mocked_request, execute_task):
+    def test_no_crash(self, execute_task, debug_notifications):
         execute_task('no_crash')
-        assert not mocked_request.called
+        assert len(debug_notifications) == 0
