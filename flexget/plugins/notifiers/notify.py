e16e2a2b7aa7cc11ae3295a1f52347c5cbebf0e4
Chase Sterling <chase.sterling@gmail.com>
2016-12-11 19:46:57 -0500

    Improve notification system documentation.

--- flexget/plugins/notifiers/notify.py
+++ flexget/plugins/notifiers/notify.py
@@ -1,7 +1,31 @@
+"""
+This plugin adds a notification framework which can be used by other plugins to send messages to the user,
+via a transport service configurable by the user.
+
+Sending Messages
+----------------
+A plugin who wishes to send messages using this notification framework should import this plugin, then call the
+`send_notification` method. Example::
+
+    from flexget import plugin
+
+    send_notification = plugin.get_plugin_by_name('notify').instance.send_notification
+    send_notification('the title', 'the message', the_notifiers)
+
+Delivering Messages
+-------------------
+To implement a plugin that can deliver messages, it should implement a `notify` method, which takes
+`(title, message, config)` as arguments. The plugin should also have a `schema` attribute which is a JSON schema that
+describes the config format for the plugin.
+
+"""
+
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 import logging
+
+from jinja2 import Template
 
 from flexget import plugin
 from flexget.event import event
@@ -11,6 +35,48 @@ from flexget.utils.template import RenderError
 log = logging.getLogger('notify')
 
 
+NOTIFY_VIA_SCHEMA = {
+    'type': 'array',
+    'items': {
+        'allOf': [
+            {'$ref': '/schema/plugins?group=notifiers'},
+            {
+                'minProperties': 1,
+                'maxProperties': 1,
+                'error_maxProperties': 'Plugin options indented 2 more spaces than the first letter of the plugin name.'
+            }
+        ]
+    }
+}
+
+
+def render_config(config, template_renderer, _path=''):
+    """
+    Recurse through config data structures attempting to render any string fields against a given context.
+    :param config: Any simple data structure as retrieved from the FlexGet config.
+    :param template_renderer: A function that should take a string or Template argument, and return the result of
+        rendering it with the appropriate context.
+    :raises: If an error is raised, it will have the additional `config_path` property to indicate where in the config
+        the error occured.
+    """
+    if isinstance(config, (str, Template)):
+        try:
+            return template_renderer(config)
+        except Exception as e:
+            e.config_path = _path
+            raise
+    elif isinstance(config, list):
+        if _path:
+            _path += '/'
+        return [render_config(v, template_renderer, _path=_path+str(i)) for i, v in enumerate(config)]
+    elif isinstance(config, dict):
+        if _path:
+            _path += '/'
+        return {k: render_config(v, template_renderer, _path=_path+k) for k, v in config.items()}
+    else:
+        return config
+
+
 class Notify(object):
     def send_notification(self, title, message, notifiers, template_renderer=None):
         """
@@ -18,9 +84,10 @@ class Notify(object):
         If `template_renderer` is specified, `title`, `message`, as well as any string options in a notifier's config
         will be rendered using this function before sending the message.
 
-        :param str title: Title of the notification.
-        :param str message: Body of the notification.
-        :param list notifiers: A list of configured notifier output plugins.
+        :param str title: Title of the notification. (some notifiers may ignore this)
+        :param str message: Main body of the notification.
+        :param list notifiers: A list of configured notifier output plugins. The `NOTIFY_VIA_SCHEMA` JSON schema
+            describes the data structure for this parameter.
         :param template_renderer: A function that should be used to render any jinja strings in the configuration.
         """
         if template_renderer:
@@ -36,21 +103,14 @@ class Notify(object):
             for notifier_name, notifier_config in notifier.items():
                 notifier = plugin.get_plugin_by_name(notifier_name).instance
 
-                rendered_config = {}
+                rendered_config = notifier_config
 
                 # If a template renderer is specified, try to render all the notifier config values
                 if template_renderer:
-                    for attribute, value in notifier_config.items():
-                        if isinstance(value, str):
-                            try:
-                                rendered_config[attribute] = template_renderer(value)
-                            except RenderError as e:
-                                log.error('Error rendering %s config field %s: %s', notifier_name, attribute, e)
-                                rendered_config[attribute] = notifier_config[attribute]
-                        else:
-                            rendered_config[attribute] = notifier_config[attribute]
-                else:
-                    rendered_config = notifier_config
+                    try:
+                        rendered_config = render_config(notifier_config, template_renderer)
+                    except RenderError as e:
+                        log.error('Error rendering %s plugin config field %s: %s', notifier_name, e.config_path, e)
 
                 log.debug('Sending a notification to `%s`', notifier_name)
                 try:
