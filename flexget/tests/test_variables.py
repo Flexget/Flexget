66ae4a8b544b59eb151e7ff760c2d086381e79c6
Chase Sterling <chase.sterling@gmail.com>
2016-12-19 20:00:59 -0500

    Add debug info to filecopy test helper

--- flexget/tests/test_variables.py
+++ flexget/tests/test_variables.py
@@ -38,6 +38,7 @@ class TestVariablesFromFile(object):
         task = execute_task('test_variables_alongside_jinja')
         assert len(task.accepted) == 1
         assert task.accepted[0]['a_field'] == 'first bar then foo end'
+        assert False
 
 
 class TestVariablesFromDB(object):
