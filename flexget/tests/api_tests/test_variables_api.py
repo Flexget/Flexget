ceffc1e477dfe73a85e9c5a3af291e8cb594af8b
Chase Sterling <chase.sterling@gmail.com>
2016-12-21 11:26:35 -0500

    Upgrade of 'secrets' plugin to 'variables' (#1579)
    [change] Rename 'secrets' plugin to 'variables', and change usage.
    Changes jinja tag to {? ?} for replacement to avoid conflicts with jinja in the config
    
    fix #1578

---
+++ flexget/tests/api_tests/test_variables_api.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
+
+from flexget.manager import Session
+from flexget.plugins.modify.variables import Variables
+from flexget.utils import json
+
+
+class TestVariablesAPI(object):
+    config = 'tasks: {}'
+
+    variables_dict = {'test_variable_db': True}
+
+    def test_variables_get(self, api_client):
+        with Session() as session:
+            s = Variables(variables=self.variables_dict)
+            session.add(s)
+
+        rsp = api_client.get('/variables/')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        assert json.loads(rsp.get_data(as_text=True)) == self.variables_dict
+
+    def test_variables_put(self, api_client):
+        rsp = api_client.get('/variables/')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        assert json.loads(rsp.get_data(as_text=True)) == {}
+
+        rsp = api_client.json_put('/variables/', data=json.dumps(self.variables_dict))
+        assert rsp.status_code == 201, 'Response code is %s' % rsp.status_code
+        assert json.loads(rsp.get_data(as_text=True)) == self.variables_dict
+
+        rsp = api_client.get('/variables/')
+        assert rsp.status_code == 200, 'Response code is %s' % rsp.status_code
+        assert json.loads(rsp.get_data(as_text=True)) == self.variables_dict
