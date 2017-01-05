fc1da1b0b77b7728eecd6e17c1adbe14011c3ed7
Chase Sterling <chase.sterling@gmail.com>
2017-01-05 01:49:11 -0500

    Add a windows implementation to on_screen_display notifier

--- flexget/plugins/notifiers/on_screen_display.py
+++ flexget/plugins/notifiers/on_screen_display.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 import logging
+import sys
+from os import path
+from time import sleep
 
 from flexget import plugin
 from flexget.event import event
@@ -19,7 +22,10 @@ class NotifyOsd(object):
         'additionalProperties': False
     }
 
-    def notify(self, title, message, config):
+    def __init__(self):
+        self.windows_classAtom = None
+
+    def linux_notify(self, title, message, config):
         try:
             from gi.repository import Notify
         except ImportError as e:
@@ -35,6 +41,49 @@ class NotifyOsd(object):
 
         if not n.show():
             raise PluginWarning('Unable to send notification for %s' % title)
+
+    def windows_notify(self, title, message, config):
+        try:
+            from win32api import GetModuleHandle, PostQuitMessage
+            from win32con import (CW_USEDEFAULT, IMAGE_ICON, IDI_APPLICATION, LR_DEFAULTSIZE, LR_LOADFROMFILE,
+                                  WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER)
+            from win32gui import (CreateWindow, DestroyWindow, LoadIcon, LoadImage, NIF_ICON, NIF_INFO, NIF_MESSAGE,
+                                  NIF_TIP, NIM_ADD, NIM_DELETE, NIM_MODIFY, RegisterClass, Shell_NotifyIcon,
+                                  UpdateWindow, WNDCLASS)
+        except ImportError:
+            raise DependencyError(__name__, 'pypiwin32', 'pywin32 module is required for desktop notifications on '
+                                                         'windows. You can install it with `pip install pypiwin32`')
+
+        # Register the window class.
+        wc = WNDCLASS()
+        hinst = wc.hInstance = GetModuleHandle(None)
+        wc.lpszClassName = "FlexGetTaskbar"
+        if not self.windows_classAtom:
+            self.windows_classAtom = RegisterClass(wc)
+        style = WS_OVERLAPPED | WS_SYSMENU
+        hwnd = CreateWindow(self.windows_classAtom, "Taskbar", style, 0, 0, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hinst,
+                            None)
+        UpdateWindow(hwnd)
+
+        hicon = LoadIcon(0, IDI_APPLICATION)
+        # Hackily grab the icon from the webui if possible
+        icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
+        try:
+            import flexget.ui
+            icon_path = flexget.ui.__path__[0] + '\\src\\favicon.ico'
+            hicon = LoadImage(hinst, icon_path, IMAGE_ICON, 0, 0, icon_flags)
+        except Exception as e:
+            logging.debug('Error trying to get flexget icon from webui folder: %s', e)
+
+        # Taskbar icon
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO
+        nid = (hwnd, 0, flags, WM_USER + 20, hicon, "FlexGet Notification", message, config['timeout'] * 1000, title)
+        Shell_NotifyIcon(NIM_ADD, nid)
+
+    if sys.platform.startswith('win'):
+        notify = windows_notify
+    else:
+        notify = linux_notify
 
 
 @event('plugin.register')
