caa3e7a7c83163144e3fda1bd15133164602b0b5
Unknown <danfocus@gmail.com>
2017-02-13 00:38:43 +0500

    Changes on request

--- flexget/plugins/sites/lostfilm.py
+++ flexget/plugins/sites/lostfilm.py
@@ -11,6 +11,7 @@ import feedparser
 from flexget import plugin
 from flexget.entry import Entry
 from flexget.event import event
+from flexget.plugin import PluginError
 from flexget.utils.requests import RequestException
 from flexget.utils.soup import get_soup
 
@@ -22,7 +23,7 @@ EPISODE_REGEXP = re.compile(
     '.*lostfilm.tv/series/.*/season_(\d+)/episode_(\d+)/.*')
 LOSTFILM_ID_REGEXP = re.compile(
     '.*static.lostfilm.tv/Images/(\d+)/Posters/poster.jpg.*')
-TEXT_REGEXP = re.compile('^(\d+)\sсезон\s(\d+)\sсерия\.\s(.+)\s\((.+)\)$')
+TEXT_REGEXP = re.compile('^\d+\sсезон\s\d+\sсерия\.\s(.+)\s\((.+)\)$')
 
 quality_map = {
     'SD': ('480p', 'avi'),
@@ -66,10 +67,12 @@ class LostFilm(object):
         config = self.build_config(config)
         if config is False:
             return
-        rss = feedparser.parse(config['url'])
+        try:
+            rss = feedparser.parse(config['url'])
+        except:
+            raise PluginError('Cannot parse rss feed')
         if rss.get('status') != 200:
-            log.error('Received not 200 (OK) status')
-            return
+            raise PluginError('Received not 200 (OK) status')
         entries = []
         for item in rss.entries:
             if item.get('link') is None:
@@ -78,23 +81,27 @@ class LostFilm(object):
             if item.get('description') is None:
                 log.debug('Item doesn\'t have a description')
                 continue
-            match = LOSTFILM_ID_REGEXP.search(item['description'])
-            if match:
-                lostfilm_num = match.group(1)
-            else:
+            try:
+                lostfilm_num = LOSTFILM_ID_REGEXP.search(
+                    item['description']).groups()
+            except:
                 log.debug('Item doesn\'t have lostfilm id in description')
                 continue
-            match = EPISODE_REGEXP.match(item['link'])
-            if match:
-                season_num = int(match.group(1))
-                episode_num = int(match.group(2))
-            else:
+            try:
+                season_num, episode_num = \
+                    [int(x) for x in
+                     EPISODE_REGEXP.search(item['link']).groups()]
+            except:
                 log.debug('Item doesn\'t have episode id in link')
                 continue
-            redirect_url = 'http://www.lostfilm.tv/v_search.php?' \
-                'c={}&s={}&e={}'.format(lostfilm_num, season_num, episode_num)
+            params = {
+                'c': lostfilm_num,
+                's': season_num,
+                'e': episode_num
+            }
+            redirect_url = 'http://www.lostfilm.tv/v_search.php'
             try:
-                response = task.requests.get(redirect_url)
+                response = task.requests.get(redirect_url, params=params)
             except RequestException as e:
                 log.error('Could not connect to redirect url: {:s}'.format(e))
                 continue
@@ -114,49 +121,53 @@ class LostFilm(object):
 
             page = get_soup(response.content)
 
-            new_title_template = episode_name_rus = episode_name_eng \
-                = series_name_rus = series_name_eng = None
+            episode_name_rus = episode_name_eng = series_name_rus = \
+                series_name_eng = None
             try:
                 series_name_rus = page.find(
-                    'div', 'inner-box--title').text.strip() or None
+                    'div', 'inner-box--title').text.strip()
                 title_eng_div = page.find(
                     'div', 'inner-box--subtitle').text.strip() or None
-                series_name_eng = title_eng_div[:-8] \
-                    if title_eng_div.endswith(', сериал') else None
+                series_name_eng = (title_eng_div.endswith(', сериал')) \
+                    and title_eng_div[:-8] or None
                 text_div = page.find('div', 'inner-box--text').text.strip() \
                     or None
-                match = TEXT_REGEXP.match(text_div)
-                if match:
-                    season_num = int(match.group(1).strip())
-                    episode_num = int(match.group(2).strip())
-                    episode_name_rus = match.group(3).strip()
-                    episode_name_eng = match.group(4).strip()
-                if series_name_eng and season_num and episode_num:
-                    new_title_template = \
-                        '{}.S{:02d}E{:02d}.{{}}.rus.LostFilm.TV.{{}}.torrent'\
-                        .format(series_name_eng, season_num, episode_num)
+                episode_name_rus, episode_name_eng = \
+                    TEXT_REGEXP.findall(text_div).pop()
             except:
+                log.debug('Cannot parse head info')
                 pass
 
             for item in page.findAll('div', 'inner-box--item'):
-                torrent_link = item.find(
-                    'div', 'inner-box--link sub').a['href'] or None
-                quality = item.find('div', 'inner-box--label').text.strip() \
-                    or None
-                if torrent_link is None or quality is None:
+                torrent_link = quality = None
+                try:
+                    torrent_link = \
+                        item.find('div', 'inner-box--link sub').a['href']
+                    quality = \
+                        item.find('div', 'inner-box--label').text.strip()
+                except:
+                    log.debug('Item doesn\'t have a link or quality')
                     continue
-                if new_title_template:
-                    new_title = new_title_template.format(
-                        quality_map.get(quality, (quality, None))[0],
-                        quality_map.get(quality, (None, 'avi'))[1]
-                        ).replace(' ', '.')
+                if torrent_link is None or quality is None:
+                    log.debug('Item doesn\'t have a link or quality')
+                    continue
+                if series_name_eng:
+                    new_title = \
+                        '.'.join([
+                            series_name_eng,
+                            'S{:02d}E{:02d}'.format(season_num, episode_num),
+                            quality_map.get(quality, (quality, 0))[0],
+                            'rus.LostFilm.TV',
+                            quality_map.get(quality, (0, 'avi'))[1],
+                            'torrent'
+                                  ]).replace(' ', '.')
                 else:
                     if item.get('title') is not None:
-                        new_title = '%s %s'.format(
+                        new_title = '{} {}'.format(
                             item['title'],
-                            quality_map.get(quality, (quality, None))[0])
+                            quality_map.get(quality, (quality, 0))[0])
                     else:
-                        log.error('Item doesn\'t have a title')
+                        log.debug('Item doesn\'t have a title')
                         continue
                 new_entry = Entry()
                 new_entry['url'] = torrent_link
