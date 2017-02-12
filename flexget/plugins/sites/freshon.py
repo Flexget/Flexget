468e1b358b0d2534858cd4432fad74088b365dab
Adrian MAURIN <adrian.maurin@gmail.com>
2017-02-12 15:44:48 +0100

    Cleanup freshon plugin to fit comments & PEP8 standards

--- flexget/plugins/sites/freshon.py
+++ flexget/plugins/sites/freshon.py
@@ -9,9 +9,20 @@ from flexget import plugin
 from flexget.entry import Entry
 from flexget.event import event
 from flexget.utils.soup import get_soup
+from flexget.utils.tools import parse_filesize
 from flexget.utils.search import normalize_unicode
 
 log = logging.getLogger('search_freshon')
+
+BASE_URL = 'https://freshon.tv'
+SEARCH_FRAGMENT = 'browse.php?search=%s&tab=%s&incldead=%s'
+DL_FRAGMENT = 'download.php?type=rss&id=%s&passkey=%s'
+LOGIN_FRAGMENT = 'login.php?action=makelogin'
+LEECHSTATUS = {
+    'all': 0,
+    'free': 3,
+    'half': 4
+}
 
 
 class SearchFreshon(object):
@@ -31,7 +42,7 @@ class SearchFreshon(object):
             'freeleech': {
                 'type': 'string',
                 'default': 'all',
-                'enum': ['free', 'half', 'all']
+                'enum': list(LEECHSTATUS.keys())
             },
         },
         'required': ['username', 'password', 'passkey'],
@@ -69,120 +80,121 @@ class SearchFreshon(object):
             free
             half
         """
-
-        base_url = 'https://freshon.tv'
-        search_fragment = 'browse.php?search=%s&tab=%s&incldead=%s'
-        dl_fragment = 'download.php?type=rss&id=%s&passkey=%s'
-        login_fragment = 'login.php?action=makelogin'
+        self.config = config
 
         if not task.requests.cookies:
-            log.debug('Logging in to Freshon.tv...')
-            params = {'username': config['username'],
-                      'password': config['password'],
-                      'login': 'Do it!'}
-            urlstr = (base_url, login_fragment)
-            lsrc = task.requests.post('/'.join(urlstr), data=params)
-            if str(config['username']) in lsrc.text:
-                log.debug('Login to FreshonTV was successful')
-            elif str('Username does not exist in the userbase') in lsrc.text:
-                log.error('Invalid username or password for FreshonTV; Check your settings')
-                raise plugin.PluginError("Invalid username or Password for FreshonTV.")
-            else:
-                log.error('Login to FreshonTV was NOT successful')
-                raise plugin.PluginError("Login to FreshonTV was NOT successful")
+            self.login(task)
 
-        if config['freeleech'] == 'all':
-            freeleech = 0
-        elif config['freeleech'] == 'free':
-            freeleech = 3
-        elif config['freeleech'] == 'half':
-            freeleech = 4
+        freeleech = LEECHSTATUS[config['freeleech']]
 
         entries = set()
         for search_string in entry.get('search_strings', [entry['title']]):
             query = normalize_unicode(search_string.replace(' ', '+'))
-            query_url_fragment = urllib.quote_plus(query.encode('utf-8'))
-            str_url = (base_url, search_fragment)
-            url = '/'.join(str_url) % (query_url_fragment, config['category'], str(freeleech))
-            log.debug('Search url: %s' % url)
-            page = task.requests.get(url).content
+            query = urllib.quote_plus(query.encode('utf-8'))
+            url = (BASE_URL, SEARCH_FRAGMENT)
+            url = '/'.join(url) % (query, config['category'], freeleech)
+            log.debug('Search url: %s', url)
+            try:
+                page = task.requests.get(url).content
+            except RequestsException:
+                log.error('Could not get page %s, skipping' % url)
+                continue
             soup = get_soup(page)
-            if soup.findAll(text=re.compile('Nothing found. Try again with a refined search string.')):
+            if soup.findAll(text=re.compile('Nothing found. Try again with '
+                                            'a refined search string.')):
                 log.debug('Search returned no results')
             else:
-                max_page_number = 0
-                # Check to see if there is more than 1 page of results
-                pager = soup.find('div', {'class': 'pager'})
-                if pager:
-                    page_links = pager.find_all('a', href=True)
-                else:
-                    page_links = []
-                if len(page_links) > 0:
-                    for lnk in page_links:
-                        link_text = lnk.text.strip()
-                        if link_text.isdigit():
-                            page_int = int(link_text)
-                            if page_int > max_page_number:
-                                max_page_number = page_int
-                else:
-                    max_page_number = 1
-
-                # limit to 10 pages
-                if max_page_number > 10:
-                    max_page_number = 10
+                page_number = self.get_page_number(soup)
 
                 # pages are 0 based
                 nextpage = 0
-                while (nextpage < max_page_number):
+                while (nextpage < page_number):
                     if (nextpage > 0):
                         newurl = url + '&page=' + str(nextpage)
-                        log.debug('-----> NEXT PAGE : %s' % newurl)
+                        log.debug('-----> NEXT PAGE : %s', newurl)
                         f1 = task.requests.get(newurl).content
                         soup = get_soup(f1)
-                    for res in soup.findAll('tr', {'class': re.compile('torrent_[0-9]*')}):
-                        entry = Entry()
-                        # skip if nuked
-                        if res.find('img', alt='Nuked') != None:
-                            continue
-
-                        link = res.find('a', attrs={'href': re.compile('dl-torrent')})
-
-                        entry['title'] = res.find('a', {'class': 'torrent_name_link'})['title']
-
-                        details_url = res.find('a', {'class': 'torrent_name_link'})['href']
-                        id = int((re.match('.*?([0-9]+)$', details_url).group(1)).strip())
-                        str_url = (base_url, dl_fragment)
-                        entry['url'] = '/'.join(str_url) % (str(id), config['passkey'])
-
-                        log.debug('Title: %s | DL LINK: %s' % (entry['title'], entry['url']))
-
-                        seeds = int(res.find('td', {'class': 'table_seeders'}).find('span').text.strip())
-                        leechers = int(res.find('td', {'class': 'table_leechers'}).find('a').text.strip())
-                        entry['torrent_seeds'] = seeds
-                        entry['torrent_leeches'] = leechers
-
-                        size = res.find('td', attrs={'class': re.compile('table_size')}).text
-                        size = re.search('(\d+(?:[.,]\d+)*)\s?([KMG]B)', size)
-
-                        if size:
-                            if size.group(2) == 'GB':
-                                entry['content_size'] = int(float(size.group(1)) * 1000 ** 3 / 1024 ** 2)
-                            elif size.group(2) == 'MB':
-                                entry['content_size'] = int(float(size.group(1)) * 1000 ** 2 / 1024 ** 2)
-                            elif size.group(2) == 'KB':
-                                entry['content_size'] = int(float(size.group(1)) * 1000 / 1024 ** 2)
-                            else:
-                                entry['content_size'] = int(float(size.group(1)) / 1024 ** 2)
-
-                        if (int(entry['torrent_seeds']) > 0):
+                    results = soup.findAll('tr', {
+                        'class': re.compile('torrent_[0-9]*')
+                    })
+                    for res in results:
+                        entry = self.parse_entry(res)
+                        if entry:
                             entries.add(entry)
-                        else:
-                            log.debug('0 SEEDS, not adding entry')
+
                     nextpage += 1
 
         return entries
 
+    def login(self, task):
+        log.debug('Logging in to Freshon.tv...')
+        params = {
+            'username': self.config['username'],
+            'password': self.config['password'],
+            'login': 'Do it!'
+        }
+        url = "%s/%s" % (BASE_URL, LOGIN_FRAGMENT)
+        lsrc = task.requests.post(url, data=params)
+        if self.config['username'] in lsrc.text:
+            log.debug('Login to FreshonTV was successful')
+        elif 'Username does not exist in the userbase' in lsrc.text:
+            log.error('Invalid credentials FreshonTV')
+            raise plugin.PluginError("Invalid credentials for FreshonTV.")
+        else:
+            log.error('Login to FreshonTV was NOT successful')
+            raise plugin.PluginError("Login to FreshonTV was NOT successful")
+
+    def get_page_number(self, html_soup):
+        page_number = 0
+        # Check to see if there is more than 1 page of results
+        pager = html_soup.find('div', {'class': 'pager'})
+        if pager:
+            page_links = pager.find_all('a', href=True)
+        else:
+            page_links = []
+        if len(page_links) > 0:
+            for lnk in page_links:
+                link_text = lnk.text.strip()
+                if link_text.isdigit():
+                    page_int = int(link_text)
+                    if page_int > page_number:
+                        page_number = page_int
+        else:
+            page_number = 1
+
+        # limit to 10 pages
+        if page_number > 10:
+            page_number = 10
+
+    def parse_entry(self, res):
+        entry = Entry()
+
+        entry['title'] = res.find('a', {'class': 'torrent_name_link'})['title']
+        # skip if nuked
+        if res.find('img', alt='Nuked'):
+            log.info('Skipping entry %s (nuked)' % entry['title'])
+            return None
+
+        details_url = res.find('a', {'class': 'torrent_name_link'})['href']
+        id = int((re.match('.*?([0-9]+)$', details_url).group(1)).strip())
+        url = (BASE_URL, DL_FRAGMENT)
+        entry['url'] = '/'.join(url) % (id, self.config['passkey'])
+
+        log.debug('Title: %s | DL LINK: %s', (entry['title'], entry['url']))
+
+        seeds = res.find('td', {'class': 'table_seeders'}) \
+            .find('span').text.strip()
+        leechers = res.find('td', {'class': 'table_leechers'}) \
+            .find('a').text.strip()
+        entry['torrent_seeds'] = int(seeds)
+        entry['torrent_leeches'] = int(leechers)
+
+        size = res.find('td', attrs={'class': re.compile('table_size')}).text
+        entry['content_size'] = parse_filesize(size)
+
+        return entry
+
 
 @event('plugin.register')
 def register_plugin():
-    plugin.register(SearchFreshon, 'freshon', groups=['search'], api_ver=2)
+    plugin.register(SearchFreshon, 'freshon', interfaces=['search'], api_ver=2)
