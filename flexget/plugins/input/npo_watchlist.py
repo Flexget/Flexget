fa21627b9bb8c44624c6f1ca38ba3e1ab4760854
Maarten Fonville <mfonville@users.noreply.github.com>
2017-03-18 09:39:24 +0100

    Update NPO.nl schema.org URL (#1738)
    * Update Schema.org URL (is now https)
    
    * Update npo_watchlist tests with new https schema

--- flexget/plugins/input/npo_watchlist.py
+++ flexget/plugins/input/npo_watchlist.py
@@ -175,7 +175,7 @@ class NPOWatchlist(object):
         log.info('Retrieving series info for %s', series_name)
         response = requests.get(series_url)
         page = get_soup(response.content)
-        tvseries = page.find('div', itemscope='itemscope', itemtype='http://schema.org/TVSeries')
+        tvseries = page.find('div', itemscope='itemscope', itemtype='https://schema.org/TVSeries')
         series_info = Entry()  # create a stub to store the common values for all episodes of this series
         series_info['npo_url'] = tvseries.find('a', itemprop='url')['href']
         series_info['npo_name'] = tvseries.find('span', itemprop='name').contents[0]
