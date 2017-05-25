302c0a51e277f810ad14684406eff1d13935c3f9
Claus Vium <cvium@users.noreply.github.com>
2017-05-25 14:15:49 +0200

    Refactor config hash management (#1817)
    * begun refactor
    
    * forgot something
    
    * fix error msg
    
    * Updated hash schema
    
    * Fixed signature
    
    * Moved config check to happen once at the end of start phase
    
    * Removed all manual plugin config checkups (hopefully)
    
    * Moved config check for remember rejected to input phase since it wont be triggered on task start anymore
    
    * Fixed initial remember task creation
    
    * Added template change test
    
    * Basic include test
    
    * Multiple files
    
    * Added include file change tests

--- flexget/plugins/operate/include.py
+++ flexget/plugins/operate/include.py
@@ -6,26 +6,14 @@ import logging
 import os
 
 import yaml
-from sqlalchemy import Column, Integer, Unicode
 
-from flexget import plugin, db_schema
+from flexget import plugin
 from flexget.config_schema import one_or_more, process_config
 from flexget.event import event
-from flexget.manager import Session
-from flexget.utils.tools import MergeException, merge_dict_from_to, get_config_hash
+from flexget.utils.tools import MergeException
 
 plugin_name = 'include'
 log = logging.getLogger(plugin_name)
-Base = db_schema.versioned_base(plugin_name, 0)
-
-
-class LastHash(Base):
-    __tablename__ = 'include_last_hash'
-
-    id = Column(Integer, primary_key=True)
-    task = Column(Unicode)
-    file = Column(Unicode)
-    hash = Column(Unicode)
 
 
 class PluginInclude(object):
@@ -51,36 +39,23 @@ class PluginInclude(object):
             files = [config]
 
         for file_name in files:
-            file_name = os.path.expanduser(file_name)
-            if not os.path.isabs(file_name):
-                file_name = os.path.join(task.manager.config_base, file_name)
-            with io.open(file_name, encoding='utf-8') as inc_file:
+            file = os.path.expanduser(file_name)
+            if not os.path.isabs(file):
+                file = os.path.join(task.manager.config_base, file)
+            with io.open(file, encoding='utf-8') as inc_file:
                 include = yaml.load(inc_file)
                 inc_file.flush()
             errors = process_config(include, plugin.plugin_schemas(interface='task'))
             if errors:
-                log.error('Included file %s has invalid config:', file_name)
+                log.error('Included file %s has invalid config:', file)
                 for error in errors:
                     log.error('[%s] %s', error.json_pointer, error.message)
-                task.abort('Invalid config in included file %s' % file_name)
+                task.abort('Invalid config in included file %s' % file)
 
-            new_hash = str(get_config_hash(include))
-            with Session() as session:
-                last_hash = session.query(LastHash).filter(LastHash.task == task.name).filter(
-                    LastHash.file == file_name).first()
-                if not last_hash:
-                    log.debug('no config hash detected for task %s with file %s, creating', task.name, file_name)
-                    last_hash = LastHash(task=task.name, file=file_name)
-                    session.add(last_hash)
-                if last_hash.hash != new_hash:
-                    log.debug('new hash detected, triggering config change event')
-                    task.config_changed()
-                last_hash.hash = new_hash
-
-            log.debug('Merging %s into task %s', file_name, task.name)
+            log.debug('Merging %s into task %s', file, task.name)
             # merge
             try:
-                merge_dict_from_to(include, task.config)
+                task.merge_config(include)
             except MergeException:
                 raise plugin.PluginError('Failed to merge include file to task %s, incompatible datatypes' % task.name)
 
