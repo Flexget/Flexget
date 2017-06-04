bb34fb773c41f971e5d7de1b9384bfa90a62cbc8
orcarmi <or.carmi82@gmail.com>
2017-06-05 00:22:05 +0300

    Changed database endpoint to use post data

--- flexget/api/core/database.py
+++ flexget/api/core/database.py
@@ -3,7 +3,7 @@ from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 import logging
 
-from flask import jsonify
+from flask import jsonify, request
 
 from flexget.db_schema import reset_schema, plugin_schemas
 from flexget.api import api, APIResource
@@ -17,8 +17,47 @@ db_api = api.namespace('database', description='Manage Flexget DB')
 class ObjectsContainer(object):
     plugin_list = {'type': 'array', 'items': {'type': 'string'}}
 
+    database_input_object = {
+        'type': 'object',
+        'properties': {
+            'operation': {'type': 'string', 'enum': ['cleanup', 'vacuum', 'list_plugins', 'plugin_reset']}
+        },
+        'required': ['operation_name'],
+        'additionalProperties': False
+    }
+
+    reset_plugin_input = database_input_object.copy()
+    reset_plugin_input['properties']['plugin_name'] = {'type': 'string'}
+
 
 plugins_schema = api.schema('plugins_list', ObjectsContainer.plugin_list)
+input_schema = api.schema('db_schema', ObjectsContainer.database_input_object)
+reset_plugin_input_schema = api.schema('db_schema', ObjectsContainer.reset_plugin_input)
+
+
+@db_api.route('/')
+class DBOperation(APIResource):
+    @api.response(200, model=base_message_schema)
+    def post(self, session=None):
+        data = request.json
+        operation = data['operation']
+        if operation == 'cleanup':
+            self.manager.db_cleanup(force=True)
+            msg = 'DB Cleanup finished'
+        elif operation == 'vacuum':
+            session.execute('VACUUM')
+            session.commit()
+            msg = 'DB VACUUM finished'
+        elif operation == 'list_plugins':
+            return jsonify(sorted(list(plugin_schemas)))
+        elif operation == 'plugin_reset':
+            plugin_name = data['plugin_name']
+            try:
+                reset_schema(plugin_name)
+                msg = 'Plugin {} DB reset was successful'.format(plugin_name)
+            except ValueError:
+                raise BadRequest('The plugin {} has no stored schema to reset'.format(plugin_name))
+        return success_response(msg)
 
 
 @db_api.route('/cleanup/')
@@ -28,42 +67,3 @@ class DBCleanup(APIResource):
         """ Make all plugins clean un-needed data from the database """
         self.manager.db_cleanup(force=True)
         return success_response('DB Cleanup finished')
-
-
-@db_api.route('/vacuum/')
-class DBVacuum(APIResource):
-    @api.response(200, model=base_message_schema)
-    def post(self, session=None):
-        """ Potentially increase performance and decrease database size"""
-        session.execute('VACUUM')
-        session.commit()
-        return success_response('DB VACUUM finished')
-
-
-plugin_parser = api.parser()
-plugin_parser.add_argument('plugin_name', required=True, help='Name of plugin to reset')
-
-
-@db_api.route('/reset_plugin/')
-class DBPluginReset(APIResource):
-    @api.response(200, model=base_message_schema)
-    @api.response(BadRequest)
-    @api.doc(parser=plugin_parser)
-    def post(self, session=None):
-        """ Reset the DB of a specific plugin """
-        args = plugin_parser.parse_args()
-        plugin = args['plugin_name']
-        try:
-            reset_schema(plugin)
-        except ValueError:
-            raise BadRequest('The plugin {} has no stored schema to reset'.format(plugin))
-        return success_response('Plugin {} DB reset was successful'.format(plugin))
-
-
-@db_api.route('/plugins/')
-class DBPluginsSchemas(APIResource):
-    @etag
-    @api.response(200, model=plugins_schema)
-    def get(self, session=None):
-        """ Get a list of plugin names available to reset """
-        return jsonify(sorted(list(plugin_schemas)))
