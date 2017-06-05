8791820be04d5547356c79b3e94915535cd39450
Claus Vium <cvium@users.noreply.github.com>
2017-06-05 12:57:26 +0200

    [add] parser - Support for DD+ audio and more channels (#1842)

--- flexget/tests/test_qualities.py
+++ flexget/tests/test_qualities.py
@@ -100,10 +100,29 @@ class TestQualityParser(object):
         ('Test.File.DTSHD', 'dtshd'),
         ('Test.File.DTS', 'dts'),
         ('Test.File.truehd', 'truehd'),
-        ('Test.File.DTSHDMA', 'dtshd')
+        ('Test.File.DTSHDMA', 'dtshd'),
+        ('Test.File.DD2.0', 'dd5.1'),
+        ('Test.File.AC35.1', 'ac3')
     ])
     def test_quality_failures(self, parser, test_quality):
         quality = parser().parse_movie(test_quality[0]).quality
+        assert str(quality) == test_quality[1], ('`%s` quality should be `%s` not `%s`' % (
+            test_quality[0], test_quality[1], quality
+        ))
+
+
+class TestQualityInternalParser(object):
+    @pytest.mark.parametrize("test_quality", [
+        ('Test.File.DD+5.1', 'dd+5.1'),
+        ('Test.File.DDP5.1', 'dd+5.1'),
+        ('Test.File.DDP7.1', 'dd+5.1'),
+        ('Test.File.DD5.1', 'dd5.1'),
+        ('Test.File.DD4.0', 'dd5.1'),
+        ('Test.File.DD2.1', 'dd5.1'),
+        ('Test.File.FLAC1.0', 'flac'),
+    ])
+    def test_quality_failures(self, test_quality):
+        quality = ParserInternal().parse_movie(test_quality[0]).quality
         assert str(quality) == test_quality[1], ('`%s` quality should be `%s` not `%s`' % (
             test_quality[0], test_quality[1], quality
         ))
@@ -183,3 +202,48 @@ class TestFilterQuality(object):
         assert entry in task.accepted, 'HR should be accepted'
         assert len(task.rejected) == 3, 'wrong number of entries rejected'
         assert len(task.accepted) == 1, 'wrong number of entries accepted'
+
+
+class TestQualityAudio(object):
+    config = """
+        tasks:
+          test_dd_audio_channels:
+            quality: "dd+5.1"
+            mock:
+              - {title: 'My Show S01E05 720p HDTV DD+7.1'}
+              - {title: 'My Show S01E05 720p HDTV DD+5.0'}
+          test_dd_audio_min:
+            quality: ">dd5.1"
+            mock:
+              - {title: 'My Show S01E05 720p HDTV DD5.1'}
+              - {title: 'My Show S01E05 720p HDTV DD+2.0'}
+          test_dd_audio_max:
+            quality: "<=dd5.1"
+            mock:
+              - {title: 'My Show S01E05 720p HDTV DD5.1'}
+              - {title: 'My Show S01E05 720p HDTV DD+5.1'}
+              - {title: 'My Show S01E05 720p HDTV DD+7.1'}
+    """
+
+    def test_dd_audio_channels(self, execute_task):
+        task = execute_task('test_dd_audio_channels')
+        entry = task.find_entry('undecided', title='My Show S01E05 720p HDTV DD+7.1')
+        assert entry, 'Entry "My Show S01E05 720p HDTV DD+7.1" should not have been rejected'
+        assert entry['quality'].audio == 'dd+5.1', 'audio "dd+7.1" should have been parsed as dd+5.1'
+
+        entry = task.find_entry('undecided', title='My Show S01E05 720p HDTV DD+5.0')
+        assert entry['quality'].audio == 'dd+5.1', 'audio "dd+5.0" should have been parsed as dd+5.1'
+
+    def test_dd_audio_min(self, execute_task):
+        task = execute_task('test_dd_audio_min')
+        assert len(task.rejected) == 1, 'should have rejected one'
+        entry = task.find_entry('undecided', title='My Show S01E05 720p HDTV DD+2.0')
+        assert entry, 'Entry "My Show S01E05 720p HDTV DD+2.0" should not have been rejected'
+        assert entry['quality'].audio == 'dd+5.1', 'audio should have been parsed as dd+5.1'
+
+    def test_dd_audio_max(self, execute_task):
+        task = execute_task('test_dd_audio_max')
+        assert len(task.rejected) == 2, 'should have rejected two'
+        entry = task.find_entry('undecided', title='My Show S01E05 720p HDTV DD5.1')
+        assert entry, 'Entry "My Show S01E05 720p HDTV DD5.1" should not have been rejected'
+        assert entry['quality'].audio == 'dd5.1', 'audio should have been parsed as dd5.1'
--- flexget/utils/qualities.py
+++ flexget/utils/qualities.py
@@ -157,13 +157,14 @@ _codecs = [
     QualityComponent('codec', 40, 'h265', '[hx].?265|hevc'),
     QualityComponent('codec', 50, '10bit', '10.?bit|hi10p')
 ]
-channels = '(?:(?:[\W_]?5[\W_]?1)|(?:[\W_]?2[\W_]?(?:0|ch)))'
+channels = '(?:(?:[^\w+]?[1-7][\W_]?(?:0|1|ch)))'
 _audios = [
     QualityComponent('audio', 10, 'mp3'),
     # TODO: No idea what order these should go in or if we need different regexps
     QualityComponent('audio', 20, 'aac', 'aac%s?' % channels),
     QualityComponent('audio', 30, 'dd5.1', 'dd%s' % channels),
     QualityComponent('audio', 40, 'ac3', 'ac3%s?' % channels),
+    QualityComponent('audio', 45, 'dd+5.1', 'dd[p+]%s' % channels),
     QualityComponent('audio', 50, 'flac', 'flac%s?' % channels),
     # The DTSs are a bit backwards, but the more specific one needs to be parsed first
     QualityComponent('audio', 60, 'dtshd', 'dts[\W_]?hd(?:[\W_]?ma)?'),
