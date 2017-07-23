6fbfa5ea52979e42932d7675c1a15817e98e6d6f
cvium <clausvium@gmail.com>
2017-07-23 14:49:53 +0200

    added support for hardlinking files and dirs

--- flexget/plugins/output/symlink.py
+++ flexget/plugins/output/symlink.py
@@ -19,18 +19,30 @@ class Symlink(object):
                 'type': 'object',
                 'properties': {
                     'to': {'type': 'string', 'format': 'path'},
-                    'existing': {'type': 'string', 'enum': ['ignore', 'fail']}
+                    'existing': {'type': 'string', 'enum': ['ignore', 'fail']},
+                    'type': {'type': 'string', 'enum': ['soft', 'hard']}
                 },
+                'required': ['to'],
                 'additionalProperties': False
             },
             {'title': 'specify path', 'type': 'string', 'format': 'path'},
         ]
     }
 
+    def prepare_config(self, config):
+        if not isinstance(config, dict):
+            config = {'to': config}
+
+        config.setdefault('existing', 'fail')
+        config.setdefault('type', 'soft')
+
+        return config
+
     @plugin.priority(0)
     def on_task_output(self, task, config):
         if not config:
             return
+        config = self.prepare_config(config)
         for entry in task.accepted:
             if 'location' not in entry:
                 entry.fail('Does not have location field for symlinking')
@@ -45,11 +57,38 @@ class Symlink(object):
                 else:
                     entry.fail(msg)
                 continue
-            log.verbose('Symlink `%s` to `%s`', lnkfrom, lnkto)
+            log.verbose('%slink `%s` to `%s`', config['type'], lnkfrom, lnkto)
             try:
-                os.symlink(lnkfrom, lnkto)
+                if config['type'] == 'soft':
+                    os.symlink(lnkfrom, lnkto)
+                else:
+                    if os.path.isdir(lnkfrom):
+                        self.hard_link_dir(lnkfrom, lnkto)
+                    else:
+                        os.link(lnkfrom, lnkto)
             except OSError as e:
-                entry.fail('Failed to create symlink, %s' % e.strerror)
+                entry.fail('Failed to create %slink, %s' % (config['type'], e))
+
+    def hard_link_dir(self, path, destination):
+        if not os.path.exists(destination):
+            try:
+                os.mkdir(destination)
+            except OSError as e:
+                # Raised when it already exists, but are there other cases?
+                log.debug('Failed to create destination dir %s: %s', destination, e)
+        # 'recursively' traverse and hard link
+        for root, dirs, files in os.walk(path):
+            dest_dir = os.path.join(destination, root)
+            for dir in dirs:
+                try:
+                    os.mkdir(dir)
+                except OSError as e:
+                    # Raised when it already exists, but are there other cases?
+                    log.debug('Failed to create subdir %s: %s', dir, e)
+            for f in files:
+                src_file = os.path.join(root, f)
+                dst_file = os.path.join(dest_dir, f)
+                os.link(src_file, dst_file)
 
 
 @event('plugin.register')
