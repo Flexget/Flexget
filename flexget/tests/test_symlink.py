2a8f3bcf3ea8dc46abe678413b0ca8131357047f
cvium <clausvium@gmail.com>
2017-07-24 19:23:51 +0200

    test pls work

--- flexget/tests/test_symlink.py
+++ flexget/tests/test_symlink.py
@@ -26,32 +26,22 @@ class TestSymlink(object):
         tasks:
           test_hardlink:
             mock:
-              - {title: 'test1.mkv', location: 'symlink_test_dir/test1.mkv'}
+              - {title: 'test1.mkv', location: '__tmp__/symlink_test_dir/test1.mkv'}
             symlink:
-              to: 'symlink_test_dir/hardlink'
+              to: '__tmp__/symlink_test_dir/hardlink'
               link_type: 'hard'
     """
 
-    def setup_method(self, test_method):
+    def setup_method(self, test_method, tmpdir):
         # Create required dirs for tests
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
+        tmp = tmpdir.mkdir(dirname)
 
         for subdir in subdirs:
-            if not os.path.exists(os.path.join(dirname, subdir)):
-                os.mkdir(os.path.join(dirname, subdir))
+            tmp.mkdir(os.path.join(dirname, subdir))
 
-    def teardown_method(self, test_method):
-        # teardown
-        shutil.rmtree(dirname)
-
-    def test_sort_by(self, execute_task):
-        os.utime('symlink_test_dir/test1.mkv', None)
+    def test_hardlink(self, execute_task, tmpdir):
+        tmpdir.join(dirname).join('test1.mkv').write('')
         execute_task('test_hardlink')
-        assert os.path.exists('symlink_test_dir/hardlink/test1.mkv'), 'symlink_test_dir/hardlink/test1.mkv should exist'
-        assert is_hard_link('symlink_test_dir/hardlink/test1.mkv', 'symlink_test_dir/test1.mkv')
-
-        try:
-            os.remove('symlink_test_dir/test1.mkv')
-        except OSError:
-            pass
+        org_file = tmpdir.join(dirname).join('test1.mkv')
+        assert os.path.exists(org_file), 'test1.mkv should exist'
+        assert is_hard_link(org_file, tmpdir.join(dirname).join('hardlink').join('test1.mkv'))
