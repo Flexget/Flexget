3299bc6859949c4cf793640ea8979ffc5203a6bc
Artem Alexandrov <artem.alexandrov@corp.mail.ru>
2017-09-24 15:14:58 +0300

    Additional options for adding Sonarr series

--- flexget/plugins/list/sonarr_list.py
+++ flexget/plugins/list/sonarr_list.py
@@ -26,7 +26,10 @@ class SonarrSet(MutableSet):
             'api_key': {'type': 'string'},
             'include_ended': {'type': 'boolean', 'default': True},
             'only_monitored': {'type': 'boolean', 'default': True},
-            'include_data': {'type': 'boolean', 'default': False}
+            'include_data': {'type': 'boolean', 'default': False},
+            'searchForMissingEpisodes': {'type': 'boolean', 'default': True},
+            'ignoreEpisodesWithoutFiles': {'type': 'boolean', 'default': False},
+            'ignoreEpisodesWithFiles': {'type': 'boolean', 'default': False}
         },
         'required': ['api_key', 'base_url'],
         'additionalProperties': False
@@ -193,6 +196,9 @@ class SonarrSet(MutableSet):
         show['profileId'] = 1
         show['qualityProfileId '] = 1
         show['rootFolderPath'] = rootfolder[0]['path']
+        show['addOptions'] = {"ignoreEpisodesWithFiles": self.config.get('ignoreEpisodesWithFiles'),
+                              "ignoreEpisodesWithoutFiles": self.config.get('ignoreEpisodesWithoutFiles'),
+                              "searchForMissingEpisodes": self.config.get('searchForMissingEpisodes')}
 
         series_url, series_headers = self.request_builder(self.config.get('base_url'), 'series',
                                                           self.config.get('port'), self.config['api_key'])
