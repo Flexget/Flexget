be3159dd2d78f59f50592d8a8d3fa5f1434fc6a4
cvium <clausvium@gmail.com>
2017-10-14 12:58:00 +0200

    changed the name of the test to better reflect its purpose

--- flexget/tests/test_seriesparser.py
+++ flexget/tests/test_seriesparser.py
@@ -608,7 +608,7 @@ class TestSeriesParser(object):
         assert s.quality.name == '720p hdtv h264 aac'
         assert not s.proper, 'detected proper'
 
-    def test_season_pack_fallback_to_episode(self, parse):
+    def test_episode_with_season_pack_match_is_not_season_pack(self, parse):
         """SeriesParser: Github issue #1986, s\d{1} parses as invalid season"""
         s = parse(name='The Show', data='The.Show.S01E01.eps3.0.some.title.720p.x264-NOGRP')
         assert s.valid
