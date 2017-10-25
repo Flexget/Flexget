de4fc4cb7d3a8aad530ccd47d4e048bf8960556c
Oleg Vivtash <oleg@vivtash.net>
2017-10-25 03:48:37 +0300

    LostFilm site/source plugin update

--- flexget/plugins/sites/lostfilm.py
+++ flexget/plugins/sites/lostfilm.py
@@ -4,9 +4,6 @@ from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 import logging
-import re
-
-import feedparser
 
 from flexget import plugin
 from flexget.entry import Entry
@@ -15,29 +12,22 @@ from flexget.plugin import PluginError
 from flexget.utils.requests import RequestException
 from flexget.utils.soup import get_soup
 
-__author__ = 'danfocus'
+__author__ = 'danfocus, duhast'
 
 log = logging.getLogger('lostfilm')
 
-EPISODE_REGEXP = re.compile(
-    '.*lostfilm.tv/series/.*/season_(\d+)/episode_(\d+)/.*')
-LOSTFILM_ID_REGEXP = re.compile(
-    '.*static.lostfilm.tv/Images/(\d+)/Posters/.*')
-TEXT_REGEXP = re.compile('^\d+\sсезон\s\d+\sсерия\.\s(.+)\s\((.+)\)$')
-
-quality_map = {
+NEW_RELEASES_URL = 'http://www.lostfilm.tv/new'
+V_SEARCH_URL = 'http://www.lostfilm.tv/v_search.php'
+QUALITY_MAP = {
     'SD': ('480p', 'avi'),
     '1080': ('1080p', 'mkv'),
     'MP4': ('720p', 'mp4'),
     'HD': ('720p', 'mkv')
 }
 
-LOSTFILM_URL = 'http://lostfilm.tv/rss.xml'
-
-
 class LostFilm(object):
     """
-    Change new lostfilm's rss links
+    Fetches new LostFilm.TV releases
 
     Example::
 
@@ -47,147 +37,128 @@ class LostFilm(object):
 
       lostfilm:
         url: <url>
+        pages: <number>
     """
 
     schema = {
         'type': ['boolean', 'object'],
         'properties': {
             'url': {'type': 'string', 'format': 'url'},
+            'pages': {'type': 'integer'}
         },
         'additionalProperties': False
     }
 
     def build_config(self, config):
-        """Set default url to config"""
+        """Set default url and pages config"""
         if isinstance(config, bool) and config is True:
-            config = {'url': LOSTFILM_URL}
+            config = {'url': NEW_RELEASES_URL, 'pages': 3}
         return config
 
     def on_task_input(self, task, config):
         config = self.build_config(config)
         if config is False:
             return
-        try:
-            rss = feedparser.parse(config['url'])
-        except:
-            raise PluginError('Cannot parse rss feed')
-        if rss.get('status') != 200:
-            raise PluginError('Received not 200 (OK) status')
+
         entries = []
-        for item in rss.entries:
-            if item.get('link') is None:
-                log.debug('Item doesn\'t have a link')
-                continue
-            if item.get('description') is None:
-                log.debug('Item doesn\'t have a description')
-                continue
+        for page_num in range(1, config['pages']):
+            # Fetch release page
+            release_url = '{:s}/page_{:d}'.format(config['url'], page_num)
+            log.debug('Fetching page {:d} of {:d}'.format(page_num, config['pages']))
             try:
-                lostfilm_num = LOSTFILM_ID_REGEXP.search(
-                    item['description']).groups()
-            except:
-                log.debug('Item doesn\'t have lostfilm id in description')
-                continue
-            try:
-                season_num, episode_num = [
-                    int(x) for x in EPISODE_REGEXP.search(
-                        item['link']).groups()]
-            except:
-                log.debug('Item doesn\'t have episode id in link')
-                continue
-            params = {
-                'c': lostfilm_num,
-                's': season_num,
-                'e': episode_num
-            }
-            redirect_url = 'http://www.lostfilm.tv/v_search.php'
-            try:
-                response = task.requests.get(redirect_url, params=params)
+                response = task.requests.get(release_url)
             except RequestException as e:
-                log.error('Could not connect to redirect url: {:s}'.format(e))
-                continue
+                raise PluginError('Could not fetch new releases page: {:s}'.format(e))
 
-            page = get_soup(response.content)
             try:
-                redirect_url = page.head.meta['content'].split('url=')[1]
+                index_page = get_soup(response.content)
             except:
-                log.error('Missing redirect')
-                continue
-
-            try:
-                response = task.requests.get(redirect_url)
-            except RequestException as e:
-                log.error('Could not connect to redirect url2: {:s}'.format(e))
-                continue
-
-            page = get_soup(response.content)
-
-            episode_name_rus = episode_name_eng = series_name_rus = None
-            series_name_eng = None
-            try:
-                series_name_rus = page.find(
-                    'div', 'inner-box--title').text.strip()
-                title_eng_div = page.find(
-                    'div', 'inner-box--subtitle').text.strip() or None
-                series_name_eng = (title_eng_div.endswith(
-                    ', сериал')) and title_eng_div[:-8] or None
-                text_div = page.find(
-                    'div', 'inner-box--text').text.strip() or None
-                episode_name_rus, episode_name_eng = TEXT_REGEXP.findall(
-                    text_div).pop()
-            except:
-                log.debug('Cannot parse head info')
-                pass
-
-            episode_id = 'S{:02d}E{:02d}'.format(
-                season_num, episode_num)
-
-            for item in page.findAll('div', 'inner-box--item'):
-                torrent_link = quality = None
+                raise PluginError('Cannot parse new releases page')
+            # Iterate over releases
+            seen_buttons = index_page.find_all('div', 'haveseen-btn')
+            for seen_btn in seen_buttons:
                 try:
-                    torrent_link = item.find(
-                        'div', 'inner-box--link sub').a['href']
-                    quality = item.find('div', 'inner-box--label').text.strip()
+                    details_pane = seen_btn.parent.find('div', 'details-pane')
+
+                    episode_name_rus = details_pane.find('div', 'alpha').text.strip()
+                    episode_name_eng = details_pane.find('div', 'beta').text.strip()
+                    series_name_rus = details_pane.parent.find('div', 'name-ru').text.strip()
+                    series_name_eng = details_pane.parent.find('div', 'name-en').text.strip()
+                    lostfilm_num, season_num, episode_num = [int(x) for x in seen_btn['data-code'].split('-')]
+                except AttributeError:
+                    log.exception('Could not parse {:s}, probably HTML markup was changed'.format(release_url))
+                    continue
+
+                params = {'c': lostfilm_num, 's': season_num, 'e': episode_num}
+                redirect_url = V_SEARCH_URL
+                try:
+                    response = task.requests.get(redirect_url, params=params)
+                except RequestException as e:
+                    log.error('Could not connect to redirect url: {:s}'.format(e))
+                    continue
+
+                page = get_soup(response.content)
+                try:
+                    redirect_url = page.head.meta['content'].split('url=')[1]  # retre.org
                 except:
-                    log.debug('Item doesn\'t have a link or quality')
+                    log.error('Missing redirect')
                     continue
-                if torrent_link is None or quality is None:
-                    log.debug('Item doesn\'t have a link or quality')
+
+                try:
+                    response = task.requests.get(redirect_url)
+                except RequestException as e:
+                    log.error('Could not connect to RETRE redirect url: {:s}'.format(e))
                     continue
-                if quality_map.get(quality):
-                    quality, file_ext = quality_map.get(quality)
-                else:
-                    file_ext = 'avi'
-                if series_name_eng:
-                    new_title = '.'.join(
-                        [
-                            series_name_eng,
-                            episode_id,
-                            quality,
-                            'rus.LostFilm.TV',
-                            file_ext,
-                            'torrent'
-                        ]
-                    ).replace(' ', '.')
-                else:
-                    if item.get('title') is not None:
-                        new_title = '{} {}'.format(
-                            item['title'],
-                            quality)
-                    else:
-                        log.debug('Item doesn\'t have a title')
+
+                page = get_soup(response.content)
+
+                episode_id = 'S{:02d}E{:02d}'.format(season_num, episode_num)
+                for item in page.findAll('div', 'inner-box--item'):
+                    torrent_link = quality = None
+                    try:
+                        torrent_link = item.find('div', 'inner-box--link sub').a['href']  # http://tracktor.in/...
+                        quality = item.find('div', 'inner-box--label').text.strip()
+                    except:
+                        log.debug('Item doesn\'t have a link or quality')
                         continue
-                new_entry = Entry()
-                new_entry['url'] = torrent_link
-                new_entry['title'] = new_title.strip()
-                if series_name_rus:
-                    new_entry['series_name_rus'] = series_name_rus
-                if episode_name_rus:
-                    new_entry['episode_name_rus'] = episode_name_rus
-                if series_name_eng:
-                    new_entry['series_name_eng'] = series_name_eng
-                if episode_name_eng:
-                    new_entry['episode_name_eng'] = episode_name_eng
-                entries.append(new_entry)
+                    if torrent_link is None or quality is None:
+                        log.debug('Item doesn\'t have a link or quality')
+                        continue
+                    if QUALITY_MAP.get(quality):
+                        quality, file_ext = QUALITY_MAP.get(quality)
+                    else:
+                        file_ext = 'avi'
+                    if series_name_eng:
+                        new_title = '.'.join(
+                            [
+                                series_name_eng,
+                                episode_id,
+                                quality,
+                                'rus.LostFilm.TV',
+                                file_ext,
+                                'torrent'
+                            ]
+                        ).replace(' ', '.')
+                    else:
+                        if item.get('title') is not None:
+                            new_title = '{} {}'.format(
+                                item['title'],
+                                quality)
+                        else:
+                            log.debug('Item doesn\'t have a title')
+                            continue
+                    new_entry = Entry()
+                    new_entry['url'] = torrent_link
+                    new_entry['title'] = new_title.strip()
+                    if series_name_rus:
+                        new_entry['series_name_rus'] = series_name_rus
+                    if episode_name_rus:
+                        new_entry['episode_name_rus'] = episode_name_rus
+                    if series_name_eng:
+                        new_entry['series_name_eng'] = series_name_eng
+                    if episode_name_eng:
+                        new_entry['episode_name_eng'] = episode_name_eng
+                    entries.append(new_entry)
 
         return entries
 
