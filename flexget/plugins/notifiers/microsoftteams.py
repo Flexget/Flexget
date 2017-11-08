9ce243721d351c61fb09add1ecb7db1e117c65bf
Petri Saarilammi <github@saarilammi.com>
2017-11-08 15:41:11 +0200

    Code style fixes and update to reflect new style of notifiers

--- flexget/plugins/notifiers/microsoftteams.py
+++ flexget/plugins/notifiers/microsoftteams.py
@@ -10,49 +10,47 @@ from flexget.utils.requests import Session as RequestSession
 
 requests = RequestSession(max_retries=3)
 
-__name__ = 'microsoftteams'
+plugin_name = 'ms_teams'
 
-log = logging.getLogger(__name__)
+log = logging.getLogger(plugin_name)
 
-class TeamsNotifier(object):
+
+class MsTeamsNotifier(object):
     """
     Example:
 
-      teams:
-        web_hook_url: <string>
-        [message: <string>]
-        [title: <string>]
-        [themecolor: <string>]
+      notify:
+        entries:
+          via:
+            ms_teams:
+              web_hook_url: <string>
+              [message: <string>]
+              [title: <string>]
+              [theme_color: <string>]
     """
     schema = {
         'type': 'object',
         'properties': {
             'web_hook_url': {'type': 'string'},
-            'message': {'type': 'string'},
             'title': {'type': 'string'},
-            'themecolor': {'type': 'string'},
+            'theme_color': {'type': 'string'},
         },
-        'required': ['web_hook_url', 'message'],
+        'required': ['web_hook_url'],
         'additionalProperties': False
     }
 
     def notify(self, web_hook_url, message, config, title=None, themecolor=None):
         """
         Send notification to Microsoft Teams
-
-        :param str web_hook_url: WebHook Url
-        :param str message: Notification message
-        :param str title: Message title
-        :return:
         """
-
-        notification = {'text': message, 'title': title, 'themecolor': themecolor}
+        notification = {'text': message, 'title': title, 'theme_color': themecolor}
 
         try:
             requests.post(config['web_hook_url'], json=notification)
         except RequestException as e:
             raise PluginWarning(e.args[0])
 
+
 @event('plugin.register')
 def register_plugin():
-    plugin.register(TeamsNotifier, __name__, api_ver=2, groups=['notifiers'])
+    plugin.register(MsTeamsNotifier, plugin_name, api_ver=2, interfaces=['notifiers'])
