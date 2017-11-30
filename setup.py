86aac7842dbe71eb06a88b0e83b2149945ef60b0
Or Carmi <or.carmi82@gmail.com>
2017-11-30 13:05:18 +0200

    removed 3.3 from classifiers and added python_requires key word to validate installation only on supported python version

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'console_scripts': ['flexget = flexget:main'],
         'gui_scripts': ['flexget-headless = flexget:main']  # This is useful on Windows to avoid a cmd popup
     },
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
@@ -51,7 +52,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
