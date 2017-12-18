c677e90750bcd44f8f6af7da37b6a7191822baf0
Journey <timtag1190@gmail.com>
2017-12-18 09:52:00 -0600

    Make comments less than 120 chars & multi line.
    I hope this is better >..<

--- flexget/plugins/list/sonarr_list.py
+++ flexget/plugins/list/sonarr_list.py
@@ -33,7 +33,10 @@ class SonarrSet(MutableSet):
             'profile_id': {'type': 'integer', 'default': 1},
             'season_folder': {'type': 'boolean', 'default': False},
             'monitored': {'type': 'boolean', 'default': True},
-            'root_folder_path': {'type': 'integer', 'default': 1}, # Users are passing actual rootFolderPath ID (number (integer) starting from 1 and going up) as apposed to an number (integer) system starting from 0. Passing root_folder_path as a string seems to make the script fail, so that's why it's coded as an integerinstead. Read comment on show['rootFolderPath'] for more information.
+            # Users are passing actual RootFolderPath ID instead of starting from 0 and counting up. 
+            # Passing root_folder_path as a String seems to make the script fail, so had to use int instead.
+            # Read comment on show['rootFolderPath'] for more information.
+            'root_folder_path': {'type': 'integer', 'default': 1},
             'series_type': {'type': 'string', 'enum': ['standard', 'daily', 'anime'], 'default': 'standard'},
             'tags': {'type': 'array', 'items': {'type': 'integer'}, 'default': [0]}
         },
@@ -205,7 +208,10 @@ class SonarrSet(MutableSet):
         show['monitored'] = self.config.get('monitored')
         show['seriesType'] = self.config.get('series_type')
         show['tags'] = self.config.get('tags')
-        show['rootFolderPath'] = rootfolder[self.config.get('root_folder_path') - 1]['path'] # Requires both rootFolderPath and path otherwise it fails, takes root_folder_path ID from config and subtracts 1 from it to due to how script is setup (users are passing actual ID so it won't be confusing to users), code may/will only seem confusing to people who look at it from behind the scenes.
+        # Requires both rootFolderPath and path otherwise it fails.
+        # takes root_folder_path ID from config and subtracts 1 from it due to how script is setup
+        # since users are passing actual ID it won't be confusing to users
+        show['rootFolderPath'] = rootfolder[self.config.get('root_folder_path') - 1]['path'] 
         show['addOptions'] = {"ignoreEpisodesWithFiles": self.config.get('ignore_episodes_with_files'),
                               "ignoreEpisodesWithoutFiles": self.config.get('ignore_episodes_without_files'),
                               "searchForMissingEpisodes": self.config.get('search_missing_episodes')}
