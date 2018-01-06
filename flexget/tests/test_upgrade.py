3f26ab3f42e9a6099a9af68e31ee10518c29c0f2
Steve Adams <stevezau@gmail.com>
2018-01-06 17:32:27 +1100

    remove unused import

--- flexget/tests/test_upgrade.py
+++ flexget/tests/test_upgrade.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
-import pytest
-
 from flexget.manager import Session
 from flexget.plugins.filter.upgrade import EntryUpgrade
 
