c024a175170a8b646d10e79bc492d8daffc7e54b
Steve Adams <stevezau@gmail.com>
2018-01-07 20:24:36 +1100

    add best_quality filter plugin

---
+++ flexget/tests/test_best_quality.py
@@ -0,0 +1,47 @@
+from __future__ import unicode_literals, division, absolute_import
+from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
+
+
+class TestBestQuality(object):
+    config = """
+        tasks:
+          best_accept:
+            best_quality:
+              on_best: accept
+            mock:
+              - {title: 'Movie.720p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+              - {title: 'Movie.1080p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+          best_skip:
+            best_quality:
+              on_best: skip
+            mock:
+              - {title: 'Movie.720p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+              - {title: 'Movie.1080p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+          lower_skip:
+            best_quality:
+              on_lower: skip
+            mock:
+              - {title: 'Movie.720p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+              - {title: 'Movie.1080p.WEB-DL.X264.AC3-GRP1', 'id': 'Movie'}
+    """
+
+    def test_best_accept(self, execute_task):
+        task = execute_task('best_accept')
+        entry = task.find_entry('accepted', title='Movie.1080p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.1080p.WEB-DL.X264.AC3-GRP1 should be accepted'
+        entry = task.find_entry('rejected', title='Movie.720p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.720p.WEB-DL.X264.AC3-GRP1 should be rejected'
+
+    def test_best_skip(self, execute_task):
+        task = execute_task('best_skip')
+        entry = task.find_entry('undecided', title='Movie.1080p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.1080p.WEB-DL.X264.AC3-GRP1 should be undecided'
+        entry = task.find_entry('rejected', title='Movie.720p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.720p.WEB-DL.X264.AC3-GRP1 should be rejected'
+
+    def test_lower_skip(self, execute_task):
+        task = execute_task('lower_skip')
+        entry = task.find_entry('undecided', title='Movie.1080p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.1080p.WEB-DL.X264.AC3-GRP1 should be undecided'
+        entry = task.find_entry('undecided', title='Movie.720p.WEB-DL.X264.AC3-GRP1')
+        assert entry, 'Movie.720p.WEB-DL.X264.AC3-GRP1 should be undecided'
