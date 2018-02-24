f9244829e74b8e5008afb8bcb219a9b3fa33fed2
cvium <clausvium@gmail.com>
2018-02-24 13:54:38 +0100

    added str

--- flexget/plugins/parsers/parser_common.py
+++ flexget/plugins/parsers/parser_common.py
@@ -208,3 +208,14 @@ class SeriesParseResult(object):
                 return self.identifier
         else:
             return self.identifier
+
+    def __str__(self):
+        # for some fucking reason it's impossible to print self.field here, if someone figures out why please
+        # tell me!
+        valid = 'INVALID'
+        if self.valid:
+            valid = 'OK'
+        return '<SeriesParseResult(data=%s,name=%s,id=%s,season=%s,season_pack=%s,episode=%s,quality=%s,proper=%s,' \
+               'status=%s)>' % \
+               (self.data, self.name, str(self.id), self.season, self.season_pack, self.episode, self.quality,
+                self.proper_count, valid)
