e2889d48cc46091bc8249ede8419fe2d3e1ed881
bagobones <bagobones@users.noreply.github.com>
2018-04-21 11:17:44 -0700

    Removed unused json import

--- flexget/plugins/input/ombi.py
+++ flexget/plugins/input/ombi.py
@@ -3,7 +3,7 @@ from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 from future.moves.urllib.parse import urlparse
 
 import logging
-from flexget.utils import requests, json
+from flexget.utils import requests
 from flexget.utils.requests import RequestException
 from flexget import plugin
 from flexget.event import event
