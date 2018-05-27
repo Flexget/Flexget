63708cb7abd5c9534726c18a52f537b20613ba63
orcarmi <ocarmi@proofpoint.com>
2018-05-27 17:49:34 +0300

    converted cache

--- flexget/utils/bittorrent.py
+++ flexget/utils/bittorrent.py
@@ -1,9 +1,6 @@
 """Torrenting utils, mostly for handling bencoding and torrent files."""
 # Torrent decoding is a short fragment from effbot.org. Site copyright says:
 # Test scripts and other short code fragments can be considered as being in the public domain.
-from __future__ import unicode_literals, division, absolute_import
-from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
-
 import binascii
 import functools
 import re
--- flexget/utils/cache.py
+++ flexget/utils/cache.py
@@ -1,11 +1,8 @@
-from __future__ import unicode_literals, division, absolute_import
-from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
-
 import hashlib
-import io
-import os
+from pathlib import Path
 
 import requests
+
 from flexget.utils.tools import log
 
 
@@ -22,56 +19,50 @@ def cached_resource(url, base_dir, force=False, max_size=250, directory='cached_
     """
     mime_type = None
     hashed_name = hashlib.md5(url.encode('utf-8')).hexdigest()
-    file_path = os.path.join(base_dir, directory, hashed_name)
-    directory = os.path.dirname(file_path)
+    file_path = Path(base_dir) / directory / hashed_name
+    directory = file_path.parent
 
-    if not os.path.exists(file_path) or force:
+    if not file_path.exists() or force:
         log.debug('caching %s', url)
         response = requests.get(url)
         response.raise_for_status()
         mime_type = response.headers.get('content-type')
         content = response.content
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        directory.mkdir(parents=True, exist_ok=True)
 
         # Checks directory size and trims if necessary.
-        size = dir_size(directory) / (1024 * 1024.0)
+        size = dir_size(directory) / (1_024 * 1_024.0)
         if not force:
             while size >= max_size:
                 log.debug('directory %s size is over the allowed limit of %s, trimming', size, max_size)
                 trim_dir(directory)
-                size = dir_size(directory) / (1024 * 1024.0)
+                size = dir_size(directory) / (1_024 * 1_024.0)
+        file_path.write_bytes(content)
 
-        with io.open(file_path, 'wb') as file:
-            file.write(content)
     return file_path, mime_type
 
 
-def dir_size(directory):
+def dir_size(directory: Path):
     """
     Sums the size of all files in a given dir. Not recursive.
 
     :param directory: Directory to check
     :return: Summed size of all files in Bytes.
     """
-    size = 0
-    for file in os.listdir(directory):
-        filename = os.path.join(directory, file)
-        size += os.path.getsize(filename)
-    return size
+    return sum(file.stat().st_size for file in directory.iterdir())
 
 
-def trim_dir(directory):
+def trim_dir(directory: Path):
     """
     Removed the least accessed file on a given dir
 
     :param directory: Directory to check
     """
 
-    def access_time(f):
-        return os.stat(os.path.join(directory, f)).st_atime
+    def access_time(file: Path):
+        return file.stat().st_atime
 
-    files = sorted(os.listdir(directory), key=access_time)
-    file_name = os.path.join(directory, files[0])
+    files = sorted(directory.iterdir(), key=access_time)
+    file_name = directory / files[0]
     log.debug('removing least accessed file: %s', file_name)
-    os.remove(file_name)
+    file_name.unlink()
