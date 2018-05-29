1605b58f3788b56e38627002c5ae52dcdf540036
blastcodem <olygraph.po@gmail.com>
2018-05-28 22:08:20 -0700

    Updated Categories from website
    Added new naming for new categories
    Added to option to new categories name (Kept old one for backward compatiblity)
    
    I can update wiki when pulled

--- flexget/plugins/sites/torrentleech.py
+++ flexget/plugins/sites/torrentleech.py
@@ -23,12 +23,23 @@ CATEGORIES = {
     # Movies
     'Cam': 8,
     'TS': 9,
+    'TS/TC': 9,
+#    R5 is deprecated
     'R5': 10,
     'DVDRip': 11,
-    'DVDR': 12,
-    'HD': 13,
+	'DVDRip/DVDScreener': 11,
+	'WEBRip': 37,
+	'HDRip': 43,
     'BDRip': 14,
+	'BlurayRip': 14,
+    'DVDR': 12,
+    'DVD-R': 12,
+    'HD': 13,
+    'Bluray': 13,
+    '4KUpscaled': 41,
+    'Real4K': 47,
     'Movie Boxsets': 15,
+    'Boxsets': 15,
     'Documentaries': 29,
 
     # TV
@@ -48,9 +59,10 @@ class UrlRewriteTorrentleech(object):
           password: xxxxxxxx  (required)
           category: HD
 
-          Category is any combination of: all, Cam, TS, R5,
-          DVDRip, DVDR, HD, BDRip, Movie Boxsets, Documentaries,
-          Episodes, TV BoxSets, Episodes HD
+          Category is any combination of: all, Cam, TS, TS/TC, DVDRip,
+				DVDRip/DVDScreener, WEBRip': 37, HDRip': 43, BDRip, DVDR,
+				DVD-R, HD, Bluray, 4KUpscaled, Real4K, Movie Boxsets,
+				Boxsets': 15, Documentaries, Episodes, TV Boxsets, Episodes HD
     """
 
     schema = {
