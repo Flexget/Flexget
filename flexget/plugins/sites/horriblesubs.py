15cd7bf1cdea4556b0bad0c3ac0ec09362cabc1c
Liboicl <liboicl@gmail.com>
2018-07-09 17:47:59 -0400

    [fix] horriblesubs: updated to work with new site layout, fixes #2166 (#2169)

--- flexget/plugins/sites/horriblesubs.py
+++ flexget/plugins/sites/horriblesubs.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 import logging
+import re
 
 from flexget import plugin
 from flexget.entry import Entry
@@ -20,6 +21,32 @@ class HorribleSubs(object):
     schema = {'type': 'boolean'}
 
     @staticmethod
+    def horrible_get_downloads(requests, title, page_url):
+        entries = []
+
+        try:
+            soup = get_soup(requests.get(page_url).content)
+        except RequestException as e:
+            log.error('HorribleSubs request failed: %s', e)
+            return entries
+        for div in soup.findAll('div', attrs={'class': 'rls-link'}):
+            ttitle = '{0} [{1}]'.format(title,re.sub(r'.*-', '', div['id']))
+            urls = []
+            for url in div.findAll('a'):
+				# skip non torrent based links
+                if 'hs-ddl-link' in url.parent.attrs['class'] or 'hs-xdcc-link' in url.parent.attrs['class']:
+                    continue
+                log.debug('Found url `%s`', url)
+                urls.append(url.attrs['href'])
+            # move magnets to last, a bit hacky
+            for url in urls[:]:
+                if url.startswith('magnet'):
+                    urls.remove(url)
+                    urls.append(url)
+            entries.append(Entry(title=ttitle, url=urls[0], urls=urls))
+        return entries
+
+    @staticmethod
     def horrible_entries(requests, page_url):
         entries = []
 
@@ -29,23 +56,19 @@ class HorribleSubs(object):
             log.error('HorribleSubs request failed: %s', e)
             return entries
 
-        for td_label in soup.findAll('td', attrs={'class': 'dl-label'}):
-            title = '[HorribleSubs] {0}'.format(str(td_label.find('i').string))
-            urls = []
+        for li_label in soup.findAll('li'):
+            title = '[HorribleSubs] {0}{1}'.format(str(li_label.find('span').next_sibling),str(li_label.find('strong').text))
             log.debug('Found title `%s`', title)
-            for span in td_label.parent.findAll('span', attrs={'class': 'dl-link'}):
-                # skip non torrent based links
-                if 'hs-ddl-link' in span.parent.attrs['class']:
-                    continue
-                url = str(span.find('a').attrs['href'])
-                log.debug('Found url `%s`', url)
-                urls.append(url)
-            # move magnets to last, a bit hacky
-            for url in urls[:]:
-                if url.startswith('magnet'):
-                    urls.remove(url)
-                    urls.append(url)
-            entries.append(Entry(title=title, url=urls[0], urls=urls))
+            url = li_label.find('a')['href']
+            episode = re.sub(r'.*#', '', url)
+            # Get show ID
+            try:
+                soup = get_soup(requests.get('https://horriblesubs.info/{0}'.format(url)).content)
+            except RequestException as e:
+                log.error('HorribleSubs request failed: %s', e)
+                return entries
+            show_id = re.sub(r'[^0-9]', '', soup(text=re.compile('hs_showid'))[0])
+            entries = HorribleSubs.horrible_get_downloads(requests, title, 'https://horriblesubs.info/api.php?method=getshows&type=show&mode=filter&showid={0}&value={1}'.format(show_id,episode))
         return entries
 
     @staticmethod
@@ -63,7 +86,7 @@ class HorribleSubs(object):
         if not config:
             return
         scraper = HorribleSubs.scraper()
-        return HorribleSubs.horrible_entries(scraper, 'http://horriblesubs.info/lib/latest.php')
+        return HorribleSubs.horrible_entries(scraper, 'https://horriblesubs.info/api.php?method=getlatest')
 
     # Search API method
     def search(self, task, entry, config):
@@ -74,7 +97,7 @@ class HorribleSubs(object):
         for search_string in entry.get('search_strings', [entry['title']]):
             log.debug('Searching `%s`', search_string)
             results = HorribleSubs.horrible_entries(
-                scraper, 'http://horriblesubs.info/lib/search.php?value={0}'.format(search_string))
+                scraper, 'https://horriblesubs.info/api.php?method=search&value={0}'.format(search_string))
             entries.extend(results)
         return entries
 
