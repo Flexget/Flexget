6c1c66495b7bd1346a7a99712308bd10d26727e5
Ian Easton <ijeaston@gmail.com>
2018-12-02 21:46:43 -0500

    Properly encode SFTP paths as URLs

--- flexget/plugins/clients/sftp.py
+++ flexget/plugins/clients/sftp.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
-from future.moves.urllib.parse import urljoin, urlparse
+from future.moves.urllib.parse import urljoin, urlparse, quote
 
 import logging
 import os
@@ -231,7 +231,7 @@ class SftpList(object):
             if is_dir and files_only:
                 return
 
-            url = urljoin(url_prefix, sftp.normalize(path))
+            url = urljoin(url_prefix, quote(sftp.normalize(path)))
             title = remotepath.basename(path)
 
             entry = Entry(title, url)
