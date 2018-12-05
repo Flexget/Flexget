654bda3e8e2b5872e9a2cd1f8f2fe0f176b04f28
erichlf <erichlf@gmail.com>
2018-12-04 21:02:35 -0500

    Add Some Error Checks to torrentday

--- flexget/plugins/sites/torrentday.py
+++ flexget/plugins/sites/torrentday.py
@@ -123,7 +123,7 @@ class UrlRewriteTorrentday(object):
             categories = [categories]
         # If there are any text categories, turn them into their id number
         categories = [c if isinstance(c, int) else CATEGORIES[c] for c in categories]
-        params = { 'cata': 'yes', 'c%s' % ','.join(str(c) for c in categories): 1, 'clear-new': 1}
+        params = { 'cata': 'yes', 'c{}'.format(','.join(str(c) for c in categories)): 1, 'clear-new': 1}
         entries = set()
         for search_string in entry.get('search_strings', [entry['title']]):
 
@@ -134,7 +134,7 @@ class UrlRewriteTorrentday(object):
             try:
                 page = requests.get(url, params=params, cookies=cookies)
             except RequestException as e:
-                raise PluginError('Could not connect to torrentday: %s' % e)
+                raise PluginError('Could not connect to torrentday: {}'.format(e))
 
             soup = get_soup(page.content)
             form = soup.find('form', id='torrents')
@@ -144,12 +144,18 @@ class UrlRewriteTorrentday(object):
                 entry = Entry()
                 # find the torrent names
                 td = tr.find('td', { 'class': 'torrentNameInfo' })
+                if not td:
+                    raise PluginError('Could not find entry torrentNameInfo for {}.'.format(search_string))
                 title = td.find('a')
+                if not title:
+                    raise PluginError('Could not determine title for {}.'.format(search_string))
                 entry['title'] = title.contents[0]
                 log.debug('title: %s', title.contents[0])
 
                 # find download link
                 torrent_url = tr.find_all('td', { 'class': 'ac' })[0]
+                if not torrent_url:
+                    raise PluginError('Could not determine download link for {}.'.format(search_string))
                 torrent_url = torrent_url.find('a').get('href')
 
                 # construct download URL
