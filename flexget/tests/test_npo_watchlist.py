2defef44293eb5a6120a12cd4aa3f34a1b3b34c1
mfonville <maarten.fonville@gmail.com>
2019-01-24 22:15:58 +0100

    Add support for NPO premium filter Also fix the crash on entries that have no runtime specified (issue #2300)

--- flexget/tests/test_npo_watchlist.py
+++ flexget/tests/test_npo_watchlist.py
@@ -31,7 +31,8 @@ class TestNpoWatchlistInfo(object):
         assert entry['npo_name'] == 'Zondag met Lubach'
         assert entry['npo_description'] == 'Zeven dagen nieuws in dertig minuten, satirisch geremixt door Arjen Lubach. Met irrelevante verhalen van relevante gasten. Of andersom. Vanuit theater Bellevue in Amsterdam: platte inhoud en diepgravende grappen.'
         assert entry['npo_runtime'] == '32'
-        assert entry['npo_version'] == 'NPO.release-1.41.4'  # specify for which version of NPO website we did run this unittest
+        assert entry['npo_premium'] is False
+        assert entry['npo_version'] == 'NPO.release-1.43.1'  # specify for which version of NPO website we did run this unittest
 
         entry = task.find_entry(url='https://www.npostart.nl/14-01-2014/VARA_101348553') is None  # episode with weird (and broken) URL and should be skipped
         entry = task.find_entry(url='https://www.npostart.nl/zembla/12-12-2013/VARA_101320582')  # check that the next episode it there though
@@ -44,9 +45,35 @@ class TestNpoWatchlistInfo(object):
         assert entry['npo_url'] == 'https://www.npostart.nl/typisch/BV_101386658'
         assert entry['npo_name'] == 'Typisch'
 
+        entry = task.find_entry(url='https://www.npostart.nl/zembla/14-10-2007/VARA_101153941')  # episode without a running time
+        assert entry['npo_runtime'] == '0'
+
         assert task.find_entry(url='https://www.npostart.nl/11-04-2014/KN_1656572') is None  # episode without a name (and broken URL) that should be skipped
 
         assert task.find_entry(url='https://www.npostart.nl/zondag-met-lubach-westeros-the-series/04-09-2017/WO_VPRO_10651334') is None  # a trailer for the series, that should not be listed
+
+
+@pytest.mark.online
+class TestNpoWatchlistPremium(object):
+    config = """
+        tasks:
+          test:
+            npo_watchlist:
+              email: '8h3ga3+7nzf7xueal70o@sharklasers.com'
+              password: 'Fl3xg3t!'
+              download_premium: yes
+    """
+
+    def test_npowatchlist_lookup(self, execute_task):
+        """npo_watchlist: Test npo watchlist lookup (ONLINE)"""
+
+        task = execute_task('test')
+        entry = task.find_entry(url='https://www.npostart.nl/dynasties/29-06-2021/POW_04072126')  # a premium serie
+        assert entry['npo_id'] == 'POW_04072126'
+        assert entry['npo_url'] == 'https://www.npostart.nl/dynasties/POW_04005761'
+        assert entry['npo_name'] == 'Dynasties'
+        assert entry['npo_runtime'] == '50'
+        assert entry['npo_premium'] is True
 
 
 @pytest.mark.online
