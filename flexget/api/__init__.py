4be543d4b083d6eac432e7461b56b0f5a4d7cd4f
paranoidi <marko.koivusalo@gmail.com>
2019-01-31 22:09:34 +0200

    Fixes to irc API plugin

--- flexget/api/__init__.py
+++ flexget/api/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals, division, absolute_import  # noqa
 from .app import api_app, api, APIResource, APIClient  # noqa
 from .core import authentication, cached, database, plugins, server, tasks, user, format_checker  # noqa
 from .plugins import (
-    tvdb_lookup, tvmaze_lookup, tmdb_lookup, irc, status, pending,
+    tvdb_lookup, tvmaze_lookup, tmdb_lookup, status, pending,
     failed, imdb_lookup, rejected, schedule, variables  # noqa
 )
