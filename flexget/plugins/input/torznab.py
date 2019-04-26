e4f5f67e6f2c4dff25cf5e4ed212ee8c31051ee8
András Maróy <andras@maroy.hu>
2019-04-26 11:56:04 +0200

    Implement torznab searcher configuration

--- flexget/plugins/input/torznab.py
+++ flexget/plugins/input/torznab.py
@@ -2,11 +2,12 @@ from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 from future.moves.urllib.parse import urlencode
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import logging
 
 from flexget import plugin
 from flexget.event import event
+from flexget.plugin import PluginError
 
 log = logging.getLogger('torznab')
 
@@ -65,6 +66,7 @@ class Torznab(object):
     @plugin.internet(log)
     def _setup_caps(self, task, searcher, categories):
         """Gets the capabilities of the torznab indexer and matches it with the provided configuration"""
+
         response = task.requests.get(self._build_url(t='caps'))
         log.debug('Raw caps response {}'.format(response.content))
         root = BeautifulSoup(response.content, 'lxml')
@@ -78,7 +80,32 @@ class Torznab(object):
             'search': 'search',
             'tvsearch': 'tv-search'
         }
-        pass
+
+        searchers = {item.name: item.attrs for item in list(xml_root.find('searching')) if type(item) == element.Tag}
+        if searchers:
+            if self._check_searcher(searchers, aliases[searcher]):
+                self.supported_params = searchers[aliases[searcher]]['supportedparams'].split(',')
+                self.params['t'] = searcher
+                log.debug("Searcher '{}' set up with '{}' parameters".format(
+                         aliases[searcher],
+                         self.supported_params))
+            elif searcher != 'search' and self._check_searcher(searchers, 'search'):
+                log.warn("'{}' searcher not availble, falling back to 'search'.".format(aliases[searcher]))
+                self.supported_params = searchers['search']['supportedparams'].split(',')
+                self.params['t'] = 'search'
+                log.debug("Searcher '{}' set up with '{}' parameters".format(
+                         aliases[searcher],
+                         self.supported_params))
+            else:
+                raise PluginError('No searcher available on {}'.format(self.base_url))
+        else:
+            raise PluginError('No searcher available on {}'.format(self.base_url))
+
+    def _check_searcher(self, searchers, searcher):
+        """Check if the given searchers is in the list, available and has supported params"""
+        return searcher in searchers.keys() and \
+            searchers[searcher]['available'] == 'yes' and \
+            searchers[searcher]['supportedparams']
 
     def _setup_categories(self, xml_root, categories):
         """Gets the available search categories for the indexer"""
