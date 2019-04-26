85d2980a895baadd4788a881ecd75bc3ec58b686
András Maróy <andras@maroy.hu>
2019-04-26 11:53:36 +0200

    Implement fetching torznab capabilities from indexer

--- flexget/plugins/input/torznab.py
+++ flexget/plugins/input/torznab.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
+from future.moves.urllib.parse import urlencode
+from bs4 import BeautifulSoup
 import logging
 
 from flexget import plugin
@@ -33,7 +35,54 @@ class Torznab(object):
 
     def search(self, task, entry, config=None):
         """Search interface"""
+        self._setup(task, config)
         return []
+
+    def _build_url(self, **kwargs):
+        """Builds the url with query parameters from the arguments"""
+        params = self.params.copy()
+        params.update(kwargs)
+        log.debug('Configured parameters: {}'.format(params))
+        url = '{}/api?'.format(self.base_url)
+        url = '{}{}'.format(url, urlencode(params))
+        return url
+
+    def _setup(self, task, config):
+        """Set up parameters"""
+        self.base_url = config['website'].rstrip('/')
+        self.supported_params = []
+        if config['searcher'] == 'tv':
+            config['searcher'] = 'tvsearch'
+
+        self.params = {
+            'apikey': config['apikey'],
+            'extended': 1,
+        }
+
+        log.debug('Config: {}'.format(config))
+        self._setup_caps(task, config['searcher'], config['categories'])
+
+    @plugin.internet(log)
+    def _setup_caps(self, task, searcher, categories):
+        """Gets the capabilities of the torznab indexer and matches it with the provided configuration"""
+        response = task.requests.get(self._build_url(t='caps'))
+        log.debug('Raw caps response {}'.format(response.content))
+        root = BeautifulSoup(response.content, 'lxml')
+        self._setup_searcher(root, searcher)
+        self._setup_categories(root, categories)
+
+    def _setup_searcher(self, xml_root, searcher):
+        """Gets the available searchers (tv, movie, etc) for the indexer and their supported parameters"""
+        aliases = {
+            'movie': 'movie-search',
+            'search': 'search',
+            'tvsearch': 'tv-search'
+        }
+        pass
+
+    def _setup_categories(self, xml_root, categories):
+        """Gets the available search categories for the indexer"""
+        pass
 
 
 @event('plugin.register')
