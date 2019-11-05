6c8f0efc02c8fdb4ee0dc4fc88d1193fd34e9b94
Or Carmi <ocarmi@proofpoint.com>
2019-11-06 00:42:46 +0200

    corrected is_dir

--- flexget/tests/conftest.py
+++ flexget/tests/conftest.py
@@ -211,7 +211,7 @@ def filecopy(request):
             dst = Path(dst)
             for f in itertools.chain(*(Path().glob(src) for src in sources)):
                 dest_path = dst
-                if dest_path.isdir():
+                if dest_path.is_dir():
                     dest_path = dest_path / f.basename()
                 log.debug('copying %s to %s', f, dest_path)
                 if not os.path.isdir(os.path.dirname(dest_path)):
