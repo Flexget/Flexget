28e60f3810d38b0fdad65e56513c5f803dc748e1
Or Carmi <ocarmi@proofpoint.com>
2019-11-07 14:27:50 +0200

    updated setup.py and version

--- setup.py
+++ setup.py
@@ -46,17 +46,12 @@ setup(
             'flexget-headless = flexget:main'
         ],  # This is useful on Windows to avoid a cmd popup
     },
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=3.6',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
