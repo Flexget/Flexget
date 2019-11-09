558607c63d8adef628db97232aa935982b42e24b
user <metaMMAproject@gmail.com>
2019-11-09 01:50:50 -0600

    Plugin now outputs serialized entries as json file. Reliant on updated 'entry.py' in 'serialize_entry' branch.

--- flexget/plugins/output/json.py
+++ flexget/plugins/output/json.py
@@ -2,12 +2,11 @@ from __future__ import unicode_literals, division, absolute_import
 from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 
 import os
-import json
 import logging
 
 from flexget import plugin
+from flexget.utils import json
 from flexget.event import event
-from flexget.utils.template import render_from_task, get_template, RenderError
 
 PLUGIN_NAME = 'make_json'
 
@@ -17,23 +16,25 @@ log = logging.getLogger(PLUGIN_NAME)
 class OutputJson(object):
     schema = {
         'type': 'object',
-        'properties': {'template': {'type': 'string'}, 'file': {'type': 'string'}},
+        'properties': {'file': {'type': 'string'}},
         'required': ['file'],
         'additionalProperties': False,
     }
 
     def on_task_output(self, task, config):
-        # Use the default template if none is specified
-        if not config.get('template'):
-            config['template'] = 'json.template'
 
-        filename = os.path.expanduser(config['template'])
         output = os.path.expanduser(config['file'])
         # Output to config directory if absolute path has not been specified
         if not os.path.isabs(output):
             output = os.path.join(task.manager.config_base, output)
+        entries = {}
+        for entry in task.accepted:
+            entry_dict = entry.serialize()
+            entries[entry['title']] = entry_dict
 
-        json.dump(output)
+        with open(output, 'w') as output_file:
+            json.dump(entries, output_file, encode_datetime=True)
+
 
 @event('plugin.register')
 def register_plugin():
