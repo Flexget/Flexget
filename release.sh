25b3d5cbe503c23c9808509e3fd2760da5792e57
Chase Sterling <chase.sterling@gmail.com>
2019-11-13 14:31:24 -0500

    Get release workflow pretty close.

--- release.sh
+++ release.sh
@@ -8,8 +8,6 @@ set -x
 
 # Only run if there are new commits
 if git log --skip 1 origin/master..origin/develop|grep '^commit '; then
-  # Activate virtual env
-  . venv/bin/activate
 
   # Bump to new release version
   python dev_tools.py bump_version release
@@ -20,9 +18,8 @@ if git log --skip 1 origin/master..origin/develop|grep '^commit '; then
 
   # Build and upload to pypi.
   python setup.py sdist bdist_wheel --universal
-  twine upload dist/*
+  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
-  # We are working on a detached head, we'll point the branches to the right commits at the end
   # Commit and tag released version
   git add flexget/_version.py
   git commit -m "v${VERSION}"
@@ -36,9 +33,9 @@ if git log --skip 1 origin/master..origin/develop|grep '^commit '; then
   # master branch should be at the release we tagged
   git branch -f master ${VERSION}
   # If either of the new branches are not fast forwards, the push will be rejected
-  git push origin master develop
+  git push origin master:test_master develop:test_develop
   # Make sure our branches push before pushing tag
-  git push --tags
+  #git push --tags
 else
   echo "No commits, skipping release"
 fi
