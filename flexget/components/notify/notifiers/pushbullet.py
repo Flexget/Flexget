b912945a199ba2ae55a193c9b60b0621c2b9f4eb
Or Carmi <ocarmi@proofpoint.com>
2019-12-01 11:36:53 +0200

    [fix] even more check. Closes #2507

--- flexget/components/notify/notifiers/pushbullet.py
+++ flexget/components/notify/notifiers/pushbullet.py
@@ -92,7 +92,8 @@ class PushbulletNotifier:
             else:
                 self.send_push(key, title, message, config.get('url'))
 
-    def send_push(self, api_key, title, body, url=None, destination=None, destination_type=None):
+    @staticmethod
+    def send_push(api_key, title, body, url=None, destination=None, destination_type=None):
         push_type = 'link' if url else 'note'
         notification = {'type': push_type, 'title': title, 'body': body}
         if url:
@@ -124,16 +125,16 @@ class PushbulletNotifier:
                 message = str(e)
             raise PluginWarning(message)
 
-        reset_time = datetime.datetime.fromtimestamp(
-            int(response.headers['X-Ratelimit-Reset'])
-        ).strftime('%Y-%m-%d %H:%M:%S')
-        remaining = response.headers['X-Ratelimit-Remaining']
-        log.debug(
-            'Pushbullet notification sent. Database operations remaining until next reset: %s. '
-            'Next reset at: %s',
-            remaining,
-            reset_time,
-        )
+        reset_time = response.headers.get('X-Ratelimit-Reset')
+        remaining = response.headers.get('X-Ratelimit-Remaining')
+        if reset_time and remaining:
+            reset_time = datetime.datetime.fromtimestamp(int(reset_time))
+            log.debug(
+                'Pushbullet notification sent. Database operations remaining until next reset: %s. '
+                'Next reset at: %s',
+                remaining,
+                reset_time,
+            )
 
 
 @event('plugin.register')
