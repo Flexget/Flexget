1fe58e28afe98039b857d20673b94ec9c553d3b6
Kristen Mills <kristen@kristen-mills.com>
2019-12-07 11:12:07 -0800

    [fix] web server inserts base_url into webui v2 (#2518)

--- flexget/ui/v2/__init__.py
+++ flexget/ui/v2/__init__.py
@@ -1,22 +1,21 @@
 import logging
 import os
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 from flask_compress import Compress
 
 from flexget.webserver import register_app
 
 log = logging.getLogger('webui')
 
-manager = None
-debug = False
+config = None
 exists = True
 
 ui_base = os.path.dirname(os.path.realpath(__file__))
 ui_dist = os.path.join(ui_base, 'dist')
 ui_assets = os.path.join(ui_dist, 'assets')
 
-webui_app = Flask(__name__)
+webui_app = Flask(__name__, template_folder=ui_dist)
 Compress(webui_app)
 webui_app.url_path = '/v2/'
 
@@ -31,12 +30,12 @@ def serve_app(path):
 def root(path='index.html'):
     if not exists:
         return send_from_directory(ui_base, 'index.html')
-    return send_from_directory(ui_dist, path)
+    return render_template('index.html', base_url=config['base_url'])
 
 
-def register_web_ui(mgr):
-    global manager, exists, debug
-    manager = mgr
+def register_web_ui(cfg):
+    global config, exists
+    config = cfg
 
     if not os.path.exists(ui_dist):
         exists = False
