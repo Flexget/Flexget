5c995b3784a5b4acbf80b926a760a1c321ee96ed
4oo4 <4oo4@users.noreply.github.com>
2019-12-19 14:29:30 +0000

    Remove unused import

--- flexget/components/notify/notifiers/gotify.py
+++ flexget/components/notify/notifiers/gotify.py
@@ -6,7 +6,6 @@ from urllib.parse import urljoin
 
 from flexget import plugin
 from flexget.event import event
-from flexget.config_schema import one_or_more
 from flexget.plugin import PluginWarning
 from flexget.utils.requests import Session as RequestSession, TimedLimiter
 
