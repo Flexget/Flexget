683002eac99258e8d46a803fd65111286f1ae4f0
Steve Adams <stevezau@gmail.com>
2020-01-02 10:46:39 +1100

    Fix tvdb firstaired (#2551)
    * Fix tvdb first aired
    
    * Fix tvdb first aired
    
    * change to warning.

--- flexget/components/thetvdb/api_tvdb.py
+++ flexget/components/thetvdb/api_tvdb.py
@@ -478,7 +478,11 @@ def find_series_id(name, language=None):
     # Cleanup results for sorting
     for s in series:
         if s['firstAired']:
-            s['firstAired'] = datetime.strptime(s['firstAired'], "%Y-%m-%d")
+            try:
+                s['firstAired'] = datetime.strptime(s['firstAired'], "%Y-%m-%d")
+            except ValueError:
+                logger.warning('Invalid firstAired date "{}" when parsing series {} ', s['firstAired'], s['seriesName'])
+                s['firstAired'] = datetime(1970, 1, 1)
         else:
             s['firstAired'] = datetime(1970, 1, 1)
 
