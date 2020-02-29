69e982ab8104fc2b123b709dfb68a65edbd43175
Thomas Riccardi <kiwiiii@gmail.com>
2020-02-29 19:10:33 +0100

    [add] rutracker.org topic pages urlrewrite
    Uses api.t-ru.org to get topic data from topic id: get torrent hash,
    use it to reconstruct a magnet URI:
    - add all 4 rutracker trackers servers
    - add topic title as Display Name
    
    Test url_rewrite.

--- flexget/tests/test_urlrewriting.py
+++ flexget/tests/test_urlrewriting.py
@@ -1,3 +1,5 @@
+import pytest
+
 from flexget.plugin import get_plugin_by_name
 
 
@@ -18,6 +20,7 @@ class TestURLRewriters:
               - {title: 'tbp torrent bad subdomain', url: 'https://torrent.thepiratebay.org/8492471/Test.avi'}
               - {title: 'nyaa', url: 'https://www.nyaa.si/view/15'}
               - {title: 'cinemageddon download', url: 'http://cinemageddon.net/details.php?id=1234'}
+              - {title: 'rutracker_topic', url: 'https://rutracker.org/forum/viewtopic.php?t=2455223'}
     """
 
     def get_urlrewriter(self, name):
@@ -66,6 +69,19 @@ class TestURLRewriters:
         assert (
             entry['url']
             == 'http://cinemageddon.net/download.php?id=1234&name=cinemageddon%20download.torrent'
+        )
+
+    @pytest.mark.online
+    def test_rutracker(self, execute_task):
+        task = execute_task('test')
+        entry = task.find_entry(title='rutracker_topic')
+        urlrewriter = self.get_urlrewriter('rutracker')
+        assert urlrewriter.url_rewritable(task, entry)
+        urlrewriter.url_rewrite(task, entry)
+        assert (
+            entry['url']
+            ==
+            'magnet:?xt=urn:btih:38527C88CFE76411EF0C20FDF36B84DFE2C2D210&tr=http:%2F%2Fbt.t-ru.org%2Fann%3Fmagnet&tr=http:%2F%2Fbt2.t-ru.org%2Fann%3Fmagnet&tr=http:%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&tr=http:%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet&dn=%D0%9F%D0%A0%D0%AB%D0%96%D0%9A%D0%98+%D0%9D%D0%90+%D0%9B%D0%AB%D0%96%D0%90%D0%A5+%D0%A1+%D0%A2%D0%A0%D0%90%D0%9C%D0%9F%D0%9B%D0%98%D0%9D%D0%90.+%D0%A1%D0%B5%D0%B7%D0%BE%D0%BD+2008-2009.+%D0%92%D0%A1%D0%95+%D0%A1%D0%9E%D0%A0%D0%95%D0%92%D0%9D%D0%9E%D0%92%D0%90%D0%9D%D0%98%D0%AF.+%D0%A1+%D1%80%D0%B0%D0%B7%D0%BD%D1%8B%D1%85+%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%BE%D0%B2.%5B%D0%A1%D0%B5%D0%B7%D0%BE%D0%BD+2008-2009%2C+%D1%80%D0%B0%D0%B7%D0%BD%D0%BE%D0%B3%D0%BE+%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0.%5D'
         )
 
 
