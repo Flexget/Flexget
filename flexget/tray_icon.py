abb16c1d5f6bcdc85ca198d00afe88017a0970fb
liiight <or.carmi82@gmail.com>
2020-03-14 17:29:41 +0200

    removed redundant partial object

--- flexget/tray_icon.py
+++ flexget/tray_icon.py
@@ -1,12 +1,9 @@
-from functools import partial
 from pathlib import Path
 
 from PIL import Image
 from pystray import Icon, Menu, MenuItem
 
 from flexget import __version__
-
-disabled_action = partial(MenuItem, action=lambda icon, item: 1, enabled=False)
 
 
 class TrayIcon:
@@ -19,13 +16,12 @@ class TrayIcon:
         self.add_items_to_menu()
 
     def add_items_to_menu(self):
-        version = disabled_action(f'Flexget {__version__}')
-        empty_item = disabled_action('')
+        version = MenuItem(f'Flexget {__version__}', None, enabled=False)
 
         shutdown = MenuItem('Shutdown', self.manager.shutdown)
         reload = MenuItem('Reload Config', self.manager.load_config)
 
-        self.menu = Menu(version, empty_item, shutdown, reload)
+        self.menu = Menu(version, Menu.SEPARATOR, shutdown, reload)
 
     def run(self):
         self.icon = Icon('Flexget', Image.open(self.path_to_image), menu=self.menu)
