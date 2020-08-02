8eae4f7f6b7513fa006cd3995ab99451b329c1b6
Tran Tuan <tuan.trananh@outlook.com>
2020-08-02 09:34:20 +0700

    switched piratebay to new API due to breaking change from 3rd party

--- flexget/components/sites/sites/piratebay.py
+++ flexget/components/sites/sites/piratebay.py
@@ -8,12 +8,15 @@ from flexget.components.sites.urlrewriting import UrlRewritingError
 from flexget.components.sites.utils import normalize_unicode, torrent_availability
 from flexget.entry import Entry
 from flexget.event import event
-from flexget.utils.soup import get_soup
+
 from flexget.utils.tools import parse_filesize
+
+import json
+from urllib.parse import urlparse, parse_qs, urlencode
 
 logger = logger.bind(name='piratebay')
 
-URL = 'https://thepiratebay.org'
+URL = 'https://apibay.org' # for TOR: http://piratebayztemzmv.onion
 
 CATEGORIES = {
     'all': 0,
@@ -27,13 +30,26 @@ CATEGORIES = {
     'comics': 602,
 }
 
+# try to maintain compatibility with old config
 SORT = {
-    'default': 99,  # This is piratebay default, not flexget default.
-    'date': 3,
-    'size': 5,
-    'seeds': 7,
-    'leechers': 9,
+    'default': 'torrent_availability',
+    'date': 'torrent_timestamp',
+    'size': 'content_size',
+    'seeds': 'torrent_seeds',
+    'leechers': 'torrent_leeches',
 }
+
+# default trackers for magnet links
+TRACKERS = [
+    'udp://tracker.coppersurfer.tk:6969/announce',
+    'udp://9.rarbg.to:2920/announce',
+    'udp://tracker.opentrackr.org:1337',
+    'udp://tracker.internetwarriors.net:1337/announce',
+    'udp://tracker.leechers-paradise.org:6969/announce',
+    'udp://tracker.coppersurfer.tk:6969/announce',
+    'udp://tracker.pirateparty.gr:6969/announce',
+    'udp://tracker.cyberia.is:6969/announce',
+]
 
 
 class UrlRewritePirateBay:
@@ -75,11 +91,12 @@ class UrlRewritePirateBay:
         if self.url != url:
             self.url = url
             parsed_url = urlparse(url)
+            # most api mirrors share the same domain with frontend, except thepiratebay.org which uses apibay.org
             self.url_match = re.compile(
-                r'^%s://(?:torrents\.)?(%s)/.*$'
+                r'^%s://(?:[a-z0-9]+\.)?(?:thepiratebay\.org(?:\:\d+)?|%s)/description\.php\?id=(\d+)$'
                 % (re.escape(parsed_url.scheme), re.escape(parsed_url.netloc))
             )
-            self.url_search = re.compile(r'^%s/search/.*$' % (re.escape(url)))
+            self.url_search = re.compile(r'^(?:thepiratebay\.org(?:\:\d+)?|%s)/search\.php\?q=.*$' % (re.escape(url)))
 
     # urlrewriter API
     def url_rewritable(self, task, entry):
@@ -102,25 +119,15 @@ class UrlRewritePirateBay:
             # TODO: Close matching was taken out of search methods, this may need to be fixed to be more picky
             entry['url'] = results[0]['url']
         else:
-            # parse download page
-            entry['url'] = self.parse_download_page(entry['url'], task.requests)
-
-    @plugin.internet(logger)
-    def parse_download_page(self, url, requests):
-        page = requests.get(url).content
-        try:
-            soup = get_soup(page)
-            tag_div = soup.find('div', attrs={'class': 'download'})
-            if not tag_div:
-                raise UrlRewritingError('Unable to locate download link from url %s' % url)
-            tag_a = tag_div.find('a')
-            torrent_url = tag_a.get('href')
-            # URL is sometimes missing the schema
-            if torrent_url.startswith('//'):
-                torrent_url = urlparse(url).scheme + ':' + torrent_url
-            return torrent_url
-        except Exception as e:
-            raise UrlRewritingError(e)
+            torrent_id = self.url_match.match(entry['url']).group(1)
+            url = '%s/t.php?id=%s' % (self.url, torrent_id)
+            logger.debug('Getting info for torrent ID {}', torrent_id)
+            page = task.requests.get(url).content
+            json_result = json.loads(page)[0]
+            if json_result['id'] == '0':
+                raise UrlRewritingError("Torrent with ID does not exist.")
+            else:
+                entry['url'] = UrlRewritePirateBay.info_hash_to_magnet(json_result['info_hash'], json_result['name'])
 
     @plugin.internet(logger)
     def search(self, task, entry, config=None):
@@ -130,76 +137,80 @@ class UrlRewritePirateBay:
         if not isinstance(config, dict):
             config = {}
         self.set_urls(config.get('url', URL))
-        sort = SORT.get(config.get('sort_by', 'seeds'))
-        if config.get('sort_reverse'):
-            sort += 1
+        sort = SORT.get(config.get('sort_by', 'default'))
+        sort_reverse = bool(config.get('sort_reverse', 'True'))
         if isinstance(config.get('category'), int):
             category = config['category']
         else:
             category = CATEGORIES.get(config.get('category', 'all'))
-        filter_url = '/0/%d/%d' % (sort, category)
+        filter_url = '&cat=%s' % category
 
         entries = set()
         for search_string in entry.get('search_strings', [entry['title']]):
             query = normalize_unicode(search_string)
 
             # TPB search doesn't like dashes or quotes
-            query = query.replace('-', ' ').replace("'", " ")
+            # query = query.replace('-', ' ').replace("'", " ")
 
             # urllib.quote will crash if the unicode string has non ascii characters, so encode in utf-8 beforehand
-            url = '%s/search/%s%s' % (self.url, quote(query.encode('utf-8')), filter_url)
+            url = '%s/q.php?q=%s%s' % (self.url, quote(query.encode('utf-8')), filter_url)
             logger.debug('Using {} as piratebay search url', url)
             page = task.requests.get(url).content
-            soup = get_soup(page)
-            for link in soup.find_all('a', attrs={'class': 'detLink'}):
-                entry = Entry()
-                entry['title'] = self.extract_title(link)
-                if not entry['title']:
-                    logger.error('Malformed search result. No title or url found. Skipping.')
-                    continue
-                href = link.get('href')
-                if href.startswith('/'):  # relative link?
-                    href = self.url + href
-                entry['url'] = href
-                row = link.parent.parent.parent
-                description = row.find_all('a', attrs={'class': 'detDesc'})
-                if description and description[0].contents[0] == "piratebay ":
-                    logger.debug('Advertisement entry. Skipping.')
-                    continue
-                tds = row.find_all('td')
-                entry['torrent_seeds'] = int(tds[-2].contents[0])
-                entry['torrent_leeches'] = int(tds[-1].contents[0])
-                entry['torrent_availability'] = torrent_availability(
-                    entry['torrent_seeds'], entry['torrent_leeches']
-                )
-                # Parse content_size
-                size_text = link.find_next(attrs={'class': 'detDesc'}).get_text()
-                if size_text:
-                    size = re.search(r'Size (\d+(\.\d+)?\xa0(?:[PTGMK])?i?B)', size_text)
-                    if size:
-                        entry['content_size'] = parse_filesize(size.group(1))
-                    else:
-                        logger.error(
-                            'Malformed search result? Title: "{}", No size? {}',
-                            entry['title'],
-                            size_text,
-                        )
+            json_results = json.loads(page)
+            if len(json_results) == 0:
+                logger.error("Error while searching piratebay for %s." % search_string)
+            for result in json_results:
+                if result['id'] == '0':
+                    # JSON when no results found:
+                    # [{
+                    #    "id":"0",
+                    #    "name":"No results returned",
+                    #    "info_hash":"0000000000000000000000000000000000000000",
+                    #    "leechers":"0",
+                    #    "seeders":"0",
+                    #    "num_files":"0",
+                    #    "size":"0",
+                    #    "username":"",
+                    #    "added":"0",
+                    #    "status":"member",
+                    #    "category":"0",
+                    #    "imdb":"",
+                    #    "total_found":"1"
+                    # }]
+                    break
+                try:
+                    entry = Entry()
+                    entry['title'] = result['name']
+                    entry['torrent_seeds'] = int(result['seeders'])
+                    entry['torrent_leeches'] = int(result['leechers'])
+                    entry['torrent_timestamp'] = int(result['added'])
+                    entry['torrent_availability'] = torrent_availability(
+                        entry['torrent_seeds'], entry['torrent_leeches']
+                    )
+                    entry['content_size'] = result['size']
+                    entry['torrent_info_hash'] = result['info_hash']
+                    entry['url'] = UrlRewritePirateBay.info_hash_to_magnet(result['info_hash'], result['name'])
+                except KeyError:
+                    logger.error("Error when parsing an entry.")
 
                 entries.add(entry)
 
-        return sorted(entries, reverse=True, key=lambda x: x.get('torrent_availability'))
+        return sorted(entries, reverse=sort_reverse, key=lambda x: x.get(sort))
 
     @staticmethod
-    def extract_title(soup):
-        """Sometimes search results have no contents. This function tries to extract something sensible."""
-        if isinstance(soup.contents, list) and soup.contents:
-            return soup.contents[0]
-        if soup.get('href') and 'torrent' in soup.get('href'):
-            return soup.get('href').rsplit('/', 1)[-1]
-
+    def info_hash_to_magnet(info_hash, name):
+        magnet = {
+            'xt': f"urn:btih:{info_hash}",
+            'dn': name,
+            'tr': TRACKERS
+        }
+        magnet_qs = urlencode(magnet, doseq=True, safe=':')
+        magnet_uri = f"magnet:?{magnet_qs}"
+        return magnet_uri
 
 @event('plugin.register')
 def register_plugin():
     plugin.register(
         UrlRewritePirateBay, 'piratebay', interfaces=['urlrewriter', 'search', 'task'], api_ver=2
     )
+
