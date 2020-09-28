f9d8f3ba42ec9e765f06bcc777f06d67cf4b41be
Chase Sterling <chase.sterling@gmail.com>
2020-09-28 14:50:27 -0400

    Add the ability to register filter functions to the logger.

--- flexget/log.py
+++ flexget/log.py
@@ -9,7 +9,7 @@ import sys
 import threading
 import uuid
 import warnings
-from typing import Iterator
+from typing import Callable, Iterator
 
 import loguru
 from loguru import logger
@@ -96,6 +96,22 @@ _startup_buffer_id = None
 _logging_started = False
 # Stores the last 100 debug messages
 debug_buffer = collections.deque(maxlen=100)
+_log_filters = []  # Stores filter functions
+
+
+def _log_filterer(record):
+    """This is the function we add to our loguru handlers. It will dynamically use all filters we add later."""
+    return all(f(record) for f in _log_filters)
+
+
+def add_filter(func: Callable[['loguru.Record'], bool]):
+    """Adds a filter function to the log handlers."""
+    _log_filters.append(func)
+
+
+def remove_filter(func: Callable[['loguru.Record'], bool]):
+    """Removes a filter function from the log handlers."""
+    _log_filters.remove(func)
 
 
 def initialize(unit_test: bool = False) -> None:
@@ -168,6 +184,7 @@ def start(
             retention=int(os.environ.get(ENV_MAXCOUNT, 9)),
             encoding='utf-8',
             format=LOG_FORMAT,
+            filter=_log_filterer,
         )
 
     # without --cron we log to console
@@ -181,7 +198,13 @@ def start(
             # Auto-detection for colorize doesn't seem to work properly for PyCharm.
             if "PYCHARM_HOSTED" in os.environ:
                 colorize = True
-            logger.add(safe_stdout, level=level, format=LOG_FORMAT, colorize=colorize)
+            logger.add(
+                safe_stdout,
+                level=level,
+                format=LOG_FORMAT,
+                colorize=colorize,
+                filter=_log_filterer,
+            )
 
     # flush what we have stored from the plugin initialization
     global _startup_buffer, _startup_buffer_id
