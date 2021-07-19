0222e2b1be277a25322c4870d4f7b2e440dc78b8
BrutuZ <BrutuZ@users.noreply.github.com>
2021-07-18 21:17:56 -0300

    Support series without series_id and forget

--- flexget/components/series/series_forget.py
+++ flexget/components/series/series_forget.py
@@ -9,24 +9,47 @@ logger = logger.bind(name='series_forget')
 
 
 class OutputSeriesRemove:
-    schema = {'type': 'boolean'}
+    schema = {
+        'oneOf': [
+            {'type': 'boolean'},
+            {'type': 'object', 'properties': {'forget': {'type': 'boolean'}}},
+        ],
+    }
 
     def on_task_output(self, task, config):
         if not config:
             return
+        if isinstance(config, bool):
+            config = {'forget': False}
+        forget = config['forget']
+
         for entry in task.accepted:
-            if 'series_name' in entry and 'series_id' in entry:
-                try:
-                    db.remove_series_entity(entry['series_name'], entry['series_id'])
-                    logger.info(
-                        'Removed episode `{}` from series `{}` download history.',
-                        entry['series_id'],
-                        entry['series_name'],
-                    )
-                except ValueError:
-                    logger.debug(
-                        'Series ({}) or id ({}) unknown.', entry['series_name'], entry['series_id']
-                    )
+            if 'series_name' in entry:
+                if 'series_id' in entry:
+                    try:
+                        db.remove_series_entity(entry['series_name'], entry['series_id'], forget)
+                        logger.info(
+                            'Removed `{}`episode `{}` from series `{}` download history.',
+                            'and forgot ' if forget else '',
+                            entry['series_id'],
+                            entry['series_name'],
+                        )
+                    except ValueError:
+                        logger.debug(
+                            'Series ({}) or id ({}) unknown.',
+                            entry['series_name'],
+                            entry['series_id'],
+                        )
+                else:
+                    try:
+                        db.remove_series(entry['series_name'], forget)
+                        logger.info(
+                            'Removed `{}`series `{}` download history.',
+                            'and forgot ' if forget else '',
+                            entry['series_name'],
+                        )
+                    except ValueError:
+                        logger.debug('Series ({}) unknown.', entry['series_name'])
 
 
 @event('plugin.register')
