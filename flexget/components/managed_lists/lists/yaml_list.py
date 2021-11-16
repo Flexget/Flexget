baab607b0a2cbd77456f535f0f1a8eb4376629f7
soloam <11949987+soloam@users.noreply.github.com>
2021-11-16 18:50:20 +0000

    [add]: yaml_list: sort filds, title on top

--- flexget/components/managed_lists/lists/yaml_list.py
+++ flexget/components/managed_lists/lists/yaml_list.py
@@ -59,7 +59,9 @@ class YamlManagedList(MutableSet):
             dict: Item with limited keys
         """
 
+
         # Title should allways be the first item
+        required_fields = []
         if self.key != 'title':
             required_fields = [self.key]
 
@@ -124,9 +126,7 @@ class YamlManagedList(MutableSet):
     def add(self, entry: Entry) -> None:
         exists = self.get(entry)
         if exists:
-            logger.warning(
-                f'Can\'t add entry "{self.key}" = "{exists.get(self.key)}", entry already exists in list'
-            )
+            logger.warning(f'Can\'t add entry "{self.key}" = "{exists.get(self.key)}", entry already exists in list')
             return
         self.entries.append(entry)
         self.save_yaml()
