0abed4f75c58ca119a65077c671ec6bc534c462e
Aidan Rowe <aidanrowe@gmail.com>
2022-05-23 21:14:19 +1000

    [add] add ntfy.sh notifier support

---
+++ flexget/components/notify/notifiers/ntfysh.py
@@ -0,0 +1,84 @@
+from http import HTTPStatus
+from urllib.parse import urljoin
+
+from requests.exceptions import RequestException
+from requests.auth import HTTPBasicAuth
+
+from flexget import plugin
+from flexget.event import event
+from flexget.plugin import PluginWarning
+from flexget.utils.requests import Session as RequestSession
+
+plugin_name = 'ntfysh'
+
+requests = RequestSession(max_retries=3)
+
+
+class NtfyshNotifier(object):
+    """
+    Example::
+
+    notify:
+      entries:
+        via:
+          - ntfysh:
+              url: <NTFY_SERVER_URL>
+              topic: <NTFY_TOPIC>
+              priority: <PRIORITY>
+
+    Configuration parameters are also supported from entries (eg. through set).
+    """
+
+    schema = {
+        'type': 'object',
+        'properties': {
+            'url': {'format': 'url'},
+            'topic': {'type': 'string'},
+            'priority': {'type': 'integer', 'default': 3},
+            'delay': {'type': 'string'},
+            'tags': {'type': 'string'},
+            'username': {'type': 'string'},
+            'password': {'type': 'string'},
+        },
+        'required': ['topic', 'url'],
+        'additionalProperties': False,
+    }
+
+    def notify(self, title, message, config):
+        """
+        Send a Ntfy.sh notification
+        """
+        base_url = config['url']
+        topic = config['topic']
+        url = urljoin(base_url, topic)
+
+        req = {
+            'url': url,
+            'data': message,
+            'params': {'title': title, 'priority': config['priority']},
+        }
+
+        if 'username' in config or 'password' in config:
+            req['auth'] = HTTPBasicAuth(config.get('username', ''), config.get('password', ''))
+
+        if 'delay' in config:
+            req['params']['delay'] = config['delay']
+        if 'tags' in config:
+            req['params']['tags'] = config['tags']
+
+        try:
+            response = requests.post(**req)
+        except RequestException as e:
+            if e.response is not None:
+                if e.response.status_code in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):
+                    message = 'Invalid username and password'
+                else:
+                    message = e.response.text()
+            else:
+                message = str(e)
+            raise PluginWarning(message)
+
+
+@event('plugin.register')
+def register_plugin():
+    plugin.register(NtfyshNotifier, plugin_name, api_ver=2, interfaces=['notifiers'])
