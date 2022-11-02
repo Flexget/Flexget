c8ab887b48774f2e01a23415d78c056521ae584f
Chase Sterling <chase.sterling@gmail.com>
2022-11-02 17:52:17 -0400

    Bump version

--- flexget/_version.py
+++ flexget/_version.py
@@ -1,10 +1,10 @@
 """
 Current FlexGet version.
-This is contained in a separate file so that it can be easily read by setup.py, and easily edited and committed by
-release scripts in continuous integration. Should (almost) never be set manually.
+This is contained in a separate file so that it can be easily read by setuptools, and easily edited and committed by
+release scripts in continuous integration. Should only need to be set manually when doing a major/minor version bump.
 
 The version should always be set to the <next release version>.dev
-The jenkins release job will automatically strip the .dev for release,
+The github actions release job will automatically strip the .dev for release,
 and update the version again for continued development.
 """
-__version__ = '3.4.3.dev'
+__version__ = '3.5.0.dev'
