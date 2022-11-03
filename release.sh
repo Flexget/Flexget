31c2cf55644f31d15b226bea3fd2de6e2e3ceefc
Chase Sterling <chase.sterling@gmail.com>
2022-11-03 13:15:12 -0400

    Fix wheel not including dependencies (due to bug in pypa/build?)

--- release.sh
+++ release.sh
@@ -26,7 +26,7 @@ if git log --skip 1 origin/master..origin/develop|grep '^commit '; then
   python dev_tools.py bundle-webui
 
   # Build and upload to pypi.
-  python -m build
+  python -m build --sdist --wheel
   twine upload dist/*
 
   # Commit and tag released version
