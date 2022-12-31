e6153dd2b4adc3b77e38f3d3fc30d8d1dfffcf34
Edward <edward@ake.rs>
2023-01-01 05:56:35 +1100

    feat(flood): Add InputFlood

--- flexget/plugins/clients/flood.py
+++ flexget/plugins/clients/flood.py
@@ -1,4 +1,3 @@
-
 import time, os, base64
 
 from loguru import logger
@@ -158,7 +157,7 @@ class FloodClient:
             raise PluginError("Not authenticated with Flood.")
 
         try:
-            response = post(f"{config['url']}/api/torrents/start", data={"hashes": hashes }, cookies={ "jwt": self.jwt})
+            response = post(f"{config['url']}/api/torrents/start", json={"hashes": hashes }, cookies={ "jwt": self.jwt})
 
             if response.status_code == 200:
                 logger.debug(f"Successfully started {len(hashes)} torrents in Flood.")
@@ -181,7 +180,7 @@ class FloodClient:
             raise PluginError("Not authenticated with Flood.")
 
         try:
-            response = post(f"{config['url']}/api/torrents/stop", data={"hashes": hashes }, cookies={ "jwt": self.jwt})
+            response = post(f"{config['url']}/api/torrents/stop", json={"hashes": hashes }, cookies={ "jwt": self.jwt})
 
             if response.status_code == 200:
                 logger.debug(f"Successfully stopped {len(hashes)} torrents in Flood.")
@@ -204,7 +203,7 @@ class FloodClient:
             raise PluginError("Not authenticated with Flood.")
 
         try:
-            response = post(f"{config['url']}/api/torrents/delete", data={"hashes": hashes, 'deleteData': deleteData}, cookies={"jwt": self.jwt})
+            response = post(f"{config['url']}/api/torrents/delete", json={"hashes": hashes, "deleteData": deleteData}, cookies={"jwt": self.jwt})
 
             if response.status_code == 200:
                 logger.debug(f"Successfully deleted {len(hashes)} torrents from Flood.")
@@ -214,19 +213,84 @@ class FloodClient:
             return False
         except RequestException as e:
             raise PluginError(f"An error occurred while attempting to delete torrents from Flood: {e}")
+
+class InputFlood:
+    """Creates entries from torrents in Flood based
     
-class OutputFlood:
+    Example:
+        from_flood:
+            url: http://localhost:3000
+            username: username
+            password: password
+    """
+
     schema = {
        'type': 'object',
         'properties': {
             'url': {'type': 'string'},
             'username': {'type': 'string'},
             'password': {'type': 'string'},
-            'action': {'type': 'string', 'enum': ['add', 'remove', 'delete']},
-            'path': {'type': 'string'},
-            'tags': {'type': 'array', 'items': {'type': 'string'}},
         },
         'additionalProperties': False,
+        'required': [ 'url' ]
+    }
+
+    def __init__(self) -> None:
+        self.flood = FloodClient()
+
+    def generate_entry(self, config: dict, torrent: dict) -> Entry:
+        """
+        Generate a task entry from a torrent in Flood.
+        """
+
+        entry = Entry(url='', title=torrent['name'])
+
+        for key, value in torrent.items():
+            entry['flood_' + key.lower()] = value
+
+        return entry
+
+    def on_task_input(self, task: Task, config: dict):
+        if self.flood.is_jwt_expired():
+            logger.debug("JWT token is expired. Re-authenticating with Flood.")
+            self.flood.authenticate(config)
+
+        entries = []
+        torrents = self.flood.list_torrents(config)
+
+        for hash, torrent in torrents.items():
+            entry = self.generate_entry(config, torrent)
+
+            if entry:
+                entries.append(entry)
+
+        return entries
+
+class OutputFlood:
+    """Add, remove, delete, start, or stop torrents in Flood.
+    
+    Example:
+        flood:
+            url: http://localhost:3000
+            username: username
+            password: password
+            path: /downloads/Flexget
+            tags: Flexget
+            action: add
+    """
+
+    schema = {
+       'type': 'object',
+        'properties': {
+            'url': {'type': 'string'},
+            'username': {'type': 'string'},
+            'password': {'type': 'string'},
+            'action': {'type': 'string', 'enum': ['add', 'remove', 'delete', 'start', 'stop']},
+            'path': {'type': 'string'},
+            'tags': {'type': 'array', 'items': {'type': 'string'}}
+        },
+        'additionalProperties': False,
+        'required': [ 'url', 'action' ]
     }
 
     def __init__(self) -> None:
@@ -283,7 +347,7 @@ class OutputFlood:
         Use the download plugin to get temp files for entry URLs.
         """
 
-        if 'download' not in task.config:
+        if config['action'] == 'add' and 'download' not in task.config:
             download = plugin.get('download', self)
             download.get_temp_files(task)
 
@@ -300,11 +364,21 @@ class OutputFlood:
         if self.flood.is_jwt_expired():
             logger.debug("JWT token is expired. Re-authenticating with Flood.")
             self.flood.authenticate(config)
-        
-        if config['action'] == 'add':
-            for entry in task.accepted:
+
+        for entry in task.accepted:
+            if config['action'] == 'add':
                 self.add_entry(config, task, entry)
+            elif 'flood_hash' in entry:
+                if config['action'] == 'remove':
+                    self.flood.delete_torrents(config, [ entry['flood_hash'] ], False)
+                elif config['action'] == 'delete':
+                    self.flood.delete_torrents(config, [ entry['flood_hash'] ], True)
+                elif config['action'] == 'start':
+                    self.flood.start_torrents(config, [ entry['flood_hash'] ])
+                elif config['action'] == 'stop':
+                    self.flood.stop_torrents(config, [ entry['flood_hash'] ])
 
 @event('plugin.register')
 def register_plugin():
+    plugin.register(InputFlood, 'from_flood', api_ver=2)
     plugin.register(OutputFlood, 'flood', api_ver=2)
