45b3d4777af29ee02c7534535ca33fc57bba118b
bagobones <bagobones@users.noreply.github.com>
2023-11-16 00:10:33 -0800

    typo

--- flexget/components/sites/sites/passthepopcorn.py
+++ flexget/components/sites/sites/passthepopcorn.py
@@ -201,7 +201,7 @@ class SearchPassThePopcorn:
                     release_res = torrent['Resolution']
                     release_res = release_res.replace("PAL", "576p") #Common PAL DVD vertial resolution
                     release_res = release_res.replace("NTSC", "480p") #Common PAL DVD vertial resolution
-                    # many older realses have a resolution defined as 624x480 for example this will split the value at take the hight
+                    # many older releases have a resolution defined as 624x480 for example this will split the value at take the hight
                     tsplit = release_res.split("x", 1)                    
                     if len(tsplit)>1:
                         release_res = tsplit[1] + 'p' 
