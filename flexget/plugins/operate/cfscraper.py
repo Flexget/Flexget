8cbae8ce59cc12ef78cf89fdfda5b94489eeeb78
vivodi <103735539+vivodi@users.noreply.github.com>
2025-01-07 11:06:48 +0800

    Deprecate cloudscraper

--- flexget/plugins/operate/cfscraper.py
+++ flexget/plugins/operate/cfscraper.py
@@ -1,59 +1,29 @@
-from collections import OrderedDict
-
 from loguru import logger
 
 from flexget import plugin
 from flexget.event import event
-from flexget.utils.requests import Session
+from flexget.plugin import PluginWarning
 
 logger = logger.bind(name='cfscraper')
 
 
 class CFScraper:
     """
-    Plugin that enables scraping of cloudflare protected sites.
+    Plugin that enables scraping of cloudflare protected sites, but it doesn't work anymore.
 
-    Example::
-      cfscraper: yes
+    This plugin is deprecated and slated for removal in FlexGet v3.14.0.
     """
 
     schema = {'type': 'boolean'}
 
     @plugin.priority(253)
     def on_task_start(self, task, config):
-        try:
-            import cloudscraper
-        except ImportError as e:
-            logger.debug('Error importing cloudscraper: {}', e)
-            raise plugin.DependencyError(
-                'cfscraper', 'cloudscraper', f'cloudscraper module required. ImportError: {e}'
-            )
-
-        class CFScrapeWrapper(Session, cloudscraper.CloudScraper):
-            """
-            This class allows the FlexGet session to inherit from CloudScraper instead of the requests.Session directly.
-            """
-
-            def Challenge_Response(self, resp, **kwargs):
-                """Make sure limiters are disabled when doing a cloudflare challenge."""
-                if not self.is_reCaptcha_Challenge(resp):
-                    # If this is a recaptcha challenge, the request gets sent straight to requests, not our subclass,
-                    # so it can't have any extra arguments that requests doesn't expect.
-                    kwargs['disable_limiters'] = True
-                return super().Challenge_Response(resp, **kwargs)
-
-        if config is True:
-            task.requests.headers = OrderedDict(
-                [
-                    ('User-Agent', task.requests.headers['User-Agent']),
-                    ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-                    ('Accept-Language', 'en-US,en;q=0.5'),
-                    ('Accept-Encoding', 'gzip, deflate'),
-                    ('Connection', 'close'),
-                    ('Upgrade-Insecure-Requests', '1'),
-                ]
-            )
-            task.requests = CFScrapeWrapper.create_scraper(task.requests, solveDepth=5)
+        logger.warning(
+            "The cfscraper plugin is deprecated and slated for removal in FlexGet v3.14.0."
+        )
+        raise PluginWarning(
+            "The cfscraper plugin is deprecated and slated for removal in FlexGet v3.14.0."
+        )
 
 
 @event('plugin.register')
