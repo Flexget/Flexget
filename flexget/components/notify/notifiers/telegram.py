ababf212a63058553d58f7cd2a3b4228253133a4
vivodi <103735539+vivodi@users.noreply.github.com>
2025-02-18 17:26:19 +0800

    [fix] Python run-time won't complain about `-> (int, int)`, but it is not correct type annotation according to PEP 484. https://stackoverflow.com/a/43443331/12340650

--- flexget/components/notify/notifiers/telegram.py
+++ flexget/components/notify/notifiers/telegram.py
@@ -357,7 +357,7 @@ class TelegramNotifier:
         usernames: list[str],
         fullnames: list[tuple[str, str]],
         groups: list[str],
-    ) -> (list[ChatIdEntry], bool):
+    ) -> tuple[list[ChatIdEntry], bool]:
         """Get chat ids for `usernames`, `fullnames` & `groups`.
         entries with a matching chat ids will be removed from the input lists.
         """
@@ -466,7 +466,9 @@ class TelegramNotifier:
 
     async def _get_bot_updates(
         self,
-    ) -> (dict[str, telegram.Chat], dict[(str, str), telegram.Chat], dict[str, telegram.Chat]):
+    ) -> tuple[
+        dict[str, telegram.Chat], dict[tuple[str, str], telegram.Chat], dict[str, telegram.Chat]
+    ]:
         """Get updated chats info from telegram"""
         # highly unlikely, but if there are more than `telegram.constants.PollingLimit.MAX_LIMIT`
         # msgs waiting for the bot, we should not miss one
