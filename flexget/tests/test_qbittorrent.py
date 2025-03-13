45065aa3fcc79817470053b31f81f74d0a08511d
vivodi <103735539+vivodi@users.noreply.github.com>
2025-03-14 03:45:29 +0800

    fix

--- flexget/tests/test_qbittorrent.py
+++ flexget/tests/test_qbittorrent.py
@@ -12,10 +12,16 @@ class TestQbittorrent:
               - title: test magnet
                 url: magnet:?xt=urn:btih:2a8959bed2be495bb0e3ea96f497d873d5faed05&dn=some.thing.720p
         tasks:
+          default:
+            qbittorrent: yes
           ratio_limit:
             qbittorrent:
               ratio_limit: 1.65
         """
+
+    def test_default(self, execute_task):
+        task = execute_task('default')
+        assert task.accepted
 
     def test_ratio_limit(self, execute_task):
         task = execute_task('ratio_limit')
