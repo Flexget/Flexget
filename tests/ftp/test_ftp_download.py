ee0df174094c9b9fe95ce3cb46ea478847ceee33
vivodi <103735539+vivodi@users.noreply.github.com>
2025-10-30 23:41:28 +0800

    fix(plugin): resolve path issue in `ftp_download`
    Closes #4703.

---
+++ tests/ftp/test_ftp_download.py
@@ -0,0 +1,69 @@
+import filecmp
+import platform
+import sys
+from pathlib import Path
+
+import pytest
+
+
+@pytest.mark.skipif(
+    platform.system() == 'Darwin',
+    reason='pytest-localftpserver is incompatible with macOS (https://github.com/oz123/pytest-localftpserver/issues/151)',
+)
+@pytest.mark.skipif(
+    platform.system() == 'Linux' and sys.version_info[:2] == (3, 14),
+    reason='pytest-localftpserver is incompatible with Python 3.14,Linux (https://github.com/oz123/pytest-localftpserver/issues/378)',
+)
+@pytest.mark.require_optional_deps
+@pytest.mark.xdist_group(name='ftp')
+class TestFtpDownload:
+    config = """
+        templates:
+          global:
+            mock:
+              - { title: file_to_download, url: to_be_populated }
+            accept_all: yes
+        tasks:
+          keep_origin:
+            ftp_download:
+              delete_origin: False
+              ftp_tmp_path: __tmp__
+              use-ssl: False
+          delete_origin:
+            ftp_download:
+              delete_origin: True
+              ftp_tmp_path: __tmp__
+              use-ssl: False
+          use_ssl:
+            ftp_download:
+              delete_origin: False
+              ftp_tmp_path: __tmp__
+              use-ssl: True
+        """
+
+    def test_keep_origin(self, execute_task, tmp_path, manager, ftpserver):
+        ftpserver.reset_tmp_dirs()
+        manager.config['templates']['global']['mock'][0]['url'] = ftpserver.put_files(
+            'a.txt', style='url', anon=False
+        )[0]
+        execute_task('keep_origin')
+        assert filecmp.cmp(Path(__file__).parent / 'a.txt', tmp_path / 'a.txt')
+        assert list(ftpserver.get_file_paths(style='rel_path', anon=False)) == ['a.txt']
+
+    def test_delete_origin(self, execute_task, tmp_path, manager, ftpserver):
+        ftpserver.reset_tmp_dirs()
+        manager.config['templates']['global']['mock'][0]['url'] = ftpserver.put_files(
+            'a.txt', style='url', anon=False
+        )[0]
+        execute_task('delete_origin')
+        assert filecmp.cmp(Path(__file__).parent / 'a.txt', tmp_path / 'a.txt')
+        assert list(ftpserver.get_file_paths(style='rel_path', anon=False)) == []
+
+    def test_use_ssl(self, execute_task, tmp_path, manager, ftpserver_TLS):  # noqa: N803
+        ftpserver_TLS.reset_tmp_dirs()
+        manager.config['templates']['global']['mock'][0]['url'] = ftpserver_TLS.put_files(
+            'a.txt', style='url', anon=False
+        )[0]
+        execute_task('use_ssl')
+        assert filecmp.cmp(Path(__file__).parent / 'a.txt', tmp_path / 'a.txt')
+        assert list(ftpserver_TLS.get_file_paths(style='rel_path', anon=False)) == ['a.txt']
